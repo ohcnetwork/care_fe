0985f109c7510be594e131625d539af286f24377
syamkumar <syam@Mozilla-community.org>
2020-03-22 00:10:16 +0530

    actions and reducers

---
+++ src/Redux/actions.tsx
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import api from './api';
+
+const requestMap: any = api;
+export const actions = {
+    FETCH_REQUEST: 'FETCH_REQUEST',
+    FETCH_REQUEST_SUCCESS: 'FETCH_REQUEST_SUCCESS',
+    FETCH_REQUEST_ERROR: 'FETCH_REQUEST_ERROR',
+    SET_DATA: 'SET_DATA',
+};
+
+const config: any = {
+    headers: {},
+};
+
+if (localStorage.getItem('care_access_token')) {
+    config.headers['Authorization'] = 'bearer ' + localStorage.getItem('care_access_token');
+}
+
+const axiosApiCall: any = axios.create(config);
+
+export const setStoreData = (key: string, value: any) => {
+    return {
+        type: actions.SET_DATA,
+        key,
+        value,
+    };
+};
+
+export const fetchDataRequest = (key: string) => {
+    return {
+        type: actions.FETCH_REQUEST,
+        key,
+    };
+};
+
+export const fetchDataRequestError = (key: string, error: any) => {
+    return {
+        type: actions.FETCH_REQUEST_ERROR,
+        key,
+        error,
+    };
+};
+
+export const fetchResponseSuccess = (key: string, data: any) => {
+    return {
+        type: actions.FETCH_REQUEST_SUCCESS,
+        key,
+        data,
+    };
+};
+
+export const fireRequest = (
+    key: string, path: any = [], params: object = {},
+) => {
+    return (dispatch: any) => {
+        const request = Object.assign({}, requestMap[key]);
+        if (path.length > 0) {
+            request.path += '/' + path.join('/');
+        }
+
+        if (request.method === undefined || request.method == 'GET') {
+            request.method = 'GET';
+            const qs = $.param(params);
+            if (qs !== '') {
+                request.path += `?${qs}`;
+            }
+        }
+
+        dispatch(fetchDataRequest(key));
+        return axiosApiCall[request.method.toLowerCase()](request.path, params)
+            .then((response: any) => {
+                dispatch(fetchResponseSuccess(key, response.data));
+                return response;
+            })
+            .catch((error: any) => {
+                dispatch(fetchDataRequestError(key, error));
+                return error.response;
+            });
+    };
+};
