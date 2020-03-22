b35d881ccfd8ee0b10301ca9e4256c4e2d11c64d
mkrishnan-codes <manu.krishnan@tarams.com>
2020-03-22 19:11:37 +0530

    Hard coded sample dashboard which shows stats

--- src/Components/Dashboard/PublicDashboard.tsx
+++ src/Components/Dashboard/PublicDashboard.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import { StatsCard } from '../Common/StatsCard';
 
-export const PublicDashboard = () => {
+interface Props {
+
+}
+export const PublicDashboard: React.FC<Props> = (props) => {
     return (
-        <div>
-            Public Dashboard
-        </div>
+        <Grid item xs={12}>
+            <Grid container justify="flex-start" spacing={5}>
+                <Grid item>
+                    <StatsCard title="Patient count" value={25} />
+                </Grid>
+                <Grid item>
+                    <StatsCard title="People in observation" value={10} />
+                </Grid>
+                <Grid item>
+                    <StatsCard title="Positive Cases" value={7} />
+                </Grid>
+                <Grid item>
+                    <StatsCard title="Negative Cases" value={18} />
+                </Grid>
+                <Grid item>
+                    <StatsCard title="Number of tests done" value={44} />
+                </Grid>
+            </Grid>
+        </Grid>
     );
-};
+}
