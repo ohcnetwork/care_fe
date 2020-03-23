49641413d544e86b1424ad9ba026dea414d45186
syamkumar <syam@Mozilla-community.org>
2020-03-23 19:08:51 +0530

    current data

--- src/Components/Dashboard/PublicDashboard.tsx
+++ src/Components/Dashboard/PublicDashboard.tsx
@@ -14,19 +14,19 @@ export const PublicDashboard = () => {
             {' '}
             <Grid container justify="flex-start" spacing={5} style={{marginTop: 20}}>
                 <Grid item>
-                    <DashboardStatsCard title="Patient count" value={25} />
+                    <DashboardStatsCard title="Patient count" value={91} />
                 </Grid>
                 <Grid item>
-                    <DashboardStatsCard title="People in observation" value={10} />
+                    <DashboardStatsCard title="People in observation (Home " value={64320} />
                 </Grid>
                 <Grid item>
-                    <DashboardStatsCard title="Positive Cases" value={7} />
+                    <DashboardStatsCard title="People in observation (Home) " value={383} />
                 </Grid>
                 <Grid item>
-                    <DashboardStatsCard title="Negative Cases" value={18} />
+                    <DashboardStatsCard title="Negative Cases" value={2987} />
                 </Grid>
                 <Grid item>
-                    <DashboardStatsCard title="Number of tests done" value={44} />
+                    <DashboardStatsCard title="Number of tests done" value={4291} />
                 </Grid>
             </Grid>
         </Grid>
