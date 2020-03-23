dc3f4c79f0aa698ab3e7dd254e68817fc3e7f917
Mansoor <mansoor.a@livestream.com>
2020-03-23 14:36:06 +0530

    Scp stuff to the VM

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 
 
 npm run build
-cp _redirects dist/
+
+tar -czf build.tar.gz dist/
+
+scp build.tar.gz netlify@34.93.26.100:/home/netlify/releases/
+ssh netlify@34.93.26.100 'cd /home/netlify/releases/; tar xf build.tar.gz; cp -rf /opt/care_fe/dist /opt/care_fe/dist.old; mv dist /opt/care_fe/dist'
