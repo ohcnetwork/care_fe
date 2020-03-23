8d09447290fa8320e435240099ea5b88af71d803
overlord839 <cristomathew7@gmail.com>
2020-03-23 14:28:40 +0530

    Hospital View

---
+++ src/Components/Common/HospitalView.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 200,
+      width: 300,
+      margin: "auto",
+      transition: "0.3s",
+      boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+     "&:hover": {
+        boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+     }
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    }
+  });
+
+export const HospitalView = (props:any) => {
+    const classes = useStyles();
+    return (
+        <Card className={classes.root} variant="outlined">
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              {props.name}
+            </Typography>
+            <Typography variant="body2" component="p">
+              Address: {props.addr}
+              <br />
+              Phone: {props.phone}
+              <br />
+              District: {props.district}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" variant="contained" color="primary">View</Button>
+          </CardActions>
+        </Card>
+      )
+}
--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -3,11 +3,13 @@ import {useRedirect, useRoutes} from 'hookrouter';
 import Header from '../Components/Common/Header';
 import {PrivateDashboard} from "../Components/Dashboard/PrivateDashboard";
 import { FacilityCreate } from '../Components/Facility/FacilityCreate';
+import { HospitalOnboarding } from '../Components/Facilities/HospitalOnboarding';
 
 const routes = {
     '/': () => <PrivateDashboard/>,
     '/privatedashboard': () => <PrivateDashboard/>,
     '/facility/create': () => <FacilityCreate/>,
+    '/facilities': () => <HospitalOnboarding />
 };
 
 const AppRouter = () => {
