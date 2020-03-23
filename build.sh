f2455a2ea6484fe8e630f56feda5978af9cff970
Mansoor <mansoor.a@livestream.com>
2020-03-23 14:39:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ npm run build
 
 tar -czf build.tar.gz dist/
 
+echo $SSH_KEY > ~/.ssh/id_rsa
+
 scp build.tar.gz netlify@34.93.26.100:/home/netlify/releases/
 ssh netlify@34.93.26.100 'cd /home/netlify/releases/; tar xf build.tar.gz; cp -rf /opt/care_fe/dist /opt/care_fe/dist.old; mv dist /opt/care_fe/dist'
