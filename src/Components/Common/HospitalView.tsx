f7ff3d21d82faffef3108a6bc4490346aee1a071
overlord839 <cristomathew7@gmail.com>
2020-03-23 23:32:23 +0530

    Hospital View

--- src/Components/Common/HospitalView.tsx
+++ src/Components/Common/HospitalView.tsx
@@ -5,12 +5,17 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import PhoneIcon from '@material-ui/icons/Phone';
+import LocationCityIcon from '@material-ui/icons/LocationCity';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 const useStyles = makeStyles({
     root: {
       minWidth: 200,
-      width: 300,
+      width: 350,
+      backgroundImage: "linear-gradient(to right top, #0a3ded, #0053f5, #0066fa, #0078fd, #0088ff, #0099ff, #00a9ff, #00b8ff, #00cbff, #00dcff, #25ecf9, #5ffbf1)",
       margin: "auto",
+      border: "5px",
       transition: "0.3s",
       boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
      "&:hover": {
@@ -31,14 +36,15 @@ export const HospitalView = (props:any) => {
         <Card className={classes.root} variant="outlined">
           <CardContent>
             <Typography variant="h5" component="h2">
-              {props.name}
+              <LocalHospitalIcon /> <b>{props.name}</b>
             </Typography>
+            <br />
             <Typography variant="body2" component="p">
-              Address: {props.addr}
+              <LocationOnIcon fontSize="small"/>  <b>Address:</b> {props.addr}
               <br />
-              Phone: {props.phone}
+              <PhoneIcon fontSize="small"/>  <b>Phone:</b> {props.phone}
               <br />
-              District: {props.district}
+              <LocationCityIcon fontSize="small"/>  <b>District:</b> {props.district}
             </Typography>
           </CardContent>
           <CardActions>
