2cd0a6ede4993d6ec263f8f2c4c2f1a0aa29889c
syamkumar <syam@Mozilla-community.org>
2020-03-23 17:57:05 +0530

    minor changes

--- src/Components/Dashboard/PublicDashboard.tsx
+++ src/Components/Dashboard/PublicDashboard.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { DashboardStatsCard } from '../Common/DashboardStatsCard';
+import {Typography} from "@material-ui/core";
 
 export const PublicDashboard = () => {
     return (
         <Grid item xs={12} style={{marginTop: '75px'}}>
-            <Grid container justify="flex-start" spacing={5}>
+            <Typography variant="h3">
+                <span>
+                    Updated On 23, March , 2020 at 18:00
+                </span>
+            </Typography>
+            {' '}
+            <Grid container justify="flex-start" spacing={5} style={{marginTop: 20}}>
                 <Grid item>
                     <DashboardStatsCard title="Patient count" value={25} />
                 </Grid>
