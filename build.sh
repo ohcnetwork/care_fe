24c92adf790111785d82bc5417287a01fd270450
Mansoor <mansoor.a@livestream.com>
2020-03-23 15:18:35 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ npm run build
 
 tar -czf build.tar.gz dist/
 
-echo $SSH_KEY > /tmp/id_rsa && chmod 400 /tmp/id_rsa
+echo $SSH_KEY > ./id_rsa && chmod 400 ./id_rsa
 
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /tmp/id_rsa build.tar.gz netlify@34.93.26.100:/home/netlify/releases/
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /tmp/id_rsa netlify@34.93.26.100 'cd /home/netlify/releases/; tar xf build.tar.gz; cp -rf /opt/care_fe/dist /opt/care_fe/dist.old; mv dist /opt/care_fe/dist'
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ./id_rsa build.tar.gz netlify@34.93.26.100:/home/netlify/releases/
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ./id_rsa netlify@34.93.26.100 'cd /home/netlify/releases/; tar xf build.tar.gz; cp -rf /opt/care_fe/dist /opt/care_fe/dist.old; mv dist /opt/care_fe/dist'
 
-rm /tmp/id_rsa
+rm ./id_rsa
