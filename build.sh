6c53512e7cdad48857fc911c7e59e0011934795c
Mansoor <mansoor.a@livestream.com>
2020-03-23 14:47:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,5 @@ echo $SSH_KEY > /tmp/id_rsa && chmod 400 /tmp/id_rsa
 
 scp -i /tmp/id_rsa build.tar.gz netlify@34.93.26.100:/home/netlify/releases/
 ssh -i /tmp/id_rsa netlify@34.93.26.100 'cd /home/netlify/releases/; tar xf build.tar.gz; cp -rf /opt/care_fe/dist /opt/care_fe/dist.old; mv dist /opt/care_fe/dist'
+
+rm /tmp/id_rsa
