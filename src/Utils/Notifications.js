a2cab59c9653d2840b32a07fe57791f6d98c92be
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-03-25 22:25:40 +0530

    added pnotify package

---
+++ src/Utils/Notifications.js
@@ -0,0 +1,8 @@
+import PNotify from 'pnotify/dist/es/PNotify';
+import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons';
+
+const Notifications = ({msg, type}) => {
+    
+}
+
+export default Notifications;
