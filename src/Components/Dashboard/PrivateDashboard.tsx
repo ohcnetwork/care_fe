25f14ba5a9ebac332f9a907fa1e75d4ec036b299
syamkumar <syam@Mozilla-community.org>
2020-03-27 20:24:18 +0530

    removed meta urls

--- src/Components/Dashboard/PrivateDashboard.tsx
+++ src/Components/Dashboard/PrivateDashboard.tsx
@@ -3,10 +3,9 @@ import Grid from '@material-ui/core/Grid';
 export const PrivateDashboard = () => {
     return (
         <Grid item xs={12} style={{marginTop: '75px'}}>
-            <iframe src="https://meta.coronasafe.network/public/dashboard/043d1168-25e2-4b17-a52b-2db04992c67f"
-                    frameBorder="0" width="100%" height="800" allowTransparency>
-
-            </iframe>
+          <h1>
+              Dashboard coming soon.
+          </h1>
 
         </Grid>
     );
