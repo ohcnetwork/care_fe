7cb55f51a7d971be6a8a2eda82b16d90f25048eb
syamkumar <syam@Mozilla-community.org>
2020-03-27 23:39:28 +0530

    removed unused api endpoints

--- src/Redux/actions.tsx
+++ src/Redux/actions.tsx
@@ -97,10 +97,6 @@ export const postLogin = (form: object) => {
     return fireRequest('login', [], form);
 };
 
-export const postSignUp = (form: object) => {
-    return fireRequest('signUp', [], form);
-};
-
 export const getCurrentUser = () => {
     return fireRequest('currentUser');
 };
