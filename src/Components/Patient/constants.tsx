d9c6153ff611707561bc88aa1af2f0d302339c6a
Puvikaran <puvikaran.s@gmail.com>
2020-03-28 16:56:23 +0530

    list, add,update patient added to Facility home

---
+++ src/Components/Patient/constants.tsx
@@ -0,0 +1,22 @@
+export const MEDICAL_HISTORY_CHOICES = [
+    {id:1, "text":"NO"},
+    {id:2, "text":"Diabetes"},
+    {id:3, "text":"Heart Disease"},
+    {id:4, "text":"HyperTension"},
+    {id:5, "text":"Kidney Diseases"}
+];
+
+export const GENDER_TYPES = [
+    {
+        "id": 1,
+        "text": "Male"
+    },
+    {
+        "id": 2,
+        "text": "Female"
+    },
+    {
+        "id": 3,
+        "text": "Other"
+    }
+]
