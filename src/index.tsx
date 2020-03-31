5248ae99bf88b1c80dc39ba74ba77c77299ac70e
syamkumar <syam@Mozilla-community.org>
2020-03-31 22:23:30 +0530

    sentry integration

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,11 @@ import * as serviceWorker from './serviceWorker';
 import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
 import {Provider} from "react-redux";
+import * as Sentry from '@sentry/browser';
 
 const store = createStore(reducer, applyMiddleware(thunk));
+
+Sentry.init({dsn: "https://8801155bd0b848a09de9ebf6f387ebc8@sentry.io/5183632"});
 
 ReactDOM.render(
     <Provider store={store}>
