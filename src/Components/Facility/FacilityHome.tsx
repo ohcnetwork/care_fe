ab017a538fa080e51499182365bc0b2aeac8934e
syamkumar <syam@Mozilla-community.org>
2020-04-01 19:36:46 +0530

    UI change

--- src/Components/Facility/FacilityHome.tsx
+++ src/Components/Facility/FacilityHome.tsx
@@ -139,7 +139,7 @@ export const FacilityHome = (props: any) => {
                 <Grid item xs={12} md={6} className="w3-center">
                     <Button fullWidth variant="contained" color="primary" size="small"
                         onClick={() => navigate(`/facility/${facilityId}/patient`)}>
-                        Add More Patients
+                        Add Details of Covid Suspects
                     </Button>
                 </Grid>
                 <Grid item xs={12} md={6} className="w3-center">
