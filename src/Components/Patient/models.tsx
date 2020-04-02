27f0951afca851e1366e7bded0077476a7a54ddc
Sandeep Thomas <sandeepthomas@outlook.com>
2020-04-02 10:03:24 +0530

    fix: update patient model

--- src/Components/Patient/models.tsx
+++ src/Components/Patient/models.tsx
@@ -4,10 +4,25 @@ export interface PatientModel {
     age?: number;
     gender?: number;
     phone_number?: string;
-    medical_history?: Array<{ id: number }>;
+    medical_history?: Array<{ disease: string | number; details: string }>;
     contact_with_carrier?: boolean;
     medical_history_details?: string;
     is_active?: boolean;
+    local_body_object?: { id: number; name: string; };
+    district_object?: { id: number; name: string; };
+    state_object?: { id: number; name: string; };
+    tele_consultation_history?: Array<any>;
+    address?: string;
+    contact_with_confirmed_carrier?: boolean;
+    contact_with_suspected_carrier?: boolean;
+    estimated_contact_date?: string;
+    past_travel?: boolean;
+    countries_travelled?: string;
+    present_health?: string;
+    has_SARI?: boolean;
+    local_body?: number;
+    district?: number;
+    state?: number;
 }
 
 export interface SampleTestModel {
@@ -22,7 +37,7 @@ export interface SampleTestModel {
 
 export interface DailyRoundsModel {
     temperature?: string;
-    temperature_measured_at?:string;
+    temperature_measured_at?: string;
     physical_examination_info?: string;
     other_details?: string;
     consultation?: number;
