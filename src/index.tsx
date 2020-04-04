a87b45d3817c0ab10da4788dbf1299b5056a76f8
syamkumar <syam@Mozilla-community.org>
2020-04-04 15:19:37 +0530

    add setry env variable

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import "./index.css";
 const store = createStore(reducer, applyMiddleware(thunk));
 
 Sentry.init({
+    environment: process.env.NODE_ENV,
   dsn: "https://8801155bd0b848a09de9ebf6f387ebc8@sentry.io/5183632"
 });
 
