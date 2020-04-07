8f9907ce99cd46cad0536cb3144b69eb1ff6cec2
albseb511 <albertsebastian511@gmail.com>
2020-04-08 03:43:23 +0530

    Added: logout dropdown effect

--- src/Components/Common/Header.tsx
+++ src/Components/Common/Header.tsx
@@ -119,17 +119,20 @@ const Header = () => {
             </button>
           );
         })}
+        <div className="dropdown border-b border-grey-500 items-center justify-start whitespace-no-wrap font-semibold cursor-pointer hover:bg-gray-100 w-full px-4 py-5">
+            PROFILE
         <div
-          className="border-b border-grey-500 inline-flex items-center justify-start whitespace-no-wrap font-semibold cursor-pointer hover:bg-gray-300 w-full px-4 py-4"
+          className="dropdown-menu hidden border-b border-grey-500 inline-flex items-center justify-start whitespace-no-wrap font-semibold cursor-pointer hover:bg-gray-300 w-full px-4 py-4"
           onClick={() => {
             localStorage.removeItem("care_access_token");
             localStorage.removeItem("care_refresh_token");
             navigate("/login");
             window.location.reload();
           }}
-        >
+          >
           <CancelIcon style={{ color: "#666", marginRight: "4px" }} />
           <span className="pl-4">Logout</span>
+        </div>
         </div>
         <div
             className="border-b border-grey-500 inline-flex items-center justify-start whitespace-no-wrap font-semibold cursor-pointer hover:bg-gray-300 w-full px-4 py-4"
