1e569435855a165a7b773706a8b02f9b29693d77
Bodhish Thomas <bodhish@gmail.com>
2020-05-12 23:09:28 +0530

    Clean up notfications.js and add click to close

--- src/Utils/Notifications.js
+++ src/Utils/Notifications.js
@@ -1,48 +1,41 @@
 import PNotify from "pnotify/dist/es/PNotify";
-import "pnotify/dist/es/PNotifyStyleMaterial";
-import "pnotify/dist/es/PNotifyButtons";
+import "pnotify/dist/PNotifyBrightTheme.css";
 
-// Set default styling.
-PNotify.defaults.styling = "material";
-// This icon setting requires the Material Icons font. (See below.)
-PNotify.defaults.icons = "material";
-
-/** Success message handler */
-export const Success = ({msg}) => {
-    PNotify.success({
-        text: msg,
+const notify = (text, type) => {
+    const notification = PNotify.alert({
+        type: type,
+        text: text,
+        styling: "brighttheme",
+        buttons: {
+            closer: false,
+            sticker: false
+        },
         modules: {
             Desktop: {
                 desktop: true
             }
         }
     });
+    notification.refs.elem.addEventListener("click", () => {
+        notification.close();
+    });
+};
+
+/** Success message handler */
+export const Success = ({ msg }) => {
+    notify(msg, 'success')
 };
 
 /** Error message handler */
-export const Error = ({msg}) => {
-    PNotify.error({
-        text: msg,
-        modules: {
-            Desktop: {
-                desktop: true
-            }
-        }
-    });
+export const Error = ({ msg }) => {
+    notify(msg, 'error')
 };
 
 /** 400 Bad Request handler */
-export const BadRequest = ({errs}) => {
-    
+export const BadRequest = ({ errs }) => {
+
     for (let [key, value] of Object.entries(errs)) {
         const errorMsg = typeof value === "string" ? `${key} - ${value}` : Array.isArray(value) ? `${key} - ${value.join(', ')}` : `Bad Request - ${key}`;
-        PNotify.error({
-            text: errorMsg,
-            modules: {
-                Desktop: {
-                    desktop: true
-                }
-            }
-        });
+        notify(errorMsg, 'error')
     }
 };
