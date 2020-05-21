55027ba7f31443bbc41fdb418ec6331fcbeedc33
gigincg <mail.gigin@gmail.com>
2020-05-21 23:15:50 +0530

    PWA Enable

--- src/index.tsx
+++ src/index.tsx
@@ -33,4 +33,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorker.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
