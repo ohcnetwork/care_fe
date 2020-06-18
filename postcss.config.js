378b183e1af0cca37feb9ebd6a3303eed0e58d17
syam <hey@syam.dev>
2020-06-18 19:31:55 +0530

    tailwind Purge Fix

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@
 const cssnano = require("cssnano");
 const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./public/**/*.html", "./src/**/*.js", "./src/**/*.jsx"],
-  efaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+  defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
 });
 module.exports = {
   plugins: [
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,5 @@ module.exports = {
   },
   variants: {},
   plugins: [require("@tailwindcss/ui")],
+  purge: false
 };
