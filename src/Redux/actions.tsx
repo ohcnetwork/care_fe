6d08da67b3d4dbdfe372700b0e5a1d157d6b4d36
psjishnu <psjishnu13@gmail.com>
2020-07-04 20:52:52 +0530

    Added double quotes and semicolon

--- src/Redux/actions.tsx
+++ src/Redux/actions.tsx
@@ -2,193 +2,195 @@ import { fireRequest } from "./fireRequest";
 
 // User
 export const postLogin = (params: object) => {
-    return fireRequest('login', [], params);
+  return fireRequest("login", [], params);
 };
 export const getCurrentUser = () => {
-    return fireRequest('currentUser');
+  return fireRequest("currentUser");
 };
 export const signupUser = (params: object) => {
-    return fireRequest("createUser", [], params)
+  return fireRequest("createUser", [], params);
 };
 export const addUser = (params: object) => {
-    return fireRequest("addUser", [], params)
+  return fireRequest("addUser", [], params);
 };
 
 export const postResetPassword = (form: object) => {
-    return fireRequest('resetPassword', [], form);
+  return fireRequest("resetPassword", [], form);
 };
 
 export const postForgotPassword = (form: object) => {
-    return fireRequest('forgotPassword', [], form);
+  return fireRequest("forgotPassword", [], form);
 };
 
 // Facility
 export const createFacility = (params: object) => {
-    return fireRequest("createFacility", [], params);
+  return fireRequest("createFacility", [], params);
 };
 export const updateFacility = (id: number, params: object) => {
-    return fireRequest('updateFacility', [id], params);
+  return fireRequest("updateFacility", [id], params);
 };
 export const getUserList = (params: object) => {
-    return fireRequest('userList', [], params);
+  return fireRequest("userList", [], params);
 };
 export const getFacilities = (params: object) => {
-    return fireRequest('listFacility', [], params);
+  return fireRequest("listFacility", [], params);
 };
 export const getFacility = (id: number) => {
-    return fireRequest('getFacility', [id], {});
+  return fireRequest("getFacility", [id], {});
 };
-
 
 // Capacity/Triage/Doctor
 export const createCapacity = (id: number | undefined, params: object, pathParam: object) => {
-    return id ? fireRequest('updateCapacity', [id], params, pathParam) : fireRequest("createCapacity", [], params, pathParam);
+  return id ? fireRequest("updateCapacity", [id], params, pathParam) : fireRequest("createCapacity", [], params, pathParam);
 };
 export const createDoctor = (id: number | undefined, params: object, pathParam: object) => {
-    return id ? fireRequest('updateDoctor', [id], params, pathParam) : fireRequest("createDoctor", [], params, pathParam);
+  return id ? fireRequest("updateDoctor", [id], params, pathParam) : fireRequest("createDoctor", [], params, pathParam);
 };
 export const createTriageForm = (params: object, pathParam: object) => {
-    return fireRequest('createTriage', [], params, pathParam)
+  return fireRequest("createTriage", [], params, pathParam);
 };
 export const getTriageInfo = (pathParam: object) => {
-    return fireRequest('getTriage', [], {}, pathParam)
+  return fireRequest("getTriage", [], {}, pathParam);
 };
 export const getTriageDetails = (id: number, pathParam: object) => {
-    return fireRequest('getTriage', [id], {}, pathParam)
+  return fireRequest("getTriage", [id], {}, pathParam);
 };
 export const listCapacity = (params: object, pathParam: object) => {
-    return fireRequest('getCapacity', [], params, pathParam);
+  return fireRequest("getCapacity", [], params, pathParam);
 };
 export const listDoctor = (params: object, pathParam: object) => {
-    return fireRequest('getDoctor', [], params, pathParam);
+  return fireRequest("getDoctor", [], params, pathParam);
 };
 export const getCapacity = (id: number, pathParam: object) => {
-    return fireRequest('getCapacity', [id], {}, pathParam);
+  return fireRequest("getCapacity", [id], {}, pathParam);
 };
 export const getDoctor = (id: number, pathParam: object) => {
-    return fireRequest('getDoctor', [id], {}, pathParam);
+  return fireRequest("getDoctor", [id], {}, pathParam);
 };
 
 //Patient
 export const searchPatient = (params: object) => {
-    return fireRequest('searchPatient', [], params);
+  return fireRequest("searchPatient", [], params);
 };
 export const searchPatientFilter = (params: object) => {
-    return fireRequest('searchPatientFilter', [], params);
+  return fireRequest("searchPatientFilter", [], params);
 };
 export const searchUser = (params: object) => {
-    return fireRequest('searchUser', [], params);
+  return fireRequest("searchUser", [], params);
 };
 export const getAllPatient = (params: object) => {
-    return fireRequest('patientList', [], params);
+  return fireRequest("patientList", [], params);
 };
 export const createPatient = (params: object) => {
-    return fireRequest('addPatient', [], params)
+  return fireRequest("addPatient", [], params);
 };
 export const getPatient = (pathParam: object) => {
-    return fireRequest('getPatient', [], {}, pathParam);
+  return fireRequest("getPatient", [], {}, pathParam);
 };
 export const updatePatient = (params: object, pathParam: object) => {
-    return fireRequest('updatePatient', [], params, pathParam)
+  return fireRequest("updatePatient", [], params, pathParam);
 };
 export const patchPatient = (params: object, pathParam: object) => {
-    return fireRequest('patchPatient', [], params, pathParam)
+  return fireRequest("patchPatient", [], params, pathParam);
 };
 export const transferPatient = (params: object, pathParam: object) => {
-    return fireRequest('transferPatient', [], params, pathParam)
-}
+  return fireRequest("transferPatient", [], params, pathParam);
+};
 export const getStates = () => {
-    return fireRequest("statesList", [])
-}
+  return fireRequest("statesList", []);
+};
 
 // District/State/Local body
 export const getDistrictByState = (pathParam: object) => {
-    return fireRequest("getDistrictByState", [], {}, pathParam)
-}
+  return fireRequest("getDistrictByState", [], {}, pathParam);
+};
 export const getLocalbodyByDistrict = (pathParam: object) => {
-    return fireRequest("getLocalbodyByDistrict", [], {}, pathParam)
-}
+  return fireRequest("getLocalbodyByDistrict", [], {}, pathParam);
+};
 
 // Sample Test
 export const getSampleTestList = (params: object, pathParam: object) => {
-    return fireRequest("sampleTestList", [], params, pathParam)
-}
+  return fireRequest("sampleTestList", [], params, pathParam);
+};
 
 export const sampleSearch = (params: object) => {
-    return fireRequest('getTestSampleList', [], params);
-}
+  return fireRequest("getTestSampleList", [], params);
+};
 export const createSampleTest = (params: object, pathParam: object) => {
-    return fireRequest('createSampleTest', [], params, pathParam)
+  return fireRequest("createSampleTest", [], params, pathParam);
 };
 export const sampleReport = (pathParam: object) => {
-    return fireRequest('sampleReport', [], {}, pathParam)
+  return fireRequest("sampleReport", [], {}, pathParam);
 };
 export const getTestList = (params: object) => {
-    return fireRequest('getTestSampleList', [], params);
+  return fireRequest("getTestSampleList", [], params);
 };
 export const getTestSample = (id: number) => {
-    return fireRequest('getTestSample', [id], {});
+  return fireRequest("getTestSample", [id], {});
 };
 export const patchSample = (id: any, params: object) => {
-    return fireRequest('patchSample', [id], params)
+  return fireRequest("patchSample", [id], params);
 };
 
 // Daily Rounds
 
 export const createDailyReport = (params: object, pathParam: object) => {
-    return fireRequest('createDailyRounds', [], params, pathParam)
+  return fireRequest("createDailyRounds", [], params, pathParam);
 };
 export const updateDailyReport = (params: object, pathParam: object) => {
-    return fireRequest('updateDailyReport', [], params, pathParam)
+  return fireRequest("updateDailyReport", [], params, pathParam);
 };
 export const getDailyReport = (params: object, pathParam: object) => {
-    return fireRequest('getDailyReports', [], params, pathParam)
+  return fireRequest("getDailyReports", [], params, pathParam);
 };
-export const getConsultationDailyRoundsDetails = (id: number, pathParam: object) => {
-    return fireRequest('getDailyReports', [id], {}, pathParam);
+export const getConsultationDailyRoundsDetails = (
+  id: number,
+  pathParam: object
+) => {
+  return fireRequest("getDailyReports", [id], {}, pathParam);
 };
 
 // Consultation
 export const createConsultation = (params: object) => {
-    return fireRequest("createConsultation", [], params);
+  return fireRequest("createConsultation", [], params);
 };
 export const getConsultationList = (params: object) => {
-    return fireRequest('getConsultationList', [], params);
+  return fireRequest("getConsultationList", [], params);
 };
 export const getConsultation = (id: number) => {
-    return fireRequest('getConsultation', [id], {});
+  return fireRequest("getConsultation", [id], {});
 };
 export const updateConsultation = (id: number, params: object) => {
-    return fireRequest("updateConsultation", [id], params);
+  return fireRequest("updateConsultation", [id], params);
 };
 //Inventory
 export const getItems = (params: object) => {
-    return fireRequest("getItems", [], params);
+  return fireRequest("getItems", [], params);
 };
 export const postInventory = (params: object, pathParams: object) => {
-    return fireRequest("createInventory", [], params, pathParams)
+  return fireRequest("createInventory", [], params, pathParams);
 };
 export const getInventoryLog = (params: object, pathParams: object) => {
-    return fireRequest("getInventoryLog", [params, "inventory"], pathParams)
+  return fireRequest("getInventoryLog", [params, "inventory"], pathParams);
 };
 export const setMinQuantity = (params: object, pathParams: object) => {
-    return fireRequest("setMinQuantity", [], params, pathParams)
+  return fireRequest("setMinQuantity", [], params, pathParams);
 };
 export const getMinQuantity = (facilityId: object, params: object) => {
-    return fireRequest("getMinQuantity", [facilityId, "min_quantity"], params);
+  return fireRequest("getMinQuantity", [facilityId, "min_quantity"], params);
 };
 export const updateMinQuantity = (pathParams: object, params: object) => {
-    return fireRequest("updateMinQuantity", [], pathParams, params);
-}
+  return fireRequest("updateMinQuantity", [], pathParams, params);
+};
 export const getInventorySummary = (facilityId: number, params: object) => {
-    return fireRequest('getInventorySummary', [facilityId, "inventorysummary"], params);
+  return fireRequest("getInventorySummary", [facilityId, "inventorysummary"], params);
 };
 export const getItemName = (id: number) => {
-    return fireRequest('getItemName', [id], {})
+  return fireRequest("getItemName", [id], {});
 };
 export const discharge = (params: object, pathParams: object) => {
-    return fireRequest("discharge", [], params, pathParams)
+  return fireRequest("discharge", [], params, pathParams);
 };
 export const dischargePatient = (params: object, pathParams: object) => {
-    return fireRequest('dischargePatient', [], params, pathParams)
+  return fireRequest("dischargePatient", [], params, pathParams);
 };
--- src/Redux/api.tsx
+++ src/Redux/api.tsx
@@ -1,298 +1,295 @@
 export default {
-
     // Auth Endpoints
     login: {
-        path: '/api/v1/auth/login/',
-        method: 'POST',
-        noAuth: true,
+      path: "/api/v1/auth/login/",
+      method: "POST",
+      noAuth: true,
     },
-
+  
     token_refresh: {
-        path: '/api/v1/auth/token/refresh',
-        method: 'POST'
+      path: "/api/v1/auth/token/refresh",
+      method: "POST",
     },
-
+  
     token_verify: {
-        path: '/api/v1/auth/token/verify',
-        method: 'POST'
+      path: "/api/v1/auth/token/verify",
+      method: "POST",
     },
-
+  
     resetPassword: {
-        path: '/api/v1/password_reset/confirm/',
-        method: 'POST',
+      path: "/api/v1/password_reset/confirm/",
+      method: "POST",
     },
-    
+  
     forgotPassword: {
-        path: '/api/v1/password_reset/',
-        method: 'POST',
+      path: "/api/v1/password_reset/",
+      method: "POST",
     },
     // User Endpoints
     currentUser: {
-        path: '/api/v1/users/getcurrentuser',
+      path: "/api/v1/users/getcurrentuser",
     },
-
+  
     userList: {
-        path: '/api/v1/users',
+      path: "/api/v1/users",
     },
-
+  
     createUser: {
-        path: '/api/v1/users/',
-        method: 'POST',
-        noAuth: true,
+      path: "/api/v1/users/",
+      method: "POST",
+      noAuth: true,
     },
-
+  
     updateUser: {
-        path: '/api/v1/users',
-        method: 'PUT'
+      path: "/api/v1/users",
+      method: "PUT",
     },
-
+  
     partialUpdateUser: {
-        path: '/api/v1/users',
-        method: 'PATCH'
+      path: "/api/v1/users",
+      method: "PATCH",
     },
-
+  
     deleteUser: {
-        path: '/api/v1/users',
-        method: 'DELETE'
+      path: "/api/v1/users",
+      method: "DELETE",
     },
-
+  
     addUser: {
-        path: "/api/v1/users/add_user/",
-        method: 'POST',
+      path: "/api/v1/users/add_user/",
+      method: "POST",
     },
-
+  
     searchUser: {
-        path: "/api/v1/users/search",
+      path: "/api/v1/users/search",
     },
-
+  
     // Facility Endpoints
-
+  
     listFacility: {
-        path: '/api/v1/facility'
+      path: "/api/v1/facility",
     },
-
+  
     createFacility: {
-        path: '/api/v1/facility/',
-        method: 'POST'
+      path: "/api/v1/facility/",
+      method: "POST",
     },
-
+  
     getFacility: {
-        path: '/api/v1/facility'
+      path: "/api/v1/facility",
     },
-
+  
     updateFacility: {
-        path: '/api/v1/facility',
-        method: 'PUT'
+      path: "/api/v1/facility",
+      method: "PUT",
     },
-
+  
     partialUpdateFacility: {
-        path: '/api/v1/facility',
-        method: 'PATCH'
+      path: "/api/v1/facility",
+      method: "PATCH",
     },
-
+  
     deleteFacility: {
-        path: '/api/v1/facility',
-        method: 'DELETE'
+      path: "/api/v1/facility",
+      method: "DELETE",
     },
-
-
-
-
+  
     getConsultationList: {
-        path: '/api/v1/consultation/'
+      path: "/api/v1/consultation/",
     },
     createConsultation: {
-        path: '/api/v1/consultation/',
-        method: 'POST',
+      path: "/api/v1/consultation/",
+      method: "POST",
     },
     getConsultation: {
-        path: '/api/v1/consultation',
+      path: "/api/v1/consultation",
     },
     updateConsultation: {
-        path: '/api/v1/consultation',
-        method: 'PUT',
+      path: "/api/v1/consultation",
+      method: "PUT",
     },
     partialUpdateConsultation: {
-        path: '/api/v1/consultation/{id}/',
-        method: 'PATCH'
+      path: "/api/v1/consultation/{id}/",
+      method: "PATCH",
     },
     deleteConsultation: {
-        path: '/api/v1/consultation/{id}/',
-        method: 'DELETE'
+      path: "/api/v1/consultation/{id}/",
+      method: "DELETE",
     },
     createDailyRounds: {
-        path: '/api/v1/consultation/{consultationId}/daily_rounds/',
-        method: 'POST'
+      path: "/api/v1/consultation/{consultationId}/daily_rounds/",
+      method: "POST",
     },
     updateDailyReport: {
-        path: '/api/v1/consultation/{consultationId}/daily_rounds/{id}/',
-        method: 'PUT'
+      path: "/api/v1/consultation/{consultationId}/daily_rounds/{id}/",
+      method: "PUT",
     },
     getDailyReports: {
-        path: '/api/v1/consultation/{consultationId}/daily_rounds',
+      path: "/api/v1/consultation/{consultationId}/daily_rounds",
     },
-
+  
     // Hospital Beds
     createCapacity: {
-        path: "/api/v1/facility/{facilityId}/capacity/",
-        method: 'POST'
+      path: "/api/v1/facility/{facilityId}/capacity/",
+      method: "POST",
     },
-
+  
     createDoctor: {
-        path: "/api/v1/facility/{facilityId}/hospital_doctor/",
-        method: 'POST'
+      path: "/api/v1/facility/{facilityId}/hospital_doctor/",
+      method: "POST",
     },
-
+  
     getCapacity: {
-        path: '/api/v1/facility/{facilityId}/capacity'
+      path: "/api/v1/facility/{facilityId}/capacity",
     },
-
+  
     getDoctor: {
-        path: '/api/v1/facility/{facilityId}/hospital_doctor'
+      path: "/api/v1/facility/{facilityId}/hospital_doctor",
     },
-
+  
     updateCapacity: {
-        path: '/api/v1/facility/{facilityId}/capacity',
-        method: 'PUT'
+      path: "/api/v1/facility/{facilityId}/capacity",
+      method: "PUT",
     },
-
+  
     updateDoctor: {
-        path: '/api/v1/facility/{facilityId}/hospital_doctor',
-        method: 'PUT'
+      path: "/api/v1/facility/{facilityId}/hospital_doctor",
+      method: "PUT",
     },
-
+  
     //Triage
     createTriage: {
-        path: '/api/v1/facility/{facilityId}/patient_stats/',
-        method: 'POST'
+      path: "/api/v1/facility/{facilityId}/patient_stats/",
+      method: "POST",
     },
     getTriage: {
-        path: '/api/v1/facility/{facilityId}/patient_stats',
+      path: "/api/v1/facility/{facilityId}/patient_stats",
     },
-
+  
     // //Care Center
     // createCenter: {
     //     path: "/api/v1/carecenter/",
     //     method: 'POST'
     // }
-
+  
     // Patient
-
+  
     searchPatient: {
-        path: "/api/v1/patient/search",
+      path: "/api/v1/patient/search",
     },
     searchPatientFilter: {
-        path: "/api/v1/patient_search"
+      path: "/api/v1/patient_search",
     },
     patientList: {
-        path: "/api/v1/patient",
+      path: "/api/v1/patient",
     },
     addPatient: {
-        path: "/api/v1/patient/",
-        method: 'POST'
+      path: "/api/v1/patient/",
+      method: "POST",
     },
     getPatient: {
-        path: '/api/v1/patient/{id}/'
+      path: "/api/v1/patient/{id}/",
     },
     updatePatient: {
-        path: '/api/v1/patient/{id}/',
-        method: 'PUT'
+      path: "/api/v1/patient/{id}/",
+      method: "PUT",
     },
     patchPatient: {
-        path: '/api/v1/patient/{id}/',
-        method: 'PATCH'
+      path: "/api/v1/patient/{id}/",
+      method: "PATCH",
     },
     transferPatient: {
-        path: '/api/v1/patient/{id}/transfer/',
-        method: 'POST'
+      path: "/api/v1/patient/{id}/transfer/",
+      method: "POST",
     },
     sampleTestList: {
-        path: '/api/v1/patient/{patientId}/test_sample/',
+      path: "/api/v1/patient/{patientId}/test_sample/",
     },
     createSampleTest: {
-        path: '/api/v1/patient/{patientId}/test_sample/',
-        method: 'POST',
+      path: "/api/v1/patient/{patientId}/test_sample/",
+      method: "POST",
     },
     sampleReport: {
-        path: '/api/v1/patient/{id}/icmr_sample'
+      path: "/api/v1/patient/{id}/icmr_sample",
     },
-
+  
     // States
     statesList: {
-        path: "/api/v1/state/",
+      path: "/api/v1/state/",
     },
-
+  
     getState: {
-        path: "/api/v1/state/{id}/",
+      path: "/api/v1/state/{id}/",
     },
-
+  
     // Districts
-
+  
     getDistrict: {
-        path: "/api/v1/district/{id}/",
+      path: "/api/v1/district/{id}/",
     },
     getDistrictByState: {
-        path: "/api/v1/state/{id}/districts/",
+      path: "/api/v1/state/{id}/districts/",
     },
-
+  
     // Local Body
     getLocalBody: {
-        path: "/api/v1/local_body/{id}/",
+      path: "/api/v1/local_body/{id}/",
     },
     getLocalbodyByDistrict: {
-        path: "/api/v1/district/{id}/local_bodies/",
+      path: "/api/v1/district/{id}/local_bodies/",
     },
-
+  
     // Sample Test
     getTestSampleList: {
-        path: '/api/v1/test_sample'
+      path: "/api/v1/test_sample",
     },
     getTestSample: {
-        path: '/api/v1/test_sample',
-        method: 'POST'
+      path: "/api/v1/test_sample",
+      method: "POST",
     },
     patchSample: {
-        path: '/api/v1/test_sample',
-        method: 'PATCH',
+      path: "/api/v1/test_sample",
+      method: "PATCH",
     },
-
+  
     //inventory
     getItems: {
-        path: '/api/v1/items/'
+      path: "/api/v1/items/",
     },
     createInventory: {
-        path: "/api/v1/facility/{facilityId}/inventory/",
-        method: 'POST'
+      path: "/api/v1/facility/{facilityId}/inventory/",
+      method: "POST",
     },
     getInventoryLog: {
-        path: "/api/v1/facility",
+      path: "/api/v1/facility",
     },
     setMinQuantity: {
-        path: "/api/v1/facility/{facilityId}/min_quantity/",
-        method: 'POST'
+      path: "/api/v1/facility/{facilityId}/min_quantity/",
+      method: "POST",
     },
     getMinQuantity: {
-        path: "/api/v1/facility",
-        method: 'GET',
+      path: "/api/v1/facility",
+      method: "GET",
     },
     updateMinQuantity: {
-        path: "/api/v1/facility/{facilityId}/min_quantity/{inventoryId}",
-        method: 'PATCH',
+      path: "/api/v1/facility/{facilityId}/min_quantity/{inventoryId}",
+      method: "PATCH",
     },
     getInventorySummary: {
-        path: '/api/v1/facility',
-        method: 'GET',
+      path: "/api/v1/facility",
+      method: "GET",
     },
     getItemName: {
-        path: '/api/v1/items',
-        method: 'GET',
+      path: "/api/v1/items",
+      method: "GET",
     },
     discharge: {
-        path: '/api/v1/patient/{external_id}/discharge_summary/',
-        method: 'POST'
+      path: "/api/v1/patient/{external_id}/discharge_summary/",
+      method: "POST",
     },
     dischargePatient: {
-        path: '/api/v1/patient/{id}/discharge_patient/',
-        method: 'POST'
-    }
-}
+      path: "/api/v1/patient/{id}/discharge_patient/",
+      method: "POST",
+    },
+  };
+  
