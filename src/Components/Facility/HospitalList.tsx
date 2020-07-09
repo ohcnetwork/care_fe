14a2e2fa91c9cd64c28443e05f300d2d3fc47c4c
psjishnu <psjishnu13@gmail.com>
2020-07-10 00:58:10 +0530

    updated download button

--- src/Components/Facility/HospitalList.tsx
+++ src/Components/Facility/HospitalList.tsx
@@ -7,7 +7,7 @@ import {
   downloadFacility,
   downloadFacilityCapacity,
   downloadFacilityDoctors,
-  downloadFacilityTriage
+  downloadFacilityTriage,
 } from "../../Redux/actions";
 import { Loading } from "../Common/Loading";
 import Pagination from "../Common/Pagination";
@@ -15,26 +15,26 @@ import { FacilityModel } from "./models";
 import { InputSearchBox } from "../Common/SearchBox";
 import PageTitle from "../Common/PageTitle";
 import { CSVLink } from "react-csv";
-import moment from 'moment';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import moment from "moment";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      root: {
-        width: '100%',
-      },
-      heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
-      },
-    }),
+  createStyles({
+    root: {
+      width: "100%",
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+  })
 );
-const now = moment().format('DD-MM-YYYY:hh:mm:ss');
+const now = moment().format("DD-MM-YYYY:hh:mm:ss");
 
 export const HospitalList = () => {
   const classes = useStyles();
@@ -105,21 +105,25 @@ export const HospitalList = () => {
   };
   const onSearchSuspects = async (searchValue: string) => {
     setIsLoading(true);
-    const res = await dispatchAction(getFacilities({ limit, offset, search_text: searchValue }));
+    const res = await dispatchAction(
+      getFacilities({ limit, offset, search_text: searchValue })
+    );
     if (res && res.data) {
       setData(res.data.results);
       setTotalCount(res.data.count);
     }
     setIsLoading(false);
-  }
+  };
 
   let facilityList: any[] = [];
   if (data && data.length) {
     facilityList = data.map((facility: any, idx: number) => {
       return (
-        <div key={`usr_${facility.id}`} className="w-full md:w-1/2 mt-6 md:px-4">
-          <div
-            className="block rounded-lg bg-white shadow h-full hover:border-primary-500 overflow-hidden">
+        <div
+          key={`usr_${facility.id}`}
+          className="w-full md:w-1/2 mt-6 md:px-4"
+        >
+          <div className="block rounded-lg bg-white shadow h-full hover:border-primary-500 overflow-hidden">
             <div className="h-full flex flex-col justify-between">
               <div className="px-6 py-4">
                 <div className="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium leading-5 bg-blue-100 text-blue-800">
@@ -129,19 +133,31 @@ export const HospitalList = () => {
                   {facility.name}
                 </div>
                 <div className="mt-2">
-                  <div className="text-gray-500 leading-relaxed font-light">District:</div>
-                  <div className="font-semibold">{facility.district_object?.name}</div>
+                  <div className="text-gray-500 leading-relaxed font-light">
+                    District:
+                  </div>
+                  <div className="font-semibold">
+                    {facility.district_object?.name}
+                  </div>
                 </div>
               </div>
               <div className="mt-2 bg-gray-50 border-t px-6 py-2">
                 <div className="flex py-4 justify-between">
                   <div>
                     <div className="text-gray-500 leading-relaxed">Phone:</div>
-                    <a href={`tel:${facility.phone_number}`} className="font-semibold">{facility.phone_number || "-"}</a>
+                    <a
+                      href={`tel:${facility.phone_number}`}
+                      className="font-semibold"
+                    >
+                      {facility.phone_number || "-"}
+                    </a>
                   </div>
                   <span className="inline-flex rounded-md shadow-sm">
-                    <button type="button" className="inline-flex items-center px-3 py-2 border border-green-500 text-sm leading-4 font-medium rounded-md text-green-700 bg-white hover:text-green-500 focus:outline-none focus:border-green-300 focus:shadow-outline-blue active:text-green-800 active:bg-gray-50 transition ease-in-out duration-150 hover:shadow"
-                      onClick={() => navigate(`/facility/${facility.id}`)}>
+                    <button
+                      type="button"
+                      className="inline-flex items-center px-3 py-2 border border-green-500 text-sm leading-4 font-medium rounded-md text-green-700 bg-white hover:text-green-500 focus:outline-none focus:border-green-300 focus:shadow-outline-blue active:text-green-800 active:bg-gray-50 transition ease-in-out duration-150 hover:shadow"
+                      onClick={() => navigate(`/facility/${facility.id}`)}
+                    >
                       View Facility
                     </button>
                   </span>
@@ -197,85 +213,81 @@ export const HospitalList = () => {
           />
         </div>
         <div className={classes.root}>
-        <Accordion>
-          <AccordionSummary
+          <Accordion>
+            <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
               aria-controls="panel1a-content"
               id="panel1a-header"
-          >
-            <Typography className={classes.heading}>Downloads</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <div className="w-1/4 text-center items-center">
-              <CSVLink
+            >
+              <Typography className={classes.heading}>Downloads</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className="w-1/4 text-center items-center">
+                <CSVLink
                   data={DownloadFile}
                   filename={`facilities-${now}.csv`}
                   target="_blank"
                   className="hidden"
                   id="facilityDownloader"
-              >
-              </CSVLink>
-              <button
-                    type="button"
-                    className="inline-flex items-center mr-2 px-1 py-3 ml-1  lg:px-3 border border-green-500 text-sm leading-4 font-medium rounded-md text-green-700 hover:bg-green-600 hover:text-white bg-white focus:outline-none focus:border-green-300 focus:shadow-outline-blue active:text-green-800 active:bg-gray-50 transition ease-in-out duration-150 hover:shadow"
-                    onClick={handleDownload}
+                ></CSVLink>
+                <button
+                  type="button"
+                  className="inline-flex items-center mr-2 px-1 py-3 ml-1  lg:px-3 border border-green-500 text-sm leading-4 font-medium rounded-md text-green-700 hover:bg-green-600 hover:text-white bg-white focus:outline-none focus:border-green-300 focus:shadow-outline-blue active:text-green-800 active:bg-gray-50 transition ease-in-out duration-150 hover:shadow"
+                  onClick={handleDownload}
                 >
                   Download Facility List
-              </button>
-            </div>
-            <div className="w-1/4 text-center items-center">
-              <CSVLink
+                </button>
+              </div>
+              <div className="w-1/4 text-center items-center">
+                <CSVLink
                   data={capacityDownloadFile}
                   filename={`facility-capacity-${now}.csv`}
                   className="hidden"
                   id="capacityDownloader"
                   target="_blank"
-              >
-              </CSVLink>
-              <button
-                    type="button"
-                    className="inline-flex items-center mr-2 px-1 py-3 ml-1  lg:px-3 border border-green-500 text-sm leading-4 font-medium rounded-md text-green-700 hover:bg-green-600 hover:text-white bg-white focus:outline-none focus:border-green-300 focus:shadow-outline-blue active:text-green-800 active:bg-gray-50 transition ease-in-out duration-150 hover:shadow"
-                    onClick={handleCapacityDownload}
+                ></CSVLink>
+                <button
+                  type="button"
+                  className="inline-flex items-center mr-2 px-1 py-3 ml-1  lg:px-3 border border-green-500 text-sm leading-4 font-medium rounded-md text-green-700 hover:bg-green-600 hover:text-white bg-white focus:outline-none focus:border-green-300 focus:shadow-outline-blue active:text-green-800 active:bg-gray-50 transition ease-in-out duration-150 hover:shadow"
+                  onClick={handleCapacityDownload}
                 >
                   Download Facility Capacity List
-              </button>
-            </div>
-            <div className="w-1/4 text-center items-center">
-              <CSVLink
+                </button>
+              </div>
+              <div className="w-1/4 text-center items-center">
+                <CSVLink
                   data={doctorsDownloadFile}
                   filename={`facility-doctors-${now}.csv`}
                   target="_blank"
                   className="hidden"
                   id="doctorsDownloader"
-              >
-              </CSVLink>
-              <button
-                    type="button"
-                    className="inline-flex items-center mr-2 px-1 py-3 ml-1  lg:px-3 border border-green-500 text-sm leading-4 font-medium rounded-md text-green-700 hover:bg-green-600 hover:text-white bg-white focus:outline-none focus:border-green-300 focus:shadow-outline-blue active:text-green-800 active:bg-gray-50 transition ease-in-out duration-150 hover:shadow"
-                    onClick={handleDoctorsDownload}
-              >
+                ></CSVLink>
+                <button
+                  type="button"
+                  className="inline-flex items-center mr-2 px-1 py-3 ml-1  lg:px-3 border border-green-500 text-sm leading-4 font-medium rounded-md text-green-700 hover:bg-green-600 hover:text-white bg-white focus:outline-none focus:border-green-300 focus:shadow-outline-blue active:text-green-800 active:bg-gray-50 transition ease-in-out duration-150 hover:shadow"
+                  onClick={handleDoctorsDownload}
+                >
                   Download Facility Doctors List
-              </button>
-            </div>
-            <div className="w-1/4 text-center items-center">
-              <CSVLink
+                </button>
+              </div>
+              <div className="w-1/4 text-center items-center">
+                <CSVLink
                   data={triageDownloadFile}
                   filename={`facility-triage-${now}.csv`}
                   target="_blank"
                   className="hidden"
                   id="triageDownloader"
-              >
-              </CSVLink>
-              <button
-                    type="button"
-                    className="inline-flex items-center mr-2 px-1 py-3 ml-1  lg:px-3 border border-green-500 text-sm leading-4 font-medium rounded-md text-green-700 hover:bg-green-600 hover:text-white bg-white focus:outline-none focus:border-green-300 focus:shadow-outline-blue active:text-green-800 active:bg-gray-50 transition ease-in-out duration-150 hover:shadow"
-                    onClick={handleTriageDownload}
-              >
+                ></CSVLink>
+                <button
+                  type="button"
+                  className="inline-flex items-center mr-2 px-1 py-3 ml-1  lg:px-3 border border-green-500 text-sm leading-4 font-medium rounded-md text-green-700 hover:bg-green-600 hover:text-white bg-white focus:outline-none focus:border-green-300 focus:shadow-outline-blue active:text-green-800 active:bg-gray-50 transition ease-in-out duration-150 hover:shadow"
+                  onClick={handleTriageDownload}
+                >
                   Download Facility Triage Data
-              </button>
-            </div>
-          </AccordionDetails>
-        </Accordion>
+                </button>
+              </div>
+            </AccordionDetails>
+          </Accordion>
         </div>
       </div>
       <div className="px-3 md:px-8">
