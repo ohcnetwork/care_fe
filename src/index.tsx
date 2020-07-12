61e82c6cb387ef848b7abf045d35f5123ad9e7f5
syamkumar <syam@mozilla-community.org>
2020-07-13 01:30:34 +0530

    minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {reducer} from "../src/Redux/Reducer";
+import {reducer} from "./Redux/Reducer";
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import App from "./App";
 import { applyMiddleware, createStore } from "redux";
