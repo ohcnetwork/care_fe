0fae42cd2294cb19f10fed1ccfbf28cd2e6f928c
syamkumar <hey@syam.dev>
2020-07-31 15:49:32 +0530

    WIP. Tab View. #483

--- src/Components/Patient/ManagePatients.tsx
+++ src/Components/Patient/ManagePatients.tsx
@@ -1,6 +1,5 @@
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
 import WarningRoundedIcon from "@material-ui/icons/WarningRounded";
 import { navigate } from "hookrouter";
 import React, { useCallback, useState } from "react";
@@ -14,7 +13,54 @@ import { PatientFilter } from "./PatientFilter";
 import { InputSearchBox } from "../Common/SearchBox";
 import { CSVLink } from "react-csv";
 import moment from 'moment';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+      <div
+          role="tabpanel"
+          hidden={value !== index}
+          id={`full-width-tabpanel-${index}`}
+          aria-labelledby={`full-width-tab-${index}`}
+          {...other}
+      >
+        {value === index && (
+            <Box p={3}>
+              <Typography>{children}</Typography>
+            </Box>
+        )}
+      </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+const useStylesTab = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper
+  },
+}));
 
 const now = moment().format('DD-MM-YYYY:hh:mm:ss');
 
@@ -31,6 +77,9 @@ const useStyles = makeStyles((theme) => ({
 export const PatientManager = (props: any) => {
   const { facilityId } = props;
   const classes = useStyles();
+  const classesTab = useStylesTab();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
   const dispatch: any = useDispatch();
   const initialData: any[] = [];
   const [data, setData] = useState(initialData);
@@ -42,7 +91,6 @@ export const PatientManager = (props: any) => {
   const [offset, setOffset] = useState(0);
   const [DownloadFile, setDownloadFile] = useState("");
   const limit = 14;
-
   const fetchData = useCallback(
     async (status: statusType) => {
       setIsLoading(true);
@@ -59,7 +107,13 @@ export const PatientManager = (props: any) => {
     },
     [diseaseStatus, dispatch, facilityId, offset]
   );
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
 
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
   const handleDownload = async () => {
     const res = await dispatch(downloadPatients());
     setDownloadFile(res.data);
@@ -164,7 +218,9 @@ export const PatientManager = (props: any) => {
                     <span className="font-semibold leading-relaxed">
                       Contact with confirmed carrier
                     </span>
-                    <WarningRoundedIcon className="text-red-500"></WarningRoundedIcon>
+                    <WarningRoundedIcon className="text-red-500">
+
+                    </WarningRoundedIcon>
                   </div>
                 )}
                 {patient.contact_with_suspected_carrier &&
@@ -173,7 +229,9 @@ export const PatientManager = (props: any) => {
                       <span className="font-semibold leading-relaxed">
                         Contact with suspected carrier
                       </span>
-                      <WarningRoundedIcon className="text-yellow-500"></WarningRoundedIcon>
+                      <WarningRoundedIcon className="text-yellow-500">
+
+                      </WarningRoundedIcon>
                     </div>
                   )}
                 <div>
@@ -291,9 +349,39 @@ export const PatientManager = (props: any) => {
           </div>
         </div>
       </div>
-      <div className="px-3 md:px-8">
-        <div className="flex flex-wrap md:-mx-4">{managePatients}</div>
+      <div className={classesTab.root}>
+        <AppBar position="static" color="default">
+          <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              variant="fullWidth"
+              aria-label="full width tabs example"
+          >
+            <Tab label="Live" {...a11yProps(1)}/>
+            <Tab label="Discharged" {...a11yProps(1)} />
+          </Tabs>
+        </AppBar>
+        <SwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+        >
+
+          <TabPanel value={value} index={0} dir={theme.direction}>
+            <div className="px-3 md:px-8">
+              <div className="flex flex-wrap md:-mx-4">{managePatients}</div>
+            </div>
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            <div className="px-3 md:px-8">
+              <div className="flex flex-wrap md:-mx-4">{managePatients}</div>
+            </div>
+          </TabPanel>
+        </SwipeableViews>
       </div>
+
     </div>
   );
 };
