7394c3d8082f371613a8030bcf2723a40c6cc318
syamkumar <hey@syam.dev>
2020-08-15 19:34:43 +0530

    Added SFLTC

--- src/Common/constants.tsx
+++ src/Common/constants.tsx
@@ -72,6 +72,7 @@ export const FACILITY_TYPES: Array<OptionsType> = [
   { id: 950, text: 'Corona Testing Labs' },
   { id: 1000, text: "Corona Care Centre" },
   { id: 1100, text: "First Line Treatment Centre" },
+  { id: 1200, text: "Second Line Treatment Center"},
   { id: 1300, text: "Shifting Centre" }
 ];
 
