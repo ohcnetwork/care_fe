cf37cdf14a535acc5e116325f55947ee6bfa2187
Bodhish Thomas <bodhish@gmail.com>
2020-08-22 17:09:01 +0530

    Redesign facility home

--- src/Components/Facility/BedTypeCard.tsx
+++ src/Components/Facility/BedTypeCard.tsx
@@ -11,7 +11,7 @@ const BedTypeCard = (props: BedTypeProps) => {
   const bed = BED_TYPES.find(i => i.id === props.room_type);
   const roomType = bed ? bed.text : "Unknown";
   return (
-    <div className="px-2 py-2 md:w-1/5 w-full">
+    <div className="px-2 py-2 lg:w-1/5 w-full">
       <div className="flex flex-col items-center shadow rounded-lg p-4 h-full justify-between">
         <div className="font-semibold text-sm mt-2">{roomType}</div>
         <div className="text-bold text-xl md:text-3xl mt-2">
@@ -21,7 +21,7 @@ const BedTypeCard = (props: BedTypeProps) => {
           Currently Occupied / Total Capacity
         </div>
         <div
-          className="py-2 mt-2 px-6 bg-white rounded-md border border-grey-500 inline-flex items-center justify-center whitespace-no-wrap text-sm font-semibold rounded cursor-pointer hover:bg-gray-300"
+          className="btn btn-default"
           onClick={() =>
             navigate(`/facility/${props.facilityId}/bed/${props.room_type}`)
           }
--- src/Components/Facility/DoctorsCountCard.tsx
+++ src/Components/Facility/DoctorsCountCard.tsx
@@ -11,12 +11,12 @@ const DoctorsCountCard = (props: DoctorsCountProps) => {
   const specialization = DOCTOR_SPECIALIZATION.find(i => i.id === props.area);
   const area = specialization ? specialization.text : "Unknown";
   return (
-    <div className="px-2 py-2 md:w-1/5 w-full">
+    <div className="px-2 py-2 lg:w-1/5 w-full">
       <div className="flex flex-col items-center shadow rounded-lg p-4 h-full justify-between">
         <div className="text-bold text-3xl mt-2">{props.count}</div>
         <div className="font-semibold text-md mt-2">{area} Doctors</div>
         <div
-          className="py-2 mt-2 px-6 bg-white rounded-md border border-grey-500 inline-flex items-center justify-center whitespace-no-wrap text-sm font-semibold rounded cursor-pointer hover:bg-gray-300"
+          className="btn btn-default"
           onClick={() =>
             navigate(`/facility/${props.facilityId}/doctor/${props.area}`)
           }
