ffbb403f8c5b493098077b005a1231c34ba80af0
Jishnu P Sreekumar <psjishnu13@gmail.com>
2020-09-22 17:47:53 +0530

    fixed prescriptionbuilder.gen.tsx

--- src/Components/Common/PrescriptionBuilder.gen.tsx
+++ src/Components/Common/PrescriptionBuilder.gen.tsx
@@ -7,7 +7,7 @@ import * as React from 'react';
 // tslint:disable-next-line:no-var-requires
 const PrescriptionBuilderBS = require('./PrescriptionBuilder.bs');
 
-import {t as Prescription__Prescription_t} from './Prescription__Prescription.gen';
+import {t as Prescription__Prescription_t} from '@coronasafe/prescription-builder/src/Types/Prescription__Prescription.gen';
 
 // tslint:disable-next-line:interface-over-type-literal
 export type Props<T1> = { readonly prescriptions: Prescription__Prescription_t[]; readonly setPrescriptions: (_1:((_1:T1) => Prescription__Prescription_t[])) => void };
