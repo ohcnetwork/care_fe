facde8cf9bbeadcefa1232d5a5d7efc2aaaf3305
AJIL-PAUL <ajilpaul.268@gmail.com>
2020-11-07 00:45:36 +0530

    added filter in external_results

--- src/Components/ExternalResult/ResultList.tsx
+++ src/Components/ExternalResult/ResultList.tsx
@@ -1,29 +1,36 @@
-import loadable from '@loadable/component';
+import loadable from "@loadable/component";
 import Grid from "@material-ui/core/Grid";
-import { navigate, useQueryParams } from 'raviger';
-import { parsePhoneNumberFromString } from 'libphonenumber-js';
-import moment from 'moment';
+import { navigate, useQueryParams } from "raviger";
+import { parsePhoneNumberFromString } from "libphonenumber-js";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { externalResultList } from "../../Redux/actions";
-import { PhoneNumberField } from '../Common/HelperInputFields';
+import { PhoneNumberField } from "../Common/HelperInputFields";
 import Pagination from "../Common/Pagination";
 import { InputSearchBox } from "../Common/SearchBox";
+import { make as SlideOver } from "../Common/SlideOver.gen";
+import ListFilter from "./ListFilter";
+
 const Loading = loadable(() => import("../Common/Loading"));
 const PageTitle = loadable(() => import("../Common/PageTitle"));
 
-function Badge(props: { color: string, icon: string, text: string }) {
+function Badge(props: { color: string; icon: string; text: string }) {
   return (
-    <span className="m-1 inline-flex items-center px-3 py-1 rounded-full text-xs font-medium leading-4 bg-gray-100 text-gray-700" title={props.text}>
-      <i className={"mr-2 text-md text-" + props.color + "-500 fas fa-" + props.icon}></i>
+    <span
+      className="m-1 inline-flex items-center px-3 py-1 rounded-full text-xs font-medium leading-4 bg-gray-100 text-gray-700"
+      title={props.text}
+    >
+      <i
+        className={
+          "mr-2 text-md text-" + props.color + "-500 fas fa-" + props.icon
+        }
+      ></i>
       {props.text}
     </span>
-  )
+  );
 }
 
-
 const RESULT_LIMIT = 30;
-
 
 export default function ResultList() {
   const dispatch: any = useDispatch();
@@ -32,6 +39,7 @@ export default function ResultList() {
   const [totalCount, setTotalCount] = useState(0);
   const [DownloadFile, setDownloadFile] = useState("");
   const [qParams, setQueryParams] = useQueryParams();
+  const [showFilters, setShowFilters] = useState(false);
 
   let manageResults: any = null;
 
@@ -40,26 +48,44 @@ export default function ResultList() {
     const params = {
       page: qParams.page || 1,
       name: qParams.name || undefined,
-      phone_number: qParams.phone_number ? parsePhoneNumberFromString(qParams.phone_number)?.format('E.164') : undefined,
-      offset: (qParams.page ? qParams.page - 1 : 0) * RESULT_LIMIT
+      phone_number: qParams.phone_number
+        ? parsePhoneNumberFromString(qParams.phone_number)?.format("E.164")
+        : undefined,
+      districts: qParams.districts || undefined,
+      wards: qParams.wards || undefined,
+      offset: (qParams.page ? qParams.page - 1 : 0) * RESULT_LIMIT,
     };
 
-    dispatch(externalResultList(params, 'externalResultList'))
+    dispatch(externalResultList(params, "externalResultList"))
       .then((res: any) => {
         if (res && res.data) {
           setData(res.data.results);
           setTotalCount(res.data.count);
         }
         setIsLoading(false);
-      }).catch(() => {
-        setIsLoading(false);
       })
-  }, [dispatch, qParams.name, qParams.page, qParams.phone_number]);
+      .catch(() => {
+        setIsLoading(false);
+      });
+  }, [
+    dispatch,
+    qParams.name,
+    qParams.page,
+    qParams.phone_number,
+    qParams.districts,
+    qParams.wards,
+  ]);
 
-  const updateQuery = (params: any) => {
-    const nParams = Object.assign({}, qParams, params);
+  const updateQuery = (filter: any) => {
+    const nParams = Object.keys(filter).reduce(
+      (a, k) =>
+        filter[k] && filter[k] !== "--"
+          ? Object.assign(a, { [k]: filter[k] })
+          : a,
+      {}
+    );
     setQueryParams(nParams, true);
-  }
+  };
 
   const handlePagination = (page: number, limit: number) => {
     updateQuery({ page, limit });
@@ -67,26 +93,38 @@ export default function ResultList() {
 
   const searchByName = (value: string) => {
     updateQuery({ name: value, page: 1 });
-  }
+  };
 
   const searchByPhone = (value: string) => {
     updateQuery({ phone_number: value, page: 1 });
-  }
+  };
 
   const handleFilter = (value: string) => {
     updateQuery({ disease_status: value, page: 1 });
-  }
+  };
+
+  const applyFilter = (data: any) => {
+    const filter = { ...qParams, ...data };
+    updateQuery(filter);
+    setShowFilters(false);
+  };
 
   let resultList: any[] = [];
   if (data && data.length) {
     resultList = data.map((result: any, idx: number) => {
       const resultUrl = `/external_results/${result.id}`;
       return (
-        <tr key={`usr_${result.id}`} onClick={() => navigate(resultUrl)} className="bg-white">
+        <tr
+          key={`usr_${result.id}`}
+          onClick={() => navigate(resultUrl)}
+          className="bg-white"
+        >
           <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-cool-gray-900">
             <div className="flex">
-              <a href="#" className="group inline-flex space-x-2 text-sm leading-5">
-
+              <a
+                href="#"
+                className="group inline-flex space-x-2 text-sm leading-5"
+              >
                 <p className="text-cool-gray-500 group-hover:text-cool-gray-900 transition ease-in-out duration-150">
                   {result.name} - {result.age} {result.age_in}
                 </p>
@@ -94,7 +132,9 @@ export default function ResultList() {
             </div>
           </td>
           <td className="px-6 py-4 text-left whitespace-no-wrap text-sm leading-5 text-cool-gray-500">
-            <span className="text-cool-gray-900 font-medium">{result.test_type}</span>
+            <span className="text-cool-gray-900 font-medium">
+              {result.test_type}
+            </span>
           </td>
           <td className="px-6 py-4 text-left whitespace-no-wrap text-sm leading-5 text-cool-gray-500">
             <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-blue-100 text-blue-800 capitalize">
@@ -104,7 +144,7 @@ export default function ResultList() {
           <td className="px-6 py-4 text-left whitespace-no-wrap text-sm leading-5 text-cool-gray-500">
             {result.result_date || "-"}
           </td>
-        </tr >
+        </tr>
       );
     });
   }
@@ -115,7 +155,8 @@ export default function ResultList() {
         <td colSpan={4}>
           <Loading />
         </td>
-      </tr>);
+      </tr>
+    );
   } else if (data && data.length) {
     manageResults = (
       <>
@@ -144,10 +185,7 @@ export default function ResultList() {
 
   return (
     <div className="px-6">
-      <PageTitle
-        title="Results"
-        hideBack={true}
-        className="mt-4" />
+      <PageTitle title="Results" hideBack={true} className="mt-4" />
       <div className="mt-5 md:grid grid-cols-1 gap-5 sm:grid-cols-3 my-4 px-2 md:px-0 relative">
         <div className="bg-white overflow-hidden shadow rounded-lg">
           <div className="px-4 py-5 sm:p-6">
@@ -163,20 +201,16 @@ export default function ResultList() {
         </div>
         <div>
           <div>
-            <div className="text-sm font-semibold mb-2">
-              Search by Name
-          </div>
+            <div className="text-sm font-semibold mb-2">Search by Name</div>
             <InputSearchBox
               search={searchByName}
               value={qParams.name}
-              placeholder='Search by Patient Name'
-              errors=''
+              placeholder="Search by Patient Name"
+              errors=""
             />
           </div>
           <div>
-            <div className="text-sm font-semibold mt-2">
-              Search by number
-          </div>
+            <div className="text-sm font-semibold mt-2">Search by number</div>
             <PhoneNumberField
               value={qParams.phone_number}
               onChange={searchByPhone}
@@ -186,12 +220,21 @@ export default function ResultList() {
           </div>
         </div>
         <div className="flex flex-col justify-between">
-          <div>
-            {/* <div className="text-sm font-semibold">Filter by Status</div> */}
-            <div className="btn btn-primary" onClick={_ => navigate('external_results/upload')}>
-              Upload List
-            </div>
-            {/* <ResultFilter filter={handleFilter} value={qParams.disease_status} /> */}
+          <div
+            className="btn mt-8 ml-auto gap-2 btn-primary"
+            onClick={(_) => navigate("external_results/upload")}
+          >
+            Upload List
+          </div>
+
+          <div className="flex ml-auto  gap-2">
+            <button
+              className="flex leading-none border-2 border-gray-200 bg-white rounded-full items-center transition-colors duration-300 ease-in focus:outline-none hover:text-green-600 focus:text-green-600 focus:border-gray-400 hover:border-gray-400 rounded-r-full px-4 py-2 text-sm"
+              onClick={(_) => setShowFilters((show) => !show)}
+            >
+              <i className="fa fa-filter mr-1" aria-hidden="true"></i>
+              <span>Filters</span>
+            </button>
           </div>
         </div>
       </div>
@@ -218,6 +261,15 @@ export default function ResultList() {
           </tbody>
         </table>
       </div>
+      <SlideOver show={showFilters} setShow={setShowFilters}>
+        <div className="bg-white min-h-screen p-4">
+          <ListFilter
+            filter={qParams}
+            onChange={applyFilter}
+            closeFilter={() => setShowFilters(false)}
+          />
+        </div>
+      </SlideOver>
     </div>
   );
-};
+}
