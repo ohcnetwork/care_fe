c7658fcc1d098025f62bd473e3f1190bab09d68a
bodhisha <bodhisha@gmail.com>
2021-04-19 04:55:37 +0530

    Add View Investigation and api integeration

--- src/Components/Facility/Investigations/ViewInvestigations.tsx
+++ src/Components/Facility/Investigations/ViewInvestigations.tsx
@@ -3,21 +3,30 @@ import { useDispatch } from "react-redux";
 import { statusType, useAbortableEffect } from "../../../Common/utils";
 import { getInvestigationSessions } from "../../../Redux/actions";
 import PageTitle from "../../Common/PageTitle";
+import moment from "moment";
+import { navigate } from "raviger";
+import loadable from "@loadable/component";
+const Loading = loadable(() => import("../../Common/Loading"));
 
 export default function ViewInvestigations(props: any) {
   const [isLoading, setIsLoading] = useState(false);
-  const { consultationId }: any = props;
+  const { facilityId, patientId, consultationId }: any = props;
   const dispatchAction: any = useDispatch();
+  const [investigationData, setInvestigationData] = useState([]);
+  interface InvestigationType {
+    session_external_id: string;
+    session_created_date: string;
+  }
 
   const fetchData = useCallback(
     async (status: statusType) => {
       setIsLoading(true);
       const res = await dispatchAction(
-        getInvestigationSessions(consultationId)
+        getInvestigationSessions({}, consultationId)
       );
       if (!status.aborted) {
         if (res && res.data) {
-          console.log(res.data);
+          setInvestigationData(res.data);
         }
         setIsLoading(false);
       }
@@ -33,12 +42,35 @@ export default function ViewInvestigations(props: any) {
   );
 
   return (
-    <div>
+    <div className="max-w-7xl mx-auto px-4">
       <PageTitle
         title="View Investigations"
         hideBack={false}
         className="mx-3 md:mx-8"
       />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="mt-4 space-y-2 ">
+          {investigationData.map((data: InvestigationType) => {
+            return (
+              <div className="flex justify-between items-center bg-white hover:bg-gray-200 cursor-pointer p-4 border rounded-lg shadow">
+                <div>{moment(data.session_created_date).format("lll")}</div>
+                <button
+                  onClick={() =>
+                    navigate(
+                      `/facility/${facilityId}/patient/${patientId}/consultation/${consultationId}/investigation/${data.session_external_id}`
+                    )
+                  }
+                  className="btn btn-default"
+                >
+                  View
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
