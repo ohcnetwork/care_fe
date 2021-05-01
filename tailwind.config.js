f645798549fe40e655ccf718b7fe45b920981c57
Divya garg <divyagarg2601@gmail.com>
2021-05-01 13:14:39 +0530

    Add emerald colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   important: true,
   theme: {
     extend: {
-       colors: {
+      colors: {
         gray: {
           100: "#FBFAFC",
           200: "#F7F5FA",
@@ -14,8 +14,24 @@ module.exports = {
           800: "#6A5F7A",
           900: "#453C52",
         },
-       },
+        emerald: {
+          50: "#ECFDF5",
+          100: "#D1FAE5",
+          200: "#A7F3D0",
+          300: "#6EE7B7",
+          400: "#34D399",
+          500: "#10B981",
+          600: "059669",
+          700: "#047857",
+          800: "#065F46",
+          900: "#064E3B"
+        }
       },
+      fontFamily: {
+        'poppins': ['Poppins'],
+        'roboto': ['Roboto']
+      }
+    },
   },
   variants: {},
   plugins: [require("@tailwindcss/ui")],
