07c5d0e2848fb132dff1cd8c1dbf28e43373b7d6
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-05-25 00:26:58 +0530

    Add Translation for Facilities

--- src/Locale/en/index.js
+++ src/Locale/en/index.js
@@ -1,9 +1,11 @@
 import Auth from "./Auth.json";
 import Common from "./Common.json";
 import Entities from "./Entities.json";
+import Facility from "./Facility.json";
 
 export default {
     ...Auth,
     ...Common,
-    ...Entities
+    ...Entities,
+    ...Facility
 }
