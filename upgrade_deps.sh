98ffebd1b362978b1b0b0df3462862c3e76e5450
ProCode2 <bpro249@gmail.com>
2021-05-29 17:30:45 +0530

    upgraded @pnotify/core to the latest version

--- upgrade_deps.sh
+++ upgrade_deps.sh
@@ -1,7 +1,21 @@
-for dep in "$@"
-do
-    echo "Upgrading to ${dep}@latest"
-    yarn add "${dep}@latest";
-    git add .
-    git commit -m "upgraded ${dep} to the latest version"
-done
+if [ $1 == "dep" ]
+then
+    for dep in "${@:2}"
+    do
+        echo "Upgrading to ${dep}@latest"
+        yarn add "${dep}@latest";
+        git add .
+        git commit -m "upgraded ${dep} to the latest version"
+    done
+elif [ $1 == "dev" ]
+then
+    for dep in "${@:2}"
+    do
+        echo "Upgrading to ${dep}@latest"
+        yarn add -D "${dep}@latest";
+        git add .
+        git commit -m "upgraded ${dep} to the latest version"
+    done
+else
+echo "nothing"
+fi
