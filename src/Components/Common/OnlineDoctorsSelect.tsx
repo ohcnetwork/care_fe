dc6109b64b932e077bc0f66a24572f210ed46d53
Khavin Shankar <khavinshankar@gmail.com>
2021-05-30 10:58:59 +0530

    removed console.log in OnlineDoctorsSelect

--- src/Components/Common/OnlineDoctorsSelect.tsx
+++ src/Components/Common/OnlineDoctorsSelect.tsx
@@ -31,7 +31,6 @@ export const OnlineDoctorsSelect = (props: any) => {
   }, []);
 
   useEffect(() => {
-    console.log("useEffect running")
     const searchTerm = state.searchTerm.toLowerCase();
     const filteredDoctors = state.users.filter(
       (doctor) =>
