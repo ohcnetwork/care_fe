48ae62fb3bfb70f9000cc9533e491ba03766f0e5
Khavin Shankar <khavinshankar@gmail.com>
2021-05-30 10:55:22 +0530

    added search functionality to OnlineDoctorsSelect

--- src/Components/Common/OnlineDoctorsSelect.tsx
+++ src/Components/Common/OnlineDoctorsSelect.tsx
@@ -1,5 +1,5 @@
 import loadable from '@loadable/component';
-import React, { useCallback, useReducer, useState } from "react";
+import React, { useCallback, useReducer, useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { statusType, useAbortableEffect } from "../../Common/utils";
 import moment from "moment";
@@ -8,9 +8,10 @@ import { getOnlineDoctors } from "../../Redux/actions";
 export const OnlineDoctorsSelect = (props: any) => {
   const dispatchAction: any = useDispatch();
   const { userId, onSelect } = props;
-  const initalState = { loading: false, users: new Array<any>() }
+  const initalState = { loading: false, users: new Array<any>(), searchTerm: "", filteredUsers: new Array<any>() }
   const [state, setState] = useState(initalState)
   const [isExpanded, setIsExpanded] = useState(false);
+  const searchFieldRef = useRef<any>(null);
 
   const fetchUsers = useCallback(
     async (status: statusType) => {
@@ -18,7 +19,7 @@ export const OnlineDoctorsSelect = (props: any) => {
       const res = await dispatchAction(getOnlineDoctors());
       if (!status.aborted) {
         if (res && res.data) {
-          setState({ loading: false, users: res.data.results });
+          setState({ ...state, loading: false, users: res.data.results });
         }
       }
     },
@@ -29,6 +30,29 @@ export const OnlineDoctorsSelect = (props: any) => {
     fetchUsers(status);
   }, []);
 
+  useEffect(() => {
+    console.log("useEffect running")
+    const searchTerm = state.searchTerm.toLowerCase();
+    const filteredDoctors = state.users.filter(
+      (doctor) =>
+        doctor.first_name.toLowerCase().includes(searchTerm) ||
+        doctor.last_name.toLowerCase().includes(searchTerm) ||
+        doctor.username.toLowerCase().includes(searchTerm)
+    );
+    setState({ ...state, filteredUsers: filteredDoctors });
+  }, [state.searchTerm, state.users]);
+
+  useEffect(() => {
+    if(isExpanded) {
+      searchFieldRef.current.focus();
+    }
+  }, [isExpanded]);
+
+  const handleSearchTermChange = (e: any) => {
+    const { value } = e.target;
+    setState({ ...state, searchTerm: value });
+  }
+
   const selectedDoctor = state.users.find((item: any) => item.id == userId);
   return (
     <div className="pb-2">
@@ -38,8 +62,17 @@ export const OnlineDoctorsSelect = (props: any) => {
         </label>
         <div className="relative">
           <span className="inline-block w-full rounded-md shadow-sm">
-            <button onClick={_ => setIsExpanded(!isExpanded)} type="button" aria-haspopup="listbox" aria-expanded="true" aria-labelledby="listbox-label" className="cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-              <div className="flex items-center justify-between">
+            <button onClick={_ => setIsExpanded(!isExpanded)} type="button" aria-haspopup="listbox" aria-expanded="true" aria-labelledby="listbox-label" className={`cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5`}>
+              <input
+                ref={searchFieldRef}
+                name="searchTerm"
+                type="text"
+                placeholder="Search by name or username"
+                className={`py-2 pl-3 w-full outline-none ${!isExpanded ? 'hidden' : ''}`}
+                value={state.searchTerm}
+                onChange={handleSearchTermChange}
+              />
+              <div className={`flex items-center justify-between ${isExpanded ? 'hidden' : ''}`}>
                 <div className="space-x-3 flex items-center">
                   <span aria-label="Online"
                     className={"flex-shrink-0 inline-block h-2 w-2 rounded-full " +
@@ -63,8 +96,8 @@ export const OnlineDoctorsSelect = (props: any) => {
           {isExpanded &&
             <div role="listbox" aria-labelledby="listbox-label" aria-activedescendant="listbox-item-3" className="multiselect-dropdown__search-dropdown w-full absolute border border-gray-400 bg-white mt-1 rounded-lg shadow-lg px-4 py-2 z-50">
               {
-                state.users.map((user: any) => {
-                  return <button onClick={_ => { setIsExpanded(false); onSelect(user.id) }} id="listbox-item-0" role="option" className="flex text-xs py-1 items-center w-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200">
+                state.filteredUsers.map((user: any) => {
+                  return <button onClick={_ => { setIsExpanded(false); onSelect(user.id); setState({...state, searchTerm: ""}) }} id="listbox-item-0" role="option" className="flex text-xs py-1 items-center w-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200">
                     <div className="flex items-center space-x-3">
                       <span aria-label="Online" className={"flex-shrink-0 inline-block h-2 w-2 rounded-full " + (moment().subtract(5, 'minutes').isBefore(user.last_login) ? "bg-green-400" : "bg-gray-300")}></span>
                       <span className="font-normal block truncate">
