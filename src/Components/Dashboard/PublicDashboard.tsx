3b1ebd0f64472ae33ed77719990761b3ce9e3712
1geek0 <typhoscientic@gmail.com>
2021-05-31 14:40:33 +0530

    Changed colour for Maharashtra. Changed redirect url for dashboard

--- src/Components/Dashboard/PublicDashboard.tsx
+++ src/Components/Dashboard/PublicDashboard.tsx
@@ -5,10 +5,10 @@ export const PublicDashboard = () => {
   const { t } = useTranslation();
   return (
     <div className="max-w-lg">
-      <h1 className="text-3xl md:text-4xl xl:text-5xl tracking-tight font-bold text-white leading-tight">
-        {t("coronasafe_network")}
+      <h1 className="text-3xl md:text-4xl xl:text-5xl tracking-tight font-bold text-black leading-tight">
+        {t("mahakavach_title")}
       </h1>
-      <div className="text-base md:text-2xl lg:text-xl pt-6 max-w-xl text-white pl-1">
+      <div className="text-base md:text-2xl lg:text-2xl pt-6 max-w-xl text-black pl-1 hidden">
         {t("goal")}
       </div>
     </div>
