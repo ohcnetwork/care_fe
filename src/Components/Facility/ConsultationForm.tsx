1041d943a2dbbbea03d98abe5040306d138632b5
Bodhish Thomas <bodhish@gmail.com>
2021-06-01 19:14:34 +0530

    Update src/Components/Facility/ConsultationForm.tsx
    Co-authored-by: Karan Sanjeev <yo@alphaman.me>

--- src/Components/Facility/ConsultationForm.tsx
+++ src/Components/Facility/ConsultationForm.tsx
@@ -357,9 +357,9 @@ export const ConsultationForm = (props: any) => {
     form[name] = value;
     if (value === "A") {
       form.admitted = "true";
+    } else {
+      form.admitted = "false";
     }
-    else
-      form.admitted = "false"
     dispatch({ type: "set_form", form });
   };
 
