751c53edeb33f3fdd13a9e7dcbecfd6cfccf9719
Karan Sanjeev <yo@alphaman.me>
2021-06-03 11:26:05 +0530

    District admin - RO test

--- cypress/integration/authentication/roles.spec.ts
+++ cypress/integration/authentication/roles.spec.ts
@@ -24,4 +24,22 @@ describe('authentication', () => {
         cy.get('dd').should('contain', 'karadmin')
         cy.get('dd').should('contain', 'DistrictAdmin')
     })
+
+    it('Login as disctrict admin - read only', () => {
+        cy.visit('http://localhost:4000/')
+
+        // Login
+        cy.get('input[name="username"]').type('karadminro')
+        cy.get('input[name="password"]').type('passwordR0FL')
+        cy.get('button').contains('Login').click()
+
+        // Check URL
+        cy.url().should('include', '/facility')
+
+        // Assert user
+        cy.get('a').contains('Profile').click()
+        cy.url().should('include', '/user/profile')
+        cy.get('dd').should('contain', 'karadminro')
+        cy.get('dd').should('contain', 'DistrictReadOnlyAdmin')
+    })
 })
