ff947b58c3a3f5b62defbdf7121d0ae28c67d93e
Sandra <sandrakakkarayil01@gmail.com>
2021-06-04 12:44:52 +0530

    Added Delete Dialog

---
+++ src/Components/Users/UserDeleteDialog.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from "@material-ui/core";
+import { WithStyles, withStyles } from "@material-ui/styles";
+
+interface ConfirmDialogProps {
+  name: string;
+  handleCancel: () => void;
+  handleOk: () => void;
+}
+
+const styles = {
+  paper: {
+    "max-width": "650px",
+    "min-width": "400px",
+  },
+};
+
+const UserDeleteDialog = (
+  props: ConfirmDialogProps & WithStyles<typeof styles>
+) => {
+  const { name, handleCancel, handleOk, classes } = props;
+
+  const [disable, setDisable] = useState(false);
+
+  const handleSubmit = () => {
+    handleOk();
+    setDisable(true);
+  };
+  return (
+    <Dialog
+      open={true}
+      classes={{
+        paper: classes.paper,
+      }}
+      onClose={handleCancel}
+    >
+      <DialogContent>
+        <DialogContentText
+          id="alert-dialog-description"
+          className="flex text-gray-800 leading-relaxed"
+        >
+          <p>Are u sure you want to delete user</p>
+          <p className="mx-1 font-semibold capitalize">{name} ?</p>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCancel} color="primary">
+          Cancel
+        </Button>
+        <button
+          onClick={handleSubmit}
+          className="font-medium btn btn-danger"
+          disabled={disable}
+        >
+          Yes
+        </button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default withStyles(styles)(UserDeleteDialog);
