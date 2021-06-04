9e405fb9215489461db423209a26bb1e35d41864
developedBySJ <developedbysj@gmail.com>
2021-06-04 22:32:09 +0530

    Add load more button in Notifications

--- src/Components/Notifications/NotificationsList.tsx
+++ src/Components/Notifications/NotificationsList.tsx
@@ -6,6 +6,7 @@ import { getNotifications } from "../../Redux/actions";
 import Pagination from "../Common/Pagination";
 import { make as SlideOver } from "../Common/SlideOver.gen";
 import moment from "moment";
+import { Button, CircularProgress } from "@material-ui/core";
 
 const Loading = loadable(() => import("../Common/Loading"));
 const PageTitle = loadable(() => import("../Common/PageTitle"));
@@ -15,7 +16,7 @@ const now = moment().format("DD-MM-YYYY:hh:mm:ss");
 
 export default function ResultList() {
   const dispatch: any = useDispatch();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [offset, setOffset] = useState(0);
   const [totalCount, setTotalCount] = useState(0);
@@ -30,16 +31,17 @@ export default function ResultList() {
       dispatch(getNotifications({ offset }))
         .then((res: any) => {
           if (res && res.data) {
-            setData(res.data.results);
+            setData((prev) => [...prev, ...res.data.results]);
             setTotalCount(res.data.count);
           }
           setIsLoading(false);
         })
         .catch(() => {
           setIsLoading(false);
+          setOffset((prev) => prev - RESULT_LIMIT);
         });
     }
-  }, [dispatch, reload, showNotifications]);
+  }, [dispatch, reload, showNotifications, offset]);
 
   // const handlePagination = (page: number, limit: number) => {
   //   updateQuery({ page, limit });
@@ -75,7 +77,7 @@ export default function ResultList() {
           onClick={() =>
             navigate(resultUrl(result.event, result.caused_objects))
           }
-          className="relative py-5 px-4 lg:px-8 hover:bg-gray-200 focus:bg-gray-200 transition ease-in-out duration-150 "
+          className="relative py-5 px-4 lg:px-8 hover:bg-gray-200 focus:bg-gray-200 transition ease-in-out duration-150 cursor-pointer"
         >
           <div className="text-lg font-bold">{result.event}</div>
           <div className="text-sm">{result.message}</div>
@@ -90,27 +92,32 @@ export default function ResultList() {
       );
     });
   }
-
-  if (isLoading || !data) {
-    manageResults = (
-      <tr className="bg-white">
-        <td colSpan={4}>
-          <Loading />
-        </td>
-      </tr>
-    );
+  if (!offset && isLoading) {
+    <div className="flex items-center justify-center">
+      <CircularProgress color="primary" />
+    </div>;
   } else if (data && data.length) {
     manageResults = (
       <>
         {resultList}
-        {totalCount > RESULT_LIMIT && (
-          <div className="mt-4 flex w-full justify-center">
-            {/* <Pagination
-              cPage={qParams.page}
-              defaultPerPage={RESULT_LIMIT}
-              data={{ totalCount }}
-              onChange={handlePagination}
-            /> */}
+        {isLoading && (
+          <div className="flex items-center justify-center">
+            <CircularProgress color="primary" />
+          </div>
+        )}
+        {totalCount > RESULT_LIMIT && offset < totalCount - RESULT_LIMIT && (
+          <div className="mt-4 flex w-full justify-center py-5 px-4 lg:px-8">
+            <Button
+              disabled={isLoading}
+              variant="contained"
+              color="primary"
+              fullWidth
+              onClick={() => {
+                setOffset((prev) => prev + RESULT_LIMIT);
+              }}
+            >
+              {isLoading ? "Loading..." : "Load More"}
+            </Button>
           </div>
         )}
       </>
@@ -143,7 +150,11 @@ export default function ResultList() {
             <div className="font-bold text-xl">Notifications</div>
             <div className="">
               <button
-                onClick={(_) => setReload(!reload)}
+                onClick={(_) => {
+                  setReload(!reload);
+                  setData([]);
+                  setOffset(0);
+                }}
                 className="inline-flex items-center font-semibold p-2 md:py-1 bg-white hover:bg-gray-300 border rounded text-xs flex-shrink-0"
               >
                 <i className="fa-fw fas fa-sync cursor-pointer mr-2" /> Reload
