dbcb2b247719c3b03d9bfaad4b1521a6fcf1373f
Peeyush Goyal <smartrohan1514@gmail.com>
2021-06-07 19:33:10 +0530

    Added Testing for Phone Field

--- cypress/integration/profile/edit.spec.ts
+++ cypress/integration/profile/edit.spec.ts
@@ -78,4 +78,64 @@ describe("Edit Profile Testing", () => {
       .first()
       .contains(`+91 ${whatsapp_num}`);
   });
+
+  // Phone Testing
+  it("Invalid Phone Number of " + username, () => {
+    // Opening editing form
+
+    const phone_num = "11111-11111";
+
+    cy.get("a").contains("Profile").click();
+    cy.url().should("include", "/user/profile");
+    cy.get("button").contains("Edit User Profile").click();
+
+    cy.get(".flag-dropdown").first().find(".arrow").click();
+    cy.get('li[data-flag-key="flag_no_84"]').click();
+
+    // Updating the Whatsapp Number field
+    cy.get("input[type='tel']")
+      .first()
+      .focus()
+      .type(`${backspace}${phone_num}`)
+      .trigger("change", { force: true })
+      .should("have.attr", "value", `+91 ${phone_num}`);
+
+    // Waiting for async call to complete
+    cy.wait(3000);
+    cy.get("form").get("button[type='submit']").click();
+    cy.get(".error-text").contains("Please enter valid phone number");
+  });
+
+  it("Valid Phone Number of " + username, () => {
+    // Opening editing form
+
+    const phone_num = "99999-99999";
+
+    cy.get("a").contains("Profile").click();
+    cy.url().should("include", "/user/profile");
+    cy.get("button").contains("Edit User Profile").click();
+
+    cy.get(".flag-dropdown").first().find(".arrow").click();
+    cy.get('li[data-flag-key="flag_no_84"]').click();
+
+    // Updating the Whatsapp Number field
+    cy.get("input[type='tel']")
+      .first()
+      .focus()
+      .type(`${backspace}${phone_num}`)
+      .trigger("change", { force: true })
+      .should("have.attr", "value", `+91 ${phone_num}`);
+
+    // Waiting for async call to complete
+    cy.wait(2000);
+    cy.get("form").get("button[type='submit']").click();
+
+    // Waiting for async call to complete
+    cy.wait(2000);
+    cy.get("dt")
+      .contains("Contact No")
+      .siblings()
+      .first()
+      .contains(`+91 ${phone_num}`);
+  });
 });
