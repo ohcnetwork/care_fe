664067ce581ebb7f673a2e918ac100e666a72869
Apurva <apurvanagar@gmail.com>
2021-06-08 22:19:36 +0530

    basic form complete

--- src/Components/DeathReport/DeathReport.tsx
+++ src/Components/DeathReport/DeathReport.tsx
@@ -4,16 +4,63 @@ import { getPatient } from "../../Redux/actions";
 import { PatientModel } from "../Patient/models";
 import { statusType, useAbortableEffect } from "../../Common/utils";
 import { GENDER_TYPES } from "../../Common/constants";
+import {
+  TextInputField,
+  MultilineInputField,
+} from "../Common/HelperInputFields";
+import { InputLabel } from "@material-ui/core";
 import moment from "moment";
 
 export default function PrintDeathReport(props: { id: string }) {
-  const [patientData, setPatientData] = useState<PatientModel>({});
+  const initialState = {
+    name: "",
+    age: "",
+    gender: "",
+    address: "",
+    phone_number: "",
+    is_declared_positive: "",
+    date_declared_positive: new Date(),
+    test_type: "",
+    date_of_test: new Date(),
+    date_of_result: new Date(),
+    srf_id: "",
+    hospital_tested_in: "",
+    hospital_died_in: "",
+    date_of_admission: new Date(),
+    date_of_death: new Date(),
+    comorbidities: "",
+    history_clinical_course: "",
+    brought_dead: "",
+    home_or_cfltc: "",
+    is_vaccinated: "",
+    kottayam_confirmation_sent: "",
+    kottayam_sample_date: "",
+    cause_of_death: "",
+  };
+
+  const [patientData, setPatientData] = useState(initialState);
   const [isLoading, setIsLoading] = useState(true);
+  const [isPrintMode, setIsPrintMode] = useState(false);
   const { id } = props;
   const dispatch: any = useDispatch();
-  const patientGender = GENDER_TYPES.find(
+
+  const getPatientGender = GENDER_TYPES.find(
     (i) => i.id === patientData.gender
   )?.text;
+
+  const patientAddress = `${patientData.address},\n${patientData.ward_object?.name},\n${patientData.local_body_object?.name},\n${patientData.district_object?.name},\n${patientData.state_object?.name}`;
+
+  let patientComorbidities: any;
+  if (
+    patientData &&
+    patientData.medical_history &&
+    patientData.medical_history.length
+  ) {
+    const medHis = patientData.medical_history;
+    patientComorbidities = medHis.map((item: any) => item.disease).join(", ");
+  } else {
+    patientComorbidities = "None";
+  }
 
   const fetchpatient = useCallback(
     async (status: statusType) => {
@@ -21,6 +68,11 @@ export default function PrintDeathReport(props: { id: string }) {
       const patientRes = await dispatch(getPatient({ id }));
       if (!status.aborted) {
         if (patientRes && patientRes.data) {
+          const patientGender = getPatientGender
+          const data = {
+            ...patientRes.data,
+            gender:
+          }
           setPatientData(patientRes.data);
         }
         setIsLoading(false);
@@ -36,19 +88,7 @@ export default function PrintDeathReport(props: { id: string }) {
     [dispatch, fetchpatient]
   );
 
-  let patientComorbidities: any;
-  if (
-    patientData &&
-    patientData.medical_history &&
-    patientData.medical_history.length
-  ) {
-    const medHis = patientData.medical_history;
-    patientComorbidities = medHis.map((item: any) => item.disease).join(", ");
-  } else {
-    patientComorbidities = "None";
-  }
-
-  return (
+  const previewData = () => (
     <div className="my-4">
       <div className="my-4 flex justify-end ">
         <button
@@ -210,6 +250,375 @@ export default function PrintDeathReport(props: { id: string }) {
           </div>
         </div>
       </div>
+    </div>
+  );
+
+  const handleChange = (e: any) => {
+    const key = e.target.name;
+    setPatientData({ ...patientData, [key]: e.target.value });
+  };
+
+  return (
+    <div>
+      {isPrintMode ? (
+        previewData()
+      ) : (
+        <div className="m-5 p-5 bg-gray-100 border rounded-xl shadow">
+          <div className="font-bold text-xl text-center mt-4 mb-5">
+            Covid-19 Death Reporting : Form 1
+          </div>
+          <div className="grid grid-rows-11">
+            <div className="grid grid-cols-1 mt-4 gap-10">
+              <div>
+                <InputLabel htmlFor="name">Name</InputLabel>
+                <TextInputField
+                  name="name"
+                  id="name"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={patientData.name}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-4 gap-10">
+              <div>
+                <InputLabel htmlFor="age">Age</InputLabel>
+                <TextInputField
+                  name="age"
+                  id="age"
+                  variant="outlined"
+                  margin="dense"
+                  type="number"
+                  value={patientData.age}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+              <div>
+                <InputLabel htmlFor="gender">Gender</InputLabel>
+                <TextInputField
+                  name="gender"
+                  id="gender"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={patientGender}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-1 mt-4">
+              <InputLabel htmlFor="address">Address</InputLabel>
+              <MultilineInputField
+                name="address"
+                id="address"
+                variant="outlined"
+                margin="dense"
+                type="text"
+                value={patientAddress}
+                onChange={(e) => handleChange(e)}
+                errors=""
+              />
+            </div>
+            <div className="grid grid-cols-2 mt-4 gap-10">
+              <div>
+                <InputLabel htmlFor="phone_number">Phone Number</InputLabel>
+                <TextInputField
+                  name="phone_number"
+                  id="phone_number"
+                  variant="outlined"
+                  margin="dense"
+                  type="tel"
+                  value={patientData.phone_number}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+              <div>
+                <InputLabel htmlFor="is_declared_positive">
+                  Whether declared positive
+                </InputLabel>
+                <TextInputField
+                  name="is_declared_positive"
+                  id="is_declared_positive"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={patientData.is_declared_positive ? "Yes" : "No"}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-4 gap-10">
+              <div>
+                <InputLabel htmlFor="date_declared_positive">
+                  Date of declaring positive
+                </InputLabel>
+                <TextInputField
+                  name="date_declared_positive"
+                  id="date_declared_positive"
+                  variant="outlined"
+                  margin="dense"
+                  type="date"
+                  value={moment(patientData.date_declared_positive).format(
+                    "YYYY-MM-DD"
+                  )}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+              <div>
+                <InputLabel htmlFor="test_type">Type of test done</InputLabel>
+                <TextInputField
+                  name="test_type"
+                  id="test_type"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={patientData.test_type}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-4 gap-10">
+              <div>
+                <InputLabel htmlFor="date_of_test">
+                  Date of sample collection for Covid testing
+                </InputLabel>
+                <TextInputField
+                  name="date_of_test"
+                  id="date_of_test"
+                  variant="outlined"
+                  margin="dense"
+                  type="date"
+                  value={moment(patientData.date_of_test).format("YYYY-MM-DD")}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+              <div>
+                <InputLabel htmlFor="date_of_result">
+                  Date of confirmation as Covid with SRF ID
+                </InputLabel>
+                <TextInputField
+                  name="date_of_result"
+                  id="date_of_result"
+                  variant="outlined"
+                  margin="dense"
+                  type="date"
+                  value={moment(patientData.date_of_result).format(
+                    "YYYY-MM-DD"
+                  )}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-4 gap-10">
+              <div>
+                <InputLabel htmlFor="hospital_tested_in">
+                  Hospital in which patient tested for SARS COV 2
+                </InputLabel>
+                <TextInputField
+                  name="hospital_tested_in"
+                  id="hospital_tested_in"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={""}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+              <div>
+                <InputLabel htmlFor="hospital_died_in">
+                  Name of the hospital in which the patient died
+                </InputLabel>
+                <TextInputField
+                  name="hospital_died_in"
+                  id="hospital_died_in"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={patientData.hospital_died_in}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-4 gap-10">
+              <div>
+                <InputLabel htmlFor="date_of_admission">
+                  Date of admission
+                </InputLabel>
+                <TextInputField
+                  name="date_of_admission"
+                  id="date_of_admission"
+                  variant="outlined"
+                  margin="dense"
+                  type="date"
+                  value={""}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+              <div>
+                <InputLabel htmlFor="date_of_death">Date of death</InputLabel>
+                <TextInputField
+                  name="date_of_death"
+                  id="date_of_death"
+                  variant="outlined"
+                  margin="dense"
+                  type="date"
+                  value={""}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-4 gap-10">
+              <div>
+                <InputLabel htmlFor="comorbidities">
+                  Mention the co-morbidities if present
+                </InputLabel>
+                <TextInputField
+                  name="comorbidities"
+                  id="comorbidities"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={patientComorbidities}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+              <div>
+                <InputLabel htmlFor="history_clinical_course">
+                  History and clinical course in the hospital
+                </InputLabel>
+                <TextInputField
+                  name="history_clinical_course"
+                  id="history_clinical_course"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={""}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-4 gap-10">
+              <div>
+                <InputLabel htmlFor="brought_dead">
+                  Whether brought dead
+                </InputLabel>
+                <TextInputField
+                  name="brought_dead"
+                  id="brought_dead"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={""}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+              <div>
+                <InputLabel htmlFor="home_or_cfltc">
+                  If yes was the deceased brought from home/CFLTC
+                </InputLabel>
+                <TextInputField
+                  name="home_or_cfltc"
+                  id="home_or_cfltc"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={""}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-4 gap-10">
+              <div>
+                <InputLabel htmlFor="is_vaccinated">
+                  Whether vaccinated
+                </InputLabel>
+                <TextInputField
+                  name="is_vaccinated"
+                  id="is_vaccinated"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={patientData.is_vaccinated ? "Yes" : "No"}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+              <div>
+                <InputLabel htmlFor="kottayam_confirmation_sent">
+                  Whether NIV/IUBCR Kottayam confirmation sent
+                </InputLabel>
+                <TextInputField
+                  name="kottayam_confirmation_sent"
+                  id="kottayam_confirmation_sent"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={""}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-4 gap-10">
+              <div>
+                <InputLabel htmlFor="kottayam_sample_date">
+                  Sample sent to NIV/IUCBR Kottayam on
+                </InputLabel>
+                <TextInputField
+                  name="kottayam_sample_date"
+                  id="kottayam_sample_date"
+                  variant="outlined"
+                  margin="dense"
+                  type="date"
+                  value={""}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+              <div>
+                <InputLabel htmlFor="cause_of_death">Cause of death</InputLabel>
+                <TextInputField
+                  name="cause_of_death"
+                  id="cause_of_death"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  value={""}
+                  onChange={(e) => handleChange(e)}
+                  errors=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mt-6 w-1/2 md:w-1/4">
+            <button
+              onClick={(_) => setIsPrintMode(true)}
+              className="bg-white btn btn-primary"
+            >
+              Preview
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
