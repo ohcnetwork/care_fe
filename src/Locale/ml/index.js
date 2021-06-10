fdccea4230b8496b534e8766deceb4ee55a9b501
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-06-10 20:37:12 +0530

    Populate JSONs to other languages

--- src/Locale/ml/index.js
+++ src/Locale/ml/index.js
@@ -2,10 +2,12 @@ import Auth from "./Auth.json";
 import Common from "./Common.json";
 import Entities from "./Entities.json";
 import Facility from "./Facility.json";
+import Patient from "./Patient.json";
 
 export default {
     ...Auth,
     ...Common,
     ...Entities,
-    ...Facility
+    ...Facility,
+    ...Patient
 }
