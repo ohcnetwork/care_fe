01d120dc697fa757de2394c48c0364ffb9d2f6e7
developedBySJ <developedbysj@gmail.com>
2021-06-23 19:52:58 +0530

    fix #1461

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -468,6 +468,7 @@ const AppRouter = (props: any) => {
                         onClick={() => {
                           localStorage.removeItem("care_access_token");
                           localStorage.removeItem("care_refresh_token");
+                          localStorage.removeItem("shift-filters");
                           navigate("/login");
                           window.location.reload();
                         }}
