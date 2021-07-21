cfea370b707913bc5bbcbbf340b1dc8fc6eb75d1
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-07-22 00:40:42 +0530

    FiO2 plot added

--- src/Components/Facility/Consultations/VentilatorPlot.tsx
+++ src/Components/Facility/Consultations/VentilatorPlot.tsx
@@ -1,9 +1,28 @@
 import moment from "moment";
+import { type } from "os";
 import { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
 import { statusType, useAbortableEffect } from "../../../Common/utils";
 import { dailyRoundsAnalyse } from "../../../Redux/actions";
 import { LinePlot } from "./components/LinePlot";
+
+interface ModalityType {
+  id: number;
+  type: string;
+  normal_rate_low: number;
+  normal_rate_high: number;
+}
+
+const modality: Array<ModalityType> = [
+  { id: 0, type: "UNKNOWN", normal_rate_low: 1, normal_rate_high: 4 },
+  { id: 5, type: "NASAL_PRONGS", normal_rate_low: 5, normal_rate_high: 10 },
+  {
+    id: 10,
+    type: "SIMPLE_FACE_MASK",
+    normal_rate_low: 11,
+    normal_rate_high: 15,
+  },
+];
 
 export const VentilatorPlot = (props: any) => {
   const { facilityId, patientId, consultationId } = props;
@@ -26,6 +45,7 @@ export const VentilatorPlot = (props: any) => {
               "ventilator_pressure_support",
               "ventilator_tidal_volume",
               "ventilator_peep",
+              "ventilator_fi02",
             ],
           },
           { consultationId }
@@ -54,6 +74,8 @@ export const VentilatorPlot = (props: any) => {
       .map((p: any) => p[name])
       .reverse();
   };
+
+  console.log(results);
 
   return (
     <div className="grid grid-row-1 md:grid-cols-2 gap-4">
@@ -111,6 +133,26 @@ export const VentilatorPlot = (props: any) => {
           name="PEEP"
           xData={dates}
           yData={yAxisData("ventilator_peep")}
+          low={5}
+          high={10}
+        />
+      </div>
+      <div className="pt-4 px-4 bg-white border rounded-lg shadow">
+        <LinePlot
+          title="PEEP"
+          name="PEEP"
+          xData={dates}
+          yData={yAxisData("ventilator_peep")}
+          low={5}
+          high={10}
+        />
+      </div>
+      <div className="pt-4 px-4 bg-white border rounded-lg shadow">
+        <LinePlot
+          title="FiO2"
+          name="FiO2"
+          xData={dates}
+          yData={yAxisData("ventilator_fi02")}
           low={5}
           high={10}
         />
