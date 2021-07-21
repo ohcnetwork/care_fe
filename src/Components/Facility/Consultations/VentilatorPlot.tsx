ac252f16d55eed28f612d9ec953321a9bb39447c
developedBySJ <developedbysj@gmail.com>
2021-07-21 15:42:38 +0530

    Add text logs

--- src/Components/Facility/Consultations/VentilatorPlot.tsx
+++ src/Components/Facility/Consultations/VentilatorPlot.tsx
@@ -5,6 +5,40 @@ import { statusType, useAbortableEffect } from "../../../Common/utils";
 import { dailyRoundsAnalyse } from "../../../Redux/actions";
 import { LinePlot } from "./components/LinePlot";
 
+interface ITextLogsSubOption {
+  date: string;
+  value?: string;
+}
+interface ITextLogsData {
+  ventilator_interface: ITextLogsSubOption[];
+  ventilator_oxygen_modality: ITextLogsSubOption[];
+  bilateral_air_entry: ITextLogsSubOption[];
+}
+
+const RenderTextLogs = ({
+  data,
+  label,
+}: {
+  data: ITextLogsSubOption[];
+  label: string;
+}) => {
+  return (
+    <>
+      <div className="text-xl font-semibold mb-3">{label}</div>
+      <div className="space-y-2">
+        {data.map((log, index: number) => (
+          <div key={index} className="flex">
+            <div className="text-sm font-semibold w-44 ">{`- ${moment(
+              log.date
+            ).format("LLL")}`}</div>
+            <div className="text-cool-gray-800 pl-2">{log.value ?? "---"}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
 export const VentilatorPlot = (props: any) => {
   const { facilityId, patientId, consultationId } = props;
   const dispatch: any = useDispatch();
@@ -25,6 +59,9 @@ export const VentilatorPlot = (props: any) => {
               "ventilator_resp_rate",
               "ventilator_pressure_support",
               "ventilator_tidal_volume",
+              "ventilator_interface",
+              "ventilator_oxygen_modality",
+              "bilateral_air_entry",
             ],
           },
           { consultationId }
@@ -53,9 +90,34 @@ export const VentilatorPlot = (props: any) => {
       .map((p: any) => p[name])
       .reverse();
   };
+  const textLogsData = Object.entries(results).reduce<ITextLogsData>(
+    (acc, cur) => {
+      const [key, value] = cur as any;
+
+      acc.ventilator_interface.push({
+        date: key,
+        value: value.ventilator_interface,
+      });
+      acc.ventilator_oxygen_modality.push({
+        date: key,
+        value: value.ventilator_oxygen_modality,
+      });
+      acc.bilateral_air_entry.push({
+        date: key,
+        value: value.bilateral_air_entry,
+      });
+
+      return acc;
+    },
+    {
+      ventilator_interface: [],
+      ventilator_oxygen_modality: [],
+      bilateral_air_entry: [],
+    }
+  );
 
   return (
-    <div className="grid grid-row-1 md:grid-cols-2 gap-4">
+    <div className="grid grid-row-1 md:grid-cols-2 gap-4 mb-4">
       <div className="pt-4 px-4 bg-white border rounded-lg shadow">
         <LinePlot
           title="PIP"
@@ -102,6 +164,24 @@ export const VentilatorPlot = (props: any) => {
           name="Tidal Volume"
           xData={dates}
           yData={yAxisData("ventilator_tidal_volume")}
+        />
+      </div>
+      <div className="p-4  bg-white border rounded-lg shadow">
+        <RenderTextLogs
+          data={textLogsData.ventilator_interface}
+          label={"Ventilator Interface"}
+        />
+      </div>
+      <div className="p-4  bg-white border rounded-lg shadow">
+        <RenderTextLogs
+          data={textLogsData.ventilator_oxygen_modality}
+          label={"Ventilator Oxygen Modality"}
+        />
+      </div>
+      <div className="p-4  bg-white border rounded-lg shadow">
+        <RenderTextLogs
+          data={textLogsData.bilateral_air_entry}
+          label={"Bilateral"}
         />
       </div>
     </div>
