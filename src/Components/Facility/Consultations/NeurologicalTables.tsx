6c2a0dd982c6d8b4fd4a7697b2b87751de3dc680
Sandra <sandrakakkarayil01@gmail.com>
2021-07-23 12:37:30 +0530

    Added LOC Description

--- src/Components/Facility/Consultations/NeurologicalTables.tsx
+++ src/Components/Facility/Consultations/NeurologicalTables.tsx
@@ -10,8 +10,8 @@ const DataTable = (props: any) => {
   return (
     <div>
       <div className="text-xl font-semibold">{title}</div>
-      <div className="flex flex-row shadow overflow-hidden sm:rounded-lg divide-y divide-cool-gray-200 my-4">
-        <div className="flex flex-col justify-between w-1/4">
+      <div className="flex flex-row shadow overflow-hidden sm:rounded-lg divide-y divide-cool-gray-200 mb-4 w-max-content">
+        <div className="flex flex-col justify-between min-w-max-content w-50">
           <div className="px-6 py-3 bg-cool-gray-50 text-center text-xs leading-4 font-medium text-cool-gray-500 uppercase tracking-wider">
             Time
           </div>
@@ -55,15 +55,19 @@ const DataDescription = (props: any) => {
     <div>
       <div className="text-xl font-semibold">{title}</div>
       <div className="p-4 bg-white border rounded-lg shadow">
-        {data.map((x: any) => {
-          if (x.left_description || x.right_description) {
-            <div>
-              <div className="text-sm font-semibold">{`- ${x.date}`}</div>
-              <div className="text-cool-gray-800 pl-2">{`Left: ${x.left_description}`}</div>
-              <div className="text-cool-gray-800 pl-2">{`Left: ${x.left_description}`}</div>
-            </div>;
-          }
-        })}
+        {data.map((x: any) => (
+          <div className="mb-2">
+            <div className="text-sm font-semibold">{`- ${x.date}`}</div>
+            <div className="text-cool-gray-800 pl-2">
+              <span className="font-semibold">Left: </span>
+              {x.left}
+            </div>
+            <div className="text-cool-gray-800 pl-2">
+              <span className="font-semibold">Right: </span>
+              {x.right}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
@@ -76,22 +80,31 @@ export const NeurologicalTable = (props: any) => {
   const [offset, setOffset] = useState(0);
   const [results, setResults] = useState({});
 
+  const LOC_OPTIONS = [
+    { id: 0, value: "Unknown" },
+    { id: 5, value: "Alert" },
+    { id: 10, value: "Drowsy" },
+    { id: 15, value: "Stuporous" },
+    { id: 20, value: "Comatose" },
+    { id: 25, value: "Cannot Be Assessed" },
+  ];
+
   const REACTION_OPTIONS = [
-    { id: 0, label: "U", value: "Unknown" },
-    { id: 5, label: "B", value: "Brisk" },
-    { id: 10, label: "S", value: "Sluggish" },
-    { id: 15, label: "F", value: "Fixed" },
-    { id: 20, label: "C", value: "Cannot Be Assessed" },
+    { id: 0, value: "Unknown" },
+    { id: 5, value: "Brisk" },
+    { id: 10, value: "Sluggish" },
+    { id: 15, value: "Fixed" },
+    { id: 20, value: "Cannot Be Assessed" },
   ];
 
   const LIMP_OPTIONS = [
-    { id: 0, label: "U", value: "Unknown" },
-    { id: 5, label: "S", value: "Strong" },
-    { id: 10, label: "M", value: "Moderate" },
-    { id: 15, label: "W", value: "Weak" },
-    { id: 20, label: "F", value: "Flexion" },
-    { id: 25, label: "E", value: "Extension" },
-    { id: 30, label: "N", value: "None" },
+    { id: 0, value: "Unknown" },
+    { id: 5, value: "Strong" },
+    { id: 10, value: "Moderate" },
+    { id: 15, value: "Weak" },
+    { id: 20, value: "Flexion" },
+    { id: 25, value: "Extension" },
+    { id: 30, value: "None" },
   ];
 
   const fetchDailyRounds = useCallback(
@@ -122,8 +135,8 @@ export const NeurologicalTable = (props: any) => {
         )
       );
       if (!status.aborted) {
-        if (res && res.data) {
-          setResults(res.data);
+        if (res && res.data && res.data.results) {
+          setResults(res.data.results);
         }
         setIsLoading(false);
       }
@@ -134,68 +147,112 @@ export const NeurologicalTable = (props: any) => {
   useAbortableEffect((status: statusType) => {
     fetchDailyRounds(status);
   }, []);
-  console.log(results);
 
   const locData: any = [];
+  const locDescription: any = [];
   const sizeData: any = [];
+  const sizeDescription: any = [];
   const reactionData: any = [];
+  const reactionDescription: any = [];
   const upperLimbData: any = [];
   const lowerLimbData: any = [];
   Object.entries(results).map((x: any) => {
-    locData.push({
-      date: moment(x[0]).format("LLL"),
-      loc: x[1].consciousness_level,
-    });
+    if (x[1].consciousness_level) {
+      locData.push({
+        date: moment(x[0]).format("LLL"),
+        loc:
+          LOC_OPTIONS.find((item) => item.id === x[1].consciousness_level)
+            ?.value || "--",
+      });
+    }
 
-    sizeData.push({
-      date: moment(x[0]).format("LLL"),
-      left: x[1].left_pupil_size || "--",
-      right: x[1].right_pupil_size || "--",
-      left_description: x[1].left_pupil_size_detail,
-      right_description: x[1].right_pupil_size_detail,
-    });
-    reactionData.push({
-      date: moment(x[0]).format("LLL"),
-      left:
-        REACTION_OPTIONS.find(
-          (item) => item.id === x[1].left_pupil_light_reaction
-        )?.value || "--",
-      right:
-        REACTION_OPTIONS.find(
-          (item) => item.id === x[1].right_pupil_light_reaction
-        )?.value || "--",
-      left_description: x[1].left_pupil_light_reaction_detail,
-      right_description: x[1].right_pupil_light_reaction_detail,
-    });
-    upperLimbData.push({
-      date: moment(x[0]).format("LLL"),
-      left:
-        LIMP_OPTIONS.find(
-          (item) => item.id === x[1].limb_response_upper_extremity_left
-        )?.value || "--",
-      right:
-        LIMP_OPTIONS.find(
-          (item) => item.id === x[1].limb_response_upper_extremity_right
-        )?.value || "--",
-    });
-    lowerLimbData.push({
-      date: moment(x[0]).format("LLL"),
-      left:
-        LIMP_OPTIONS.find(
-          (item) => item.id === x[1].limb_response_lower_extremity_left
-        )?.value || "--",
-      right:
-        LIMP_OPTIONS.find(
-          (item) => item.id === x[1].limb_response_lower_extremity_right
-        )?.value || "--",
-    });
+    if (x[1].consciousness_level_detail) {
+      locDescription.push({
+        date: moment(x[0]).format("LLL"),
+        loc: x[1].consciousness_level_detail,
+      });
+    }
+
+    if (x[1].left_pupil_size || x[1].right_pupil_size) {
+      sizeData.push({
+        date: moment(x[0]).format("LLL"),
+        left: x[1].left_pupil_size || "-",
+        right: x[1].right_pupil_size || "-",
+      });
+    }
+
+    if (x[1].left_pupil_size_detail || x[1].right_pupil_size_detail) {
+      sizeDescription.push({
+        date: moment(x[0]).format("LLL"),
+        left: x[1].left_pupil_size_detail || "-",
+        right: x[1].right_pupil_size_detail || "-",
+      });
+    }
+
+    if (x[1].left_pupil_light_reaction || x[1].right_pupil_light_reaction) {
+      reactionData.push({
+        date: moment(x[0]).format("LLL"),
+        left:
+          REACTION_OPTIONS.find(
+            (item) => item.id === x[1].left_pupil_light_reaction
+          )?.value || "--",
+        right:
+          REACTION_OPTIONS.find(
+            (item) => item.id === x[1].right_pupil_light_reaction
+          )?.value || "--",
+      });
+    }
+
+    if (
+      x[1].left_pupil_light_reaction_detail ||
+      x[1].right_pupil_light_reaction_detail
+    ) {
+      reactionDescription.push({
+        date: moment(x[0]).format("LLL"),
+        left: x[1].left_pupil_light_reaction_detail || "-",
+        right: x[1].right_pupil_light_reaction_detail || "-",
+      });
+    }
+    if (
+      x[1].limb_response_upper_extremity_left ||
+      x[1].limb_response_upper_extremity_right
+    ) {
+      upperLimbData.push({
+        date: moment(x[0]).format("LLL"),
+        left:
+          LIMP_OPTIONS.find(
+            (item) => item.id === x[1].limb_response_upper_extremity_left
+          )?.value || "--",
+        right:
+          LIMP_OPTIONS.find(
+            (item) => item.id === x[1].limb_response_upper_extremity_right
+          )?.value || "--",
+      });
+    }
+
+    if (
+      x[1].limb_response_lower_extremity_left ||
+      x[1].limb_response_lower_extremity_right
+    ) {
+      lowerLimbData.push({
+        date: moment(x[0]).format("LLL"),
+        left:
+          LIMP_OPTIONS.find(
+            (item) => item.id === x[1].limb_response_lower_extremity_left
+          )?.value || "--",
+        right:
+          LIMP_OPTIONS.find(
+            (item) => item.id === x[1].limb_response_lower_extremity_right
+          )?.value || "--",
+      });
+    }
   });
 
   return (
-    <div className="mt-4">
-      <div>
+    <div className="mt-2">
+      <div className="mb-6">
         <div className="text-xl font-semibold">Level Of Consciousness</div>
-        <div className="flex flex-row shadow overflow-hidden sm:rounded-lg divide-y divide-cool-gray-200 my-4">
+        <div className="flex flex-row shadow overflow-hidden sm:rounded-lg divide-y divide-cool-gray-200 my-4 w-max-content">
           <div
             style={{ direction: "rtl" }}
             className="flex flex-row overflow-x-auto"
@@ -212,16 +269,38 @@ export const NeurologicalTable = (props: any) => {
             ))}
           </div>
         </div>
+        <div>
+          <div className="text-xl font-semibold">
+            Level Of Consciousness Description
+          </div>
+          <div className="p-4 bg-white border rounded-lg shadow">
+            {locDescription.map((x: any) => (
+              <div className="mb-2">
+                <div className="text-sm font-semibold">{`- ${x.date}`}</div>
+                <div className="text-cool-gray-800 pl-2">{x.loc}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <DataTable title={"Pupil Size"} data={sizeData} />
-      <DataDescription title={"Pupil Size Description"} data={sizeData} />
-      <DataTable title={"Pupil Reaction"} data={reactionData} />
-      <DataDescription
-        title={"Pupil Reaction Description"}
-        data={reactionData}
-      />
-      <DataTable title={"Upper Extremity"} data={upperLimbData} />
-      <DataTable title={"Lower Extremity"} data={lowerLimbData} />
+      <div className="mb-6">
+        <DataTable title={"Pupil Size"} data={sizeData} />
+        <DataDescription
+          title={"Pupil Size Description"}
+          data={sizeDescription}
+        />
+      </div>
+      <div className="mb-6">
+        <DataTable title={"Pupil Reaction"} data={reactionData} />
+        <DataDescription
+          title={"Pupil Reaction Description"}
+          data={reactionDescription}
+        />
+      </div>
+      <div className="mb-6">
+        <DataTable title={"Upper Extremity"} data={upperLimbData} />
+        <DataTable title={"Lower Extremity"} data={lowerLimbData} />
+      </div>
     </div>
   );
 };
