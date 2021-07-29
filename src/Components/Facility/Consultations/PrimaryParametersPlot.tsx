8b1c3848a383809d69157b71d1546f38b54fd5e0
Divya Garg <divyagarg2601@gmail.com>
2021-07-29 07:00:37 +0530

    Add pagination in primary parameters plots

--- src/Components/Facility/Consultations/PrimaryParametersPlot.tsx
+++ src/Components/Facility/Consultations/PrimaryParametersPlot.tsx
@@ -6,6 +6,7 @@ import { statusType, useAbortableEffect } from "../../../Common/utils";
 import { dailyRoundsAnalyse } from "../../../Redux/actions";
 import { LinePlot } from "./components/LinePlot";
 import { StackedLinePlot } from "./components/StackedLinePlot";
+import Pagination from "../../Common/Pagination";
 
 export const PrimaryParametersPlot = (props: any) => {
   const { facilityId, patientId, consultationId } = props;
@@ -14,6 +15,7 @@ export const PrimaryParametersPlot = (props: any) => {
   const [offset, setOffset] = useState(0);
   const [results, setResults] = useState({});
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalCount, setTotalCount] = useState(0);
 
   const fetchDailyRounds = useCallback(
     async (status: statusType) => {
@@ -21,7 +23,7 @@ export const PrimaryParametersPlot = (props: any) => {
       const res = await dispatch(
         dailyRoundsAnalyse(
           {
-            offset,
+            page: currentPage,
             fields: [
               "bp",
               "pulse",
@@ -42,18 +44,19 @@ export const PrimaryParametersPlot = (props: any) => {
       if (!status.aborted) {
         if (res && res.data) {
           setResults(res.data.results);
+          setTotalCount(res.data.count);
         }
         setIsLoading(false);
       }
     },
-    [consultationId, dispatch, offset]
+    [consultationId, dispatch, currentPage]
   );
 
   useAbortableEffect(
     (status: statusType) => {
       fetchDailyRounds(status);
     },
-    [consultationId]
+    [consultationId, currentPage]
   );
 
   const handlePagination = (page: number, limit: number) => {
@@ -228,6 +231,15 @@ export const PrimaryParametersPlot = (props: any) => {
             }
           })}
         </div>
+      </div>
+
+      <div className="mt-4 flex w-full justify-center">
+        <Pagination
+          cPage={currentPage}
+          defaultPerPage={36}
+          data={{ totalCount }}
+          onChange={handlePagination}
+        />
       </div>
     </div>
   );
