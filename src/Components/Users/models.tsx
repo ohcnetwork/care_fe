5fe01b5ce54879c11da2581d460e8da7d924e36f
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-08-11 14:42:32 +0530

    restore email param in `UserModel`

--- src/Components/Users/models.tsx
+++ src/Components/Users/models.tsx
@@ -3,6 +3,7 @@ export interface UserModel {
   username?: string;
   first_name?: string;
   last_name?: string;
+  email?: string;
   user_type?: number;
   local_body?: number;
   district?: number;
