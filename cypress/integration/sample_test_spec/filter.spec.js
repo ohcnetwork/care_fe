b1cbcdc6e1ed769c0ce37fade60b7b151e021459
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-09-07 06:31:54 +0000

    sample test basic test added

--- cypress/integration/sample_test_spec/filter.spec.js
+++ cypress/integration/sample_test_spec/filter.spec.js
@@ -1,10 +1,17 @@
 /// <reference types="cypress" />
 
 describe("Sample Filter", () => {
-  beforeEach(() => {
+  before(() => {
     cy.login("devdistrictadmin", "Coronasafe@123");
-    cy.contains("Sample Test", "a").click();
-    cy.contains("Advaned Filters").click();
+    cy.saveLocalStorage();
+  });
+
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.visit("http://localhost:4000");
+    cy.get("a").contains("Sample Test").click();
+    cy.contains("Advanced Filters").click();
+    cy.wait(2000);
   });
 
   it("Filter by Status", () => {
@@ -17,5 +24,7 @@ describe("Sample Filter", () => {
 
   afterEach(() => {
     cy.contains("Apply").click();
+    cy.wait(1000);
+    cy.saveLocalStorage();
   });
 });
