94c348e4a07cadf4d1e44d7274a13353baca4f0b
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-09-08 23:40:42 +0530

    fixing death_report test

--- cypress/integration/death_report_spec/death_report.spec.js
+++ cypress/integration/death_report_spec/death_report.spec.js
@@ -4,9 +4,24 @@ const user = { username: "devdistrictadmin", password: "Coronasafe@123" };
 const address = "C-106,\nSector-H,\nAliganj,\nLucknow,\nUttar Pradesh";
 
 describe("Death Report", () => {
-  it("Add Data And Submit " + user.username, () => {
+  before(() => {
     cy.login(user.username, user.password);
+    cy.saveLocalStorage();
+  })
 
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.visit('http://localhost:4000');
+    cy.get("a").contains("Patients").click();
+    cy.url().should("include", "/patients");
+    cy.contains('Details').click();
+    cy.url().then((url) => {
+      const patient_id = url.split('/')[6]
+      cy.visit(`https://localhost:4000/death_report/${patient_id}`)
+    })
+  })
+  
+  it("Add Data And Submit " + user.username, () => {
     // Paitents Page
     cy.get("a").contains("Patients").click();
     cy.url().should("include", "/patients");
