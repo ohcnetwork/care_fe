6b44a2fe216cdd308d9111191d26ad172dc8aa68
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-09-09 01:23:28 +0530

    add shifting manage, filter tests

---
+++ cypress/integration/shifting/filter.spec.js
@@ -0,0 +1,97 @@
+/// <reference types="cypress" />
+
+
+describe("Edit Profile Testing", () => {
+    before(() => {
+      cy.login('devdistrictadmin', 'Coronasafe@123');
+      cy.saveLocalStorage();
+    });
+  
+    beforeEach(() => {
+      cy.restoreLocalStorage();
+      cy.visit('http://localhost:4000');
+      cy.get("a").contains("Shifting").click();
+      cy.url().should("include", "/shifting");
+    });
+  
+    it('filter by origin facility', () => {
+        // set name for origin facility field
+    })
+
+    it('filter by shifting approval facility', () => {
+        // set name for shifting approval facility field
+    })
+
+    it('filter by assigned facility', () => {
+
+    })
+
+    it('filter by assigned to facility', () => {
+
+    })
+
+    it('filter by ordering', () => {
+        ['DESC Created Date', 'ASC Modified Date', 'DESC Modified Date', 'ASC Created Date']
+        .forEach((select) => {
+            cy.get('[name="ordering"]').select(select)
+            cy.contains('Apply').click().wait(1000)
+            //click filter
+        })
+    })
+
+    it('filter by emergency case', () => {
+        ['yes', 'no']
+        .forEach((select) => {
+            cy.get('[name="emergency"]').select(select)
+            cy.contains('Apply').click().wait(1000)
+            //click filter 
+        })
+    })
+
+    it('filter by KASP', () => {
+        ['yes', 'no']
+        .forEach((select) => {
+            cy.get('[name="is_kasp"]').select(select);
+            cy.contains('Apply').click().wait(1000)
+            //click filter
+        })
+    })
+
+    it('filter by upshift case', () => {
+        ['yes', 'no']
+        .forEach((select) => {
+            cy.get('[name="is_up_shift"]').select(select);
+            cy.contains('Apply').click().wait(1000)
+            //click filter
+        })
+    })
+
+    it('filter by disease status', () => {
+        ['POSITIVE', 'SUSPECTED', 'NEGATIVE', 'RECOVERED', 'EXPIRED']
+        .forEach((select) => {
+            cy.get('[name="disease_status"]').select(select)
+            cy.contains('Apply').click().wait(1000)
+            //click filter
+        })
+    })
+
+    it('filter by patient phone number', () => {
+        cy.contains(/+91.+/).invoke('text').then((phoneNumber) => {
+            cy.get('[name="patient_phone_number"]').type(phoneNumber)
+            cy.contains('Apply').click().wait(1000)
+        })
+    })
+
+    it('filter by created date', () => {
+
+    })
+
+    it('filter by modified date', () => {
+
+    })
+  
+    afterEach(() => {
+      cy.saveLocalStorage();
+    });
+  });
+  
---
+++ cypress/integration/shifting/manage.spec.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+
+describe("Edit Profile Testing", () => {
+  before(() => {
+    cy.login('devdistrictadmin', 'Coronasafe@123');
+    cy.saveLocalStorage();
+  });
+
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+    cy.visit('http://localhost:4000');
+    cy.get("a").contains("Shifting").click();
+    cy.url().should("include", "/shifting");
+  });
+
+  it("checks if all download button works", () => {
+    cy.get('svg.MuiSvgIcon-root.cursor-pointer').then((downloadButtons) => {
+        downloadButtons.forEach((button) => {
+            button.click()
+        })
+    })
+  });
+
+  it("switch between list view and board view", () => {
+    cy.contains('List View').click();
+    cy.wait(1000);
+    cy.contains('Board View').click();
+  })
+
+  it("search patient", () => {
+    cy.get('[name="search"]').type('Akhil')
+    cy.url().should('include', 'Akhil')
+  })
+
+  it("switch between active/completed", () => {
+    cy.contains('Completed').click().wait(2000)
+    cy.contains('Active').should('have.class', 'bg-gray-200')
+    cy.contains('Completed').should('have.class', 'bg-white')
+
+    cy.contains('Active').click().wait(2000)
+    cy.contains('Active').should('have.class', 'bg-white')
+    cy.contains('Completed').should('have.class', 'bg-gray-200')
+  })
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+  });
+});
