2fb5c666a4848fc1bd47af85b1a941134e2a8595
Khavin Shankar <khavinshankar@gmail.com>
2021-10-14 15:27:59 +0530

    feat: added breadcrumbs component

---
+++ src/Components/Common/Breadcrumbs.tsx
@@ -0,0 +1,100 @@
+import { usePath } from "raviger";
+import { useState } from "react";
+
+export default function Breadcrumbs(props: any) {
+  const path = usePath().split("/");
+  const [showFullPath, setShowFullPath] = useState(false);
+  return (
+    <div className="">
+      <div className="max-w-lg mx-auto py-4 px-4 sm:px-6 lg:px-8 lg:py-4">
+        <nav className="flex" aria-label="Breadcrumb">
+          <ol className="flex items-center space-x-1">
+            <li>
+              <div>
+                <a href="/" className="text-gray-500 hover:text-gray-700">
+                  <svg
+                    className="flex-shrink-0 h-5 w-5"
+                    x-description="Heroicon name: solid/home"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                  </svg>
+                  <span className="sr-only">Home</span>
+                </a>
+              </div>
+            </li>
+            {!showFullPath && path.length > 3 && (
+              <li>
+                <div className="flex items-center">
+                  <svg
+                    className="flex-shrink-0 h-5 w-5 text-gray-300"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    aria-hidden="true"
+                  >
+                    <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z"></path>
+                  </svg>
+                  <span
+                    onClick={() => setShowFullPath(true)}
+                    className="inline-flex items-center px-2.5 py-1 ml-1 mt-0.5 rounded-full text-xs font-medium bg-gray-500 hover:bg-gray-700"
+                  >
+                    <svg
+                      className="mx-0.25 h-1.5 w-1.5 text-white"
+                      fill="currentColor"
+                      viewBox="0 0 8 8"
+                    >
+                      <circle cx="4" cy="4" r="3"></circle>
+                    </svg>
+                    <svg
+                      className="mx-0.25 h-1.5 w-1.5 text-white"
+                      fill="currentColor"
+                      viewBox="0 0 8 8"
+                    >
+                      <circle cx="4" cy="4" r="3"></circle>
+                    </svg>
+                    <svg
+                      className="mx-0.25 h-1.5 w-1.5 text-white"
+                      fill="currentColor"
+                      viewBox="0 0 8 8"
+                    >
+                      <circle cx="4" cy="4" r="3"></circle>
+                    </svg>
+                  </span>
+                </div>
+              </li>
+            )}
+            {path.slice(showFullPath ? 0 : -2).map((section: string) => {
+              return (
+                section && (
+                  <li>
+                    <div className="flex items-center">
+                      <svg
+                        className="flex-shrink-0 h-5 w-5 text-gray-300"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        aria-hidden="true"
+                      >
+                        <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z"></path>
+                      </svg>
+                      <a
+                        href="/facility"
+                        className="ml-1 text-sm font-medium text-gray-500 hover:text-gray-700"
+                      >
+                        {section}
+                      </a>
+                    </div>
+                  </li>
+                )
+              );
+            })}
+          </ol>
+        </nav>
+      </div>
+    </div>
+  );
+}
