db885b3f4be069ec4dc28d3c5927019f62c135cf
Gigin George <mail.gigin@gmail.com>
2021-12-13 02:40:14 +0530

    Improve Loading Screen

--- src/Components/Facility/Consultations/LiveFeed.tsx
+++ src/Components/Facility/Consultations/LiveFeed.tsx
@@ -23,7 +23,7 @@ const LiveFeed = (props: any) => {
     if (loading === true)
       loadingTimeout = setTimeout(() => {
         setLoading(false);
-      }, 5000);
+      }, 6000);
     return () => {
       if (loadingTimeout) clearTimeout(loadingTimeout);
     };
@@ -193,7 +193,7 @@ const LiveFeed = (props: any) => {
     <div className="mt-4 px-6 mb-20">
       <PageTitle title="Live Feed" hideBack={true} />
       <div className="mt-4 flex flex-col">
-        <div className="mt-4 flex flex-col md:flex-row">
+        <div className="mt-4 relative flex flex-col md:flex-row">
           {sourceUrl ? (
             <>
               <ReactPlayer
@@ -218,18 +218,38 @@ const LiveFeed = (props: any) => {
                   }
                 }}
               />
-              {loading && (
-                <div
-                  className="absolute bg-gray-500 bg-opacity-75 z-5 transition-opacity"
-                  style={{ height: "360px", width: "640px" }}
-                >
-                  {/* div with "Loading" at the center */}
-                  <div className="flex justify-center items-center h-full">
-                    <div className="text-white text-2xl">Moving Camera</div>
-                    <div className="text-white text-3xl">...</div>
-                  </div>
+              <div
+                className={
+                  (loading ? "absolute" : "hidden") +
+                  " bg-gray-500 bg-opacity-75 z-5 transition-opacity"
+                }
+                style={{ height: "360px", width: "640px" }}
+              >
+                {/* div with "Loading" at the center */}
+                <div className="flex justify-center items-center h-full">
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      stroke-width="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                  <div className="text-white text-2xl">Moving Camera</div>
                 </div>
-              )}
+              </div>
             </>
           ) : (
             <div className="w-full max-w-xl bg-gray-500 flex flex-col justify-center items-center">
