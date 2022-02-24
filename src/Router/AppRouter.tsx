5f8562e48a024b69083fc10660c3a311a872db19
Swapnil J <76519654+developedBySJ@users.noreply.github.com>
2022-02-24 10:39:25 +0530

    Tele ICU Spoke List View | Issue#2094 (#2113)
    Co-authored-by: Gigin George <mail.gigin@gmail.com>

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -62,8 +62,12 @@ import { DailyRoundListDetails } from "../Components/Patient/DailyRoundListDetai
 import HubDashboard from "../Components/Dashboard/HubDashboard";
 import { SideBar } from "../Components/Common/SideBar";
 import { Feed } from "../Components/Facility/Consultations/Feed";
+import { TeleICUFacility } from "../Components/TeleIcu/Facility";
+import { Feed } from "../Components/Facility/Consultations/Feed";
 
+const get = require("lodash.get");
 
+const img = process.env.REACT_APP_LIGHT_LOGO;
 const logoBlack = process.env.REACT_APP_BLACK_LOGO;
 
 const routes = {
@@ -363,10 +367,70 @@ const routes = {
         tab={tab}
       />
     ),
+  "/teleicu/facility": () => <TeleICUFacility />,
 };
 
-const AppRouter = () => {
+let menus = [
+  {
+    title: "Facilities",
+    link: "/facility",
+    icon: "fas fa-hospital",
+  },
+  {
+    title: "Patients",
+    link: "/patients",
+    icon: "fas fa-user-injured",
+  },
+  {
+    title: "Assets",
+    link: "/assets",
+    icon: "fas fa-shopping-cart",
+  },
+  {
+    title: "Sample Test",
+    link: "/sample",
+    icon: "fas fa-medkit",
+  },
+  {
+    title: "Shifting",
+    link: "/shifting",
+    icon: "fas fa-ambulance",
+  },
+  {
+    title: "Resource",
+    link: "/resource",
+    icon: "fas fa-heartbeat",
+  },
+  {
+    title: "External Results",
+    link: "/external_results",
+    icon: "fas fa-vials",
+  },
+  {
+    title: "Users",
+    link: "/users",
+    icon: "fas fa-user-friends",
+  },
+  {
+    title: "Tele ICU",
+    link: "/teleicu/facility",
+    icon: "fas fa-video",
+  },
+  {
+    title: "Profile",
+    link: "/user/profile",
+    icon: "fas fa-user-secret",
+  },
+  {
+    title: "Notice Board",
+    link: "/notice_board/",
+    icon: "fas fa-comment-alt",
+  },
+];
+
+const AppRouter = (props: any) => {
   useRedirect("/", "/facility");
+  useRedirect("/teleicu", "/teleicu/facility");
   const pages = useRoutes(routes);
   const path = usePath();
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
