69a906a42d16698bffc11bb1fe16d16cc3f97b15
Shivank Kacker <kacker.shivank@gmail.com>
2022-05-12 10:58:57 +0530

    fixed some errors

--- src/Components/Facility/ConsultationForm.tsx
+++ src/Components/Facility/ConsultationForm.tsx
@@ -573,7 +573,7 @@ export const ConsultationForm = (props: any) => {
                       <MultiSelectField
                         name="symptoms"
                         variant="outlined"
-                        value={state.form?.symptoms}
+                        value={state.form.symptoms}
                         options={symptomChoices}
                         onChange={handleSymptomChange}
                       />
@@ -581,11 +581,11 @@ export const ConsultationForm = (props: any) => {
                     </div>
                   </div>
                   <div>
-                    {state.form?.hasSymptom && (
+                    {state.form.hasSymptom && (
                       <div id="symptoms_onset_date-div">
                         <DateInputField
                           label="Date of onset of the symptoms*"
-                          value={state.form?.symptoms_onset_date}
+                          value={state.form.symptoms_onset_date}
                           onChange={(date) =>
                             handleDateChange(date, "symptoms_onset_date")
                           }
