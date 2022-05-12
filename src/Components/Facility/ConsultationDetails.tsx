a34d7821b4b41f2e2f6705200e0015da0eab84df
Rithvik Nishad <rithvikn2001@gmail.com>
2022-05-12 21:33:13 +0530

    changes consultation details title

--- src/Components/Facility/ConsultationDetails.tsx
+++ src/Components/Facility/ConsultationDetails.tsx
@@ -171,7 +171,7 @@ export const ConsultationDetails = (props: any) => {
       <div className="px-2 pb-2">
         <nav className="flex justify-between flex-wrap">
           <PageTitle
-            title="Patient Details"
+            title="Patient Dashboard"
             className="sm:m-0 sm:p-0"
             breadcrumbs={true}
           />
