be9f74ab9b5cc34c3be655a97a911c3e572723e0
Shivank Kacker <kacker.shivank@gmail.com>
2022-05-17 14:07:22 +0530

    added 404 error

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -1,13 +1,5 @@
-import {
-  useRedirect,
-  useRoutes,
-  navigate,
-  usePath,
-  Link,
-  Redirect,
-} from "raviger";
+import { useRedirect, useRoutes, usePath, Redirect } from "raviger";
 import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
 import { BedCapacityForm } from "../Components/Facility/BedCapacityForm";
 import { ConsultationDetails } from "../Components/Facility/ConsultationDetails";
 import TreatmentSummary from "../Components/Facility/TreatmentSummary";
@@ -73,18 +65,12 @@ import { Feed } from "../Components/Facility/Consultations/Feed";
 import { TeleICUFacility } from "../Components/TeleIcu/Facility";
 import TeleICUPatientPage from "../Components/TeleIcu/Patient";
 import { TeleICUPatientsList } from "../Components/TeleIcu/PatientList";
+import Error404 from "../Components/ErrorPages/404";
 
-const get = require("lodash.get");
-
-const img = process.env.REACT_APP_LIGHT_LOGO;
 const logoBlack = process.env.REACT_APP_BLACK_LOGO;
 
 const routes = {
-  "/hub": () => (
-    <>
-      <HubDashboard />
-    </>
-  ),
+  "/hub": () => <HubDashboard />,
   "/": () => <HospitalList />,
   "/users": () => <ManageUsers />,
   "/user/add": () => <UserAdd />,
@@ -373,6 +359,7 @@ const routes = {
   "/facility/:facilityId/patient/:patientId/consultation/:consultationId/treatment-summary":
     ({ facilityId, patientId, consultationId }: any) => (
       <TreatmentSummary
+        facilityId={facilityId}
         consultationId={consultationId}
         patientId={patientId}
         dailyRoundsListData={[]}
@@ -400,73 +387,10 @@ const routes = {
   ),
 };
 
-let menus = [
-  {
-    title: "Facilities",
-    link: "/facility",
-    icon: "fas fa-hospital",
-  },
-  {
-    title: "Patients",
-    link: "/patients",
-    icon: "fas fa-user-injured",
-  },
-  {
-    title: "TeleICU",
-    link: "/teleicu",
-    icon: "fas fa-bed-pulse",
-  },
-  {
-    title: "Assets",
-    link: "/assets",
-    icon: "fas fa-shopping-cart",
-  },
-  {
-    title: "Sample Test",
-    link: "/sample",
-    icon: "fas fa-medkit",
-  },
-  {
-    title: "Shifting",
-    link: "/shifting",
-    icon: "fas fa-ambulance",
-  },
-  {
-    title: "Resource",
-    link: "/resource",
-    icon: "fas fa-heartbeat",
-  },
-  {
-    title: "External Results",
-    link: "/external_results",
-    icon: "fas fa-vials",
-  },
-  {
-    title: "Users",
-    link: "/users",
-    icon: "fas fa-user-friends",
-  },
-  {
-    title: "Tele ICU",
-    link: "/teleicu/facility",
-    icon: "fas fa-video",
-  },
-  {
-    title: "Profile",
-    link: "/user/profile",
-    icon: "fas fa-user-secret",
-  },
-  {
-    title: "Notice Board",
-    link: "/notice_board/",
-    icon: "fas fa-comment-alt",
-  },
-];
-
-const AppRouter = (props: any) => {
+const AppRouter = () => {
   useRedirect("/", "/facility");
   useRedirect("/teleicu", "/teleicu/facility");
-  const pages = useRoutes(routes);
+  const pages = useRoutes(routes) || <Error404 />;
   const path = usePath();
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
