82355f56d1e6315a73d2ea4e382e4498ed53f8ce
Ritesh <riteshaggarwal2001@gmail.com>
2022-05-18 14:38:42 +0530

    add loading spinner to resource page comment section

--- src/Components/Resource/CommentSection.tsx
+++ src/Components/Resource/CommentSection.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
 import { statusType, useAbortableEffect } from "../../Common/utils";
 import { getResourceComments, addResourceComments } from "../../Redux/actions";
-import { Button } from "@material-ui/core";
+import { Button, CircularProgress } from "@material-ui/core";
 import * as Notification from "../../Utils/Notifications.js";
 import moment from "moment";
 import Pagination from "../Common/Pagination";
@@ -12,7 +12,7 @@ interface CommentSectionProps {
 }
 const CommentSection = (props: CommentSectionProps) => {
   const dispatch: any = useDispatch();
-  let initialData: any = [];
+  const initialData: any = [];
   const [comments, setComments] = useState(initialData);
   const [commentBox, setCommentBox] = useState("");
   const [isLoading, setIsLoading] = useState(true);
@@ -56,7 +56,7 @@ const CommentSection = (props: CommentSectionProps) => {
     const payload = {
       comment: commentBox,
     };
-    dispatch(addResourceComments(props.id, payload)).then((res: any) => {
+    dispatch(addResourceComments(props.id, payload)).then((_: any) => {
       Notification.Success({ msg: "Comment added successfully" });
       fetchData();
     });
@@ -81,27 +81,34 @@ const CommentSection = (props: CommentSectionProps) => {
         </Button>
       </div>
       <div className=" w-full">
-        {comments.map((comment: any) => (
-          <div className="flex p-4 bg-white rounded-lg text-gray-800 mt-4 flex-col w-full border border-gray-300">
-            <div className="flex  w-full ">
-              <p className="text-justify">{comment.comment}</p>
-            </div>
-            <div className="mt-3">
-              <span className="text-xs text-gray-500">
-                {moment(comment.modified_date).format("LLL") || "-"}
-              </span>
-            </div>
-            <div className=" flex mr-auto bg-gray-100 border items-center rounded-md py-1 pl-2 pr-3">
-              <div className="flex justify-center items-center w-8 h-8 rounded-full bg-primary-700 uppercase text-white p-1">
-                {comment.created_by_object?.first_name?.charAt(0) || "U"}
+        {isLoading ? (
+          <CircularProgress />
+        ) : (
+          comments.map((comment: any) => (
+            <div
+              key={comment.id}
+              className="flex p-4 bg-white rounded-lg text-gray-800 mt-4 flex-col w-full border border-gray-300"
+            >
+              <div className="flex  w-full ">
+                <p className="text-justify">{comment.comment}</p>
               </div>
-              <span className="text-gray-700 text-sm pl-2">
-                {comment.created_by_object?.first_name || "Unknown"}{" "}
-                {comment.created_by_object?.last_name}
-              </span>
+              <div className="mt-3">
+                <span className="text-xs text-gray-500">
+                  {moment(comment.modified_date).format("LLL") || "-"}
+                </span>
+              </div>
+              <div className=" flex mr-auto bg-gray-100 border items-center rounded-md py-1 pl-2 pr-3">
+                <div className="flex justify-center items-center w-8 h-8 rounded-full bg-primary-700 uppercase text-white p-1">
+                  {comment.created_by_object?.first_name?.charAt(0) || "U"}
+                </div>
+                <span className="text-gray-700 text-sm pl-2">
+                  {comment.created_by_object?.first_name || "Unknown"}{" "}
+                  {comment.created_by_object?.last_name}
+                </span>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
       {totalCount > limit && (
         <div className="mt-4 flex w-full justify-center">
