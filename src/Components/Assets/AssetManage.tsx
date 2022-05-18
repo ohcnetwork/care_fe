54d01aa8011b633e9aded4ff470cd30075910b97
ARYAN PATEL <patelaryan7751@gmail.com>
2022-05-18 16:18:13 +0530

    fixed the unhandled qr code asset id situation

--- src/Components/Assets/AssetManage.tsx
+++ src/Components/Assets/AssetManage.tsx
@@ -40,17 +40,23 @@ const AssetManage = (props: AssetManageProps) => {
         dispatch(getAsset(assetId)),
         dispatch(listAssetTransaction({ asset: assetId, limit, offset })),
       ]);
-      if (!status.aborted) {
-        setIsLoading(false);
-        if (!assetData.data)
-          Notification.Error({
-            msg: "Something went wrong..!",
-          });
-        else {
-          setAsset(assetData.data);
-          setTransactions(transactionsData.data.results);
-          setTotalCount(transactionsData.data.count);
+      try {
+        if (!status.aborted) {
+          setIsLoading(false);
+          if (!assetData.data)
+            Notification.Error({
+              msg: "Something went wrong..!",
+            });
+          else {
+            setAsset(assetData.data);
+            setTransactions(transactionsData.data.results);
+            setTotalCount(transactionsData.data.count);
+          }
         }
+      } catch (err) {
+        Notification.Error({
+          msg: "Invalid QRCode !! Please Scan a valid QR code .",
+        });
       }
     },
     [dispatch, assetId, offset]
@@ -124,7 +130,7 @@ const AssetManage = (props: AssetManageProps) => {
     if (txns.length > 0) {
       setTransactionDetails(
         transactions.map((transaction: AssetTransaction) => (
-          <tr>
+          <tr key={`transaction_id_${transaction.id}`}>
             <td className="px-6 py-4 text-left whitespace-no-wrap text-sm leading-5 text-cool-gray-500">
               <span className="text-cool-gray-900 font-medium">
                 {transaction.from_location.name}
@@ -171,142 +177,163 @@ const AssetManage = (props: AssetManageProps) => {
   if (isPrintMode) return <PrintPreview />;
   return (
     <div className="px-2 pb-2">
-      <PageTitle
-        title={asset?.name || "Asset"}
-        crumbsReplacements={{ [assetId]: { name: asset?.name } }}
-      />
-      <div className="bg-white rounded-lg md:p-6 p-3 shadow">
-        <div className="text-2xl font-semibold mb-4">{asset?.name}</div>
-        <div className="md:flex justify-between">
-          <div className="mb-2">
-            <div className="grid grid-cols-3 gap-6">
-              <Typography className="flex flex-col">
-                <span className="font-bold">Location</span>
-                <span>{asset?.location_object.name || "--"}</span>
-              </Typography>
-              <Typography className="flex flex-col">
-                <span className="font-bold">Facility</span>
-                <span>{asset?.location_object.facility.name || "--"}</span>
-              </Typography>
-              <Typography className="flex flex-col">
-                <span className="font-bold">Serial Number</span>
-                <span>{asset?.serial_number || "--"}</span>
-              </Typography>
-              <Typography className="flex flex-col">
-                <span className="font-bold">Warranty Details</span>
-                <span>{asset?.warranty_details || "--"}</span>
-              </Typography>
-              <Typography className="flex flex-col">
-                <span className="font-bold">Type</span>
-                <span>{asset?.asset_type || "--"}</span>
-              </Typography>
-              <Typography className="flex flex-col">
-                <span className="font-bold">Vendor Name</span>
-                <span>{asset?.vendor_name || "--"}</span>
-              </Typography>
-              <Typography className="flex flex-col">
-                <span className="font-bold">Customer Support Name</span>
-                <span>{asset?.support_name || "--"}</span>
-              </Typography>
-              <Typography className="flex flex-col">
-                <span className="font-bold">Contact Phone Number</span>
-                <span>{asset?.support_phone || "--"}</span>
-              </Typography>
-              <Typography className="flex flex-col">
-                <span className="font-bold">Contact Email</span>
-                <span>{asset?.support_email || "--"}</span>
-              </Typography>
-              <Typography className="flex flex-col">
-                <span className="font-bold">Status</span>
-                <span>{status(asset?.status)}</span>
-              </Typography>
-              <Typography className="flex flex-col">
-                <span className="font-bold">Working status</span>
-                <span>{working_status(asset?.is_working)}</span>
-              </Typography>
-              {!asset?.is_working && (
-                <Typography className="flex flex-col">
-                  <span className="font-bold">Not working reason</span>
-                  <span>{asset?.not_working_reason || "--"}</span>
-                </Typography>
-              )}
+      {asset?.name ? (
+        <>
+          {" "}
+          <PageTitle
+            title={asset?.name || "Asset"}
+            crumbsReplacements={{ [assetId]: { name: asset?.name } }}
+          />
+          <div className="bg-white rounded-lg md:p-6 p-3 shadow">
+            <div className="text-2xl font-semibold mb-4">{asset?.name}</div>
+            <div className="md:flex justify-between">
+              <div className="mb-2">
+                <div className="grid grid-cols-3 gap-6">
+                  <Typography className="flex flex-col">
+                    <span className="font-bold">Location</span>
+                    <span>{asset?.location_object.name || "--"}</span>
+                  </Typography>
+                  <Typography className="flex flex-col">
+                    <span className="font-bold">Facility</span>
+                    <span>{asset?.location_object.facility.name || "--"}</span>
+                  </Typography>
+                  <Typography className="flex flex-col">
+                    <span className="font-bold">Serial Number</span>
+                    <span>{asset?.serial_number || "--"}</span>
+                  </Typography>
+                  <Typography className="flex flex-col">
+                    <span className="font-bold">Warranty Details</span>
+                    <span>{asset?.warranty_details || "--"}</span>
+                  </Typography>
+                  <Typography className="flex flex-col">
+                    <span className="font-bold">Type</span>
+                    <span>{asset?.asset_type || "--"}</span>
+                  </Typography>
+                  <Typography className="flex flex-col">
+                    <span className="font-bold">Vendor Name</span>
+                    <span>{asset?.vendor_name || "--"}</span>
+                  </Typography>
+                  <Typography className="flex flex-col">
+                    <span className="font-bold">Customer Support Name</span>
+                    <span>{asset?.support_name || "--"}</span>
+                  </Typography>
+                  <Typography className="flex flex-col">
+                    <span className="font-bold">Contact Phone Number</span>
+                    <span>{asset?.support_phone || "--"}</span>
+                  </Typography>
+                  <Typography className="flex flex-col">
+                    <span className="font-bold">Contact Email</span>
+                    <span>{asset?.support_email || "--"}</span>
+                  </Typography>
+                  <Typography className="flex flex-col">
+                    <span className="font-bold">Status</span>
+                    <span>{status(asset?.status)}</span>
+                  </Typography>
+                  <Typography className="flex flex-col">
+                    <span className="font-bold">Working status</span>
+                    <span>{working_status(asset?.is_working)}</span>
+                  </Typography>
+                  {!asset?.is_working && (
+                    <Typography className="flex flex-col">
+                      <span className="font-bold">Not working reason</span>
+                      <span>{asset?.not_working_reason || "--"}</span>
+                    </Typography>
+                  )}
+                </div>
+              </div>
+              <div className="flex mt-2 flex-col gap-1">
+                <div className="mb-3 flex justify-center">
+                  <QRCode
+                    bgColor="#FFFFFF"
+                    fgColor="#000000"
+                    level="Q"
+                    size={128}
+                    value={asset?.id || ""}
+                  />
+                </div>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => setIsPrintMode(true)}
+                >
+                  Print QR
+                </button>
+                <button
+                  onClick={() =>
+                    navigate(
+                      `/facility/${asset?.location_object.facility.id}/assets/${asset?.id}`
+                    )
+                  }
+                  id="update-asset"
+                  className="btn-primary btn"
+                >
+                  <i className="fas fa-pencil-alt text-white mr-2"></i>
+                  Update Asset
+                </button>
+                <button
+                  onClick={() => navigate(`/assets/${asset?.id}/configure`)}
+                  id="update-asset"
+                  className="btn-primary btn"
+                >
+                  <i className="fas fa-cog text-white mr-2"></i>
+                  Configure Asset
+                </button>
+              </div>
             </div>
           </div>
-          <div className="flex mt-2 flex-col gap-1">
-            <div className="mb-3 flex justify-center">
-              <QRCode
-                bgColor="#FFFFFF"
-                fgColor="#000000"
-                level="Q"
-                size={128}
-                value={asset?.id || ""}
-              />
+          <div className="bg-white rounded-lg md:p-6 p-3 shadow mt-2">
+            <div className="text-xl font-semibold">Transaction History</div>
+            <div className="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
+              <table className="min-w-full divide-y divide-cool-gray-200">
+                <thead>
+                  <tr>
+                    <th className="px-6 py-3 bg-cool-gray-50 text-left text-xs leading-4 font-medium text-cool-gray-500 uppercase tracking-wider">
+                      Moved from
+                    </th>
+                    <th className="px-6 py-3 bg-cool-gray-50 text-left text-xs leading-4 font-medium text-cool-gray-500 uppercase tracking-wider">
+                      Moved to
+                    </th>
+                    <th className="px-6 py-3 bg-cool-gray-50 text-left text-xs leading-4 font-medium text-cool-gray-500 uppercase tracking-wider">
+                      Moved By
+                    </th>
+                    <th className="px-6 py-3 bg-cool-gray-50 text-left text-xs leading-4 font-medium text-cool-gray-500 uppercase tracking-wider">
+                      Moved On
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-cool-gray-200">
+                  {transactionDetails}
+                </tbody>
+              </table>
             </div>
-            <button
-              className="btn btn-primary"
-              onClick={() => setIsPrintMode(true)}
-            >
-              Print QR
-            </button>
-            <button
-              onClick={() =>
-                navigate(
-                  `/facility/${asset?.location_object.facility.id}/assets/${asset?.id}`
-                )
-              }
-              id="update-asset"
-              className="btn-primary btn"
-            >
-              <i className="fas fa-pencil-alt text-white mr-2"></i>
-              Update Asset
-            </button>
-            <button
-              onClick={() => navigate(`/assets/${asset?.id}/configure`)}
-              id="update-asset"
-              className="btn-primary btn"
-            >
-              <i className="fas fa-cog text-white mr-2"></i>
-              Configure Asset
-            </button>
+            {totalCount > limit && (
+              <div className="mt-4 flex w-full justify-center">
+                <Pagination
+                  cPage={currentPage}
+                  defaultPerPage={limit}
+                  data={{ totalCount }}
+                  onChange={handlePagination}
+                />
+              </div>
+            )}
           </div>
+        </>
+      ) : (
+        <div className="grid place-items-center mt-2">
+          <img src="/images/errorhappened.png" alt="Error Happened" />
+          <h1 className="text-xl text-center mt-2">
+            The QR Code scanned was invalid. Please try again with a valid QR
+            code.
+          </h1>
+          <button
+            onClick={() => {
+              navigate("/assets");
+            }}
+            className="btn-primary btn md:mt-7 md:w-fit mt-2"
+          >
+            Go back
+          </button>
         </div>
-      </div>
-      <div className="bg-white rounded-lg md:p-6 p-3 shadow mt-2">
-        <div className="text-xl font-semibold">Transaction History</div>
-        <div className="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
-          <table className="min-w-full divide-y divide-cool-gray-200">
-            <thead>
-              <tr>
-                <th className="px-6 py-3 bg-cool-gray-50 text-left text-xs leading-4 font-medium text-cool-gray-500 uppercase tracking-wider">
-                  Moved from
-                </th>
-                <th className="px-6 py-3 bg-cool-gray-50 text-left text-xs leading-4 font-medium text-cool-gray-500 uppercase tracking-wider">
-                  Moved to
-                </th>
-                <th className="px-6 py-3 bg-cool-gray-50 text-left text-xs leading-4 font-medium text-cool-gray-500 uppercase tracking-wider">
-                  Moved By
-                </th>
-                <th className="px-6 py-3 bg-cool-gray-50 text-left text-xs leading-4 font-medium text-cool-gray-500 uppercase tracking-wider">
-                  Moved On
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-cool-gray-200">
-              {transactionDetails}
-            </tbody>
-          </table>
-        </div>
-        {totalCount > limit && (
-          <div className="mt-4 flex w-full justify-center">
-            <Pagination
-              cPage={currentPage}
-              defaultPerPage={limit}
-              data={{ totalCount }}
-              onChange={handlePagination}
-            />
-          </div>
-        )}
-      </div>
+      )}
     </div>
   );
 };
