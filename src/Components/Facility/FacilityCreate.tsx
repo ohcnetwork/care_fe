9fab8b1aae8a0e5ac83142afa4af8537f8477ca7
Rithvik Nishad <rithvikn2001@gmail.com>
2022-05-19 18:53:26 +0530

    rm unused import

--- src/Components/Facility/FacilityCreate.tsx
+++ src/Components/Facility/FacilityCreate.tsx
@@ -13,7 +13,6 @@ import {
 import Popover from "@material-ui/core/Popover";
 import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
 import MyLocationIcon from "@material-ui/icons/MyLocation";
-import { makeStyles } from "@material-ui/styles";
 import { navigate } from "raviger";
 import loadable from "@loadable/component";
 import { parsePhoneNumberFromString } from "libphonenumber-js";
