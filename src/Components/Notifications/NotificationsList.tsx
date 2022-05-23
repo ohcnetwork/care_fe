7ea3eb3207361b46f3bcb759e62ad47f8fe87647
Khavin Shankar <khavinshankar@gmail.com>
2022-05-23 08:44:14 +0530

    ui (NotificationsList): shows read notification in a dim color

--- src/Components/Notifications/NotificationsList.tsx
+++ src/Components/Notifications/NotificationsList.tsx
@@ -1,5 +1,4 @@
-import loadable from "@loadable/component";
-import { navigate, useQueryParams } from "raviger";
+import { navigate } from "raviger";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import {
@@ -8,10 +7,8 @@ import {
   updateUserPnconfig,
   getPublicKey,
 } from "../../Redux/actions";
-import Pagination from "../Common/Pagination";
 import { make as SlideOver } from "../Common/SlideOver.gen";
 import { SelectField } from "../Common/HelperInputFields";
-import { InputLabel } from "@material-ui/core";
 import moment from "moment";
 import { useSelector } from "react-redux";
 import { Button, CircularProgress } from "@material-ui/core";
@@ -19,14 +16,15 @@ import { NOTIFICATION_EVENTS } from "../../Common/constants";
 import { Error } from "../../Utils/Notifications.js";
 import clsx from "clsx";
 import { useTranslation } from "react-i18next";
-
-const Loading = loadable(() => import("../Common/Loading"));
-const PageTitle = loadable(() => import("../Common/PageTitle"));
+import classNames from "classnames";
 
 const RESULT_LIMIT = 14;
-const now = moment().format("DD-MM-YYYY:hh:mm:ss");
 
-export default function ResultList({ expanded = false }) {
+export default function ResultList({
+  expanded = false,
+}: {
+  expanded: boolean;
+}) {
   const rootState: any = useSelector((rootState) => rootState);
   const { currentUser } = rootState;
   const { t } = useTranslation();
@@ -93,34 +91,34 @@ export default function ResultList({ expanded = false }) {
           .then(function (subscription) {
             subscription
               ?.unsubscribe()
-              .then(async function (successful) {
+              .then(async function () {
                 const data = {
                   pf_endpoint: "",
                   pf_p256dh: "",
                   pf_auth: "",
                 };
-                const res = await dispatch(
+                await dispatch(
                   updateUserPnconfig(data, { username: username })
                 );
 
                 setIsSubscribed("NotSubscribed");
                 setIsSubscribing(false);
               })
-              .catch(function (e) {
+              .catch(function (error) {
                 Error({
-                  msg: "Unsubscribe failed.",
+                  msg: `Unsubscribe failed, ${error.message}`,
                 });
               });
           })
-          .catch(function (e) {
+          .catch(function (error) {
             Error({
-              msg: `Subscription Error`,
+              msg: `Subscription Error, ${error.message}`,
             });
           });
       })
-      .catch(function (e) {
+      .catch(function (error) {
         Error({
-          msg: `Service Worker Error`,
+          msg: `Service Worker Error, ${error.message}`,
         });
       });
   };
@@ -190,7 +188,7 @@ export default function ResultList({ expanded = false }) {
   //   updateQuery({ page, limit });
   // };
 
-  let resultUrl = (event: string, data: any) => {
+  const resultUrl = (event: string, data: any) => {
     switch (event) {
       case "PATIENT_CREATED":
         return `/facility/${data.facility}/patient/${data.patient}`;
@@ -207,25 +205,28 @@ export default function ResultList({ expanded = false }) {
       case "INVESTIGATION_SESSION_CREATED":
         return `/facility/${data.facility}/patient/${data.patient}/consultation/${data.consultation}/investigation/${data.session}`;
       case "MESSAGE":
-        return `/notice_board/`;
+        return "/notice_board/";
       default:
         return "#";
     }
   };
 
-  let getNotificationTitle = (id: string) =>
+  const getNotificationTitle = (id: string) =>
     NOTIFICATION_EVENTS.find((notification) => notification.id === id)?.text;
 
   let resultList: any[] = [];
   if (data && data.length) {
-    resultList = data.map((result: any, idx: number) => {
+    resultList = data.map((result: any) => {
       return (
         <div
           key={`usr_${result.id}`}
           onClick={() =>
             navigate(resultUrl(result.event, result.caused_objects))
           }
-          className="relative py-5 px-4 lg:px-8 hover:bg-gray-200 focus:bg-gray-200 transition ease-in-out duration-150 cursor-pointer"
+          className={classNames(
+            "relative py-5 px-4 lg:px-8 hover:bg-gray-200 focus:bg-gray-200 transition ease-in-out duration-150 cursor-pointer",
+            { "text-gray-500": result.read_at }
+          )}
         >
           <div className="text-lg font-bold">
             {getNotificationTitle(result.event)}
