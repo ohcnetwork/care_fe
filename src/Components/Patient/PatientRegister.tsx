d5f507ec4023460217a406a66f1af4918c83fe16
Gokulram <gokulram2010418@ssn.edu.in>
2022-05-25 18:57:40 +0530

    fixed eslint errors in PatientRegister.tsx

--- src/Components/Patient/PatientRegister.tsx
+++ src/Components/Patient/PatientRegister.tsx
@@ -25,11 +25,11 @@ import {
   GENDER_TYPES,
   MEDICAL_HISTORY_CHOICES,
   TEST_TYPE,
-  DESIGNATION_HEALTH_CARE_WORKER,
+  // DESIGNATION_HEALTH_CARE_WORKER,
   VACCINES,
 } from "../../Common/constants";
 import countryList from "../../Common/static/countries.json";
-import statesList from "../../Common/static/states.json";
+// import statesList from "../../Common/static/states.json";
 import { statusType, useAbortableEffect } from "../../Common/utils";
 import {
   createPatient,
@@ -65,10 +65,10 @@ const Loading = loadable(() => import("../Common/Loading"));
 const PageTitle = loadable(() => import("../Common/PageTitle"));
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const debounce = require("lodash.debounce");
-// eslint-disable-next-line
-const placesList = countryList.concat(
-  statesList.filter((i: string) => i !== "Kerala")
-);
+
+// const placesList = countryList.concat(
+//   statesList.filter((i: string) => i !== "Kerala")
+// );
 
 interface PatientRegisterProps extends PatientModel {
   facilityId: number;
@@ -103,8 +103,7 @@ const diseaseStatus = [...DISEASE_STATUS];
 const bloodGroups = [...BLOOD_GROUPS];
 
 const testType = [...TEST_TYPE];
-// eslint-disable-next-line
-const designationOfHealthWorkers = [...DESIGNATION_HEALTH_CARE_WORKER];
+// const designationOfHealthWorkers = [...DESIGNATION_HEALTH_CARE_WORKER];
 const vaccines = ["Select", ...VACCINES];
 
 const initForm: any = {
@@ -508,8 +507,8 @@ export const PatientRegister = (props: PatientRegisterProps) => {
     const errors = { ...initError };
     let invalidForm = false;
     let error_div = "";
-    // eslint-disable-next-line
-    Object.keys(state.form).forEach((field, i) => {
+
+    Object.keys(state.form).forEach((field) => {
       let phoneNumber, emergency_phone_number;
       switch (field) {
         case "address":
@@ -853,13 +852,13 @@ export const PatientRegister = (props: PatientRegisterProps) => {
       dispatch({ type: "set_form", form });
     }
   };
-  // eslint-disable-next-line
-  const handleCheckboxFieldChange = (e: any) => {
-    const form = { ...state.form };
-    const { checked, name } = e.target;
-    form[name] = checked;
-    dispatch({ type: "set_form", form });
-  };
+
+  // const handleCheckboxFieldChange = (e: any) => {
+  //   const form = { ...state.form };
+  //   const { checked, name } = e.target;
+  //   form[name] = checked;
+  //   dispatch({ type: "set_form", form });
+  // };
 
   const handleMedicalCheckboxChange = (e: any, id: number) => {
     const form = { ...state.form };
