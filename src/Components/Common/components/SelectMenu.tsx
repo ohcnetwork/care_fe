9905778ac8fc03e728883cf49c52691e31ff63dc
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-17 14:57:11 +0530

    adds select menu

---
+++ src/Components/Common/components/SelectMenu.tsx
@@ -0,0 +1,108 @@
+import { Fragment } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { Check, KeyboardArrowDown } from "@material-ui/icons";
+import classNames from "classnames";
+
+type Props<T> = {
+  options: { title: string; description: string; value: T }[];
+  onSelect: (value: T) => void;
+  selected?: T;
+  label?: string;
+};
+
+export default function SelectMenu<T>(props: Props<T>) {
+  const options = props.options.map((option) => {
+    return {
+      ...option,
+      current: option.value === props.selected,
+    };
+  });
+
+  const selected = options.find((option) => option.current) || options[0];
+
+  return (
+    <Listbox
+      value={selected}
+      onChange={(selection) => {
+        props.onSelect(selection.value);
+      }}
+    >
+      {({ open }) => (
+        <>
+          <Listbox.Label className="sr-only">{props.label}</Listbox.Label>
+          <div className="relative">
+            <div className="inline-flex shadow-sm rounded-md divide-x divide-primary-600">
+              <div className="relative z-0 inline-flex shadow-sm rounded-md divide-x divide-primary-600">
+                <div className="relative inline-flex items-center bg-primary-500 py-2 pl-3 pr-4 border border-transparent rounded-l-md shadow-sm text-white">
+                  <Check className="h-5 w-5" aria-hidden="true" />
+                  <p className="ml-2.5 text-sm font-medium">{selected.title}</p>
+                </div>
+                <Listbox.Button className="relative inline-flex items-center bg-primary-500 p-2 rounded-l-none rounded-r-md text-sm font-medium text-white hover:bg-primary-600 focus:outline-none focus:z-10 ">
+                  <span className="sr-only">Change published status</span>
+                  <KeyboardArrowDown
+                    className="h-5 w-5 text-white"
+                    aria-hidden="true"
+                  />
+                </Listbox.Button>
+              </div>
+            </div>
+
+            <Transition
+              show={open}
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="origin-top-right absolute z-10 left-0 mt-2 w-72 rounded-md shadow-lg overflow-hidden bg-white divide-y divide-gray-200 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                {options.map((option) => (
+                  <Listbox.Option
+                    key={option.title}
+                    className={({ active }) =>
+                      classNames(
+                        active ? "text-white bg-primary-500" : "text-gray-900",
+                        "cursor-default select-none relative p-4 text-sm"
+                      )
+                    }
+                    value={option}
+                  >
+                    {({ selected, active }) => (
+                      <div className="flex flex-col">
+                        <div className="flex justify-between">
+                          <p
+                            className={
+                              selected ? "font-semibold" : "font-normal"
+                            }
+                          >
+                            {option.title}
+                          </p>
+                          {selected ? (
+                            <span
+                              className={
+                                active ? "text-white" : "text-primary-500"
+                              }
+                            >
+                              <Check className="h-5 w-5" aria-hidden="true" />
+                            </span>
+                          ) : null}
+                        </div>
+                        <p
+                          className={classNames(
+                            active ? "text-primary-200" : "text-gray-500",
+                            "mt-2"
+                          )}
+                        >
+                          {option.description}
+                        </p>
+                      </div>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </>
+      )}
+    </Listbox>
+  );
+}
