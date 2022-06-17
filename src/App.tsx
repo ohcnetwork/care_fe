c300ddc3219859e926a70b64d571e5a91eaf4934
Shivank Kacker <kacker.shivank@gmail.com>
2022-06-17 10:27:40 +0530

    Removed App.css and index.scss, moved css imports to index.css (#2638)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import SessionRouter from "./Router/SessionRouter";
 import AppRouter from "./Router/AppRouter";
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentUser } from "./Redux/actions";
-import "./App.css";
 import { useAbortableEffect, statusType } from "./Common/utils";
 import axios from "axios";
 
