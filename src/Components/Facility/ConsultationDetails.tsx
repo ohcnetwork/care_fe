951e193628f82e32d448c030ab86fb42c1dd3cef
Pranshu1902 <pranshu1902@gmail.com>
2022-07-03 21:20:32 +0530

    resolved spacing

--- src/Components/Facility/ConsultationDetails.tsx
+++ src/Components/Facility/ConsultationDetails.tsx
@@ -495,8 +495,8 @@ export const ConsultationDetails = (props: any) => {
               )}
             </div>
 
-            <div className="flex px-4 flex-col md:flex-row">
-              <div className="flex w-3/4">
+            <div className="flex px-4 flex-col lg:flex-row gap-2">
+              <div className="flex flex-col w-3/4 h-full">
                 {/*consultationData.other_symptoms && (
                   <div className="capitalize">
                     <span className="font-semibold leading-relaxed">
@@ -524,7 +524,7 @@ export const ConsultationDetails = (props: any) => {
                   </div>
                 )}
               </div>
-              <div className="flex flex-col lg:flex-row gap-2 text-right">
+              <div className="flex flex-col lg:flex-row gap-2 text-right h-full">
                 <button className="btn btn-primary" onClick={handleClickOpen}>
                   <i className="fas fa-clipboard-list"></i>
                   &nbsp; Discharge Summary
