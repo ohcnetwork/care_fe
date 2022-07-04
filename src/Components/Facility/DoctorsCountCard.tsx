75ff1e3776441bd6cf966fb2a1a123def4c61364
Kunal Kumar Jha <kunatastic@gmail.com>
2022-07-04 10:27:51 +0530

    revamp: the doctor type card

--- src/Components/Facility/DoctorsCountCard.tsx
+++ src/Components/Facility/DoctorsCountCard.tsx
@@ -48,26 +48,30 @@ const DoctorsCountCard = (props: DoctorsCountProps) => {
     <div className="px-2 py-2 lg:w-1/5 w-full">
       <div className="flex flex-col items-center shadow rounded-lg p-4 h-full justify-between">
         <div className="text-bold text-3xl mt-2">{props.count}</div>
-        <div className="font-semibold text-md mt-2">{area} Doctors</div>
+        <div className="font-semibold text-md mt-2 text-center">
+          {area} Doctors
+        </div>
 
-        <RoleButton
-          className="btn btn-default w-full my-2"
-          handleClickCB={() =>
-            navigate(`/facility/${props.facilityId}/doctor/${props.area}`)
-          }
-          disableFor="readOnly"
-          buttonType="html"
-        >
-          Edit
-        </RoleButton>
-        <RoleButton
-          className="btn btn-danger w-full my-2"
-          handleClickCB={() => setOpenDeleteDialog(true)}
-          disableFor="readOnly"
-          buttonType="html"
-        >
-          Delete
-        </RoleButton>
+        <div className="flex justify-evenly w-full gap-2 mt-2 pt-2 border border-t-gray-300">
+          <RoleButton
+            className="btn btn-default w-1/2"
+            handleClickCB={() =>
+              navigate(`/facility/${props.facilityId}/doctor/${props.area}`)
+            }
+            disableFor="readOnly"
+            buttonType="html"
+          >
+            Edit
+          </RoleButton>
+          <RoleButton
+            className="btn btn-danger w-1/2"
+            handleClickCB={() => setOpenDeleteDialog(true)}
+            disableFor="readOnly"
+            buttonType="html"
+          >
+            Delete
+          </RoleButton>
+        </div>
 
         <Dialog
           maxWidth={"md"}
