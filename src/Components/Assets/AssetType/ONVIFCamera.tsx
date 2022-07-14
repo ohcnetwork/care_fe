7d168cbec6708ea82eaf0c7c958b0a09e35c7e21
ARYAN PATEL <patelaryan7751@gmail.com>
2022-07-14 11:48:31 +0530

    Added svg icons in seperate file

--- src/Components/Assets/AssetType/ONVIFCamera.tsx
+++ src/Components/Assets/AssetType/ONVIFCamera.tsx
@@ -13,6 +13,7 @@ import { getCameraConfig } from "../../../Utils/transformUtils";
 import CameraConfigure from "../configure/CameraConfigure";
 import Loading from "../../Common/Loading";
 import { checkIfValidIP } from "../../../Common/validation";
+import Camerasvg from "../AssetIcons/Camerasvg";
 
 interface ONVIFCameraProps {
   assetId: string;
@@ -121,20 +122,11 @@ const ONVIFCamera = (props: ONVIFCameraProps) => {
             <CardContent>
               <div className="mt-2 grid grid-cols-1 md:grid-cols-2">
                 <div className="text-primary-500 m-auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
+                  <Camerasvg
                     className="h-64 w-64"
-                    id="Icons"
-                    version="1.1"
+                    fill="currentColor"
                     viewBox="0 0 40 40"
-                  >
-                    <circle cx="16" cy="20" r="2" />
-                    <g>
-                      <path d="M30.7,10c0.6-1.8,0.4-3.9-0.8-5.6C29.7,4.2,29.4,4,29.1,4H2.9C2.6,4,2.3,4.2,2.1,4.4C1,6.1,0.7,8.2,1.3,10H30.7z" />
-                      <path d="M3,16c0,7.2,5.8,13,13,13s13-5.8,13-13v-4H3V16z M24,14h2c0.6,0,1,0.4,1,1s-0.4,1-1,1h-2c-0.6,0-1-0.4-1-1S23.4,14,24,14z    M16,14c3.3,0,6,2.7,6,6s-2.7,6-6,6s-6-2.7-6-6S12.7,14,16,14z" />
-                    </g>
-                  </svg>
+                  />
                 </div>
 
                 <div className="mt-2 grid gap-4 grid-cols-1 lg:grid-cols-2 col-span-1">
