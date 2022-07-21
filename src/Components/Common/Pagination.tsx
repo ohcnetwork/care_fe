c579080e9418a99b590c47e0dc2b101e19f3c449
Pranshu Aggarwal <70687348+Pranshu1902@users.noreply.github.com>
2022-07-21 15:32:41 +0530

    Update src/Components/Common/Pagination.tsx
    Co-authored-by: Swapnil J <76519654+developedBySJ@users.noreply.github.com>

--- src/Components/Common/Pagination.tsx
+++ src/Components/Common/Pagination.tsx
@@ -134,7 +134,7 @@ const Pagination = (props: PaginationProps) => {
           Previous
         </button>
         <button
-          {...(currentPage === totalPage ? { disabled: true } : {})}
+          disabled={currentPage === totalPage}
           onClick={() => handleChangePage(">")}
           className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:ring-primary focus:border-primary-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
         >
