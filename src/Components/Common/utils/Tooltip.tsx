4d6b78739fe9ea9044e8222b8383218072dd0edb
Khavin Shankar <khavinshankar@gmail.com>
2022-08-02 18:30:54 +0530

    refactor (ToolTip): refactored clsx

--- src/Components/Common/utils/Tooltip.tsx
+++ src/Components/Common/utils/Tooltip.tsx
@@ -12,13 +12,14 @@ export default function ToolTip(props : {
     const [status, show] = useState(false);
 
     const style = clsx({
-        "absolute bg-black/50 backdrop-blur rounded text-white transition px-2 py-1 z-50 w-[150px] text-center block" : true,
-        "bottom-[calc(100%+5px)] left-[calc(50%-75px)]" : position === "TOP",
-        "top-[calc(100%+5px)] left-[calc(50%-75px)]" : position === "BOTTOM",
-        "right-[calc(100%+5px)] top-[calc(50%-75px)]" : position === "LEFT",
-        "left-[calc(100%+5px)] top-[calc(50%-75px)]" : position === "RIGHT",
-        "visible opacity-100 -translate-y-1" : status === true,
-        "invisible opacity-0 translate-y-0" : status === false
+        "absolute bg-black/50 backdrop-blur rounded text-white transition px-2 py-1 z-50 w-[150px] text-center block",
+        position === "TOP" && "bottom-[calc(100%+5px)] left-[calc(50%-75px)]",
+        position === "BOTTOM" && "top-[calc(100%+5px)] left-[calc(50%-75px)]",
+        position === "LEFT" && "right-[calc(100%+5px)] top-[calc(50%-75px)]",
+        position === "RIGHT" && "left-[calc(100%+5px)] top-[calc(50%-75px)]",
+        status === true 
+        ? "visible opacity-100 -translate-y-1"
+        : "invisible opacity-0 translate-y-0"
     })
     
     return (
