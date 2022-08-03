b88e19ee57da958df24a2cb930d47b7c99365b4d
Pranshu1902 <pranshu1902@gmail.com>
2022-08-03 19:10:45 +0530

    fixed ui

--- src/Components/ExternalResult/ResultList.tsx
+++ src/Components/ExternalResult/ResultList.tsx
@@ -370,7 +370,7 @@ export default function ResultList() {
             </dl>
           </div>
         </div>
-        <div className="md:mt-2">
+        <div className="mt-2">
           <div>
             <div className="text-sm font-semibold mb-2">Search by Name</div>
             <InputSearchBox
@@ -391,14 +391,14 @@ export default function ResultList() {
           </div>
         </div>
         <div className="mt-4 lg:mt-0 ml-auto flex flex-col justify-evenly gap-4">
-          <div className="flex justify-end gap-2">
-            <div
+          <div className="flex flex-col md:flex-row md:justify-end gap-2">
+            <button
               className="btn btn-primary"
               onClick={(_) => navigate("external_results/upload")}
             >
               Upload List
-            </div>
-            <div
+            </button>
+            <button
               className={clsx(
                 "btn btn-primary",
                 downloadLoading && "pointer-events-none"
@@ -413,7 +413,7 @@ export default function ResultList() {
                 )}
                 Export
               </span>
-            </div>
+            </button>
           </div>
           <div className="flex ml-auto gap-2 md:pt-0 pt-2">
             <button
