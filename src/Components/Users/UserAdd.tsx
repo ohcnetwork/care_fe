18a01a55ee6f18788ee0ffb88cfe14513bc51a5d
cp-Coder <gupta.abhiuday.109@gmail.com>
2022-08-03 21:07:13 +0530

    Passed district as prop to FacilitySelect component

--- src/Components/Users/UserAdd.tsx
+++ src/Components/Users/UserAdd.tsx
@@ -679,6 +679,7 @@ export const UserAdd = (props: UserProps) => {
                     name="facilities"
                     selected={selectedFacility}
                     setSelected={setFacility}
+                    district={currentUser.data.district}
                     errors={state.errors.facilities}
                   />
                 )}
