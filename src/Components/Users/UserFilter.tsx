d69bb30e56d38be24ee300e822b276f78c324f3e
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2022-08-06 14:12:50 +0530

    Rename filter to whatsapp

--- src/Components/Users/UserFilter.tsx
+++ src/Components/Users/UserFilter.tsx
@@ -189,7 +189,7 @@ export default function UserFilter(props: any) {
                 name="alt_phone_number"
                 value={filterState.alt_phone_number}
                 onChange={handleChange}
-                label="Alt Phone Number"
+                label="WhatsApp Phone Number"
                 className="bg-white h-10 shadow-sm md:text-sm md:leading-5 md:h-9 mr-1"
               />
             </div>
