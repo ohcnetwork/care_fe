ab1a003d87d1d784a155fd5a32508dfdfe067144
Gokulram A <gokulram2010418@ssn.edu.in>
2022-08-15 09:35:30 +0530

    Made advanced filters section on patients view responsive

--- src/Components/Patient/PatientFilterV2.tsx
+++ src/Components/Patient/PatientFilterV2.tsx
@@ -414,7 +414,7 @@ export default function PatientFilterV2(props: any) {
 
   return (
     <div>
-      <div className="flex justify-between">
+      <div className="flex justify-between flex-wrap">
         <button className="btn btn-default" onClick={closeFilter}>
           <i className="fas fa-times mr-2" />
           Cancel
@@ -429,7 +429,10 @@ export default function PatientFilterV2(props: any) {
           <i className="fas fa-times mr-2" />
           Clear Filter
         </button>
-        <button className="btn btn-primary" onClick={applyFilter}>
+        <button
+          className="btn btn-primary w-full sm:w-fit mt-2 sm:mt-0"
+          onClick={applyFilter}
+        >
           <i className="fas fa-check mr-2" />
           Apply
         </button>
