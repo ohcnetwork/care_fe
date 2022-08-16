77f0e7a3b52e7519d874e16791fafac6f5f91a23
Mohammed Rabil <rabil@techie.com>
2022-08-16 18:13:55 +0530

    Changed inventory test based on UI change

--- cypress/e2e/facility_spec/inventory.cy.ts
+++ cypress/e2e/facility_spec/inventory.cy.ts
@@ -8,17 +8,20 @@ describe("Inventory Management Section", () => {
 
   beforeEach(() => {
     cy.restoreLocalStorage();
-    cy.get("a").contains("Facility").click({ force: true });
+    cy.get("a")
+      .should("contain", "Facility")
+      .contains("Facility")
+      .click({ force: true });
     cy.contains("Inventory Management").click();
   });
 
   it("Adds Inventory", () => {
-    cy.contains("Add Inventory").click();
+    cy.contains("Manage Inventory").click();
     cy.get("[name='id']").select("PPE");
     cy.get("[name='isIncoming']").select("Add Stock");
     cy.get("[name='quantity']").type("120");
     cy.get("[name='unit']").select("Items");
-    cy.get("button").contains("Add Inventory").click();
+    cy.get("button").contains("Add/Update Inventory").click();
     cy.verifyNotification("Inventory created successfully");
   });
 
