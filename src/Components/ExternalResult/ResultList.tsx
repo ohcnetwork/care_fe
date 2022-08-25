ae267c09a08640cc74e8ee1d1fa0dfa9555b0fe1
Pranshu1902 <pranshu1902@gmail.com>
2022-08-25 12:26:11 +0530

    resolved bug

--- src/Components/ExternalResult/ResultList.tsx
+++ src/Components/ExternalResult/ResultList.tsx
@@ -498,8 +498,8 @@ export default function ResultList() {
             {manageResults}
           </tbody>
         </table>
-        <div className="bg-white divide-y divide-gray-200">{pagination}</div>
       </div>
+      <div className="bg-white divide-y divide-gray-200">{pagination}</div>
       <CSVLink
         data={downloadFile}
         filename={`external-result--${now}.csv`}
