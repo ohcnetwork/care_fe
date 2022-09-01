f20aef9a74d8949fc9394027798b185b485fef7a
Shivank Kacker <kacker.shivank@gmail.com>
2022-09-01 14:07:02 +0530

    Renamed Advice to General Instructions (#3440)
    * Renamed Advice to General Instructions
    
    * renamed in updates page

--- src/Components/Facility/ConsultationDetails.tsx
+++ src/Components/Facility/ConsultationDetails.tsx
@@ -677,7 +677,7 @@ export const ConsultationDetails = (props: any) => {
                   <div className="bg-white overflow-hidden shadow rounded-lg">
                     <div className="px-4 py-5 sm:p-6">
                       <h3 className="text-lg font-semibold leading-relaxed text-gray-900">
-                        Advice
+                        General Instructions
                       </h3>
                       <div className="mt-2">
                         <ReadMore
