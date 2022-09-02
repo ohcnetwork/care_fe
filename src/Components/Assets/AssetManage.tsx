2c405f595559eed01582537a898e8697157c577d
Rithvik Nishad <rithvikn2001@gmail.com>
2022-09-02 06:39:12 +0530

    warranty card bg to gray

--- src/Components/Assets/AssetManage.tsx
+++ src/Components/Assets/AssetManage.tsx
@@ -113,18 +113,18 @@ const AssetManage = (props: AssetManageProps) => {
     return (
       <div className="flex flex-wrap gap-8 my-3 mx-6">
         {/* Front Side */}
-        <div className="rounded-2xl shadow-xl hover:shadow-2xl bg-fuchsia-700 hover:scale-[1.01] hover:bg-fuchsia-600 text-white p-6 w-96 h-56 transition-all">
+        <div className="rounded-2xl shadow-xl hover:shadow-2xl bg-gray-700 hover:scale-[1.01] hover:bg-gray-600 text-white p-6 w-96 h-56 transition-all">
           <div className="flex justify-end px-2">
             {asset?.manufacturer ? (
               <i className="font-bold text-2xl">{asset?.manufacturer}</i>
             ) : (
-              <i className="text-2xl text-fuchsia-400">Manufacturer Unknown</i>
+              <i className="text-2xl text-gray-400">Manufacturer Unknown</i>
             )}
           </div>
           <div className="flex justify-center pt-6 flex-col">
             <span
               className={`uppercase tracking-widest font-bold text-xl ${
-                !asset?.serial_number && "text-fuchsia-400"
+                !asset?.serial_number && "text-gray-400"
               }`}
             >
               {asset?.serial_number || "--"}
@@ -135,7 +135,7 @@ const AssetManage = (props: AssetManageProps) => {
             <div className=" flex flex-col justify-start">
               <span
                 className={`uppercase tracking-widest font-bold text-xl ${
-                  !asset?.warranty_amc_end_of_validity && "text-fuchsia-400"
+                  !asset?.warranty_amc_end_of_validity && "text-gray-400"
                 }`}
               >
                 {(asset?.warranty_amc_end_of_validity &&
@@ -149,7 +149,7 @@ const AssetManage = (props: AssetManageProps) => {
             <div className=" flex flex-col items-end">
               <span
                 className={`tracking-wide font-bold text-lg ${
-                  !asset?.serial_number && "text-fuchsia-400"
+                  !asset?.serial_number && "text-gray-400"
                 }`}
               >
                 {asset?.vendor_name || "--"}
@@ -160,7 +160,7 @@ const AssetManage = (props: AssetManageProps) => {
         </div>
 
         {/* Back Side */}
-        <div className="rounded-2xl shadow-xl hover:shadow-2xl bg-fuchsia-700 hover:scale-[1.01] hover:bg-fuchsia-600 text-white p-6 w-96 h-56 transition-all">
+        <div className="rounded-2xl shadow-xl hover:shadow-2xl bg-gray-700 hover:scale-[1.01] hover:bg-gray-600 text-white p-6 w-96 h-56 transition-all">
           <div className="flex flex-col px-2 items-center">
             <span className="tracking-wide text-sm mb-6 justify-center">
               CUSTOMER SUPPORT DETAILS
@@ -175,18 +175,18 @@ const AssetManage = (props: AssetManageProps) => {
             {asset?.support_phone ? (
               <a
                 href={`tel:${asset?.support_phone}`}
-                className="group flex items-center justify-between text-white rounded hover:bg-fuchsia-500 py-2 px-3 transition-all"
+                className="group flex items-center justify-between text-white rounded hover:bg-gray-500 py-2 px-3 transition-all"
               >
-                <span className="tracking-wide font-medium text-fuchsia-50">
+                <span className="tracking-wide font-medium text-gray-50">
                   {asset?.support_phone}
                 </span>
-                <div className="ml-3 text-fuchsia-300 group-hover:text-fuchsia-100 transition-all">
+                <div className="ml-3 text-gray-300 group-hover:text-gray-100 transition-all">
                   <span className="text-sm">CALL</span>
                   <i className="fas fa-phone-alt ml-2" />
                 </div>
               </a>
             ) : (
-              <span className="tracking-wide text-sm text-fuchsia-400">
+              <span className="tracking-wide text-sm text-gray-400">
                 No Support Number Provided
               </span>
             )}
@@ -194,18 +194,18 @@ const AssetManage = (props: AssetManageProps) => {
             {asset?.support_email ? (
               <a
                 href={`mailto:${asset?.support_email}`}
-                className="group flex items-center justify-between text-white rounded hover:bg-fuchsia-500 py-2 px-3 transition-all"
+                className="group flex items-center justify-between text-white rounded hover:bg-gray-500 py-2 px-3 transition-all"
               >
-                <span className="tracking-wide font-medium text-fuchsia-50">
+                <span className="tracking-wide font-medium text-gray-50">
                   {asset?.support_email}
                 </span>
-                <div className="ml-3 text-fuchsia-300 group-hover:text-fuchsia-100 transition-all">
+                <div className="ml-3 text-gray-300 group-hover:text-gray-100 transition-all">
                   <span className="text-sm">MAIL</span>
                   <i className="fas fa-envelope ml-2" />
                 </div>
               </a>
             ) : (
-              <span className="tracking-wide text-sm text-fuchsia-400">
+              <span className="tracking-wide text-sm text-gray-400">
                 No Support Email Provided
               </span>
             )}
