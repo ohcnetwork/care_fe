6359b7609c1befc29e278be9547f39a8843a80ad
Rithvik Nishad <rithvikn2001@gmail.com>
2022-09-28 12:51:25 +0530

    Adds fields to Asset model as per asset registration v2 (#3458)
    * adds new fields
    
    * rename to customer support number in detail
    
    * refactors and updates in detail page
    
    * detail page adds description and notes
    
    * remove asset attribute `warranty_details`
    
    * new asset form
    
    * select menu not so greeny
    
    * Implements Tailwind Component: `RadioGroup`
    
    * card halfway
    
    * update asset manage page
    
    * warranty card bg to gray
    
    * remove `@material-ui/core` from asset create
    
    * make Asset Manage responsive
    
    * changes
    
    * adds color for error
    
    * Tailwind Component `TextInputFieldV2`
    
    * remove accidentally tracked `SearchSelectV2` component
    
    * fixes `btn-primary` styling and removes clsx in changed files
    
    * fix select menu overflow issue
    
    * fix htmlFor typo
    
    * stash
    
    * use Switch for Working Status
    
    * fix `SelectMenu` to be `w-full`
    
    * back to single column layout
    
    * implement sidebar
    
    * persistent sidebar
    
    * stash
    
    * make utility: `scrollTo`
    
    * use utility scrollTo
    
    * typed sections
    
    * implements utility `useVisibility`
    
    * make currentSection highlight and navigate
    
    * cleaning
    
    * update docs
    
    * revert scrollTo utility merge
    
    * center the form
    
    * make responsive

---
+++ src/Components/Common/components/Switch.tsx
@@ -0,0 +1,55 @@
+import { ErrorHelperText } from "../HelperInputFields";
+
+type SwitchProps<T> = {
+  name?: string;
+  className?: string;
+  label?: string;
+  value?: T;
+  options: T[];
+  optionLabel?: (option: T) => string;
+  optionClassName?: (option: T) => string | false;
+  required?: boolean;
+  onChange: (option: T) => void;
+  error?: string;
+};
+
+export default function SwitchV2<T>(props: SwitchProps<T>) {
+  return (
+    <div className={props.className}>
+      {props.label && (
+        <label htmlFor="is_working" className="mb-3">
+          {props.label}
+          {props.required && " *"}
+        </label>
+      )}
+      <ul role="list" className="flex">
+        {props.options.map((option, index) => {
+          const selected = option === props.value;
+          const additionalClassNames = selected
+            ? (props.optionClassName && props.optionClassName(option)) ||
+              "bg-primary-500 hover:bg-primary-600 text-white border-primary-500 focus:ring-primary-500 focus:border-primary-500"
+            : "bg-gray-50 hover:bg-gray-200 border-gray-400 focus:ring-primary-500 focus:border-primary-500";
+
+          return (
+            <li
+              tabIndex={0}
+              className={`cursor-pointer px-4 p-2 first:rounded-l-lg last:rounded-r-lg shadow-sm focus:ring-1 border transition-all duration-200 ease-in-out outline-none ${additionalClassNames}`}
+              key={index}
+              onClick={() => props.onChange(option)}
+            >
+              <span
+                className={`select-none text-sm ${
+                  selected ? "font-semibold" : "font-normal"
+                }`}
+              >
+                {(props.optionLabel && props.optionLabel(option)) ||
+                  String(option)}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+      <ErrorHelperText error={props.error} />
+    </div>
+  );
+}
