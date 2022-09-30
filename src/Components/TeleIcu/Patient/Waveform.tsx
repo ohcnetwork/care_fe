93a916572b979d9bfdf5f75e82959d5a16e441f4
Aryan Patel <patelaryan7751@gmail.com>
2022-09-30 17:40:35 +0530

    Made TeleViatlsCard responsive

--- src/Components/TeleIcu/Patient/Waveform.tsx
+++ src/Components/TeleIcu/Patient/Waveform.tsx
@@ -16,7 +16,14 @@ export type WaveformType = {
   "wave-name": string;
 };
 
-export default function Waveform(props: { wave: WaveformType, color?: string, title: string, metrics?: boolean, classes?: string, defaultSpace?: boolean }) {
+export default function Waveform(props: {
+  wave: WaveformType;
+  color?: string;
+  title: string;
+  metrics?: boolean;
+  classes?: string;
+  defaultSpace?: boolean;
+}) {
   const wave = props.wave;
   const data = wave.data.split(" ").map(Number);
   const [queueData, setQueueData] = useState<number[]>([]);
@@ -29,7 +36,7 @@ export default function Waveform(props: { wave: WaveformType, color?: string, ti
   useEffect(() => {
     setQueueData(queueData.concat(data));
     setXData(Array.from(Array(viewable).keys()));
-    
+
     let seconds = 1;
     setLastStream(0);
     let timer = setInterval(() => {
@@ -67,34 +74,29 @@ export default function Waveform(props: { wave: WaveformType, color?: string, ti
         {props.metrics && (
           <div className="flex flex-row flex-wrap justify-end gap-2 text-[10px] text-gray-400">
             <div>
-              Lowest: {Math.min(...queueData.slice(0, viewable))}
+              <div>Lowest: {Math.min(...queueData.slice(0, viewable))}</div>
+              <div>Highest: {Math.max(...queueData.slice(0, viewable))}</div>
+              <div>Stream Length: {data.length}</div>
             </div>
             <div>
-              Highest: {Math.max(...queueData.slice(0, viewable))}
+              <div>Buffer Length: {queueData.length}</div>
+              <div>Flow Rate: {Number(tpf).toFixed(2)} ms</div>
+              <div>Sampling Rate: {wave["sampling rate"]}</div>
             </div>
             <div>
-              Stream Length: {data.length}
+              Lag:{" "}
+              {Number((tpf * (queueData.length - viewable)) / 1000).toFixed(2)}{" "}
+              sec
             </div>
-            <div>
-              Buffer Length: {queueData.length}
-            </div>
-            <div>
-              Flow Rate: {Number(tpf).toFixed(2)} ms
-            </div>
-            <div>
-              Sampling Rate: {wave["sampling rate"]}
-            </div>
-            <div>
-              Lag: {Number(tpf * (queueData.length - viewable) / 1000).toFixed(2)} sec
-            </div>
-            <div>
-              Last response: {lastStream} sec ago
-            </div>
-            {queueData.length > viewable &&
-            <button className="text-blue-400" onClick={()=>setQueueData(queueData.slice((-1*viewable)))}>
-              Clear Buffer
-            </button>
-            }
+            <div>Last response: {lastStream} sec ago</div>
+            {queueData.length > viewable && (
+              <button
+                className="text-blue-400"
+                onClick={() => setQueueData(queueData.slice(-1 * viewable))}
+              >
+                Clear Buffer
+              </button>
+            )}
           </div>
         )}
       </div>
