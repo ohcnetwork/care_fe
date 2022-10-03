a5425083ffcc3cbfa5af9bc4949d53ffffa10dbb
Aryan Patel <patelaryan7751@gmail.com>
2022-10-03 23:15:01 +0530

    Added a global hook to give screen width and height at any component

---
+++ src/Common/hooks/useWindowDimensions.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+//usage of this hook
+
+/* const Component = () => {
+    const { height, width } = useWindowDimensions();
+  
+    return (
+      <div>
+        width: {width} ~ height: {height}
+      </div>
+    );
+  }
+*/
