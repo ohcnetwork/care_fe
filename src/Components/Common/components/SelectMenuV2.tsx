a3380b41a123a7c759cdb1acdc6266ca24d5e944
Rithvik Nishad <rithvikn2001@gmail.com>
2022-10-03 18:24:20 +0530

    support option value

--- src/Components/Common/components/SelectMenuV2.tsx
+++ src/Components/Common/components/SelectMenuV2.tsx
@@ -3,32 +3,32 @@ import { Listbox, Transition } from "@headlessui/react";
 
 type OptionCallback<T, R> = (option: T) => R;
 
-type SelectMenuProps<T> = {
+type SelectMenuProps<T, V = T> = {
   id?: string;
   options: T[];
   value: T | undefined;
   placeholder?: React.ReactNode;
-  optionLabel?: OptionCallback<T, React.ReactNode>;
+  optionLabel: OptionCallback<T, React.ReactNode>;
   optionSelectedLabel?: OptionCallback<T, React.ReactNode>;
   optionDescription?: OptionCallback<T, React.ReactNode>;
   optionIcon?: OptionCallback<T, React.ReactNode>;
+  optionValue?: OptionCallback<T, V>;
   className?: string;
 } & (
   | {
       required?: false;
-      onChange: OptionCallback<T | undefined, void>;
+      onChange: OptionCallback<V | undefined, void>;
     }
   | {
       required: true;
-      onChange: OptionCallback<T, void>;
+      onChange: OptionCallback<V, void>;
     }
 );
 
-const SelectMenuV2 = <T,>(props: SelectMenuProps<T>) => {
+const SelectMenuV2 = <T, V>(props: SelectMenuProps<T, V>) => {
   const valueOptions = props.options.map((option) => {
-    const label = props.optionLabel
-      ? props.optionLabel(option)
-      : (option as string);
+    const label = props.optionLabel(option);
+
     return {
       label,
       selectedLabel: props.optionSelectedLabel
@@ -36,11 +36,11 @@ const SelectMenuV2 = <T,>(props: SelectMenuProps<T>) => {
         : label,
       description: props.optionDescription && props.optionDescription(option),
       icon: props.optionIcon && props.optionIcon(option),
-      value: option,
+      value: props.optionValue ? props.optionValue(option) : option,
     };
   });
 
-  const placeholder = props.placeholder || "Select";
+  const placeholder = props.placeholder ?? "Select";
   const defaultOption = {
     label: placeholder,
     selectedLabel: <p className="font-normal text-gray-600">{placeholder}</p>,
@@ -56,93 +56,88 @@ const SelectMenuV2 = <T,>(props: SelectMenuProps<T>) => {
   return (
     <div className={props.className}>
       <Listbox
-        value={options.find((o) => props.value === o.value)}
+        value={options.find((o) => props.value == o.value) || defaultOption}
         onChange={(selection: any) => props.onChange(selection.value)}
       >
-        {({ open, value }) => {
-          console.log(value);
-          return (
-            <>
-              <Listbox.Label className="sr-only">
-                {props.placeholder}
-              </Listbox.Label>
-              <div className="relative">
-                <Listbox.Button className="w-full flex rounded bg-gray-200 ring-0 focus:ring-1 focus:ring-primary-500 focus:border-primary-500 transition-all duration-200 ease-in-out">
-                  <div className="relative z-0 flex items-center w-full">
-                    <div className="relative flex-1 flex items-center py-3 pl-3 pr-4 focus:z-10">
-                      <div className="ml-2 text-sm text-gray-700">
-                        {value.icon}
-                      </div>
-                      <p className="ml-2.5 text-sm font-medium">
-                        {value.selectedLabel}
-                      </p>
+        {({ open, value }) => (
+          <>
+            <Listbox.Label className="sr-only">
+              {props.placeholder}
+            </Listbox.Label>
+            <div className="relative">
+              <Listbox.Button className="w-full flex rounded bg-gray-200 ring-0 focus:ring-1 focus:ring-primary-500 focus:border-primary-500 transition-all duration-200 ease-in-out">
+                <div className="relative z-0 flex items-center w-full">
+                  <div className="relative flex-1 flex items-center py-3 pl-3 pr-4 focus:z-10">
+                    <div className="ml-2 text-sm text-gray-700">
+                      {value.icon}
                     </div>
-                    <i className="p-2 mr-2 text-sm fa-solid fa-chevron-down" />
+                    <p className="ml-2.5 text-sm font-medium">
+                      {value.selectedLabel}
+                    </p>
                   </div>
-                </Listbox.Button>
-                <Transition
-                  show={open}
-                  as={Fragment}
-                  leave="transition ease-in duration-200"
-                  enter="transition ease-out duration-100"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <Listbox.Options className="origin-top-right absolute z-10 mt-2 w-full rounded-md xl:rounded-lg shadow-lg overflow-auto max-h-96 bg-gray-100 divide-y divide-gray-300 ring-1 ring-gray-400 focus:outline-none">
-                    {options.map((option, index) => (
-                      <Listbox.Option
-                        id={`${props.id}-option-${option.value}`}
-                        key={index}
-                        className={({ active }) =>
-                          `cursor-default select-none relative p-4 text-sm transition-all duration-100 ease-in-out ${
-                            active
-                              ? "text-white bg-primary-500"
-                              : "text-gray-900"
-                          }`
-                        }
-                        value={option}
-                      >
-                        {({ selected, active }) => (
-                          <div className="flex flex-col">
-                            <div className="flex justify-between">
-                              <p
-                                className={
-                                  selected ? "font-semibold" : "font-normal"
-                                }
-                              >
-                                {option.label}
-                              </p>
-                              {option.icon && (
-                                <span
-                                  className={`transition-all duration-100 ease-in-out ${
-                                    active ? "text-white" : "text-primary-500"
-                                  }`}
-                                >
-                                  {option.icon}
-                                </span>
-                              )}
-                            </div>
-                            {option.description && (
-                              <p
-                                className={`mt-2 ${
-                                  active ? "text-primary-200" : "text-gray-500"
+                  <i className="p-2 mr-2 text-sm fa-solid fa-chevron-down" />
+                </div>
+              </Listbox.Button>
+              <Transition
+                show={open}
+                as={Fragment}
+                leave="transition ease-in duration-200"
+                enter="transition ease-out duration-100"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="origin-top-right absolute z-10 mt-2 w-full rounded-md xl:rounded-lg shadow-lg overflow-auto max-h-96 bg-gray-100 divide-y divide-gray-300 ring-1 ring-gray-400 focus:outline-none">
+                  {options.map((option, index) => (
+                    <Listbox.Option
+                      id={`${props.id}-option-${option.value}`}
+                      key={index}
+                      className={({ active }) =>
+                        `cursor-default select-none relative p-4 text-sm transition-all duration-100 ease-in-out ${
+                          active ? "text-white bg-primary-500" : "text-gray-900"
+                        }`
+                      }
+                      value={option}
+                    >
+                      {({ selected, active }) => (
+                        <div className="flex flex-col">
+                          <div className="flex justify-between">
+                            <p
+                              className={
+                                selected ? "font-semibold" : "font-normal"
+                              }
+                            >
+                              {option.label}
+                            </p>
+                            {option.icon && (
+                              <span
+                                className={`transition-all duration-100 ease-in-out ${
+                                  active ? "text-white" : "text-primary-500"
                                 }`}
                               >
-                                {option.description}
-                              </p>
+                                {option.icon}
+                              </span>
                             )}
                           </div>
-                        )}
-                      </Listbox.Option>
-                    ))}
-                  </Listbox.Options>
-                </Transition>
-              </div>
-            </>
-          );
-        }}
+                          {option.description && (
+                            <p
+                              className={`mt-2 ${
+                                active ? "text-primary-200" : "text-gray-500"
+                              }`}
+                            >
+                              {option.description}
+                            </p>
+                          )}
+                        </div>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </>
+        )}
       </Listbox>
     </div>
   );
