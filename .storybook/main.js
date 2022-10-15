4a381e3162705c6242505b16d5d8b57614f9dbe9
Rithvik Nishad <rithvikn2001@gmail.com>
2022-10-15 18:20:02 +0530

    storybook: add-on `storybook-dark-mode`

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "storybook-dark-mode",
   ],
   framework: "@storybook/react",
   core: {
