30dd38c62492f0828748f520b3ea90b8a5248d48
Gokulram A <gokulram2010418@ssn.edu.in>
2022-10-16 10:25:53 +0530

    minor ui fix

--- src/Components/Facility/HospitalList.tsx
+++ src/Components/Facility/HospitalList.tsx
@@ -710,32 +710,6 @@ export const HospitalList = (props: any) => {
         </div>
       </div>
 
-      <AccordionTW
-        expandIcon={
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-            />
-          </svg>
-        }
-      >
-        <AccordionSummaryTW>
-          <h3>Hello Title</h3>
-        </AccordionSummaryTW>
-        <AccordionDetailsTW>
-          <p>Hello World</p>
-        </AccordionDetailsTW>
-      </AccordionTW>
-
       <div>
         <SlideOver show={showFilters} setShow={setShowFilters}>
           <div className="bg-white min-h-screen p-4">
