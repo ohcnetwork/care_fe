1111b768bfb2064bb7bd2b41ec48b05952b6d815
Khavin Shankar <khavinshankar@gmail.com>
2022-10-17 10:06:50 +0530

    ui (AsyncAutoComplete): changed bg color

--- src/Components/Form/AutoCompleteAsync.tsx
+++ src/Components/Form/AutoCompleteAsync.tsx
@@ -106,7 +106,7 @@ const AutoCompleteAsync = (props: Props) => {
                     key={item.id}
                     className={({ active }) =>
                       `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? "bg-teal-600 text-white" : "text-gray-900"
+                        active ? "text-white bg-primary-500" : "text-gray-900"
                       }`
                     }
                     value={item}
@@ -123,7 +123,7 @@ const AutoCompleteAsync = (props: Props) => {
                         {selected ? (
                           <span
                             className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? "text-white" : "text-teal-600"
+                              active ? "text-white" : "text-primary-500"
                             }`}
                           >
                             <Check className="h-5 w-5" aria-hidden="true" />
