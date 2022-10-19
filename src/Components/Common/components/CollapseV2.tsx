d693bc12500fff88c255c456773e87fb1646ec2e
Gokulram A <gokulram2010418@ssn.edu.in>
2022-10-19 17:43:02 +0530

    patient registration fields alignment fix

--- src/Components/Common/components/CollapseV2.tsx
+++ src/Components/Common/components/CollapseV2.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function CollapseV2(props: {
   children: JSX.Element | JSX.Element[];
@@ -6,20 +6,49 @@ export default function CollapseV2(props: {
   className?: string;
 }) {
   const content = React.useRef<HTMLDivElement>(null);
+  const [innerDivState, setInnerDivState] = useState(false);
+  const [outerDivState, setOuterDivState] = useState(false);
+
+  useEffect(() => {
+    if (props.opened) {
+      setOuterDivState(props.opened);
+      setTimeout(() => {
+        setInnerDivState(props.opened);
+      }, 1);
+    } else {
+      setInnerDivState(props.opened);
+      setTimeout(() => {
+        setOuterDivState(props.opened);
+      }, 300);
+    }
+  }, [props.opened]);
 
   return (
     <div
-      className={`transition-[height] ease-in-out duration-300 overflow-hidden ${props.className}`}
-      ref={content}
+      className="transition-all ease-in-out duration-300"
       style={
-        props.opened
+        outerDivState
           ? {
-              height: content.current ? content.current.scrollHeight : "0px",
+              display: "block",
             }
-          : { height: "0px" }
+          : { display: "none" }
       }
     >
-      {props.children}
+      <div
+        className={`transition-all ease-in-out duration-300 overflow-hidden ${
+          props.className ? props.className : ""
+        }`}
+        ref={content}
+        style={
+          innerDivState
+            ? {
+                maxHeight: content.current?.scrollHeight,
+              }
+            : { maxHeight: "0px" }
+        }
+      >
+        {props.children}
+      </div>
     </div>
   );
 }
