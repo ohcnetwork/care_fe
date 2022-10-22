0cdf936050dd555bdcbf5dc47e8deecba88d3228
Rithvik Nishad <rithvikn2001@gmail.com>
2022-10-22 18:18:35 +0530

    improve `animate-ping`

--- src/Components/Patient/ManagePatients.tsx
+++ src/Components/Patient/ManagePatients.tsx
@@ -623,8 +623,8 @@ export const PatientManager = (props: any) => {
                           startIcon="notes-medical"
                           text="No Consultation Filed"
                         />
-                        <span className="flex absolute h-3 w-3 -top-1 -right-1">
-                          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                        <span className="flex absolute h-3 w-3 -top-1 -right-1 items-center justify-center">
+                          <span className="animate-ping absolute inline-flex h-4 w-4 center rounded-full bg-red-400"></span>
                           <span className="relative inline-flex rounded-full h-3 w-3 bg-red-600"></span>
                         </span>
                       </span>
