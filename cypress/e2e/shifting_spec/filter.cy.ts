f2f14ac2ead4d05583d5d9f95ecc0832cd21d43c
Mohammed Rabil <rabil@techie.com>
2022-11-01 04:52:27 +0530

    Remove commented spec tests in shifting filters

--- cypress/e2e/shifting_spec/filter.cy.ts
+++ cypress/e2e/shifting_spec/filter.cy.ts
@@ -12,94 +12,94 @@ describe("Shifting section filter", () => {
     cy.get("button").should("contain", "Filters").contains("Filters").click();
   });
 
-  // it("filter by origin facility", () => {
-  //   cy.intercept(/\/api\/v1\/getallfacilities/).as("facilities_filter");
-  //   cy.get("[name='orgin_facility']").type("harsha").wait("@facilities_filter");
-  //   cy.get("[name='orgin_facility']").type("{downarrow}{enter}");
-  //   cy.contains("Apply").click();
-  // });
+  it("filter by origin facility", () => {
+    cy.intercept(/\/api\/v1\/getallfacilities/).as("facilities_filter");
+    cy.get("[name='orgin_facility']").type("harsha").wait("@facilities_filter");
+    cy.get("[name='orgin_facility']").type("{downarrow}{enter}");
+    cy.contains("Apply").click();
+  });
 
-  // it("filter by shifting approval facility", () => {
-  //   cy.intercept(/\/api\/v1\/getallfacilities/).as("facilities_filter");
-  //   cy.get("[name='shifting_approving_facility']")
-  //     .type("test")
-  //     .wait("@facilities_filter");
-  //   cy.get("[name='shifting_approving_facility']").type("{downarrow}{enter}");
-  //   cy.contains("Apply").click();
-  // });
+  it("filter by shifting approval facility", () => {
+    cy.intercept(/\/api\/v1\/getallfacilities/).as("facilities_filter");
+    cy.get("[name='shifting_approving_facility']")
+      .type("test")
+      .wait("@facilities_filter");
+    cy.get("[name='shifting_approving_facility']").type("{downarrow}{enter}");
+    cy.contains("Apply").click();
+  });
 
-  // it("filter by assigned facility", () => {
-  //   cy.intercept(/\/api\/v1\/getallfacilities/).as("facilities_filter");
-  //   cy.get("[name='assigned_facility']")
-  //     .type("center")
-  //     .wait("@facilities_filter");
-  //   cy.get("[name='assigned_facility']").type("{downarrow}{enter}");
-  //   cy.contains("Apply").click();
-  // });
+  it("filter by assigned facility", () => {
+    cy.intercept(/\/api\/v1\/getallfacilities/).as("facilities_filter");
+    cy.get("[name='assigned_facility']")
+      .type("center")
+      .wait("@facilities_filter");
+    cy.get("[name='assigned_facility']").type("{downarrow}{enter}");
+    cy.contains("Apply").click();
+  });
 
-  // it("filter by assigned to facility", () => {
-  //   cy.intercept(/\/api\/v1\/getallfacilities/).as("facilities_filter");
-  //   cy.get("[name='assigned_to']").type("test").wait("@facilities_filter");
-  //   cy.get("[name='assigned_to']").type("{downarrow}{enter}");
-  //   cy.contains("Apply").click();
-  // });
+  it("filter by assigned to facility", () => {
+    cy.intercept(/\/api\/v1\/getallfacilities/).as("facilities_filter");
+    cy.get("[name='assigned_to']").type("test").wait("@facilities_filter");
+    cy.get("[name='assigned_to']").type("{downarrow}{enter}");
+    cy.contains("Apply").click();
+  });
 
-  // it("filter by ordering", () => {
-  //   [
-  //     "DESC Created Date",
-  //     "ASC Modified Date",
-  //     "DESC Modified Date",
-  //     "ASC Created Date",
-  //   ].forEach((select) => {
-  //     cy.get("[name='ordering']").select(select).wait(100);
-  //     cy.intercept(/\/api\/v1\/shift/).as("shifting_filter");
-  //     cy.contains("Apply").click().wait("@shifting_filter");
-  //     cy.contains("Filters").click();
-  //   });
-  //   cy.contains("Cancel").click();
-  // });
+  it("filter by ordering", () => {
+    [
+      "DESC Created Date",
+      "ASC Modified Date",
+      "DESC Modified Date",
+      "ASC Created Date",
+    ].forEach((select) => {
+      cy.get("[name='ordering']").select(select).wait(100);
+      cy.intercept(/\/api\/v1\/shift/).as("shifting_filter");
+      cy.contains("Apply").click().wait("@shifting_filter");
+      cy.contains("Filters").click();
+    });
+    cy.contains("Cancel").click();
+  });
 
-  // it("filter by emergency case", () => {
-  //   ["yes", "no"].forEach((select) => {
-  //     cy.get("[name='emergency']").select(select).wait(100);
-  //     cy.intercept(/\/api\/v1\/shift/).as("shifting_filter");
-  //     cy.contains("Apply").click().wait("@shifting_filter");
-  //     cy.contains("Filters").click();
-  //   });
-  //   cy.contains("Cancel").click();
-  // });
+  it("filter by emergency case", () => {
+    ["yes", "no"].forEach((select) => {
+      cy.get("[name='emergency']").select(select).wait(100);
+      cy.intercept(/\/api\/v1\/shift/).as("shifting_filter");
+      cy.contains("Apply").click().wait("@shifting_filter");
+      cy.contains("Filters").click();
+    });
+    cy.contains("Cancel").click();
+  });
 
-  // it("filter by antenatal", () => {
-  //   ["yes", "no"].forEach((select) => {
-  //     cy.get("[name='is_antenatal']").select(select);
-  //     cy.intercept(/\/api\/v1\/shift/).as("shifting_filter");
-  //     cy.contains("Apply").click().wait("@shifting_filter");
-  //     cy.contains("Filters").click();
-  //   });
-  //   cy.contains("Cancel").click();
-  // });
+  it("filter by antenatal", () => {
+    ["yes", "no"].forEach((select) => {
+      cy.get("[name='is_antenatal']").select(select);
+      cy.intercept(/\/api\/v1\/shift/).as("shifting_filter");
+      cy.contains("Apply").click().wait("@shifting_filter");
+      cy.contains("Filters").click();
+    });
+    cy.contains("Cancel").click();
+  });
 
-  // it("filter by upshift case", () => {
-  //   ["yes", "no"].forEach((select) => {
-  //     cy.get("[name='is_up_shift']").select(select);
-  //     cy.intercept(/\/api\/v1\/shift/).as("shifting_filter");
-  //     cy.contains("Apply").click().wait("@shifting_filter");
-  //     cy.contains("Filters").click();
-  //   });
-  //   cy.contains("Cancel").click();
-  // });
+  it("filter by upshift case", () => {
+    ["yes", "no"].forEach((select) => {
+      cy.get("[name='is_up_shift']").select(select);
+      cy.intercept(/\/api\/v1\/shift/).as("shifting_filter");
+      cy.contains("Apply").click().wait("@shifting_filter");
+      cy.contains("Filters").click();
+    });
+    cy.contains("Cancel").click();
+  });
 
-  // it("filter by disease status", () => {
-  //   ["POSITIVE", "SUSPECTED", "NEGATIVE", "RECOVERED", "EXPIRED"].forEach(
-  //     (select) => {
-  //       cy.get("[name='disease_status']").select(select);
-  //       cy.intercept(/\/api\/v1\/shift/).as("shifting_filter");
-  //       cy.contains("Apply").click().wait("@shifting_filter");
-  //       cy.contains("Filters").click();
-  //     }
-  //   );
-  //   cy.contains("Cancel").click();
-  // });
+  it("filter by disease status", () => {
+    ["POSITIVE", "SUSPECTED", "NEGATIVE", "RECOVERED", "EXPIRED"].forEach(
+      (select) => {
+        cy.get("[name='disease_status']").select(select);
+        cy.intercept(/\/api\/v1\/shift/).as("shifting_filter");
+        cy.contains("Apply").click().wait("@shifting_filter");
+        cy.contains("Filters").click();
+      }
+    );
+    cy.contains("Cancel").click();
+  });
 
   it("filter by patient phone number", () => {
     cy.contains("Cancel").click().wait(100);
