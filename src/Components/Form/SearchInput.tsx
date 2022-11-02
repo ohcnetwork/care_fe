a0075590c2d68f57422777440603908a03ce0051
Rithvik Nishad <rithvikn2001@gmail.com>
2022-11-02 13:35:38 +0530

    `SearchInput`: hotkey for clear + custom focus hotkey

--- src/Components/Form/SearchInput.tsx
+++ src/Components/Form/SearchInput.tsx
@@ -6,7 +6,16 @@ import TextFormField, { TextFormFieldProps } from "./FormFields/TextFormField";
 type SearchInputProps = TextFormFieldProps & {
   className?: string | undefined;
   debouncePeriod?: number;
-};
+} & (
+    | {
+        hotkey: string[];
+        hotkeyIcon: React.ReactNode;
+      }
+    | {
+        hotkey?: undefined;
+        hotkeyIcon?: undefined;
+      }
+  );
 
 export default function SearchInput({
   debouncePeriod = 500,
@@ -14,39 +23,47 @@ export default function SearchInput({
   ...props
 }: SearchInputProps) {
   // Debounce related
-  const [value, setValue] = useState(() => props.value);
+  const [value, setValue] = useState(() => props.value || "");
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      onChange && onChange({ name: props.name, value: value || "" });
-    }, debouncePeriod);
-
+    const timeoutId = setTimeout(
+      () => onChange && onChange({ name: props.name, value: value || "" }),
+      debouncePeriod
+    );
     return () => clearTimeout(timeoutId);
   }, [value, debouncePeriod, props.name, onChange]);
 
-  // Shortcut key related
+  // Focus hotkey related
   const ref = createRef<HTMLInputElement>();
   useKeyboardShortcut(
-    [isAppleDevice ? "Meta" : "Control", "K"],
+    props.hotkey || [isAppleDevice ? "Meta" : "Control", "K"],
     () => ref.current?.focus(),
     {
       overrideSystem: true,
     }
   );
 
-  const shortcutKey = isAppleDevice ? (
-    "⌘K"
-  ) : (
-    <div className="flex font-medium gap-1">
-      <span className="text-gray-400">Ctrl</span>
-      <span className="text-gray-500">K</span>
-    </div>
-  );
+  const shortcutKeyIcon =
+    props.hotkeyIcon ||
+    (isAppleDevice ? (
+      "⌘K"
+    ) : (
+      <div className="flex font-medium gap-1">
+        <span className="text-gray-400">Ctrl</span>
+        <span className="text-gray-500">K</span>
+      </div>
+    ));
+
+  // Escape hotkey to clear related
+  useKeyboardShortcut(["Escape"], () => setValue(""), {
+    ignoreInputFields: false,
+  });
 
   return (
     <TextFormField
       {...props}
+      type="search"
       ref={ref}
-      className={`${props.className} placeholder-shown:bg-white`}
+      className={`${props.className} enabled:bg-white`}
       leading={
         props.leading || <i className="text-gray-600 uil uil-search-alt" />
       }
@@ -54,10 +71,17 @@ export default function SearchInput({
         props.trailing || (
           <div className="hidden md:flex absolute inset-y-0 right-0 py-1.5 pr-1.5">
             <kbd className="inline-flex items-center rounded border border-gray-200 px-2 font-sans text-sm font-medium text-gray-500">
-              {shortcutKey}
+              {shortcutKeyIcon}
             </kbd>
           </div>
         )
+      }
+      trailingFocused={
+        <div className="hidden md:flex absolute inset-y-0 right-0 py-1.5 pr-1.5 gap-1">
+          <kbd className="inline-flex items-center rounded border border-gray-200 px-2 font-sans text-sm font-medium text-gray-500">
+            Esc
+          </kbd>
+        </div>
       }
       value={value}
       onChange={({ value }) => setValue(value)}
