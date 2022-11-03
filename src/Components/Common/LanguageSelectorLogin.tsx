a7ac5a0c079770b9ba0c95a3d4b2b1408a063f99
Shivank Kacker <kacker.shivank@gmail.com>
2022-11-03 21:24:26 +0530

    Login page revamp (#3853)
    * Login page revamp
    
    * fixed language selector
    
    * responsiveness and final touches
    
    * done
    
    * added forgot password
    
    * shifted coronasafe logo, changed urls
    
    * minor changes
    
    * minor changes 2
    
    * fixed bugs

---
+++ src/Components/Common/LanguageSelectorLogin.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { LANGUAGE_NAMES } from "../../Locale/config";
+import clsx from "clsx";
+
+export const LanguageSelectorLogin = () => {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    document.documentElement.setAttribute("lang", i18n.language);
+  }, [i18n]);
+
+  const handleLanguage = (value: string) => {
+    i18n.changeLanguage(value);
+    if (window && window.localStorage) {
+      localStorage.setItem("i18nextLng", value);
+      document.documentElement.setAttribute("lang", i18n.language);
+    }
+  };
+
+  return (
+    <div className="text-sm text-gray-800 flex flex-col items-center mt-8">
+      Available in:
+      <br />
+      <div className="inline-flex gap-3 flex-wrap">
+        {Object.keys(LANGUAGE_NAMES).map((e: string) => (
+          <button
+            key={e}
+            onClick={() => handleLanguage(e)}
+            className={clsx({
+              "text-primary-600 underline": i18n.language === e,
+              "text-primary-400 hover:text-primary-600": true,
+            })}
+          >
+            {LANGUAGE_NAMES[e]}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LanguageSelectorLogin;
