2ea777f1010aeff04fa00b443127a73a743a14bc
Rithvik Nishad <rithvikn2001@gmail.com>
2022-11-03 15:36:04 +0530

    Redesigned Sidebar (#3843)
    * update color pallete
    
    * adds `SidebarItem` and `ShrinkedSidebarItem`
    
    * Deprecate existing tooltip
    
    * Implement tooltip classes
    
    * Adds component: `SidebarUserCard`
    
    * Adds icons for sidebar
    
    * Adds component: `SidebarItem`
    
    * Update `NotificationList` to use `SidebarItem`
    
    * Adds components: `DesktopSidebar`, `MobileSidebar`
    
    * Replace old sidebar with new sidebar
    
    * Delete old sidebar
    
    * remove old sidebar preference trace
    
    * fix tooltip accidentally hidden
    
    * fix bad merge
    
    * fix bad merge
    
    * fix missing `postcss-loader`
    
    * sidebar overflow: scroll
    
    * show notification count
    
    * notification animation
    
    * notification when shrinked
    
    * fix tooltip not visible
    
    * use unicons instead of svgs

---
+++ src/Components/Common/Sidebar/SidebarItem.tsx
@@ -0,0 +1,75 @@
+import { Link, usePath } from "raviger";
+import { useTranslation } from "react-i18next";
+
+export type SidebarIcon = React.ReactNode;
+
+type SidebarItemProps = {
+  text: string;
+  icon: SidebarIcon;
+  external?: true | undefined;
+  badgeCount?: number | undefined;
+} & ({ to: string; do?: undefined } | { to?: undefined; do: () => void });
+
+type SidebarItemBaseProps = SidebarItemProps & { shrinked?: boolean };
+const SidebarItemBase = ({
+  shrinked,
+  external,
+  ...props
+}: SidebarItemBaseProps) => {
+  const path = usePath();
+  const { t } = useTranslation();
+  const selected = props.to && path?.includes(props.to);
+
+  return (
+    <Link
+      className={`relative w-full flex-1 min-h-[40px] md:flex-none md:h-11 text-white ${
+        selected
+          ? "font-bold bg-primary-900"
+          : "font-normal bg-primary-800 hover:bg-primary-700"
+      } tooltip transition-all duration-200 ease-in-out cursor-pointer`}
+      target={external && "_blank"}
+      rel={external && "noreferrer"}
+      href={props.to ?? ""}
+      onClick={props.do}
+    >
+      <span className={`tooltip-text tooltip-right ${!shrinked && "hidden"}`}>
+        {props.text}
+      </span>
+      <div
+        className={`flex items-center h-full ${
+          shrinked ? "justify-center" : "justify-start pl-10 pr-9"
+        } transition-all duration-200 ease-in-out`}
+      >
+        <div className="flex-none text-lg">{props.icon}</div>
+        <span
+          className={`${
+            shrinked ? "hidden" : "grow"
+          } w-full flex items-center text-sm pl-4 tracking-wide`}
+        >
+          {t(props.text)}
+        </span>
+        {external && !shrinked && <i className="uil uil-external-link-alt" />}
+      </div>
+
+      {!!props.badgeCount && (
+        <span
+          className={`absolute flex items-center justify-center text-white font-semibold bg-primary-500 ${
+            shrinked
+              ? "right-3 top-0.5 h-4 w-5 text-[9px] rounded-md"
+              : "right-9 inset-y-0 h-6 my-auto text-xs px-2 rounded-md"
+          } z-10 transition-all duration-200 ease-in-out animate-pulse`}
+        >
+          {props.badgeCount > 9 ? "9+" : props.badgeCount}
+        </span>
+      )}
+    </Link>
+  );
+};
+
+export const SidebarItem = (props: SidebarItemProps) => (
+  <SidebarItemBase {...props} />
+);
+
+export const ShrinkedSidebarItem = (props: SidebarItemProps) => (
+  <SidebarItemBase shrinked {...props} />
+);
