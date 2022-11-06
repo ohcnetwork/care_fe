6cc77e68c0bd54a52e73d01f80213926a15e481a
cp-Coder <gupta.abhiuday.109@gmail.com>
2022-11-06 12:16:58 +0530

    test(auth): updated forgot password test

--- cypress/e2e/auth_spec/forget_password.cy.ts
+++ cypress/e2e/auth_spec/forget_password.cy.ts
@@ -3,17 +3,17 @@ import { cy, describe, beforeEach, it } from "local-cypress";
 describe("Forgot Password", () => {
   beforeEach(() => {
     cy.awaitUrl("/", true);
-    cy.get("a").contains("Forgot password?").click().wait(100);
+    cy.get("button").contains("Forgot password?").click().wait(100);
   });
 
   it("Send Password Reset Link", () => {
-    cy.get("[name='username']").type("dummy_user_1");
+    cy.get("input[id='forgot_username']").type("dummy_user_1");
     cy.contains("Send Reset Link").click().wait(1000);
     cy.contains("Password Reset Email Sent").should("exist");
   });
 
   it("Go to Login page", () => {
-    cy.get("a").contains("Login").click();
-    cy.url().should("contain", "/login");
+    cy.get("button").contains("Back to login").click();
+    cy.url().should("contain", "/");
   });
 });
