b75397e22a2b5d1758a2945ef7c59fe3cb9d3283
Rithvik Nishad <rithvikn2001@gmail.com>
2022-11-07 13:53:08 +0530

    remove console.log

--- src/Common/hooks/useIsAuthorized.ts
+++ src/Common/hooks/useIsAuthorized.ts
@@ -3,6 +3,5 @@ import { AuthorizedForCB } from "../../Utils/AuthorizeFor";
 
 export const useIsAuthorized = (authorizeFor: AuthorizedForCB) => {
   const state: any = useSelector((state) => state);
-  console.log(state.currentUser.data.user_type);
   return authorizeFor(state.currentUser.data.user_type);
 };
