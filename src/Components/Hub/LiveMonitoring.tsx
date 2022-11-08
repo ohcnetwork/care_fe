8a314948cc598157a99c82b4dd31d076f7661a59
Mohammed Rabil <rabil@techie.com>
2022-11-08 19:37:48 +0530

    Add page title

--- src/Components/Hub/LiveMonitoring.tsx
+++ src/Components/Hub/LiveMonitoring.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import PageTitle from "../Common/PageTitle";
 
 const LiveMonitoring = () => {
-  return <div>LiveMonitoring</div>;
+  return (
+    <div>
+      <PageTitle
+        title="Camera Feed"
+        breadcrumbs={false}
+        hideBack={true}
+        focusOnLoad={true}
+      />
+    </div>
+  );
 };
 
 export default LiveMonitoring;
