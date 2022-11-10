df79b6b029465454c620ad908e0293873162bf54
Gigin George <mail.gigin@gmail.com>
2022-11-10 20:04:15 +0530

    Minor Refactor; Fix Linting Errors

--- src/Components/Facility/BedManagement.tsx
+++ src/Components/Facility/BedManagement.tsx
@@ -239,7 +239,6 @@ export const BedManagement = (props: BedManagementProps) => {
     <div>
       <PageTitle
         title="Bed Management"
-        hideBack={false}
         className="mx-3 md:mx-8"
         crumbsReplacements={{
           [facilityId]: { name: facilityName },
