edfcc53b7cf4b1980082653322b614986b1504cf
Rithvik Nishad <rithvikn2001@gmail.com>
2022-11-15 17:30:39 +0530

    clean up

--- src/Common/hooks/useFilters.tsx
+++ src/Common/hooks/useFilters.tsx
@@ -7,10 +7,9 @@ import { KASP_STRING } from "../constants";
 
 export type FilterState = Record<string, unknown>;
 export type FilterParamKeys = string | string[];
-
 interface FilterBadgeProps {
   name: string;
-  value?: string | undefined;
+  value?: string;
   paramKey: FilterParamKeys;
 }
 
@@ -20,42 +19,23 @@ interface FilterBadgeProps {
  */
 export default function useFilters({ limit = 14 }: { limit?: number }) {
   const hasPagination = limit > 0;
-
   const [showFilters, setShowFilters] = useState(false);
   const [qParams, setQueryParams] = useQueryParams();
 
   const updateQuery = (filter: FilterState) => {
-    setQueryParams(
-      Object.assign(
-        {},
-        qParams,
-        hasPagination ? { page: 1, limit, ...filter } : filter
-      ),
-      {
-        replace: true,
-      }
-    );
+    filter = hasPagination ? { page: 1, limit, ...filter } : filter;
+    setQueryParams(Object.assign({}, qParams, filter), { replace: true });
   };
-
   const updatePage = (page: number) => {
     if (!hasPagination) return;
     setQueryParams(Object.assign({}, qParams, { page }), { replace: true });
   };
-
-  const applyFilter = (filter: FilterState) => {
-    updateQuery(filter);
-  };
-
-  const removeFilter = (param: string) => {
-    updateQuery({ [param]: "" });
-  };
-
-  const removeFilters = (keys: string[]) => {
+  const removeFilter = (param: string) => updateQuery({ [param]: "" });
+  const removeFilters = (keys: string[]) =>
     updateQuery(keys.reduce((acc, key) => ({ ...acc, [key]: "" }), qParams));
-  };
 
   const FilterBadge = ({ name, value, paramKey }: FilterBadgeProps) => {
-    if (Array.isArray(paramKey)) {
+    if (Array.isArray(paramKey))
       return (
         <GenericFilterBadge
           key={name}
@@ -71,8 +51,6 @@ export default function useFilters({ limit = 14 }: { limit?: number }) {
           onRemove={() => removeFilters(paramKey)}
         />
       );
-    }
-
     return (
       <GenericFilterBadge
         name={name}
@@ -82,39 +60,28 @@ export default function useFilters({ limit = 14 }: { limit?: number }) {
     );
   };
 
-  const badgeConstructorUtils = {
+  const badgeUtils = {
     badge(name: string, paramKey: FilterParamKeys) {
       return { name, paramKey };
     },
-
     value(name: string, paramKey: FilterParamKeys, value: string) {
       return { name, value, paramKey };
     },
-
     phoneNumber(name = "Phone Number", paramKey = "phone_number") {
-      return {
-        name: name,
-        value: qParams[paramKey] as string,
-        paramKey: paramKey,
-      };
+      return { name, value: qParams[paramKey] as string, paramKey };
     },
-
     range(name: string, paramKey: string, minKey = "min", maxKey = "max") {
       const paramKeys = [paramKey + "_" + minKey, paramKey + "_" + maxKey];
       const values = [qParams[paramKeys[0]], qParams[paramKeys[1]]];
-
       if (values[0] === values[1])
         return [{ name, value: values[0], paramKey: paramKeys }];
-
       return [name + " " + minKey, name + " " + maxKey].map((name, i) => {
         return { name, value: values[i], paramKey: paramKeys[i] };
       });
     },
-
     dateRange(name = "Date", paramKey = "date") {
-      return badgeConstructorUtils.range(name, paramKey, "after", "before");
+      return badgeUtils.range(name, paramKey, "after", "before");
     },
-
     boolean(
       name: string,
       paramKey: string,
@@ -138,23 +105,19 @@ export default function useFilters({ limit = 14 }: { limit?: number }) {
         "";
       return { name, value, paramKey };
     },
-
     kasp(nameSuffix = "", paramKey = "is_kasp") {
       const name = nameSuffix ? KASP_STRING + " " + nameSuffix : KASP_STRING;
       const [trueValue, falseValue] = [KASP_STRING, "Non " + KASP_STRING];
-      return badgeConstructorUtils.boolean(name, paramKey, {
-        trueValue,
-        falseValue,
-      });
+      return badgeUtils.boolean(name, paramKey, { trueValue, falseValue });
     },
   };
 
   const FilterBadges = ({
     badges,
   }: {
-    badges: (utils: typeof badgeConstructorUtils) => FilterBadgeProps[];
+    badges: (utils: typeof badgeUtils) => FilterBadgeProps[];
   }) => {
-    const compiledBadges = badges(badgeConstructorUtils);
+    const compiledBadges = badges(badgeUtils);
     const { t } = useTranslation();
     return (
       <div className="flex items-center gap-2 my-2 flex-wrap w-full col-span-3">
@@ -167,11 +130,9 @@ export default function useFilters({ limit = 14 }: { limit?: number }) {
 
   const Pagination = ({ totalCount }: { totalCount: number }) => {
     if (!hasPagination) {
-      const errorMsg =
-        "Do not attempt to render Pagination component, when limit is <= 0 with the useFilters";
+      const errorMsg = "Do not render Pagination component, when limit is <= 0";
       return <span className="bg-red-500 text-white">{errorMsg}</span>;
     }
-
     return (
       <div
         className={`mt-4 flex w-full justify-center ${
@@ -190,23 +151,16 @@ export default function useFilters({ limit = 14 }: { limit?: number }) {
 
   return {
     qParams,
-
+    resultsPerPage: limit,
     /**
      * Updates the query params and resets to page 1.
      * To prevent reset to page 1, pass the `page` property along with the obj.
      */
     updateQuery,
-
-    /**
-     * Updates the query params with the specified page.
-     */
+    /** Temp. alias of `updateQuery` until the new Filters slideover. Do not use. */
+    applyFilter: updateQuery,
+    /** Updates the query params with the specified page. */
     updatePage,
-
-    /**
-     * Updates query params with the filters.
-     */
-    applyFilter,
-
     /**
      * Removes the filter from query param
      * @param param is the key of the filter to be removed.
@@ -218,23 +172,19 @@ export default function useFilters({ limit = 14 }: { limit?: number }) {
      * @param params is the list of keys to be removed.
      */
     removeFilters,
-
     FilterBadge,
     FilterBadges,
     Pagination,
-
     // TODO: update this props to be compliant with new FiltersSlideOver when #3996 is merged.
     advancedFilter: {
       show: showFilters,
       setShow: setShowFilters,
       filter: qParams,
       onChange: (filter: FilterState) => {
-        applyFilter(filter);
+        updateQuery(filter);
         setShowFilters(false);
       },
       closeFilter: () => setShowFilters(false),
     },
-
-    resultsPerPage: limit,
   };
 }
