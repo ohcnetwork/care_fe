ca01af7515509371daacd6c4ca739bc8bc16db6a
Aryan Patel <patelaryan7751@gmail.com>
2022-11-21 11:18:21 +0530

    Added a feature take coveImage photos directly from Care

---
+++ src/Components/Facility/CoverImageCam.tsx
@@ -0,0 +1,66 @@
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
+
+const CoverImageCam = forwardRef((props, ref) => {
+  const videoRef = useRef<any>(null);
+  const photoRef = useRef<any>(null);
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 300 } })
+      .then((stream) => {
+        const video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error("error:", err);
+      });
+  };
+
+  const paintToCanvas = () => {
+    const video = videoRef.current;
+    const photo = photoRef.current;
+    const ctx = photo.getContext("2d");
+
+    const width = 320;
+    const height = 240;
+    photo.width = width;
+    photo.height = height;
+
+    return setInterval(() => {
+      ctx.drawImage(video, 0, 0, width, height);
+    }, 200);
+  };
+
+  useImperativeHandle(ref, () => ({
+    getScreenshot() {
+      const photo = photoRef.current;
+      const data = photo.toDataURL("image/png");
+      console.log(data);
+      return data;
+    },
+  }));
+
+  return (
+    <div>
+      {/* <button onClick={() => takePhoto()}>Take a photo</button> */}
+      <video
+        style={{ display: "none" }}
+        onCanPlay={() => paintToCanvas()}
+        ref={videoRef}
+      />
+      <canvas ref={photoRef} />
+    </div>
+  );
+});
+
+export default CoverImageCam;
--- src/Components/Facility/CoverImageEditModal.tsx
+++ src/Components/Facility/CoverImageEditModal.tsx
@@ -1,11 +1,12 @@
 import { Modal } from "@material-ui/core";
 import axios from "axios";
-import { ChangeEventHandler, useEffect, useState } from "react";
+import { ChangeEventHandler, useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { deleteFacilityCoverImage } from "../../Redux/actions";
 import { Success } from "../../Utils/Notifications";
 import useDragAndDrop from "../../Utils/useDragAndDrop";
 import { sleep } from "../../Utils/utils";
+import CoverImageCam from "./CoverImageCam";
 import { FacilityModel } from "./models";
 
 interface Props {
@@ -27,7 +28,25 @@ const CoverImageEditModal = ({
   const [isUploading, setIsUploading] = useState(false);
   const [selectedFile, setSelectedFile] = useState<any>();
   const [preview, setPreview] = useState<string>();
+  const [isCameraOpen, setIsCameraOpen] = useState<boolean>(false);
+  const webRef = useRef<any>(null);
+  const [previewImage, setPreviewImage] = useState(null);
 
+  const captureImage = () => {
+    console.log(webRef.current.getScreenshot());
+    setPreviewImage(webRef.current.getScreenshot());
+    fetch(webRef.current.getScreenshot())
+      .then((res) => res.blob())
+      .then((blob) => {
+        console.log(blob, "jjjjj");
+        const myFile = new File([blob], "image.png", {
+          type: blob.type,
+        });
+
+        console.log(myFile);
+        setSelectedFile(myFile);
+      });
+  };
   const closeModal = () => {
     setPreview(undefined);
     setSelectedFile(undefined);
@@ -108,119 +127,197 @@ const CoverImageEditModal = ({
   return (
     <Modal open={open} onClose={closeModal}>
       <div className="h-screen w-full absolute flex items-center justify-center bg-modal">
-        <form className="m-4 bg-white rounded-xl w-11/12 max-w-3xl min-h-[24rem] max-h-screen overflow-auto flex flex-col shadow">
-          <div className="px-6 py-6 flex flex-col bg-gray-300">
-            <span className="text-xl font-medium">Edit Cover Photo</span>
-            <span className="mt-1 text-gray-700">{facility.name}</span>
-          </div>
-
-          {hasImage ? (
-            <div className="flex-1 flex m-8 rounded-lg items-center justify-center">
-              <img
-                src={imgSrc}
-                alt={facility.name}
-                className="w-full h-full object-cover"
-              />
+        {!isCameraOpen ? (
+          <form className="m-4 bg-white rounded-xl w-11/12 max-w-3xl min-h-[24rem] max-h-screen overflow-auto flex flex-col shadow">
+            <div className="px-6 py-6 flex flex-col bg-gray-300">
+              <span className="text-xl font-medium">Edit Cover Photo</span>
+              <span className="mt-1 text-gray-700">{facility.name}</span>
             </div>
-          ) : (
-            <div
-              onDragOver={dragProps.onDragOver}
-              onDragLeave={dragProps.onDragLeave}
-              onDrop={onDrop}
-              className={`px-3 py-6 flex-1 flex flex-col m-8 rounded-lg items-center justify-center border-[3px] border-dashed ${
-                dragProps.dragOver && "border-primary-500"
-              } ${
-                dragProps.fileDropError !== ""
-                  ? "border-red-500"
-                  : "border-gray-500"
-              }`}
-            >
-              <svg
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 48 48"
-                aria-hidden="true"
-                className={`w-12 h-12 stroke-[2px] ${
-                  dragProps.dragOver && "text-primary-500"
+            {hasImage ? (
+              <div className="flex-1 flex m-8 rounded-lg items-center justify-center">
+                <img
+                  src={imgSrc}
+                  alt={facility.name}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            ) : (
+              <div
+                onDragOver={dragProps.onDragOver}
+                onDragLeave={dragProps.onDragLeave}
+                onDrop={onDrop}
+                className={`px-3 py-6 flex-1 flex flex-col m-8 rounded-lg items-center justify-center border-[3px] border-dashed ${
+                  dragProps.dragOver && "border-primary-500"
                 } ${
                   dragProps.fileDropError !== ""
-                    ? "text-red-500"
-                    : "text-gray-600"
+                    ? "border-red-500"
+                    : "border-gray-500"
                 }`}
               >
-                <path d="M28 8H12a4 4 0 0 0-4 4v20m32-12v8m0 0v8a4 4 0 0 1-4 4H12a4 4 0 0 1-4-4v-4m32-4-3.172-3.172a4 4 0 0 0-5.656 0L28 28M8 32l9.172-9.172a4 4 0 0 1 5.656 0L28 28m0 0 4 4m4-24h8m-4-4v8m-12 4h.02" />
-              </svg>
-              <p
-                className={`text-sm ${
-                  dragProps.dragOver && "text-primary-500"
-                } ${
-                  dragProps.fileDropError !== ""
-                    ? "text-red-500"
-                    : "text-gray-700"
-                } text-center`}
-              >
-                {dragProps.fileDropError !== ""
-                  ? dragProps.fileDropError
-                  : "Drag & drop image to upload"}
-              </p>
-              <p className="mt-4 text-gray-700 font-medium text-center">
-                No cover photo uploaded for this facility. <br />
-                Recommended aspect ratio for facility cover photo is 1:1.
-              </p>
-            </div>
-          )}
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 48 48"
+                  aria-hidden="true"
+                  className={`w-12 h-12 stroke-[2px] ${
+                    dragProps.dragOver && "text-primary-500"
+                  } ${
+                    dragProps.fileDropError !== ""
+                      ? "text-red-500"
+                      : "text-gray-600"
+                  }`}
+                >
+                  <path d="M28 8H12a4 4 0 0 0-4 4v20m32-12v8m0 0v8a4 4 0 0 1-4 4H12a4 4 0 0 1-4-4v-4m32-4-3.172-3.172a4 4 0 0 0-5.656 0L28 28M8 32l9.172-9.172a4 4 0 0 1 5.656 0L28 28m0 0 4 4m4-24h8m-4-4v8m-12 4h.02" />
+                </svg>
+                <p
+                  className={`text-sm ${
+                    dragProps.dragOver && "text-primary-500"
+                  } ${
+                    dragProps.fileDropError !== ""
+                      ? "text-red-500"
+                      : "text-gray-700"
+                  } text-center`}
+                >
+                  {dragProps.fileDropError !== ""
+                    ? dragProps.fileDropError
+                    : "Drag & drop image to upload"}
+                </p>
+                <p className="mt-4 text-gray-700 font-medium text-center">
+                  No cover photo uploaded for this facility. <br />
+                  Recommended aspect ratio for facility cover photo is 1:1.
+                </p>
+              </div>
+            )}
 
-          <div className="flex flex-col sm:flex-row p-4 gap-2">
-            <div>
-              <label className="rounded-lg bg-white py-2 px-4 text-primary-500 font-medium border border-primary-500 hover:text-primary-400 hover:border-primary-400 text-sm flex gap-1 items-center justify-center cursor-pointer transition-all">
-                <i className="fas fa-cloud-upload-alt mr-2"></i>Upload an image
-                <input
-                  title="changeFile"
-                  type="file"
-                  accept="image/*"
-                  className="hidden"
-                  onChange={onSelectFile}
-                />
-              </label>
-            </div>
-            <div className="sm:flex-1" />
-            <button
-              type="button"
-              className="rounded-lg bg-gray-100 hover:bg-gray-300 py-2 px-4 text-slate-600 hover:text-slate-800 font-medium text-sm flex gap-1 items-center justify-center  transition-all"
-              onClick={(e) => {
-                e.stopPropagation();
-                closeModal();
-                dragProps.setFileDropError("");
-              }}
-              disabled={isUploading}
-            >
-              Cancel
-            </button>
-            {facility.read_cover_image_url && (
+            <div className="flex flex-col sm:flex-row p-4 gap-2">
+              <div>
+                <label className="rounded-lg bg-white py-2 px-4 text-primary-500 font-medium border border-primary-500 hover:text-primary-400 hover:border-primary-400 text-sm flex gap-1 items-center justify-center cursor-pointer transition-all">
+                  <i className="fas fa-cloud-upload-alt mr-2"></i>Upload an
+                  image
+                  <input
+                    title="changeFile"
+                    type="file"
+                    accept="image/*"
+                    className="hidden"
+                    onChange={onSelectFile}
+                  />
+                </label>
+              </div>
+              <div className="sm:flex-1" />
               <button
                 type="button"
-                className="rounded-lg bg-danger-500 py-2 px-4 text-white font-medium text-sm flex gap-1 items-center justify-center transition-all"
-                onClick={handleDelete}
+                className="rounded-lg bg-primary-500 py-2 px-4 text-white font-medium hover:bg-primary-400 text-sm flex gap-3 items-center justify-center transition-all"
+                onClick={() => {
+                  setIsCameraOpen(true);
+                }}
+              >
+                {" "}
+                Open Camera
+              </button>
+              <button
+                type="button"
+                className="rounded-lg bg-gray-100 hover:bg-gray-300 py-2 px-4 text-slate-600 hover:text-slate-800 font-medium text-sm flex gap-1 items-center justify-center  transition-all"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  closeModal();
+                  dragProps.setFileDropError("");
+                }}
                 disabled={isUploading}
               >
-                Delete
+                Cancel
               </button>
-            )}
-            <button
-              type="button"
-              className="rounded-lg bg-primary-500 py-2 px-4 text-white font-medium hover:bg-primary-400 text-sm flex gap-3 items-center justify-center transition-all"
-              onClick={handleUpload}
-              disabled={isUploading}
-            >
-              {isUploading ? (
-                <i className="fa-solid fa-spinner animate-spin" />
-              ) : (
-                <i className="fa-solid fa-floppy-disk" />
+              {facility.read_cover_image_url && (
+                <button
+                  type="button"
+                  className="rounded-lg bg-danger-500 py-2 px-4 text-white font-medium text-sm flex gap-1 items-center justify-center transition-all"
+                  onClick={handleDelete}
+                  disabled={isUploading}
+                >
+                  Delete
+                </button>
               )}
-              <span>{isUploading ? "Uploading..." : "Save"}</span>
-            </button>
+              <button
+                type="button"
+                className="rounded-lg bg-primary-500 py-2 px-4 text-white font-medium hover:bg-primary-400 text-sm flex gap-3 items-center justify-center transition-all"
+                onClick={handleUpload}
+                disabled={isUploading}
+              >
+                {isUploading ? (
+                  <i className="fa-solid fa-spinner animate-spin" />
+                ) : (
+                  <i className="fa-solid fa-floppy-disk" />
+                )}
+                <span>{isUploading ? "Uploading..." : "Save"}</span>
+              </button>
+            </div>
+          </form>
+        ) : (
+          <div className="m-4 bg-white rounded-xl w-11/12 max-w-3xl min-h-[24rem] max-h-screen overflow-auto flex flex-col shadow">
+            <div className="px-6 py-6 flex flex-col bg-gray-300">
+              <span className="text-xl font-medium">Capture Cover Photo</span>
+              <span className="mt-1 text-gray-700">{facility.name}</span>
+            </div>
+            <div className="flex-1 flex m-8 rounded-lg items-center justify-center">
+              {!previewImage ? (
+                <>
+                  <CoverImageCam ref={webRef} />
+                </>
+              ) : (
+                <>
+                  <img src={previewImage} />
+                </>
+              )}
+            </div>
+            <div className="flex flex-col sm:flex-row p-4 gap-2">
+              <div>
+                {!previewImage ? (
+                  <>
+                    <div>
+                      <button
+                        className="btn btn-primary m-3"
+                        onClick={() => {
+                          captureImage();
+                        }}
+                      >
+                        Capture
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div>
+                      <button
+                        className="btn btn-danger m-3"
+                        onClick={() => {
+                          setPreviewImage(null);
+                        }}
+                      >
+                        Retake
+                      </button>
+                      <button
+                        onClick={handleUpload}
+                        className="btn btn-primary m-3"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </>
+                )}
+              </div>
+              <div className="sm:flex-1" />
+
+              <button
+                className="btn btn-danger m-3"
+                onClick={() => {
+                  setPreviewImage(null);
+                  setIsCameraOpen(false);
+                }}
+              >
+                Close Camera
+              </button>
+            </div>
           </div>
-        </form>
+        )}
       </div>
     </Modal>
   );
