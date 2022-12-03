5165535275d78a84b764bdf0954a71acae8f3626
Rithvik Nishad <rithvikn2001@gmail.com>
2022-12-03 21:22:35 +0530

    Removes unicons support and introduces `CareIcons` library for internal use (#4071)

--- src/Components/Assets/AssetManage.tsx
+++ src/Components/Assets/AssetManage.tsx
@@ -11,6 +11,7 @@ import QRCode from "qrcode.react";
 import AssetWarrantyCard from "./AssetWarrantyCard";
 import { formatDate } from "../../Utils/utils";
 import Chip from "../../CAREUI/display/Chip";
+import CareIcon from "../../CAREUI/icons/CareIcon";
 import ButtonV2 from "../Common/components/ButtonV2";
 const PageTitle = loadable(() => import("../Common/PageTitle"));
 const Loading = loadable(() => import("../Common/Loading"));
@@ -149,7 +150,7 @@ const AssetManage = (props: AssetManageProps) => {
       <div className="flex flex-col">
         <div className="flex flex-start items-center">
           <div className="w-8">
-            <i className={`uil uil-${item.icon} text-gray-700 text-lg`}></i>
+            <CareIcon className={`care-l-${item.icon} h-5 fill-gray-700`} />
           </div>
           <div className="text-gray-700 break-words">{item.label}</div>
         </div>
@@ -243,7 +244,7 @@ const AssetManage = (props: AssetManageProps) => {
                 }
                 id="update-asset"
               >
-                <i className="uil uil-pen text-white mr-4" />
+                <CareIcon className="care-l-pen h-5 mr-4" />
                 Update
               </ButtonV2>
               {asset?.asset_class && (
@@ -251,7 +252,7 @@ const AssetManage = (props: AssetManageProps) => {
                   onClick={() => navigate(`/assets/${asset?.id}/configure`)}
                   id="configure-asset"
                 >
-                  <i className="uil uil-setting text-white mr-4"></i>
+                  <CareIcon className="care-l-setting h-5 mr-4" />
                   Configure
                 </ButtonV2>
               )}
