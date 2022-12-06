b62ffae2de7f2196d1b618260d1a9bd22be532fa
Naman Gogia <naman114@pop-os.localdomain>
2022-12-06 16:34:09 +0530

    refactor: create separate FacilityCard component

--- src/Components/Facility/HospitalList.tsx
+++ src/Components/Facility/HospitalList.tsx
@@ -3,12 +3,7 @@ import { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { statusType, useAbortableEffect } from "../../Common/utils";
 
-import {
-  DOWNLOAD_TYPES,
-  FACILITY_FEATURE_TYPES,
-  FACILITY_TYPES,
-  KASP_STRING,
-} from "../../Common/constants";
+import { DOWNLOAD_TYPES, FACILITY_TYPES } from "../../Common/constants";
 import {
   getPermittedFacilities,
   downloadFacility,
@@ -18,11 +13,10 @@ import {
   getState,
   getDistrict,
   getLocalBody,
-  sendNotificationMessages,
 } from "../../Redux/actions";
 import loadable from "@loadable/component";
 
-import { InputLabel, TextField } from "@material-ui/core";
+import { InputLabel } from "@material-ui/core";
 import { FacilityModel } from "./models";
 import { CSVLink } from "react-csv";
 import moment from "moment";
@@ -32,14 +26,12 @@ import GetAppIcon from "@material-ui/icons/GetApp";
 import { make as SlideOver } from "../Common/SlideOver.gen";
 import FacillityFilter from "./FacilityFilter";
 import { useTranslation } from "react-i18next";
-import * as Notification from "../../Utils/Notifications.js";
-import { Modal } from "@material-ui/core";
 import SelectMenu from "../Common/components/SelectMenu";
 import AccordionV2 from "../Common/components/AccordionV2";
-import ButtonV2 from "../Common/components/ButtonV2";
 import SearchInput from "../Form/SearchInput";
-import { getFacilityFeatureIcon } from "./FacilityHome";
 import useFilters from "../../Common/hooks/useFilters";
+import { FacilityCard } from "./FacilityCard";
+
 const Loading = loadable(() => import("../Common/Loading"));
 const PageTitle = loadable(() => import("../Common/PageTitle"));
 
@@ -73,8 +65,6 @@ export const HospitalList = () => {
   const rootState: any = useSelector((rootState) => rootState);
   const { currentUser } = rootState;
   const userType = currentUser.data.user_type;
-  const [notifyMessage, setNotifyMessage] = useState("");
-  const [notifyModalFor, setNotifyModalFor] = useState(undefined);
   // state to change download button to loading while file is not ready
   const [downloadLoading, setDownloadLoading] = useState(false);
   const { t } = useTranslation();
@@ -252,39 +242,10 @@ export const HospitalList = () => {
     }
   };
 
-  const handleNotifySubmit = async (id: any) => {
-    const data = {
-      facility: id,
-      message: notifyMessage,
-    };
-    if (data.message.trim().length >= 1) {
-      const res = await dispatchAction(sendNotificationMessages(data));
-      if (res && res.status == 204) {
-        Notification.Success({
-          msg: "Facility Notified",
-        });
-        setNotifyModalFor(undefined);
-      } else {
-        Notification.Error({ msg: "Something went wrong..." });
-      }
-    } else {
-      Notification.Error({
-        msg: "Notification should contain atleast 1 character.",
-      });
-    }
-  };
-
   let facilityList: any[] = [];
   if (data && data.length) {
     facilityList = data.map((facility: any) => (
-      <FacilityCard
-        facility={facility}
-        userType={userType}
-        notifyModalFor={notifyModalFor}
-        setNotifyModalForCB={setNotifyModalFor}
-        handleNotifySubmitCB={handleNotifySubmit}
-        setNotifyMessageCB={setNotifyMessage}
-      />
+      <FacilityCard facility={facility} userType={userType} />
     ));
   }
 
@@ -495,201 +456,6 @@ export const HospitalList = () => {
       />
       <div className="mt-4 pb-4">
         <div>{manageFacilities}</div>
-      </div>
-    </div>
-  );
-};
-
-const FacilityCard = (props: {
-  facility: any;
-  userType: any;
-  notifyModalFor: number | undefined;
-  setNotifyModalForCB: (id: any) => void;
-  handleNotifySubmitCB: (id: any) => void;
-  setNotifyMessageCB: (message: string) => void;
-}) => {
-  const {
-    facility,
-    userType,
-    notifyModalFor,
-    setNotifyModalForCB,
-    handleNotifySubmitCB,
-    setNotifyMessageCB,
-  } = props;
-  const { t } = useTranslation();
-
-  return (
-    <div key={`usr_${facility.id}`} className="w-full">
-      <div className="block rounded-lg overflow-clip bg-white shadow h-full hover:border-primary-500">
-        <div className="flex h-full">
-          <div className="group md:flex hidden w-1/4 self-stretch shrink-0 bg-gray-300 items-center justify-center relative z-0">
-            {(facility.read_cover_image_url && (
-              <img
-                src={facility.read_cover_image_url}
-                alt={facility.name}
-                className="w-full h-full object-cover"
-              />
-            )) || (
-              <i className="fas fa-hospital text-4xl block text-gray-500" />
-            )}
-          </div>
-          <div className="h-full w-full grow">
-            <div className="group md:hidden flex w-full self-stretch shrink-0 bg-gray-300 items-center justify-center relative z-0">
-              {(facility.read_cover_image_url && (
-                <img
-                  src={facility.read_cover_image_url}
-                  alt={facility.name}
-                  className="w-full max-h-40 sm:max-h-52 object-cover"
-                />
-              )) || (
-                <i className="fas fa-hospital text-4xl block text-gray-500 p-10" />
-              )}
-            </div>
-
-            <div className="h-fit md:h-full flex flex-col justify-between w-full">
-              <div className="pl-4 md:pl-2 pr-4 py-2 w-full ">
-                <div className="flow-root">
-                  {facility.kasp_empanelled && (
-                    <div className="float-right mt-2 inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium leading-5 bg-yellow-100 text-yellow-800">
-                      {KASP_STRING}
-                    </div>
-                  )}
-                  <div className="float-left font-bold text-xl capitalize">
-                    {facility.name}
-                  </div>
-                </div>
-
-                <div className="flex gap-2 flex-wrap mt-2">
-                  <div className="px-2.5 py-0.5 rounded-md font-medium text-sm leading-5 bg-blue-100 text-blue-800 flex items-center">
-                    {facility.facility_type}
-                  </div>
-                  {facility.features?.map(
-                    (feature: number, i: number) =>
-                      FACILITY_FEATURE_TYPES.some((f) => f.id === feature) && (
-                        <div
-                          key={i}
-                          className="bg-primary-100 text-primary-600 px-2.5 py-0.5 rounded-md font-medium text-sm leading-5 flex gap-2 items-center"
-                          title={
-                            FACILITY_FEATURE_TYPES.filter(
-                              (f) => f.id === feature
-                            )[0]?.name
-                          }
-                        >
-                          {getFacilityFeatureIcon(feature)}
-                          {
-                            FACILITY_FEATURE_TYPES.filter(
-                              (f) => f.id === feature
-                            )[0]?.name
-                          }
-                        </div>
-                      )
-                  )}
-                </div>
-
-                <div className="mt-2 flex justify-between">
-                  <div className="flex flex-col">
-                    <div className="font-semibold">
-                      {facility.local_body_object?.name}
-                    </div>
-                  </div>
-                </div>
-                <a
-                  href={`tel:${facility.phone_number}`}
-                  className="text-sm font-medium tracking-widest"
-                >
-                  {facility.phone_number || "-"}
-                </a>
-              </div>
-              <div className="bg-gray-50 border-t px-2 md:px-6 py-3 flex-none flex justify-between w-full flex-wrap gap-2">
-                <div>
-                  {userType !== "Staff" ? (
-                    <ButtonV2
-                      className="flex gap-3 bg-white"
-                      shadow
-                      ghost
-                      onClick={() => setNotifyModalForCB(facility.id)}
-                    >
-                      <i className="far fa-comment-dots"></i>
-                      Notify
-                    </ButtonV2>
-                  ) : (
-                    <></>
-                  )}
-                  <Modal
-                    open={notifyModalFor === facility.id}
-                    onClose={() => setNotifyModalForCB(undefined)}
-                    aria-labelledby="Notify This Facility"
-                    aria-describedby="Type a message and notify this facility"
-                    className=""
-                  >
-                    <div className="h-screen w-full absolute flex items-center justify-center bg-modal">
-                      <form
-                        onSubmit={(event) => {
-                          event.preventDefault();
-                          handleNotifySubmitCB(notifyModalFor);
-                        }}
-                        className="bg-white rounded shadow p-8 m-4 max-h-full text-center flex flex-col max-w-lg w-2/3 min-w-max-content"
-                      >
-                        <div className="mb-4">
-                          <h1 className="text-2xl">Notify: {facility.name}</h1>
-                        </div>
-                        <div>
-                          <TextField
-                            id="NotifyModalMessageInput"
-                            rows={6}
-                            multiline
-                            required
-                            className="w-full border p-2 max-h-64"
-                            onChange={(e) => setNotifyMessageCB(e.target.value)}
-                            placeholder="Type your message..."
-                            variant="outlined"
-                          />
-                        </div>
-                        <div className="flex flex-col-reverse md:flex-row gap-2 mt-4 justify-end">
-                          <button
-                            type="button"
-                            className="btn-danger btn mr-2 w-full md:w-auto"
-                            onClick={() => setNotifyModalForCB(undefined)}
-                          >
-                            Cancel
-                          </button>
-                          <button
-                            type="submit"
-                            className="btn-primary btn mr-2 w-full md:w-auto"
-                          >
-                            Send Notification
-                          </button>
-                        </div>
-                      </form>
-                    </div>
-                  </Modal>
-                </div>
-                <div className="flex gap-2 ">
-                  <ButtonV2
-                    className="flex gap-3 bg-white"
-                    shadow
-                    ghost
-                    onClick={() => navigate(`/facility/${facility.id}`)}
-                  >
-                    <i className="fas fa-hospital"></i>
-                    {t("Facility")}
-                  </ButtonV2>
-                  <ButtonV2
-                    className="flex gap-3 bg-white"
-                    shadow
-                    ghost
-                    onClick={() =>
-                      navigate(`/facility/${facility.id}/patients`)
-                    }
-                  >
-                    <i className="fas fa-user-injured"></i>
-                    {t("Patients")}
-                  </ButtonV2>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
