539c72d7fabbb236c9559424740ad03bab759eb9
rithviknishad <rithvikn2001@gmail.com>
2022-12-13 15:22:58 +0530

    Update app popup

--- src/Components/Common/UpdatableApp.tsx
+++ src/Components/Common/UpdatableApp.tsx
@@ -1,4 +1,8 @@
-import { ReactNode, useEffect, useState } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { Fragment, ReactNode, useEffect, useState } from "react";
+import CareIcon from "../../CAREUI/icons/CareIcon";
+import { classNames } from "../../Utils/utils";
+import ButtonV2 from "./components/ButtonV2";
 
 const META_URL = "/build-meta.json";
 const APP_VERSION_KEY = "app-version";
@@ -25,28 +29,35 @@ const checkForUpdate = async () => {
     // Trigger an update if key: 'app-version' not present in localStorage
     // or does not match with metaVersion.
     console.info("App can be updated.");
-    return meta.version;
+    return meta.version as string;
   }
 };
 
 const UpdatableApp = ({ children }: UpdatableAppProps) => {
-  const [hasUpdate, setHasUpdate] = useState<string>();
+  const [newVersion, setNewVersion] = useState<string>();
 
   useEffect(() => {
-    checkForUpdate().then(setHasUpdate);
+    checkForUpdate().then(setNewVersion);
   }, []);
 
-  const updateApp = () => {
-    if (!hasUpdate) return;
+  const updateApp = async () => {
+    if (!newVersion) return;
 
-    window.location.reload();
-    localStorage.setItem(APP_VERSION_KEY, hasUpdate);
+    // Service worker cache should be cleared with caches.delete()
+    if ("caches" in window) {
+      caches.keys().then((names) => names.forEach(caches.delete));
+    }
+
+    setTimeout(() => {
+      window.location.reload();
+      localStorage.setItem(APP_VERSION_KEY, newVersion);
+    }, 1000);
   };
 
   return (
-    <div className="absolute">
+    <div className="relative">
       {children}
-      {hasUpdate && <UpdateAppPopup onUpdate={updateApp} />}
+      {newVersion && <UpdateAppPopup onUpdate={updateApp} />}
     </div>
   );
 };
@@ -58,5 +69,53 @@ interface UpdateAppPopupProps {
 }
 
 const UpdateAppPopup = ({ onUpdate }: UpdateAppPopupProps) => {
-  return <div onClick={onUpdate}></div>;
+  const [show, setShow] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setShow(true), 1000);
+  }, []);
+
+  const updateApp = () => {
+    setIsUpdating(true);
+    onUpdate();
+  };
+
+  return (
+    <Transition
+      show={show}
+      as={Fragment}
+      enter="ease-out duration-300"
+      enterFrom="opacity-0 scale-95 -translate-y-10"
+      enterTo="opacity-100 scale-100 translate-y-0"
+      leave="ease-in duration-200"
+      leaveFrom="opacity-100 scale-100"
+      leaveTo="opacity-0 scale-95"
+    >
+      <Popover className="z-50 fixed top-6 left-1/2 -translate-x-1/2 bg-alert-600 text-white rounded-xl py-4 px-5 shadow-2xl shadow-alert-900">
+        <div className="flex items-center gap-4">
+          <CareIcon
+            className={classNames(
+              "care-l-sync text-2xl",
+              isUpdating && "animate-spin"
+            )}
+          />
+          <span className="mr-4 flex flex-col">
+            <p className="font-semibold">Software Update</p>
+            <p className="font-medium text-sm">
+              A new version of CARE is available
+            </p>
+          </span>
+          <ButtonV2
+            disabled={isUpdating}
+            onClick={updateApp}
+            variant="alert"
+            className="bg-alert-500 enabled:hover:bg-alert-400"
+          >
+            {isUpdating ? "Updating..." : "Update"}
+          </ButtonV2>
+        </div>
+      </Popover>
+    </Transition>
+  );
 };
