f8ce3799def71c610772e9218b7ebfdfdd7b1d23
Pranshu Aggarwal <pranshu1902@gmail.com>
2022-12-13 20:12:20 +0530

    reverted to svg button

--- src/Components/Patient/FileUpload.tsx
+++ src/Components/Patient/FileUpload.tsx
@@ -1134,28 +1134,45 @@ export const FileUpload = (props: FileUploadProps) => {
                 errors={editFileNameError}
               />
             </div>
-            {btnloader ? (
-              <div className="flex justify-center mt-4">
-                <CircularProgress />
-              </div>
-            ) : (
-              <div className="flex flex-col-reverse md:flex-row gap-2 mt-4 justify-end">
-                <button
-                  type="submit"
-                  className="btn-primary btn mr-2 w-full md:w-auto"
-                  disabled={modalDetails?.name === editFileName}
+            <div className="flex flex-col-reverse md:flex-row gap-2 mt-4 justify-end">
+              <button
+                type="submit"
+                disabled={modalDetails?.name === editFileName || btnloader}
+                className="btn-primary btn mr-2 w-full md:w-auto"
+              >
+                <svg
+                  className={`animate-spin -ml-1 mr-3 h-5 w-5 text-primary ${
+                    !btnloader ? " hidden " : " "
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
                 >
-                  Proceed
-                </button>
-                <button
-                  type="button"
-                  className="btn-danger btn mr-2 w-full md:w-auto"
-                  onClick={(_) => setModalOpenForEdit(false)}
-                >
-                  Cancel
-                </button>
-              </div>
-            )}
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                Proceed
+              </button>
+              <button
+                type="button"
+                className="btn-danger btn mr-2 w-full md:w-auto"
+                disabled={btnloader}
+                onClick={(_) => setModalOpenForEdit(false)}
+              >
+                Cancel
+              </button>
+            </div>
           </form>
         </div>
       </Modal>
