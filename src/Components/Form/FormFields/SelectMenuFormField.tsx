4eb6e1aa7908bca5f25f63cc0610b2cf4b0ede58
rithviknishad <rithvikn2001@gmail.com>
2022-12-17 05:46:55 +0530

    tw-comp: `SelectMenuFormField`

---
+++ src/Components/Form/FormFields/SelectMenuFormField.tsx
@@ -0,0 +1,44 @@
+import SelectMenuV2 from "../SelectMenuV2";
+import FormField from "./FormField";
+import {
+  FormFieldBaseProps,
+  resolveFormFieldChangeEventHandler,
+} from "./Utils";
+
+type OptionCallback<T, R> = (option: T) => R;
+
+type Props<T, V = T> = FormFieldBaseProps<V> & {
+  placeholder?: React.ReactNode;
+  options: T[];
+  optionLabel: OptionCallback<T, React.ReactNode>;
+  optionSelectedLabel?: OptionCallback<T, React.ReactNode>;
+  optionDescription?: OptionCallback<T, React.ReactNode>;
+  optionIcon?: OptionCallback<T, React.ReactNode>;
+  optionValue?: OptionCallback<T, V>;
+};
+
+const SelectMenuFormField = <T, V>(props: Props<T, V>) => {
+  const { name } = props;
+  const handleChange = resolveFormFieldChangeEventHandler(props);
+
+  return (
+    <FormField props={props}>
+      <SelectMenuV2
+        id={props.id}
+        options={props.options}
+        disabled={props.disabled}
+        value={props.value}
+        placeholder={props.placeholder}
+        optionLabel={props.optionLabel}
+        optionSelectedLabel={props.optionSelectedLabel}
+        optionDescription={props.optionDescription}
+        optionIcon={props.optionIcon}
+        optionValue={props.optionValue}
+        required={props.required}
+        onChange={(value: any) => handleChange({ name, value })}
+      />
+    </FormField>
+  );
+};
+
+export default SelectMenuFormField;
--- src/Components/Patient/PatientCategorySelect.tsx
+++ src/Components/Patient/PatientCategorySelect.tsx
@@ -1,40 +1,27 @@
 import { PatientCategoryID, PATIENT_CATEGORIES } from "../../Common/constants";
-import FormField from "../Form/FormFields/FormField";
-import {
-  FormFieldBaseProps,
-  resolveFormFieldChangeEventHandler,
-} from "../Form/FormFields/Utils";
-import SelectMenuV2 from "../Form/SelectMenuV2";
-
-type Props = FormFieldBaseProps<PatientCategoryID> & {
-  placeholder?: string;
-  /**
-   * Voluntarily restricting `required` prop to false, as Patient Category is a
-   * required field.
-   */
-  required: true;
-};
+import SelectMenuFormField from "../Form/FormFields/SelectMenuFormField";
+import { FormFieldBaseProps } from "../Form/FormFields/Utils";
 
 /**
  * A `FormField` component to select patient category and is always a mandatory
  * field.
  */
-export default function PatientCategorySelect(props: Props) {
-  const { name } = props;
-  const handleChange = resolveFormFieldChangeEventHandler(props);
+export default function PatientCategorySelect(
+  props: FormFieldBaseProps<PatientCategoryID>
+) {
   return (
-    <FormField props={props}>
-      <SelectMenuV2
-        id={props.id}
-        placeholder={props.placeholder}
-        disabled={props.disabled}
-        required
-        value={props.value}
-        options={PATIENT_CATEGORIES}
-        optionValue={(option) => option.id}
-        optionLabel={(option) => option.text}
-        onChange={(value: PatientCategoryID) => handleChange({ name, value })}
-      />
-    </FormField>
+    <SelectMenuFormField
+      {...props}
+      required
+      options={PATIENT_CATEGORIES}
+      optionValue={(option) => option.id}
+      optionLabel={(option) => option.text}
+      optionSelectedLabel={(option) => (
+        <span className="flex gap-3 items-center">
+          <div className={`h-2 w-2 rounded-full bg-${option.twClass}`} />
+          <p>{option.text}</p>
+        </span>
+      )}
+    />
   );
 }
