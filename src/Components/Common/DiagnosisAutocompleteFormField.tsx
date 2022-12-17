e9c563385236c7da59ed4fddcda846405fdfc618
rithviknishad <rithvikn2001@gmail.com>
2022-12-17 16:56:05 +0530

    tw-comp: `DiagnosisAutocompleteFormField`

---
+++ src/Components/Common/DiagnosisAutocompleteFormField.tsx
@@ -0,0 +1,32 @@
+import CareIcon from "../../CAREUI/icons/CareIcon";
+import { useAsyncOptions } from "../../Common/hooks/useAsyncOptions";
+import { listICD11Diagnosis } from "../../Redux/actions";
+import { ICD11DiagnosisModel } from "../Facility/models";
+import AutocompleteMultiSelectFormField from "../Form/FormFields/AutocompleteMultiselect";
+import { FormFieldBaseProps } from "../Form/FormFields/Utils";
+
+type Props =
+  | ({ multiple?: false | undefined } & FormFieldBaseProps<string>)
+  | ({ multiple: true } & FormFieldBaseProps<string[]>);
+
+export function DiagnosisSelectFormField(props: Props) {
+  const { fetchOptions, isLoading, options } =
+    useAsyncOptions<ICD11DiagnosisModel>();
+
+  if (props.multiple) {
+    return (
+      <AutocompleteMultiSelectFormField
+        {...props}
+        options={options}
+        optionLabel={(option) => option.label}
+        optionValue={(option) => option.id}
+        onQuery={(query) => fetchOptions(listICD11Diagnosis({ query }, ""))}
+        dropdownIcon={
+          isLoading && (
+            <CareIcon className="care-l-spinner animate-spin -mb-1.5" />
+          )
+        }
+      />
+    );
+  }
+}
