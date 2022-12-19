0e507d34af0155e497488cca4045dd5bdcc4a15d
rithviknishad <rithvikn2001@gmail.com>
2022-12-19 12:49:15 +0530

    upgrade Pagination component

--- src/Components/Common/Pagination.tsx
+++ src/Components/Common/Pagination.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import CareIcon from "../../CAREUI/icons/CareIcon";
 import { useAbortableEffect, statusType } from "../../Common/utils";
+import ButtonV2 from "./components/ButtonV2";
 
 interface PaginationProps {
   data: { totalCount: number };
@@ -28,11 +30,9 @@ const Pagination = (props: PaginationProps) => {
 
   const getPageNumbers = () => {
     const totalPage = Math.ceil(data.totalCount / rowsPerPage);
-    const pageNumbers = [];
+    if (totalPage === 0) return [1];
 
-    if (totalPage === 0) {
-      return [1];
-    }
+    const pageNumbers = [];
 
     if (currentPage === 1 && currentPage === totalPage) {
       pageNumbers.push(currentPage);
@@ -61,105 +61,113 @@ const Pagination = (props: PaginationProps) => {
     return pageNumbers.sort((a, b) => a - b);
   };
 
-  const handleChangePage = (action: any) => {
-    let newPage = 1;
-    const totalPage = Math.ceil(data.totalCount / rowsPerPage);
-
-    switch (action) {
-      case "<":
-        newPage = currentPage - 1;
-        break;
-
-      case ">":
-        newPage = currentPage + 1;
-        break;
-
-      case ">>>":
-        newPage = totalPage;
-        break;
-
-      default:
-        break;
-    }
-
-    setCurrentPage(newPage);
-    onChange(newPage, rowsPerPage);
-  };
-
-  const goToPage = (page: any) => {
-    setCurrentPage(page);
-    onChange(page, rowsPerPage);
-  };
-
-  const renderNavigationBtn = (label: any, disabled: any, classes: string) => {
-    return (
-      <button
-        disabled={disabled}
-        onClick={() => handleChangePage(label.toLowerCase())}
-        className={`${classes} -ml-px relative bg-white text-gray-600 hover:text-gray-800 hover:bg-gray-200 inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium focus:z-10 focus:outline-none focus:border-primary-300 focus:ring-primary transition ease-in-out duration-150  ${
-          !disabled ? "cursor-pointer" : "cursor-not-allowed"
-        }`}
-      >
-        {label}
-      </button>
-    );
-  };
-
   const totalCount = data.totalCount;
   if (!totalCount) {
     return null;
   }
   const totalPage = Math.ceil(totalCount / rowsPerPage);
   const pageNumbers = getPageNumbers();
-  const firstBtnDisable = currentPage === 1;
-  const prevBtnDisable = currentPage - 1 <= 0;
-  const nextBtnDisable = currentPage + 1 >= totalPage;
-  const lastBtnDisable = totalPage === 0 || currentPage === totalPage;
+
+  const goToPage = (page: number) => {
+    setCurrentPage(page);
+    onChange(page, rowsPerPage);
+  };
+
+  const NavButtons = {
+    First: () => (
+      <NavButton
+        tooltip="Jump to first page"
+        children={<CareIcon className="care-l-angle-double-left text-lg" />}
+        onClick={() => goToPage(1)}
+        disabled={currentPage === 1}
+      />
+    ),
+
+    Previous: () => (
+      <NavButton
+        tooltip="Previous"
+        onClick={() => goToPage(currentPage - 1)}
+        disabled={currentPage - 1 <= 0}
+        children={<CareIcon className="care-l-angle-left text-lg" />}
+      />
+    ),
+
+    Page: ({ page }: { page: number }) => (
+      <NavButton
+        onClick={() => goToPage(page)}
+        selected={currentPage === page}
+        tooltip={`Move to page ${page}`}
+      >
+        {page}
+      </NavButton>
+    ),
+
+    Next: () => (
+      <NavButton
+        tooltip="Next"
+        children={<CareIcon className="care-l-angle-right text-lg" />}
+        onClick={() => goToPage(currentPage + 1)}
+        disabled={currentPage + 1 >= totalPage}
+      />
+    ),
+
+    Last: () => (
+      <NavButton
+        tooltip="Jump to last page"
+        children={<CareIcon className="care-l-angle-double-right text-lg" />}
+        onClick={() => goToPage(totalPage)}
+        disabled={totalPage === 0 || currentPage === totalPage}
+      />
+    ),
+  };
 
   return (
-    <div className="mx-auto mb-4">
+    <div className="mx-auto my-4">
+      {/* Mobile view */}
       <div className="flex-1 flex justify-between sm:hidden">
-        <button
-          disabled={firstBtnDisable}
-          onClick={() => handleChangePage("<")}
-          className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:ring-primary focus:border-primary-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 cursor-pointer"
-        >
-          {"<"}
-        </button>
-        <button
-          disabled={lastBtnDisable}
-          onClick={() => handleChangePage(">")}
-          className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:ring-primary focus:border-primary-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 cursor-pointer"
-        >
-          {">"}
-        </button>
+        <NavButtons.First />
+        <NavButtons.Previous />
       </div>
-      <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-        <div>
-          <nav className="relative z-0 inline-flex shadow-sm">
-            {renderNavigationBtn("<<<", firstBtnDisable, "rounded-l-md")}
-            {renderNavigationBtn("<", prevBtnDisable, "")}
-            {pageNumbers.map((pageNo) => (
-              <button
-                type="button"
-                key={`page_${pageNo}`}
-                className={`-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium focus:z-10 focus:outline-none focus:border-primary-300 focus:ring-primary transition ease-in-out duration-150 ${
-                  currentPage === pageNo
-                    ? "bg-primary-500 text-white"
-                    : "bg-white text-gray-600 hover:text-gray-800 hover:bg-gray-200"
-                }`}
-                onClick={() => goToPage(pageNo)}
-              >
-                {pageNo}
-              </button>
-            ))}
-            {renderNavigationBtn(">", nextBtnDisable, "")}
-            {renderNavigationBtn(">>>", lastBtnDisable, "rounded-r-md")}
-          </nav>
-        </div>
-      </div>
+
+      {/* Desktop view */}
+      <nav className="hidden sm:flex-1 sm:items-center sm:justify-between relative sm:inline-flex rounded-lg bg-white border border-secondary-300">
+        <NavButtons.First />
+        <NavButtons.Previous />
+
+        {pageNumbers.map((page) => (
+          <NavButtons.Page key={page} page={page} />
+        ))}
+
+        <NavButtons.Next />
+        <NavButtons.Last />
+      </nav>
     </div>
   );
 };
 
 export default Pagination;
+
+interface NavButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+  tooltip: string;
+  disabled?: boolean | undefined;
+  selected?: boolean | undefined;
+}
+
+const NavButton = (props: NavButtonProps) => {
+  return (
+    <ButtonV2
+      disabled={props.disabled}
+      onClick={props.onClick}
+      ghost={!props.selected}
+      variant={props.selected === undefined ? "secondary" : "primary"}
+      className="rounded-none text-sm font-bold tooltip"
+    >
+      {props.children}
+      <span className="tooltip-text tooltip-bottom -translate-x-1/2 text-xs font-normal">
+        {props.tooltip}
+      </span>
+    </ButtonV2>
+  );
+};
