f516a24206d63f334cd7efd525f731a53aa16e81
skks1212 <kacker.shivank@gmail.com>
2022-12-28 15:45:47 +0530

    trying to make tests pass

--- src/Components/Facility/FacilityCard.tsx
+++ src/Components/Facility/FacilityCard.tsx
@@ -203,6 +203,7 @@ export const FacilityCard = (props: { facility: any; userType: any }) => {
                         border
                         ghost
                         className="h-[38px]"
+                        name="facility-details"
                       >
                         <CareIcon className="care-l-hospital text-lg" />
                         <span className="hidden md:inline">
