efea7e4b9d21ae5225616531ea090101523de6aa
Rithvik Nishad <rithvikn2001@gmail.com>
2023-01-17 14:17:13 +0530

    fix #4589 (#4597)

--- src/Components/Patient/PatientInfoCard.tsx
+++ src/Components/Patient/PatientInfoCard.tsx
@@ -231,7 +231,7 @@ export default function PatientInfoCard(props: {
                   align="start"
                   className="w-full"
                 >
-                  <CareIcon className={`care-l-${action[2]}`} />
+                  <CareIcon className={`care-l-${action[2]} text-lg`} />
                   <p className="font-semibold">{action[1]}</p>
                 </ButtonV2>
                 {action[4] && action[4][0] && (
