11a05484ecb6915b39d3f3eecd0cf17ce4dbf999
rithviknishad <rithvikn2001@gmail.com>
2023-01-21 07:09:10 +0530

    adds `app_loader_image`

--- src/Components/Common/Loading.tsx
+++ src/Components/Common/Loading.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-const img = "https://cdn.coronasafe.network/break-chain.webp";
+import useConfig from "../../Common/hooks/useConfig";
 const Loading = () => {
+  const { app_loader_image } = useConfig();
   return (
     <Grid container>
       <Grid item xs={12} sm={12} md={12} lg={12} style={{ display: "flex" }}>
         <Grid container justify="center" alignItems="center">
           <div className="App">
             <header className="App-header">
-              <img src={img} className="App-logo" alt="logo" />
+              <img src={app_loader_image} className="App-logo" alt="logo" />
             </header>
           </div>
         </Grid>
