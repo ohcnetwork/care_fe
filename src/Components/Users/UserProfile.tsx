1d24987506ca5728a7536228d9d6c18a76c4d01b
rithviknishad <rithvikn2001@gmail.com>
2023-01-27 21:42:59 +0530

    fix date time format issue

--- src/Components/Users/UserProfile.tsx
+++ src/Components/Users/UserProfile.tsx
@@ -278,8 +278,9 @@ export default function UserProfile() {
         gender: states.form.gender,
         age: states.form.age,
         doctor_qualification: states.form.doctor_qualification,
-        doctor_experience_commenced_on:
-          states.form.doctor_experience_commenced_on,
+        doctor_experience_commenced_on: moment(
+          states.form.doctor_experience_commenced_on
+        ).format("YYYY-MM-DD"),
         doctor_medical_council_registration:
           states.form.doctor_medical_council_registration,
       };
