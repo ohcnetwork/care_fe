bfac4d879ae7a27c0a4bd077a91d468dbde4fe3d
Khavin Shankar <khavinshankar@gmail.com>
2023-01-30 23:14:31 +0530

    fix (CameraConfigure): fixed bed select going under card

--- src/Components/Assets/configure/CameraConfigure.tsx
+++ src/Components/Assets/configure/CameraConfigure.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { AssetData } from "../AssetTypes";
-import { Card, CardContent } from "@material-ui/core";
 import LiveFeed from "../../Facility/Consultations/LiveFeed";
 import { BedSelect } from "../../Common/BedSelect";
 import { BedModel } from "../../Facility/models";
 import { getCameraConfig } from "../../../Utils/transformUtils";
 import { Submit } from "../../Common/components/ButtonV2";
 import TextFormField from "../../Form/FormFields/TextFormField";
+import Card from "../../../CAREUI/display/Card";
 
 interface CameraConfigureProps {
   asset: AssetData;
@@ -32,51 +32,47 @@ export default function CameraConfigure(props: CameraConfigureProps) {
 
   return (
     <div className="mb-5">
-      <Card>
+      <Card className="mt-4">
         <form onSubmit={addPreset} className="">
-          <CardContent>
-            <div className="mt-2 grid gap-4 grid-cols-1 md:grid-cols-2">
-              <div>
-                <label id="asset-type">Bed</label>
-                <BedSelect
-                  name="bed"
-                  className="overflow-y-scoll mt-2"
-                  setSelected={(selected) => setBed(selected as BedModel)}
-                  selected={bed}
-                  error=""
-                  multiple={false}
-                  location={asset?.location_object?.id}
-                  facility={asset?.location_object?.facility?.id}
-                />
-              </div>
-              <div>
-                <label id="location">Preset Name</label>
-                <TextFormField
-                  name="name"
-                  id="location"
-                  type="text"
-                  value={newPreset}
-                  className="mt-2"
-                  onChange={(e) => setNewPreset(e.value)}
-                  error=""
-                />
-              </div>
+          <div className="mt-2 grid gap-4 grid-cols-1 md:grid-cols-2">
+            <div>
+              <label id="asset-type">Bed</label>
+              <BedSelect
+                name="bed"
+                className="overflow-y-scoll mt-2 z-50"
+                setSelected={(selected) => setBed(selected as BedModel)}
+                selected={bed}
+                error=""
+                multiple={false}
+                location={asset?.location_object?.id}
+                facility={asset?.location_object?.facility?.id}
+              />
             </div>
-            <div className="flex justify-end mt-4">
-              <Submit label="Add Preset" />
+            <div>
+              <label id="location">Preset Name</label>
+              <TextFormField
+                name="name"
+                id="location"
+                type="text"
+                value={newPreset}
+                className="mt-2"
+                onChange={(e) => setNewPreset(e.value)}
+                error=""
+              />
             </div>
-          </CardContent>
+          </div>
+          <div className="flex justify-end mt-4">
+            <Submit label="Add Preset" />
+          </div>
         </form>
       </Card>
-      <Card>
-        <CardContent>
-          <LiveFeed
-            middlewareHostname={facilityMiddlewareHostname}
-            asset={getCameraConfig(asset)}
-            showRefreshButton={true}
-            refreshPresetsHash={refreshPresetsHash}
-          />
-        </CardContent>
+      <Card className="mt-4">
+        <LiveFeed
+          middlewareHostname={facilityMiddlewareHostname}
+          asset={getCameraConfig(asset)}
+          showRefreshButton={true}
+          refreshPresetsHash={refreshPresetsHash}
+        />
       </Card>
     </div>
   );
