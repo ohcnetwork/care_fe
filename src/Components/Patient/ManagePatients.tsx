5ae20f141ede601acacc44462d0c91701a63754f
rithviknishad <rithvikn2001@gmail.com>
2023-01-30 20:47:43 +0530

    fix loader issue

--- src/Components/Patient/ManagePatients.tsx
+++ src/Components/Patient/ManagePatients.tsx
@@ -73,7 +73,7 @@ const PatientCategoryDisplayText: Record<PatientCategory, string> = {
 
 export const PatientManager = () => {
   const dispatch: any = useDispatch();
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<any[]>();
   const [isLoading, setIsLoading] = useState(false);
   const [totalCount, setTotalCount] = useState(0);
   const {
