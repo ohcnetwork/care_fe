d0793ba6cc551a839ece36fcd2fb8b1ba96eeb4b
rithviknishad <rithvikn2001@gmail.com>
2023-01-30 12:32:47 +0530

    fix #4693

--- src/Components/Common/FilterButtons.tsx
+++ src/Components/Common/FilterButtons.tsx
@@ -1,3 +1,4 @@
+import CareIcon from "../../CAREUI/icons/CareIcon";
 import ButtonV2 from "./components/ButtonV2";
 type Callback = () => void;
 type Props = {
@@ -8,27 +9,15 @@ type Props = {
 
 const FilterButtons = ({ onClose, onClear, onApply }: Props) => {
   return (
-    <div className="flex flex-col md:flex-row items-center mb-4 md:max-w-sm w-full fixed -ml-4 pl-4 pr-8 z-10 -mt-8 pt-8 pb-8 md:pb-4 bg-gray-50">
-      <ButtonV2
-        variant="secondary"
-        ghost
-        onClick={onClose}
-        className="w-full flex-1"
-      >
-        <i className="text-base fa-solid fa-xmark" /> Cancel
+    <div className="flex items-center mb-4 w-full max-w-sm fixed -ml-4 pl-2 md:pl-4 pr-4 md:pr-8 z-10 -mt-8 pt-8 pb-4 bg-gray-50">
+      <ButtonV2 variant="secondary" ghost onClick={onClose}>
+        <CareIcon className="care-l-times text-lg hidden sm:block" /> Cancel
       </ButtonV2>
-      <ButtonV2
-        variant="danger"
-        ghost
-        onClick={onClear}
-        className="w-full my-2 md:my-0 flex-1 min-w-fit"
-      >
-        <i className="text-base fa-solid fa-xmark" /> Clear Filters
+      <ButtonV2 variant="danger" ghost onClick={onClear}>
+        <CareIcon className="care-l-filter-slash text-lg" /> Clear Filters
       </ButtonV2>
       <div className="flex-1" />
-      <ButtonV2 onClick={onApply} className="w-full flex-1">
-        Apply
-      </ButtonV2>
+      <ButtonV2 onClick={onApply}>Apply</ButtonV2>
     </div>
   );
 };
