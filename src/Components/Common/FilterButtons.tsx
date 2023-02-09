9c79d63fcc3c25fe42a2118800a07a4ccf8353b5
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2023-02-09 13:59:36 +0530

    Only change button-secondary-ghost

--- src/Components/Common/FilterButtons.tsx
+++ src/Components/Common/FilterButtons.tsx
@@ -13,11 +13,11 @@ const FilterButtons = ({ onClose, onClear, onApply }: Props) => {
 
   return (
     <div className="flex items-center mb-4 w-full max-w-sm fixed -ml-4 pl-2 md:pl-4 pr-4 md:pr-8 z-10 -mt-8 pt-8 pb-4 bg-gray-50">
-      <ButtonV2 variant="secondary" onClick={onClose} className="mr-3">
+      <ButtonV2 variant="secondary" ghost onClick={onClose} className="mr-3">
         <CareIcon className="care-l-times text-lg hidden sm:block" />
         <span>{t("Cancel")}</span>
       </ButtonV2>
-      <ButtonV2 variant="danger" onClick={onClear}>
+      <ButtonV2 variant="danger" ghost onClick={onClear}>
         <CareIcon className="care-l-filter-slash text-lg" />
         <span>{t("Clear Filters")}</span>
       </ButtonV2>
