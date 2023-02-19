72f81ee4967c017d92e208c69979a452bbb5e3f5
Khavin Shankar <khavinshankar@gmail.com>
2023-02-19 14:23:14 +0530

    added logs for messages

--- src/Common/hooks/useMessageListener.ts
+++ src/Common/hooks/useMessageListener.ts
@@ -5,6 +5,7 @@ type onMessage = (data: any) => void;
 export const useMessageListener = (onMessage: onMessage) => {
   useEffect(() => {
     navigator.serviceWorker.onmessage = (e) => {
+      console.log(e.data);
       onMessage(e.data);
     };
 
