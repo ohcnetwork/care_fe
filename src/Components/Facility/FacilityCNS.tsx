1bd2f9861720bc1271758a6f696c204b9e03a777
rithviknishad <rithvikn2001@gmail.com>
2023-03-07 19:27:13 +0530

    adds link for patient

--- src/Components/Facility/FacilityCNS.tsx
+++ src/Components/Facility/FacilityCNS.tsx
@@ -1,3 +1,4 @@
+import { Link } from "raviger";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import CareIcon from "../../CAREUI/icons/CareIcon";
@@ -98,13 +99,16 @@ export default function FacilityCNS({ facilityId }: { facilityId: string }) {
         {monitors?.map(({ patient, socketUrl }) => (
           <div className="group p-2 rounded-lg bg-black">
             <div className="flex flex-wrap gap-4 text-white w-full tracking-wider p-2">
-              <span className="font-bold uppercase">{patient.name}</span>
-              <div className="flex space-x-2 divide-x divide-white">
-                <span>{patient.age}y</span>
-                <span className="font-bold">
-                  {GENDER_TYPES.find((g) => g.id === patient.gender)?.text}
-                </span>
-              </div>
+              <Link
+                href={`/facility/${facilityId}/patient/${patient.id}/consultation/${patient.last_consultation?.id}`}
+                className="font-bold uppercase"
+              >
+                {patient.name}
+              </Link>
+              <span>
+                {patient.age}y |{" "}
+                {GENDER_TYPES.find((g) => g.id === patient.gender)?.icon}
+              </span>
               <span className="flex-1 flex items-center justify-end gap-2 text-end">
                 <CareIcon className="care-l-bed text-lg" />
                 {patient.last_consultation?.current_bed?.bed_object?.name}
