868d635cd6f3155873fe824ca8f594753d11f5ae
Ayush Maheshwari <66112864+ayushjnv1@users.noreply.github.com>
2023-03-08 16:00:55 +0530

    remove MUI circularProgress & grid (#5047)
    * remove MUI circularProgress & grid
    
    * Update src/Components/Auth/Login.tsx
    
    Co-authored-by: Rithvik Nishad <rithvikn2001@gmail.com>
    
    * Update src/Components/Auth/Login.tsx
    
    Co-authored-by: Rithvik Nishad <rithvikn2001@gmail.com>
    
    ---------
    
    Co-authored-by: Rithvik Nishad <rithvikn2001@gmail.com>

--- src/Components/Auth/Login.tsx
+++ src/Components/Auth/Login.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { postForgotPassword, postLogin } from "../../Redux/actions";
-import { Grid, CircularProgress } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import ReCaptcha from "react-google-recaptcha";
 import * as Notification from "../../Utils/Notifications.js";
@@ -11,6 +10,7 @@ import LanguageSelectorLogin from "../Common/LanguageSelectorLogin";
 import CareIcon from "../../CAREUI/icons/CareIcon";
 import useConfig from "../../Common/hooks/useConfig";
 import { classNames } from "../../Utils/utils";
+import CircularProgress from "../Common/components/CircularProgress";
 import { LocalStorageKeys } from "../../Common/constants";
 
 export const Login = (props: { forgot?: boolean }) => {
@@ -307,15 +307,15 @@ export const Login = (props: { forgot?: boolean }) => {
                   size="large"
                   className="font-extrabold"
                 />
-                <Grid container justify="center">
+                <div className="justify-start">
                   {isCaptchaEnabled && (
-                    <Grid item className="px-8 py-4">
+                    <div className="px-8 py-4 grid">
                       <ReCaptcha
                         sitekey={recaptcha_site_key}
                         onChange={onCaptchaChange}
                       />
                       <span className="text-red-500">{errors.captcha}</span>
-                    </Grid>
+                    </div>
                   )}
 
                   <div className="w-full flex justify-between items-center py-4">
@@ -342,7 +342,7 @@ export const Login = (props: { forgot?: boolean }) => {
                       {t("login")}
                     </button>
                   )}
-                </Grid>
+                </div>
               </div>
             </form>
             <LanguageSelectorLogin />
@@ -391,7 +391,7 @@ export const Login = (props: { forgot?: boolean }) => {
                   size="large"
                   className="font-extrabold"
                 />
-                <Grid container justify="center">
+                <div className="justify-start">
                   {loading ? (
                     <div className="flex items-center justify-center">
                       <CircularProgress className="text-primary-500" />
@@ -404,7 +404,7 @@ export const Login = (props: { forgot?: boolean }) => {
                       {t("send_reset_link")}
                     </button>
                   )}
-                </Grid>
+                </div>
               </div>
             </form>
             <LanguageSelectorLogin />
