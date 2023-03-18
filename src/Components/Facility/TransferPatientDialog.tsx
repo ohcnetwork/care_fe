3b8eb3d5462b5ca0a1dc2ebc2878754ea6097e2d
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-03-18 16:28:12 +0530

    refactor

--- src/Components/Facility/TransferPatientDialog.tsx
+++ src/Components/Facility/TransferPatientDialog.tsx
@@ -1,30 +1,15 @@
-import {
-  // Card,
-  // CardContent,
-  // Dialog,
-  DialogActions,
-  // DialogContent,
-  // DialogTitle,
-  // InputLabel,
-} from "@material-ui/core";
 import { navigate } from "raviger";
 import moment from "moment";
-import React, { useReducer, useState } from "react";
+import { useReducer, useState } from "react";
 import { useDispatch } from "react-redux";
 import { transferPatient } from "../../Redux/actions";
 import * as Notification from "../../Utils/Notifications.js";
-// import {
-//   DateInputField,
-//   ErrorHelperText,
-//   SelectField,
-// } from "../Common/HelperInputFields";
 import { DupPatientModel } from "./models";
 import { OptionsType } from "../../Common/constants";
 import { Cancel, Submit } from "../Common/components/ButtonV2";
 import DateFormField from "../Form/FormFields/DateFormField";
 import { SelectFormField } from "../Form/FormFields/SelectFormField";
 import { FieldLabel } from "../Form/FormFields/FormField";
-// import DialogModal from "../Common/Dialog";
 
 interface Props {
   patientList: Array<DupPatientModel>;
@@ -91,13 +76,7 @@ const TransferPatientDialog = (props: Props) => {
   const handleDateChange = (date: any, field: string) => {
     if (moment(date).isValid()) {
       const form = { ...state.form };
-      console.log(moment(date.value).format("YYYY-MM-DD"));
-      console.log(date);
-      console.log(field);
-      console.log(date.value);
-      console.log(date.value.toISOString());
-      console.log(moment(date.value.toISOString()).format("YYYY-MM-DD"));
-      // form[field] = moment(date).format("YYYY-MM-DD");
+      form[field] = date.value.toISOString();
       dispatch({ type: "set_form", form });
     }
   };
@@ -161,18 +140,6 @@ const TransferPatientDialog = (props: Props) => {
 
   return (
     <div>
-      {/* <Card elevation={2} className="mb-8 rounded overflow-visible">
-      <CardContent> */}
-      {/* <h1 className="font-bold text-purple-500 text-left text-xl mb-4">
-        Patient Transfer Form
-      </h1> */}
-      {/* <DialogModal
-      title="Patient Transfer Form"
-      show={true}
-      onClose={handleCancel}
-      className="w-3/4 md:w-1/2"
-    > */}
-      {/* <DialogTitle id="test-sample-title">Patient Transfer Form</DialogTitle> */}
       <div>
         <div className="grid gap-4 grid-cols-1">
           <div>
@@ -183,17 +150,6 @@ const TransferPatientDialog = (props: Props) => {
           </div>
           <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
             <div>
-              {/* <InputLabel>Patient*</InputLabel>
-              <SelectField
-                name="patient"
-                variant="outlined"
-                margin="dense"
-                showEmpty={true}
-                value={state.form.patient}
-                options={patientOptions}
-                onChange={handleChange}
-                errors={state.errors.patient}
-              /> */}
               <FieldLabel className="text-sm">Patient</FieldLabel>
               <SelectFormField
                 id="patient"
@@ -206,20 +162,8 @@ const TransferPatientDialog = (props: Props) => {
                 onChange={handleChange}
                 error={state.errors.patient}
               />
-              {/* <ErrorHelperText error={state.errors.patient} /> */}
             </div>
             <div>
-              {/* <InputLabel>Date of birth*</InputLabel>
-              <DateInputField
-                fullWidth={true}
-                value={state.form.date_of_birth}
-                onChange={(date) => handleDateChange(Date(date), "date_of_birth")}
-                errors={state.errors.date_of_birth}
-                inputVariant="outlined"
-                margin="dense"
-                openTo="year"
-                disableFuture={true}
-              /> */}
               <DateFormField
                 required
                 name="date_of_birth"
@@ -235,15 +179,14 @@ const TransferPatientDialog = (props: Props) => {
           </div>
         </div>
       </div>
-      <DialogActions className="justify-between flex flex-col md:flex-row gap-2">
+      <div className="justify-between flex flex-col md:flex-row gap-2">
         <Cancel onClick={handleCancel} disabled={isLoading} />
         <Submit
           disabled={isLoading}
           onClick={handleSubmit}
           label="Transfer Suspect / Patient"
         />
-      </DialogActions>
-      {/* </DialogModal> */}
+      </div>
     </div>
   );
 };
