89120b2d3e756bff57404626382b74a0cfc5016a
Aakash Singh <46787056+sainak@users.noreply.github.com>
2023-04-04 23:11:30 +0530

    vite migration (#5140)
    * initial vite migration
    
    * fix npm dev command
    
    * add workaround for react-phone-input-2
    
    * pin node v18
    
    * fix storybook and pwa
    
    * remove node 16 engine from package.json
    
    * prune packages
    
    * Fix PUBLIC_URL env
    
    * storybook: adds required font
    
    * storybook: upgrade to SB v7, run migration scripts
    
    * storybook: remove
    
    * storybook: setup from scratch, add stories
    
    * storybook: use vite builder and update out dir
    
    * storybook: fix build command
    
    * storybook: fix missing include in vite config
    
    * storybook: revert to default out dir
    
    * add missing entry for VITE_PUBLIC_URL
    
    * update readme and cypress workflow
    
    * cypress: fix rescript build command
    
    * remove hardcoded variables from index.html
    
    * change env variables prefix to REACT_ to maintain compatibility
    
    * remove cra service worker files
    
    * fix service worker implementation
    
    ---------
    
    Co-authored-by: Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
    Co-authored-by: rithviknishad <rithvikn2001@gmail.com>
    Co-authored-by: Rithvik Nishad <mail@rithviknishad.dev>

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+  core: {
+    builder: {
+      name: "@storybook/builder-vite",
+      options: {
+        viteConfigPath: "./.storybook/vite.config.ts",
+      },
+    },
+  },
+};
+export default config;
