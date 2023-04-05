e934f5e9dc58748e3895af7927059bb324d2066f
rithviknishad <rithvikn2001@gmail.com>
2023-04-05 18:56:42 +0530

    this should work now :)

--- src/Components/Common/Plausible.tsx
+++ src/Components/Common/Plausible.tsx
@@ -1,14 +1,35 @@
+import { useLocationChange } from "raviger";
 import useConfig from "../../Common/hooks/useConfig";
 import Script from "./Script";
+import { useEffect } from "react";
 
 export default function Plausible() {
   const { site_url, analytics_server_url } = useConfig();
+
+  useEffect(() => triggerPageView(), []);
+  useLocationChange(() => triggerPageView());
 
   return (
     <Script
       defer
       data-domain={site_url}
-      src={`${analytics_server_url}/js/script.js`}
+      src={`${analytics_server_url}/js/script.manual.js`}
     />
   );
 }
+
+const triggerPageView = () => {
+  const plausible = (window as any).plausible;
+
+  const url = window.location.href;
+  // Replace every all-numeric sequences between two slashes and uuids with "_ID_REDACTED_"
+  const redactedUrl = url
+    .replace(/\/\d+\//g, "/_ID_REDACTED_/")
+    .replace(
+      /[a-f\d]{8}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{12}/gi,
+      "_ID_REDACTED_"
+    );
+
+  // Send the pageview event to Plausible
+  plausible("pageview", { u: redactedUrl });
+};
