7068aec47006cb3f3d5f20f7552ca0cedb3cf5e5
rithviknishad <rithvikn2001@gmail.com>
2023-04-06 10:23:37 +0530

    fix empty qparams not being removed and blacklist some qparams

--- src/Components/Common/Plausible.tsx
+++ src/Components/Common/Plausible.tsx
@@ -18,28 +18,28 @@ export default function Plausible() {
   );
 }
 
+const BLACKLISTED_QUERY_PARAMS = ["page", "limit"];
+
 const triggerPageView = () => {
   const plausible = (window as any).plausible;
 
   const url = new URL(window.location.href);
 
-  // Remove all empty query parameters and replace all non-empty ones with
-  // "[REDACTED]"
-  url.searchParams.forEach((value, key) => {
-    if (!value) {
+  // Remove all blacklisted and empty query parameters
+  [...url.searchParams.entries()].map(([key, value]) => {
+    if (value === "" || BLACKLISTED_QUERY_PARAMS.includes(key)) {
       url.searchParams.delete(key);
-    } else {
-      url.searchParams.set(key, "REDACTED");
     }
   });
 
-  // Replace all `/<num>` and `/uuid` with "/_ID_REDACTED_"
   const redactedUrl = url
     .toString()
+    // Replace all uuids in the URL with "ID_REDACTED"
     .replace(
       /[a-f\d]{8}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{12}/gi,
       "ID_REDACTED"
     )
+    // Replace all numbers in the URL's path params with "ID_REDACTED"
     .replace(/\/\d+/g, "/ID_REDACTED");
 
   // Send the pageview event to Plausible
