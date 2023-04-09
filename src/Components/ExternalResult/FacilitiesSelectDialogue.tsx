be024a18ec264e8c41afa132f4590038c7da08fe
AbhishekPSingh07 <79076050+AbhishekPSingh07@users.noreply.github.com>
2023-04-09 18:35:53 +0530

    Place buttons in new line in Search Facility Popup #5287
    Done the Neccesarry UI changes

--- src/Components/ExternalResult/FacilitiesSelectDialogue.tsx
+++ src/Components/ExternalResult/FacilitiesSelectDialogue.tsx
@@ -30,7 +30,7 @@ const FacilitiesSelectDialog = (props: Props) => {
         showAll={false}
         multiple={false}
       />
-      <div className="mt-4 flex justify-between">
+      <div className="mt-4 flex flex-col">
         <Cancel onClick={handleCancel} />
         <Submit
           onClick={handleOk}
