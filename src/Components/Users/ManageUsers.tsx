5c7a514fa0706137138c8fb502082840311863e1
Rithvik Nishad <rithvikn2001@gmail.com>
2023-04-18 16:53:41 +0530

    Update src/Components/Users/ManageUsers.tsx

--- src/Components/Users/ManageUsers.tsx
+++ src/Components/Users/ManageUsers.tsx
@@ -439,7 +439,7 @@ export default function ManageUsers() {
         setOpen={setExpandFacilityList}
         slideFrom="right"
         title="Facilities"
-        dialogClass="w-[350px] p-6"
+        dialogClass="md:w-[400px]"
         onCloseClick={() => {
           //fetchData({ aborted: false });
         }}
