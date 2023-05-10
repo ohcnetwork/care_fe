ce43c3d042e94b03d5c4d393fb217a3036d798dd
rithviknishad <rithvikn2001@gmail.com>
2023-05-10 19:28:46 +0530

    add back button

--- src/Components/Medicine/ManagePrescriptions.tsx
+++ src/Components/Medicine/ManagePrescriptions.tsx
@@ -1,4 +1,6 @@
+import useAppHistory from "../../Common/hooks/useAppHistory";
 import { PrescriptionActions } from "../../Redux/actions";
+import ButtonV2 from "../Common/components/ButtonV2";
 import Page from "../Common/components/Page";
 import PrescriptionBuilder from "./PrescriptionBuilder";
 
@@ -8,19 +10,29 @@ interface Props {
 
 export default function ManagePrescriptions({ consultationId }: Props) {
   const actions = PrescriptionActions(consultationId);
+  const { goBack } = useAppHistory();
 
   return (
     <Page title="Manage Prescriptions">
-      <div className="flex flex-col gap-10 rounded sm:rounded-xl bg-white p-6 sm:p-12 transition-all w-full max-w-4xl mx-auto divide-y-2 divide-dashed divide-gray-600">
-        <div>
-          <h3 className="font-semibold text-lg mb-4">
-            Prescription Medications
-          </h3>
-          <PrescriptionBuilder actions={actions} />
+      <div className="flex flex-col gap-10 rounded sm:rounded-xl bg-white p-6 sm:p-12 transition-all w-full max-w-4xl mx-auto">
+        <div className="flex flex-col gap-10 divide-y-2 divide-dashed divide-gray-600">
+          <div>
+            <h3 className="font-semibold text-lg mb-4">
+              Prescription Medications
+            </h3>
+            <PrescriptionBuilder actions={actions} />
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg mb-4 mt-8">
+              PRN Prescriptions
+            </h3>
+            <PrescriptionBuilder actions={actions} is_prn />
+          </div>
         </div>
-        <div>
-          <h3 className="font-semibold text-lg mb-4 mt-8">PRN Prescriptions</h3>
-          <PrescriptionBuilder actions={actions} is_prn />
+        <div className="flex w-full justify-end">
+          <ButtonV2 variant="secondary" border onClick={() => goBack()}>
+            Back
+          </ButtonV2>
         </div>
       </div>
     </Page>
