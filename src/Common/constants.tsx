f5c38ba2b1e852e8a84887e13f232e58f5053c78
Rithvik Nishad <rithvikn2001@gmail.com>
2023-05-28 21:33:25 +0530

    make text user friendly

--- src/Common/constants.tsx
+++ src/Common/constants.tsx
@@ -343,7 +343,7 @@ export const ADMITTED_TO = [
   { id: "2", text: "ICU" },
   { id: "6", text: "Bed with oxygen support" },
   { id: "7", text: "Regular" },
-  { id: "None", text: "None" },
+  { id: "None", text: "No bed assigned" },
 ];
 
 export const RESPIRATORY_SUPPORT = [
