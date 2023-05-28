0ea3c5c9a939512511b3eb8453223aa37af693f0
nikhilagastya <karthikagastya2002@gmail.com>
2023-05-28 13:53:10 +0530

    Closed change of Material UI components in Notice Board

--- src/Components/Notifications/NoticeBoard.tsx
+++ src/Components/Notifications/NoticeBoard.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getNotifications } from "../../Redux/actions";
-import PageTitle from "../Common/PageTitle";
-import { Card, CardContent } from "@material-ui/core";
+import Page from "../Common/components/Page";
 import Loading from "../Common/Loading";
 import { formatDate } from "../../Utils/utils";
 import { useTranslation } from "react-i18next";
@@ -36,22 +35,20 @@ export const NoticeBoard: any = () => {
     notices = (
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-6">
         {data.map((item) => (
-          <Card key={`usr_${item.id}`} className="rounded w-full shadow">
-            <CardContent>
-              <div className="bg-white">
-                <div className="text-justify text-lg">{item.message}</div>
-                <div className="text-gray-700 text-md mt-2">
-                  {`${item.caused_by.first_name} ${item.caused_by.last_name}`} -{" "}
-                  <span className="text-primary-700 font-bold">
-                    {item.caused_by.user_type}
-                  </span>
-                </div>
-                <div className="text-xs text-gray-900">
-                  {t("on")}: {formatDate(item.created_date)}
-                </div>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg ">
+            <div className="px-6 py-4">
+              <div className="text-justify text-lg">{item.message}</div>
+              <div className="text-gray-700 text-md mb-2 mt-2">
+                {`${item.caused_by.first_name} ${item.caused_by.last_name}`} -{" "}
+                <span className="text-primary-700 font-bold">
+                  {item.caused_by.user_type}
+                </span>
               </div>
-            </CardContent>
-          </Card>
+              <div className="text-gray-900 text-xs">
+                {t("on")}: {formatDate(item.created_date)}
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     );
@@ -84,11 +81,11 @@ export const NoticeBoard: any = () => {
   if (isLoading) return <Loading />;
   return (
     <div className="px-6">
-      <PageTitle
+      <Page
         title={t("Notice Board")}
         hideBack={true}
         breadcrumbs={false}
-      />
+      ></Page>
       <div>{notices}</div>
     </div>
   );
