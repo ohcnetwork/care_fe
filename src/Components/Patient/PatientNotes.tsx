de17919acdd8cbd5102adcc950a4ede048cc8b62
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2023-06-10 11:39:50 +0530

    wrapping of text patient note bug fixed

--- src/Components/Patient/PatientNotes.tsx
+++ src/Components/Patient/PatientNotes.tsx
@@ -141,8 +141,8 @@ const PatientNotes = (props: PatientNotesProps) => {
                 key={note.id}
                 className="flex p-4 bg-white rounded-lg text-gray-800 mt-4 flex-col w-full border border-gray-300"
               >
-                <span className="whitespace-pre">{note.note}</span>
-                <div className="mt-3">
+                <span className="max-w-full pl-2">{note.note}</span>
+                <div className="mt-3 pl-2">
                   <span className="text-xs text-gray-500">
                     {formatDate(note.created_date) || "-"}
                   </span>
