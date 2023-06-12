d370619931d8edabf11acfe9c4a3bb74835ad6f4
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2023-06-12 23:17:21 +0530

    Reverted unwanted changes

--- vite.config.ts
+++ vite.config.ts
@@ -74,7 +74,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       "/api": {
-        target: "http://localhost:9000",
+        target: "https://careapi.ohc.network",
         changeOrigin: true,
       },
     },
@@ -83,7 +83,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       "/api": {
-        target: "http://localhost:9000",
+        target: "https://careapi.ohc.network",
         changeOrigin: true,
       },
     },
