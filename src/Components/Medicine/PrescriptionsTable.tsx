e32dd00aeae9e50e6be67be0088ad19b4e881e9f
rithviknishad <rithvikn2001@gmail.com>
2023-06-12 19:17:41 +0530

    show old medicine if medibase medicine is null

--- src/Components/Medicine/PrescriptionsTable.tsx
+++ src/Components/Medicine/PrescriptionsTable.tsx
@@ -200,7 +200,7 @@ export default function PrescriptionsTable({
               list={
                 prescriptions?.map((obj) => ({
                   ...obj,
-                  medicine: obj.medicine_object?.name,
+                  medicine: obj.medicine_object?.name ?? obj.medicine_old,
                   route__pretty:
                     obj.route && t("PRESCRIPTION_ROUTE_" + obj.route),
                   frequency__pretty:
