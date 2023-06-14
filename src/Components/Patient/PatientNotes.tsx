d5c768788ce0d1460352b565e645cbbd3d210d59
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2023-06-14 10:44:46 +0530

    Issue resolved for word wrap with whitespace pre

--- src/Components/Patient/PatientNotes.tsx
+++ src/Components/Patient/PatientNotes.tsx
@@ -141,8 +141,10 @@ const PatientNotes = (props: PatientNotesProps) => {
                 key={note.id}
                 className="flex p-4 bg-white rounded-lg text-gray-800 mt-4 flex-col w-full border border-gray-300"
               >
-                <span className="max-w-full pl-2">{note.note}</span>
-                <div className="mt-3 pl-2">
+                <span className="whitespace-pre-wrap break-words">
+                  {note.note}
+                </span>
+                <div className="mt-3">
                   <span className="text-xs text-gray-500">
                     {formatDate(note.created_date) || "-"}
                   </span>
