ab13a8de201284c2e1a05b4176bbe109ca8bd2ef
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2023-06-23 18:43:06 +0530

    Auto refresh file list after upload

--- src/Components/Patient/FileUpload.tsx
+++ src/Components/Patient/FileUpload.tsx
@@ -946,6 +946,7 @@ export const FileUpload = (props: FileUploadProps) => {
           setFile(null);
           setUploadFileName("");
           setReload(!reload);
+          fetchData({ aborted: false });
           Notification.Success({
             msg: "File Uploaded Successfully",
           });
