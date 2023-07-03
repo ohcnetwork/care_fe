6da64289574f74befab6294022fdd9033a0012da
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2023-07-03 12:07:55 +0530

    revert mistake

--- vite.config.ts
+++ vite.config.ts
@@ -81,7 +81,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       "/api": {
-        target: "http://localhost:9000",
+        target: "https://careapi.ohc.network",
         changeOrigin: true,
       },
     },
@@ -90,7 +90,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       "/api": {
-        target: "http://localhost:9000",
+        target: "https://careapi.ohc.network",
         changeOrigin: true,
       },
     },
