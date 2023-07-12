dce1c938617094715cd4aecda19d657a2eb357b9
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2023-07-12 19:29:30 +0530

    merge develop

--- cypress/pageobject/Asset/AssetCreation.ts
+++ cypress/pageobject/Asset/AssetCreation.ts
@@ -77,7 +77,86 @@ export class AssetPage {
     cy.get("#submit").contains("Create Asset").click();
   }
 
+  clickCreateAddMore() {
+    cy.get("[data-testid=create-asset-add-more-button]").click();
+  }
+
   verifySuccessNotification(message: string) {
     cy.verifyNotification(message);
+  }
+
+  openCreatedAsset() {
+    cy.get("[data-testid=created-asset-list]").first().click();
+  }
+
+  editAssetDetails(
+    name: string,
+    description: string,
+    qrId: string,
+    manufacturer: string,
+    supportName: string,
+    vendorName: string,
+    notes: string
+  ) {
+    cy.get("[data-testid=asset-update-button]").click();
+    cy.get("[data-testid=asset-name-input] input").clear().type(name);
+    cy.get("[data-testid=asset-description-input] textarea")
+      .clear()
+      .type(description);
+    cy.get("[data-testid=asset-qr-id-input] input").clear().type(qrId);
+    cy.get("[data-testid=asset-manufacturer-input] input")
+      .clear()
+      .type(manufacturer);
+    cy.get("[data-testid=asset-support-name-input] input")
+      .clear()
+      .type(supportName);
+    cy.get("[data-testid=asset-vendor-name-input] input")
+      .clear()
+      .type(vendorName);
+    cy.get("[data-testid=asset-notes-input] textarea").clear().type(notes);
+  }
+
+  clickUpdateAsset() {
+    cy.get("#submit").contains("Update").click();
+  }
+
+  deleteAsset() {
+    cy.get("[data-testid=asset-delete-button]").click();
+    cy.get("#submit").contains("Confirm").click();
+  }
+
+  verifyEmptyAssetNameError() {
+    cy.get("[data-testid=asset-name-input] span").should(
+      "contain",
+      "Asset name can't be empty"
+    );
+  }
+
+  verifyEmptyLocationError() {
+    cy.get("[data-testid=asset-location-input] span").should(
+      "contain",
+      "Select a location"
+    );
+  }
+
+  verifyEmptyAssetTypeError() {
+    cy.get("[data-testid=asset-type-input] span").should(
+      "contain",
+      "Select an asset type"
+    );
+  }
+
+  verifyEmptyStatusError() {
+    cy.get("[data-testid=asset-working-status-input] span").should(
+      "contain",
+      "Field is required"
+    );
+  }
+
+  verifyEmptyPhoneError() {
+    cy.get("#customer-support-phone-div span").should(
+      "contain",
+      "Please enter valid phone number"
+    );
   }
 }
--- src/Common/hooks/useFullscreen.ts
+++ src/Common/hooks/useFullscreen.ts
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 
-export default function useFullscreen(): [boolean, (value: boolean) => void] {
+export default function useFullscreen(): [
+  boolean,
+  (value: boolean, element?: HTMLElement) => void
+] {
   const [isFullscreen, _setIsFullscreen] = useState(
     !!document.fullscreenElement
   );
@@ -15,12 +18,11 @@ export default function useFullscreen(): [boolean, (value: boolean) => void] {
       document.removeEventListener("fullscreenchange", onFullscreenChange);
   }, []);
 
-  const setFullscreen = (value: boolean) => {
-    if (value) {
-      document.documentElement.requestFullscreen();
-    } else {
-      document.exitFullscreen();
-    }
+  const setFullscreen = (value: boolean, element?: HTMLElement) => {
+    const fullscreenElement = element ?? document.documentElement;
+
+    if (value) fullscreenElement.requestFullscreen();
+    else document.exitFullscreen();
   };
 
   return [isFullscreen, setFullscreen];
