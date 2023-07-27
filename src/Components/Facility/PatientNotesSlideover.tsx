5ee889da2f08c5ec857432b940b6270abfcb3654
Rithvik Nishad <rithvikn2001@gmail.com>
2023-07-27 04:44:07 +0000

    Apply suggestions from code review

--- src/Components/Facility/PatientNotesSlideover.tsx
+++ src/Components/Facility/PatientNotesSlideover.tsx
@@ -59,9 +59,7 @@ export default function PatientNotesSlideover(props: PatientNotesProps) {
     <div className="flex gap-1">
       {show && (
         <Link
-          className={classNames(
-            "flex h-8 w-8 cursor-pointer items-center justify-center rounded bg-primary-800 text-gray-100 text-opacity-70 hover:bg-primary-700 hover:text-opacity-100"
-          )}
+          className="flex h-8 w-8 cursor-pointer items-center justify-center rounded bg-primary-800 text-gray-100 text-opacity-70 hover:bg-primary-700 hover:text-opacity-100"
           href={`/facility/${facilityId}/patient/${patientId}/notes`}
         >
           <CareIcon className="care-l-window-maximize text-lg transition-all delay-150 duration-300 ease-out" />
@@ -70,16 +68,14 @@ export default function PatientNotesSlideover(props: PatientNotesProps) {
       <div
         className={classNames(
           "flex h-8 w-8 cursor-pointer items-center justify-center rounded bg-primary-800 text-gray-100 text-opacity-70 hover:bg-primary-700 hover:text-opacity-100",
-          show ? "rotate-180" : ""
+          show && "rotate-180"
         )}
         onClick={() => setShow(!show)}
       >
         <CareIcon className="care-l-angle-up text-lg transition-all delay-150 duration-300 ease-out" />
       </div>
       <div
-        className={classNames(
-          "flex h-8 w-8 cursor-pointer items-center justify-center rounded bg-primary-800 text-gray-100 text-opacity-70 hover:bg-primary-700 hover:text-opacity-100"
-        )}
+        className="flex h-8 w-8 cursor-pointer items-center justify-center rounded bg-primary-800 text-gray-100 text-opacity-70 hover:bg-primary-700 hover:text-opacity-100"
         onClick={() => setShowPatientNotesPopup(false)}
       >
         <CareIcon className="care-l-times text-lg transition-all delay-150 duration-300 ease-out" />
