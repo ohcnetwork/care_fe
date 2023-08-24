4692a89541bdca50f9d3a8401ac7f8b15adcf4c0
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2023-08-24 16:57:20 +0530

    nits

--- vite.config.ts
+++ vite.config.ts
@@ -80,7 +80,8 @@ export default defineConfig({
     port: 4000,
     proxy: {
       "/api": {
-        target: "https://careapi.ohc.network",
+        target: "http://localhost:9000",
+        // target: "https://careapi.ohc.network",
         changeOrigin: true,
       },
     },
@@ -89,7 +90,8 @@ export default defineConfig({
     port: 4000,
     proxy: {
       "/api": {
-        target: "https://careapi.ohc.network",
+        target: "http://localhost:9000",
+        // target: "https://careapi.ohc.network",
         changeOrigin: true,
       },
     },
