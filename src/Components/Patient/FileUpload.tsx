64d261cc950c22b1623ce39258eb4da0472f7bff
Ashesh <3626859+Ashesh3@users.noreply.github.com>
2023-09-06 19:27:48 +0530

    open pdf file preview in new tab (#6226)

--- src/Components/Patient/FileUpload.tsx
+++ src/Components/Patient/FileUpload.tsx
@@ -481,15 +481,20 @@ export const FileUpload = (props: FileUploadProps) => {
     };
     const responseData = await dispatch(retrieveUpload(data, id));
     const file_extension = getExtension(responseData.data.read_signed_url);
-    setFileState({
-      ...file_state,
-      open: true,
-      name: responseData.data.name,
-      extension: file_extension,
-      isImage: ExtImage.includes(file_extension),
-    });
-    downloadFileUrl(responseData.data.read_signed_url);
-    setFileUrl(responseData.data.read_signed_url);
+    if (file_extension === "pdf") {
+      window.open(responseData.data.read_signed_url, "_blank");
+      setFileState({ ...file_state, open: false });
+    } else {
+      setFileState({
+        ...file_state,
+        open: true,
+        name: responseData.data.name,
+        extension: file_extension,
+        isImage: ExtImage.includes(file_extension),
+      });
+      downloadFileUrl(responseData.data.read_signed_url);
+      setFileUrl(responseData.data.read_signed_url);
+    }
   };
 
   const validateEditFileName = (name: any) => {
