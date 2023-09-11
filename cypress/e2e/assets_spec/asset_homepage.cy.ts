16b0bd848d7cde1f4e0624f4907e6bfba656280e
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2023-09-12 00:42:20 +0530

    add search by qr and ID

--- cypress/e2e/assets_spec/asset_homepage.cy.ts
+++ cypress/e2e/assets_spec/asset_homepage.cy.ts
@@ -5,15 +5,17 @@ import { AssetSearchPage } from "../../pageobject/Asset/AssetSearch";
 import { AssetQRScanPage } from "../../pageobject/Asset/AssetQRScan";
 import { AssetPagination } from "../../pageobject/Asset/AssetPagination";
 import { AssetFilters } from "../../pageobject/Asset/AssetFilters";
+import LoginPage from "../../pageobject/Login/LoginPage";
 
 describe("Asset Tab", () => {
   const assetSearchPage = new AssetSearchPage();
   const assetQRScanPage = new AssetQRScanPage();
   const assetPagination = new AssetPagination();
   const assetFilters = new AssetFilters();
+  const loginPage = new LoginPage();
 
   before(() => {
-    cy.loginByApi("devdistrictadmin", "Coronasafe@123");
+    loginPage.loginAsDisctrictAdmin();
     cy.saveLocalStorage();
   });
 
@@ -24,11 +26,27 @@ describe("Asset Tab", () => {
 
   // search for a element
 
-  it("Search Asset Name", () => {
-    const initialUrl = cy.url();
-    assetSearchPage.typeSearchKeyword("dummy camera 30");
+  it("Search Asset Name/QR_ID/Serial_number", () => {
+    assetSearchPage.typeSearchKeyword("dummy camera 10");
     assetSearchPage.pressEnter();
-    assetSearchPage.verifyUrlChanged(initialUrl);
+    assetSearchPage.verifyBadgeContent(
+      "Name/Serial No./QR ID: dummy camera 10"
+    );
+    assetSearchPage.clickAssetByName("Dummy Camera 10");
+    assetSearchPage.clickUpdateButton();
+    assetSearchPage.clearAndTypeQRCode("340543-05935-04953-05234-04");
+    assetSearchPage.clearAndTypeSerialNumber("8989898989898");
+    assetSearchPage.clickAssetSubmitButton();
+    assetSearchPage.visitAssetsPage();
+    assetSearchPage.typeSearchKeyword("340543-05935-04953-05234-04");
+    assetSearchPage.pressEnter();
+    assetSearchPage.verifyAssetListContains("Dummy Camera 10");
+    assetSearchPage.verifyBadgeContent(
+      "Name/Serial No./QR ID: 340543-05935-04953-05234-04"
+    );
+    assetSearchPage.typeSearchKeyword("8989898989898");
+    assetSearchPage.verifyAssetListContains("Dummy Camera 10");
+    assetSearchPage.verifyBadgeContent("Name/Serial No./QR ID: 8989898989898");
   });
 
   // scan a asset qr code
