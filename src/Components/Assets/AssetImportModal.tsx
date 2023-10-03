410a7d553792012646287e6015c5b57584a586e7
Ashesh <3626859+Ashesh3@users.noreply.github.com>
2023-10-03 20:22:47 +0530

    Refactor Asset Model Import Formatting (#6388)
    * Refactor Asset Model Import Formatting
    
    * fixes to warranty_amc_end_of_validity
    
    * Fix asset import file for cypress

--- src/Components/Assets/AssetImportModal.tsx
+++ src/Components/Assets/AssetImportModal.tsx
@@ -85,11 +85,11 @@ const AssetImportModal = ({ open, onClose, facility }: Props) => {
             }
           }
         }
-      } catch (e) {
+      } catch (e: any) {
         setPreview(undefined);
         console.log(e);
         Notification.Error({
-          msg: "Invalid file",
+          msg: "Invalid file: " + e.message,
         });
         setSelectedFile(undefined);
       }
@@ -113,7 +113,7 @@ const AssetImportModal = ({ open, onClose, facility }: Props) => {
     let error = false;
 
     for (const asset of preview || []) {
-      const asset_data = JSON.stringify({
+      const asset_data: any = {
         name: asset.name,
         asset_type: asset.asset_type,
         asset_class: asset.asset_class,
@@ -129,11 +129,15 @@ const AssetImportModal = ({ open, onClose, facility }: Props) => {
         qr_code_id: asset.qr_code_id,
         manufacturer: asset.manufacturer,
         meta: { ...asset.meta },
-        warranty_amc_end_of_validity: asset.warranty_amc_end_of_validity,
-        last_serviced_on: asset.last_serviced_on,
         note: asset.notes,
-        cancelToken: { promise: {} },
-      });
+      };
+
+      if (asset.last_serviced_on)
+        asset_data["last_serviced_on"] = asset.last_serviced_on;
+
+      if (asset.warranty_amc_end_of_validity)
+        asset_data["warranty_amc_end_of_validity"] =
+          asset.warranty_amc_end_of_validity;
 
       const response = await fetch("/api/v1/asset/", {
         method: "POST",
@@ -142,7 +146,7 @@ const AssetImportModal = ({ open, onClose, facility }: Props) => {
           Authorization:
             "Bearer " + localStorage.getItem(LocalStorageKeys.accessToken),
         },
-        body: asset_data,
+        body: JSON.stringify(asset_data),
       });
       const data = await response.json();
       if (response.status !== 201) {
