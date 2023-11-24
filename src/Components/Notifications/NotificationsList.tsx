50234afd6b395db4c8dfab168a807cb205423ca8
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2023-11-24 20:02:33 +0530

    test

--- src/Components/Notifications/NotificationsList.tsx
+++ src/Components/Notifications/NotificationsList.tsx
@@ -358,7 +358,6 @@ export default function NotificationsList({
     manageResults = (
       <>
         {data
-          .filter((notification: any) => notification.event != "PUSH_MESSAGE")
           .filter((notification: any) =>
             showUnread ? notification.read_at === null : true
           )
