a6fd9f67cc714c8b572e2a961cfe79a3fac266d3
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2023-11-27 18:00:37 +0530

    facility csv download test (#6730)

---
+++ cypress/pageobject/Facility/FacilityHome.ts
@@ -0,0 +1,31 @@
+// cypress/support/pageObjects/FacilityHome.ts
+
+class FacilityHome {
+  // Selectors
+  exportButton = "#export-button";
+  searchButton = "#search";
+  menuItem = "[role='menuitem']";
+
+  // Operations
+  clickExportButton() {
+    cy.get(this.exportButton).click();
+  }
+
+  clickSearchButton() {
+    cy.get(this.searchButton).click();
+  }
+
+  clickMenuItem(itemName: string) {
+    cy.get(this.menuItem).contains(itemName).click();
+  }
+
+  csvDownloadIntercept(alias: string, queryParam: string) {
+    cy.intercept("GET", `**/api/v1/facility/?csv${queryParam}`).as(alias);
+  }
+
+  verifyDownload(alias: string) {
+    cy.wait(`@${alias}`).its("response.statusCode").should("eq", 200);
+  }
+}
+
+export default FacilityHome;
