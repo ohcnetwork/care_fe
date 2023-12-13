341fd04804845c695005aa110770e83e12f774a7
Pranshu1902 <pranshu1902@gmail.com>
2023-12-13 21:25:34 +0530

    fix offset calls

--- src/Components/Facility/PatientNotesList.tsx
+++ src/Components/Facility/PatientNotesList.tsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { statusType, useAbortableEffect } from "../../Common/utils";
-// import {
-//   getPatientNotes,
-//   getPatientNotesByConsultation,
-// } from "../../Redux/actions";
+import { useEffect, useState } from "react";
 import { RESULTS_PER_PAGE_LIMIT } from "../../Common/constants";
 import CircularProgress from "../Common/components/CircularProgress";
 import PatientNoteCard from "./PatientNoteCard";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { NoteType } from "./PatientNoteCard";
 import useQuery from "../../Utils/request/useQuery";
 import routes from "../../Redux/api";
+import { PatientNotesModel } from "./models";
 
 interface PatientNotesProps {
   patientId: any;
@@ -21,7 +15,7 @@ interface PatientNotesProps {
 }
 
 interface StateType {
-  notes: NoteType[];
+  notes: PatientNotesModel[];
   cPage: number;
   totalPages: number;
 }
@@ -34,7 +28,6 @@ const PatientNotesList: React.FC<
   const { reload, setReload } = props;
   // console.log(props);
 
-  // const dispatch: any = useDispatch();
   const initialData: StateType = { notes: [], cPage: 1, totalPages: 1 };
   const [state, setState] = useState(initialData);
   const [isLoading, setIsLoading] = useState(true);
@@ -45,12 +38,11 @@ const PatientNotesList: React.FC<
     },
     query: {
       consultation: consultationId,
-      offset: state.cPage * RESULTS_PER_PAGE_LIMIT,
+      offset: (state.cPage - 1) * RESULTS_PER_PAGE_LIMIT,
     },
     prefetch: reload,
     onResponse: ({ res, data }) => {
       setIsLoading(true);
-      console.log(data);
       if (res?.status === 200 && data) {
         setState((prevState: any) => ({
           ...prevState,
@@ -58,79 +50,17 @@ const PatientNotesList: React.FC<
           totalPages: Math.ceil(data.count / pageSize),
         }));
         setReload(false);
-        // if (!data?.next) {
-        //   setState({
-        //     notes: data?.results,
-        //     cPage: 1,
-        //     totalPages: Math.ceil(data.count / pageSize),
-        //   });
-        // } else {
-        //   setState((prevState: any) => ({
-        //     ...prevState,
-        //     notes: [...prevState.notes, ...data.results],
-        //     cPage: 2,
-        //     totalPages: Math.ceil(data.count / pageSize),
-        //   }));
-        // }
-        setIsLoading(false);
       }
+      setIsLoading(false);
     },
   });
-  // console.log(res);
 
-  // const fetchData = useCallback(
-  //   async (page = 1, status: statusType = { aborted: false }) => {
-  //     setIsLoading(true);
-  //     const res = await dispatch(
-  //       consultationId
-  //         ? getPatientNotesByConsultation(
-  //             props.patientId,
-  //             consultationId,
-  //             pageSize,
-  //             (page - 1) * pageSize
-  //           )
-  //         : getPatientNotes(props.patientId, pageSize, (page - 1) * pageSize)
-  //     );
-  //     if (!status.aborted) {
-  //       if (res && res.data) {
-  //         // if (page === 1) {
-  //         //   setState({
-  //         //     notes: res.data?.results,
-  //         //     cPage: page,
-  //         //     totalPages: Math.ceil(res.data.count / pageSize),
-  //         //   });
-  //         // } else {
-  //         //   setState((prevState: any) => ({
-  //         //     ...prevState,
-  //         //     notes: [...prevState.notes, ...res.data.results],
-  //         //     cPage: page,
-  //         //     totalPages: Math.ceil(res.data.count / pageSize),
-  //         //   }));
-  //         // }
-  //       }
-  //       setIsLoading(false);
-  //     }
-  //   },
-  //   [props.patientId, dispatch]
-  // );
-
-  // useEffect(() => {
-  //   if (reload) {
-  //     fetchData(1);
-  //     setReload(false);
-  //   }
-  // }, [reload]);
-
-  // useAbortableEffect(
-  //   (status: statusType) => {
-  //     fetchData(1, status);
-  //   },
-  //   [fetchData]
-  // );
+  useEffect(() => {
+    setReload(true);
+  }, []);
 
   const handleNext = () => {
     if (state.cPage < state.totalPages) {
-      // fetchData(state.cPage + 1);
       setState((prevState: any) => ({
         ...prevState,
         cPage: prevState.cPage + 1,
