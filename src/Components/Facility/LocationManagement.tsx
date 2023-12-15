2e65424ba3a864df7ff9df576dc6a58f90373281
Abhiuday <gupta.abhiuday.109@gmail.com>
2023-12-15 09:19:14 +0530

    fix(actions): removed unnecessary actions for duty staff

--- src/Components/Facility/LocationManagement.tsx
+++ src/Components/Facility/LocationManagement.tsx
@@ -304,11 +304,11 @@ const Location = (props: LocationProps) => {
           />
         )}
       </div>
-      <div className="flex gap-3 pt-3">
+      <div className="mt-3 flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
         <ButtonV2
           variant="secondary"
           border
-          className="w-full lg:w-auto"
+          className="w-full md:w-auto"
           onClick={() => setToggle((prev) => !prev)}
         >
           <CareIcon className="care-l-user text-lg" />
@@ -319,7 +319,7 @@ const Location = (props: LocationProps) => {
           id="manage-bed-button"
           variant="secondary"
           border
-          className="w-full lg:w-auto"
+          className="w-full md:w-auto"
           href={`location/${id}/beds`}
         >
           <CareIcon className="care-l-bed text-lg" />
