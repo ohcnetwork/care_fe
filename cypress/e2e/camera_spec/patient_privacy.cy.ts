2a3225521b27ea13a2029e537ede2a53c7b64fd6
Pranshu1902 <pranshu1902@gmail.com>
2023-12-29 15:45:41 +0530

    refactor tests

--- cypress/e2e/camera_spec/patient_privacy.cy.ts
+++ cypress/e2e/camera_spec/patient_privacy.cy.ts
@@ -13,12 +13,14 @@ describe("Patient Privacy", () => {
     cy.get("input[id='name']").type("Dummy Patient 16");
     cy.contains("a", "Dummy Patient 16").click();
     cy.wait("@consultation");
+    // assign bed to patient
     cy.get("button").contains("Assign Bed").click();
     cy.get("input[name='bed']").type("Dummy Bed 6");
     cy.get("li[role='option']").contains("Dummy Bed 6").click();
     cy.wait(2000);
     cy.get("button").contains("Move to bed").click();
     cy.wait(2000);
+    // open the feeds page
     cy.url().then((urlValue) => cy.awaitUrl(urlValue + "/feed"));
     cy.wait("@consultation");
   });
