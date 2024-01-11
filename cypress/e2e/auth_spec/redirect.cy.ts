363b9445f4e6332b62a612be561372c1fee475bf
Om Thorat <76207818+Om-Thorat@users.noreply.github.com>
2024-01-12 01:56:51 +0530

    used await url instead

--- cypress/e2e/auth_spec/redirect.cy.ts
+++ cypress/e2e/auth_spec/redirect.cy.ts
@@ -11,21 +11,21 @@ describe("redirect", () => {
   });
 
   it("Check if login redirects to the right url", () => {
-    cy.visit("/resource/board");
+    cy.awaitUrl("/resource/board", true);
     loginPage.loginManuallyAsStaff();
     loginPage.CheckIfLoggedIn();
     cy.url().should("include", "/resource/board");
   });
 
   it("Check if the redirect param works", () => {
-    cy.visit("login?redirect=http://localhost:4000/resource/board");
+    cy.awaitUrl("login?redirect=http://localhost:4000/resource/board", true);
     loginPage.loginManuallyAsStaff();
     loginPage.CheckIfLoggedIn();
     cy.url().should("include", "/resource/board");
   });
 
   it("Check to ensure that redirect is the same origin", () => {
-    cy.visit("login?redirect=https://google.com");
+    cy.awaitUrl("login?redirect=https://google.com", true);
     loginPage.loginManuallyAsStaff();
     loginPage.CheckIfLoggedIn();
     cy.url().should("include", "/facility");
