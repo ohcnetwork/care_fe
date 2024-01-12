a98fb8fa47c06998e2bc6c8e515fda589f00ce61
Om Thorat <76207818+Om-Thorat@users.noreply.github.com>
2024-01-12 14:25:27 +0530

    Use the right function

--- cypress/e2e/auth_spec/redirect.cy.ts
+++ cypress/e2e/auth_spec/redirect.cy.ts
@@ -7,26 +7,26 @@ describe("redirect", () => {
   const loginPage = new LoginPage();
 
   beforeEach(() => {
-    cy.log("Logging in the user staffdev:Coronasafe@123");
+    cy.log("Logging in the user devdistrictadmin:Coronasafe@123");
   });
 
   it("Check if login redirects to the right url", () => {
     cy.awaitUrl("/resource/board", true);
-    loginPage.loginAsDisctrictAdmin();
+    loginPage.loginManuallyAsDistrictAdmin();
     loginPage.CheckIfLoggedIn();
     cy.url().should("include", "/resource/board");
   });
 
   it("Check if the redirect param works", () => {
     cy.awaitUrl("login?redirect=http://localhost:4000/resource/board", true);
-    loginPage.loginAsDisctrictAdmin();
+    loginPage.loginManuallyAsDistrictAdmin();
     loginPage.CheckIfLoggedIn();
     cy.url().should("include", "/resource/board");
   });
 
   it("Check to ensure that redirect is the same origin", () => {
     cy.awaitUrl("login?redirect=https://google.com", true);
-    loginPage.loginAsDisctrictAdmin();
+    loginPage.loginManuallyAsDistrictAdmin();
     loginPage.CheckIfLoggedIn();
     cy.url().should("include", "/facility");
   });
