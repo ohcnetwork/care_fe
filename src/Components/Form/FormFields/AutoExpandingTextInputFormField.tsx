a5f62144b80a9ea8e2ef277bec0a622b8c1ba39d
Kshitij Verma <kshitijv250@gmail.com>
2024-01-18 17:47:50 +0530

    added required comments

--- src/Components/Form/FormFields/AutoExpandingTextInputFormField.tsx
+++ src/Components/Form/FormFields/AutoExpandingTextInputFormField.tsx
@@ -12,10 +12,11 @@ const AutoExpandingTextInputFormField = (
   useEffect(() => {
     if (myref.current == null) return;
     const text = myref.current.textContent?.split("\n");
-    const len = (text?.length == 0 ? 1 : text?.length) || 1;
-    const height = Math.min(len * 18, (props.maxHeight || 160) - 28) + 28;
-    myref.current.style.cssText =
-      "min-height:36px; height:46px; height:" + height + "px";
+    const len = text?.length || 1;
+    // 28 is the height of the textarea when there is no text
+    // 18 is the height of each line
+    const height = Math.min(len * 18, (props.maxHeight || 160) - 28) + 28; // 160 is the max height of the textarea if not specified
+    myref.current.style.cssText = "height:" + height + "px";
   });
 
   return <TextAreaFormField ref={myref} {...props} className="w-full" />;
