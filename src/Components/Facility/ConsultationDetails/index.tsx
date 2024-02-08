82d30d4c52a8b8062c52a2044f5b74d1139d9f0d
Abhiuday <gupta.abhiuday.109@gmail.com>
2024-02-08 20:21:38 +0530

    fix(consultation): resolved suggestion

--- src/Components/Facility/ConsultationDetails/index.tsx
+++ src/Components/Facility/ConsultationDetails/index.tsx
@@ -401,16 +401,15 @@ export const ConsultationDetails = (props: any) => {
             </div>
           </div>
         </div>
-        {consultationData.diagnoses &&
-          consultationData.diagnoses.length > 0 && (
-            <div className="col-span-1 mt-2 overflow-hidden rounded-lg bg-white shadow">
-              <div className="px-4 py-2">
-                <DiagnosesListAccordion
-                  diagnoses={consultationData.diagnoses ?? []}
-                />
-              </div>
+        {consultationData.diagnoses?.length && (
+          <div className="col-span-1 mt-2 overflow-hidden rounded-lg bg-white shadow">
+            <div className="px-4 py-2">
+              <DiagnosesListAccordion
+                diagnoses={consultationData.diagnoses ?? []}
+              />
             </div>
-          )}
+          </div>
+        )}
         <div className="mt-4 w-full border-b-2 border-gray-200">
           <div className="overflow-x-auto sm:flex sm:items-baseline">
             <div className="mt-4 sm:mt-0">
