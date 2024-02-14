5e0727d88f2801c059d051a80d9ed25f124f6606
Kshitij Verma <101321276+kshitijv256@users.noreply.github.com>
2024-02-14 12:54:23 +0530

    Added support for multi-line text input for Doctor Notes (#6977)
    * Changed TextFormField to TextAreaFormField
    
    * made doctor notes input size dynamic
    
    * fixed max height of textareafield
    
    * made max height variable
    
    * replaced ids with ref
    
    * removed id prop
    
    * added required comments
    
    * added required comments
    
    * lineheight computed dynamically

--- src/Components/Facility/ConsultationDoctorNotes/index.tsx
+++ src/Components/Facility/ConsultationDoctorNotes/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import * as Notification from "../../../Utils/Notifications.js";
 import Page from "../../Common/components/Page";
-import TextFormField from "../../Form/FormFields/TextFormField";
 import ButtonV2 from "../../Common/components/ButtonV2";
 import CareIcon from "../../../CAREUI/icons/CareIcon";
 import { NonReadOnlyUsers } from "../../../Utils/AuthorizeFor";
@@ -13,6 +12,7 @@ import request from "../../../Utils/request/request.js";
 import useQuery from "../../../Utils/request/useQuery.js";
 import useKeyboardShortcut from "use-keyboard-shortcut";
 import { isAppleDevice } from "../../../Utils/utils.js";
+import AutoExpandingTextInputFormField from "../../Form/FormFields/AutoExpandingTextInputFormField.js";
 
 interface ConsultationDoctorNotesProps {
   patientId: string;
@@ -120,12 +120,14 @@ const ConsultationDoctorNotes = (props: ConsultationDoctorNotesProps) => {
         />
 
         <div className="relative mx-4 flex items-center">
-          <TextFormField
+          <AutoExpandingTextInputFormField
+            id="doctor_consultation_notes"
+            maxHeight={160}
+            rows={1}
             name="note"
             value={noteField}
             onChange={(e) => setNoteField(e.value)}
             className="grow"
-            type="text"
             errorClassName="hidden"
             placeholder="Type your Note"
             disabled={!patientActive}
