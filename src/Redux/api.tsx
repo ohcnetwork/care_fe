2cc66f08e2f49d9dadf21fd038e724e9bfadbd5d
Ashesh <3626859+Ashesh3@users.noreply.github.com>
2024-02-14 12:51:53 +0530

    Update Uptime endpoint for AssetUptime (#7210)
    * Track uptime for Locations
    
    * Refactor Uptime component to accept route and params
    
    * Remove unused import in api.tsx

--- src/Redux/api.tsx
+++ src/Redux/api.tsx
@@ -27,7 +27,7 @@ import {
   AssetService,
   AssetServiceUpdate,
   AssetTransaction,
-  AssetUptimeRecord,
+  AvailabilityRecord,
   PatientAssetBed,
 } from "../Components/Assets/AssetTypes";
 import {
@@ -370,6 +370,11 @@ const routes = {
   partialUpdateFacilityAssetLocation: {
     path: "/api/v1/facility/{facility_external_id}/asset_location/{external_id}/",
     method: "PATCH",
+  },
+  getFacilityAssetLocationAvailability: {
+    path: "/api/v1/facility/{facility_external_id}/asset_location/{external_id}/availability/",
+    method: "GET",
+    TRes: Type<PaginatedResponse<AvailabilityRecord>>(),
   },
 
   // Asset bed
@@ -1116,6 +1121,11 @@ const routes = {
     TRes: Type<AssetData>(),
     TBody: Type<Partial<AssetData>>(),
   },
+  listAssetAvailability: {
+    path: "/api/v1/asset/{external_id}/availability/",
+    method: "GET",
+    TRes: Type<PaginatedResponse<AvailabilityRecord>>(),
+  },
 
   // Asset transaction endpoints
 
@@ -1288,19 +1298,6 @@ const routes = {
       TRes: Type<IHealthFacility>(),
       TBody: Type<IcreateHealthFacilityTBody>(),
     },
-  },
-
-  // Asset Availability endpoints
-
-  listAssetAvailability: {
-    path: "/api/v1/asset_availability/",
-    method: "GET",
-    TRes: Type<PaginatedResponse<AssetUptimeRecord>>(),
-  },
-  getAssetAvailability: {
-    path: "/api/v1/asset_availability/{id}",
-    method: "GET",
-    TRes: Type<AssetUptimeRecord>(),
   },
 
   // Prescription endpoints
