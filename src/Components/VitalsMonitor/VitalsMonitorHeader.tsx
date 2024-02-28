ab678269f76b18791edb84f9383eaecee88caa8d
Devdeep Ghosh <63492939+thedevildude@users.noreply.github.com>
2024-02-28 09:46:00 +0530

    Introduce (i) Button Popover Component in HL7Monitor and VentilatorPatientVitalsMonitor (#7228)
    * created a VitalsMonitorAssetPopover component
    
    * replaced asset name with (i) button popover
    
    * replaced DialogModal with headlessUI popover
    
    * minor mobile ui fix HL7Monitor
    
    * removed redundant code
    
    * minor css fix and dynamic icon feature added
    
    * spacing fix
    
    * local fix to improper gender type
    
    * reusable header for VitalsMonitor
    
    * minor css fix
    
    * minor popover css fix
    
    * added hideHeader optional prop to VitalsMonitor
    
    * hide header from VitalsMonitor in ConsultationUpdatesTab
    
    * created and implemented VitalsMonitorFooter component
    
    * hide header and footer from VitalsMonitor asset configure
    
    ---------
    
    Co-authored-by: Rithvik Nishad <mail@rithviknishad.dev>

---
+++ src/Components/VitalsMonitor/VitalsMonitorHeader.tsx
@@ -0,0 +1,56 @@
+import { PatientAssetBed } from "../Assets/AssetTypes";
+import { Link } from "raviger";
+import CareIcon from "../../CAREUI/icons/CareIcon";
+import { GENDER_TYPES } from "../../Common/constants";
+
+interface VitalsMonitorHeaderProps {
+  patientAssetBed?: PatientAssetBed;
+}
+
+const VitalsMonitorHeader = ({ patientAssetBed }: VitalsMonitorHeaderProps) => {
+  const { patient, bed } = patientAssetBed ?? {};
+  return (
+    <div className="flex items-center justify-between tracking-wide">
+      <div className="flex flex-col gap-2 md:flex-row md:items-center">
+        {patient ? (
+          <Link
+            href={`/facility/${patient.last_consultation?.facility}/patient/${patient.id}/consultation/${patient.last_consultation?.id}`}
+            className="font-bold uppercase text-white"
+          >
+            {patient?.name}
+          </Link>
+        ) : (
+          <span className="flex items-center gap-1 text-gray-500">
+            <CareIcon className="care-l-ban" />
+            No Patient
+          </span>
+        )}
+        {patient && (
+          <span className="text-xs font-bold text-gray-400 md:text-sm">
+            {patient.age}y;{" "}
+            {GENDER_TYPES.find((g) => g.id === patient.gender)?.icon}
+          </span>
+        )}
+      </div>
+      <div className="flex flex-col items-end gap-2 text-xs md:flex-row md:items-center md:text-sm">
+        {bed && (
+          <Link
+            className="flex flex-col items-end gap-2 text-gray-500 md:flex-row md:items-center"
+            href={`/facility/${patient?.facility_object?.id}/location/${bed?.location_object?.id}/beds`}
+          >
+            <span className="flex items-center gap-1 hover:text-white">
+              <CareIcon className="care-l-bed text-sm md:text-base" />
+              <span>{bed.name}</span>
+            </span>
+            <span className="flex items-center gap-1 hover:text-white">
+              <CareIcon className="care-l-location-point text-sm md:text-base" />
+              <span>{bed.location_object?.name}</span>
+            </span>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default VitalsMonitorHeader;
