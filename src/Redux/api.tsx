cd23b661f3ef08e9e58c93fd449004ae51bd0019
Rithvik Nishad <mail@rithviknishad.dev>
2024-03-04 19:09:06 +0530

    Adds support for viewing discharged patients of a facility (#7148)
    * chore: fix type errors
    
    * Implements Discharged Patients List route
    
    * Add discharged patients view button in facility home
    
    * hide live/discharged switch tab for non state admins

--- src/Redux/api.tsx
+++ src/Redux/api.tsx
@@ -929,8 +929,13 @@ const routes = {
     path: "/api/v1/consultation/{id}/discharge_patient/",
     method: "POST",
   },
-  //Profile
+  listFacilityDischargedPatients: {
+    path: "/api/v1/facility/{facility_external_id}/discharged_patients/",
+    method: "GET",
+    TRes: Type<PaginatedResponse<PatientModel>>(),
+  },
 
+  //Profile
   checkUsername: {
     path: "/api/v1/users/{username}/check_availability/",
     method: "GET",
