6ad89e4f4f73ae70a865cb7b61654e68bb0df994
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-03-08 20:13:06 +0530

    fix ui layout of comment-box in shift page

--- src/Components/Shifting/CommentsSection.tsx
+++ src/Components/Shifting/CommentsSection.tsx
@@ -9,10 +9,63 @@ import { IComment } from "../Resource/models";
 import PaginatedList from "../../CAREUI/misc/PaginatedList";
 import request from "../../Utils/request/request";
 
-interface CommentSectionProps {
+interface CommentProps {
   id: string;
+  comment: string;
+  created_by_object: any;
+  modified_date: string;
 }
-const CommentSection = (props: CommentSectionProps) => {
+
+const Comment = ({
+  id,
+  comment,
+  created_by_object,
+  modified_date,
+}: CommentProps) => {
+  const { t } = useTranslation();
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+  };
+
+  const truncatedComment = comment.split(" ").slice(0, 20).join(" ");
+  const remainingComment = comment.split(" ").slice(20).join(" ");
+
+  return (
+    <div
+      key={id}
+      className="mt-4 flex  flex-col rounded-lg border border-gray-500 bg-white p-4 text-gray-800"
+    >
+      <div className="flex  w-full ">
+        <p className="text-justify">
+          {expanded ? comment : truncatedComment}
+          {!expanded && remainingComment && (
+            <button onClick={toggleExpanded} className="ml-2 text-blue-500">
+              Read more
+            </button>
+          )}
+        </p>
+      </div>
+      <div className="mt-3">
+        <span className="text-xs text-gray-500">
+          {modified_date ? formatDateTime(modified_date) : "-"}
+        </span>
+      </div>
+      <div className=" mr-auto flex items-center rounded-md border bg-gray-100 py-1 pl-2 pr-3">
+        <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary-700 p-1 uppercase text-white">
+          {created_by_object?.first_name?.charAt(0) || t("unknown")}
+        </div>
+        <span className="pl-2 text-sm text-gray-700">
+          {created_by_object?.first_name || t("unknown")}{" "}
+          {created_by_object?.last_name}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+const CommentSection = ({ id }: { id: string }) => {
   const [commentBox, setCommentBox] = useState("");
   const { t } = useTranslation();
 
@@ -27,21 +80,17 @@ const CommentSection = (props: CommentSectionProps) => {
       return;
     }
     const { res } = await request(routes.addShiftComments, {
-      pathParams: { id: props.id },
+      pathParams: { id: id },
       body: payload,
     });
     if (res?.ok) {
       Notification.Success({ msg: t("comment_added_successfully") });
-
       setCommentBox("");
     }
   };
 
   return (
-    <PaginatedList
-      route={routes.getShiftComments}
-      pathParams={{ id: props.id }}
-    >
+    <PaginatedList route={routes.getShiftComments} pathParams={{ id: id }}>
       {(_, query) => (
         <div className="flex w-full flex-col">
           <textarea
@@ -81,36 +130,3 @@ const CommentSection = (props: CommentSectionProps) => {
 };
 
 export default CommentSection;
-
-export const Comment = ({
-  id,
-  comment,
-  created_by_object,
-  modified_date,
-}: IComment) => {
-  const { t } = useTranslation();
-  return (
-    <div
-      key={id}
-      className="mt-4 flex w-full flex-col rounded-lg border border-gray-300 bg-white p-4 text-gray-800"
-    >
-      <div className="flex  w-full ">
-        <p className="text-justify">{comment}</p>
-      </div>
-      <div className="mt-3">
-        <span className="text-xs text-gray-500">
-          {modified_date ? formatDateTime(modified_date) : "-"}
-        </span>
-      </div>
-      <div className=" mr-auto flex items-center rounded-md border bg-gray-100 py-1 pl-2 pr-3">
-        <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary-700 p-1 uppercase text-white">
-          {created_by_object?.first_name?.charAt(0) || t("unknown")}
-        </div>
-        <span className="pl-2 text-sm text-gray-700">
-          {created_by_object?.first_name || t("unknown")}{" "}
-          {created_by_object?.last_name}
-        </span>
-      </div>
-    </div>
-  );
-};
