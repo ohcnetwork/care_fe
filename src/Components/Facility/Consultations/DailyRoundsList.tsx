40bd84ab05f9cd2c18b8681e02d07bb36011b84c
Devdeep Ghosh <devarmforce01@gmail.com>
2024-03-11 19:31:19 +0530

    removed filter overlapping with card

--- src/Components/Facility/Consultations/DailyRoundsList.tsx
+++ src/Components/Facility/Consultations/DailyRoundsList.tsx
@@ -41,7 +41,7 @@ export default function DailyRoundsList({ consultation }: Props) {
             />
           </div>
 
-          <div className="-mt-2 flex max-h-[100vh] min-h-full w-full flex-col gap-4">
+          <div className="flex max-h-[100vh] min-h-full w-full flex-col gap-4">
             <div className="flex flex-col gap-4 overflow-y-auto overflow-x-hidden px-3">
               <PaginatedList.WhenEmpty className="flex w-full justify-center border-b border-gray-200 bg-white p-5 text-center text-2xl font-bold text-gray-500">
                 <span className="flex justify-center rounded-lg bg-white p-3 text-gray-700  ">
