2198895001601d1d19e62caf877234a94cbc2a53
bishwas-10 <120932783+bishwas-10@users.noreply.github.com>
2024-03-12 10:23:21 +0545

    removed console log

--- src/Components/Facility/Consultations/Feed.tsx
+++ src/Components/Facility/Consultations/Feed.tsx
@@ -400,7 +400,7 @@ export const Feed: React.FC<IFeedProps> = ({ consultationId }) => {
   }
 
   if (getConsultationLoading) return <Loading />;
-  console.log(currentPreset);
+
   return (
     <div className="flex h-[calc(100vh-1.5rem)] flex-col px-2">
       <div className="flex flex-wrap items-center justify-between gap-2">
