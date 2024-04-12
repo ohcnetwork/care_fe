84493b617daf4cd28bd7400f36271dc9aa0d0940
khavinshankar <khavinshankar@gmail.com>
2024-04-12 11:56:28 +0530

    minor ui fix

--- src/Components/Form/MultiSelectMenuV2.tsx
+++ src/Components/Form/MultiSelectMenuV2.tsx
@@ -130,7 +130,7 @@ const MultiSelectMenuV2 = <T, V>(props: Props<T, V>) => {
                 </Listbox.Button>
               </div>
               <DropdownTransition show={open}>
-                <Listbox.Options className="cui-dropdown-base absolute top-12">
+                <Listbox.Options className="cui-dropdown-base absolute top-full">
                   {options.map((option, index) => (
                     <Listbox.Option
                       id={`${props.id}-option-${index}`}
@@ -187,7 +187,10 @@ export const MultiSelectOptionChip = (props: MultiSelectOptionChipProps) => {
       {props.onRemove && (
         <p
           className="cursor-pointer rounded-full hover:bg-white"
-          onClick={props.onRemove}
+          onClick={(e) => {
+            e.stopPropagation();
+            props.onRemove?.();
+          }}
         >
           <CareIcon icon="l-times" className="text-base" />
         </p>
