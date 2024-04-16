51eb35c22477f18689bef881de5dc381b0ee6977
Manasvi Gaur <120303997+manasvi-gaur@users.noreply.github.com>
2024-04-16 12:11:07 +0530

    Update src/Components/Form/FormFields/TextFormField.tsx
    Co-authored-by: Bodhish Thomas <bodhish@gmail.com>

--- src/Components/Form/FormFields/TextFormField.tsx
+++ src/Components/Form/FormFields/TextFormField.tsx
@@ -105,7 +105,7 @@ const TextFormField = forwardRef((props: TextFormFieldProps, ref) => {
         </div>
       ) : (
         <>
-          <div className="absolute inset-y-0 right-7 flex translate-y-0 items-center pr-3 opacity-100 transition-all delay-300 duration-500 ease-in-out peer-focus:translate-y-1 peer-focus:opacity-0">
+          <div className="absolute inset-y-0 right-0 md:right-6 flex translate-y-0 items-center pr-3 opacity-100 transition-all delay-300 duration-500 ease-in-out peer-focus:translate-y-1 peer-focus:opacity-0">
             {trailing}
           </div>
           <div className="absolute inset-y-0 right-0 md:right-6 flex -translate-y-1 items-center pr-3 opacity-0 transition-all delay-300 duration-500 ease-in-out peer-focus:translate-y-0 peer-focus:opacity-100">
