c81225f9dd34d59040b9b339b2b52c85c8fa8b74
Aakash Singh <mail@singhaakash.dev>
2024-04-30 12:44:01 +0530

    upgrade storybook to v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -8,11 +9,7 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: "@storybook/react-vite",
-    options: {
-      builder: {
-        viteConfigPath: "./.storybook/vite.config.mts",
-      },
-    },
+    options: {},
   },
   docs: {
     autodocs: "tag",
