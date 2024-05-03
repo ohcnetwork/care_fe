5f8da4a7ccae35c33e307572054beb56812caf11
rithviknishad <mail@rithviknishad.dev>
2024-05-03 16:20:06 +0530

    remove support for copying from previous log update

--- src/Components/Patient/DailyRounds.tsx
+++ src/Components/Patient/DailyRounds.tsx
@@ -20,7 +20,6 @@ import { SymptomsSelect } from "../Common/SymptomsSelect";
 import TemperatureFormField from "../Common/TemperatureFormField";
 import { Cancel, Submit } from "../Common/components/ButtonV2";
 import Page from "../Common/components/Page";
-import CheckBoxFormField from "../Form/FormFields/CheckBoxFormField";
 import RangeAutocompleteFormField from "../Form/FormFields/RangeAutocompleteFormField";
 import { SelectFormField } from "../Form/FormFields/SelectFormField";
 import TextAreaFormField from "../Form/FormFields/TextAreaFormField";
@@ -47,7 +46,6 @@ const initForm: any = {
   admitted_to: "",
   taken_at: null,
   rounds_type: "NORMAL",
-  clone_last: false,
   systolic: null,
   diastolic: null,
   pulse: null,
@@ -111,7 +109,6 @@ export const DailyRounds = (props: any) => {
   const [consultationSuggestion, setConsultationSuggestion] = useState<any>("");
   const [prevReviewInterval, setPreviousReviewInterval] = useState(-1);
   const [prevAction, setPreviousAction] = useState("NO_ACTION");
-  const [hasPreviousLog, setHasPreviousLog] = useState(false);
   const [initialData, setInitialData] = useState<any>({
     ...initForm,
     action: "",
@@ -184,13 +181,6 @@ export const DailyRounds = (props: any) => {
       setPatientName("");
       setFacilityName("");
     }
-    if (consultationId && !id) {
-      const { data } = await request(routes.getDailyReports, {
-        pathParams: { consultationId },
-        query: { limit: 1, offset: 0 },
-      });
-      setHasPreviousLog(!!data?.count);
-    }
     dispatch({ type: "set_form", form: formData });
     setInitialData(formData);
   }, [consultationId, id, patientId]);
@@ -235,55 +225,18 @@ export const DailyRounds = (props: any) => {
     return !invalidForm;
   };
 
-  const handleSubmit = async (e: any) => {
+  const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
     const validForm = validateForm();
     if (validForm) {
       setIsLoading(true);
-      const baseData = {
-        clone_last: state.form.clone_last ?? false,
+      const data = {
         rounds_type: state.form.rounds_type,
         patient_category: state.form.patient_category,
         taken_at: state.form.taken_at
           ? state.form.taken_at
           : new Date().toISOString(),
       };
-
-      let data: any;
-
-      if (state.form.clone_last !== true) {
-        data = {
-          ...baseData,
-          additional_symptoms: state.form.additional_symptoms,
-          other_symptoms: state.form.additional_symptoms?.includes(9)
-            ? state.form.other_symptoms
-            : undefined,
-          admitted_to:
-            (state.form.admitted === "Select"
-              ? undefined
-              : state.form.admitted_to) || undefined,
-          physical_examination_info: state.form.physical_examination_info,
-          other_details: state.form.other_details,
-          consultation: consultationId,
-          action: prevAction,
-          review_interval: Number(prevReviewInterval),
-        };
-        if (["NORMAL", "TELEMEDICINE"].includes(state.form.rounds_type)) {
-          data = {
-            ...data,
-            bp: state.form.bp ?? {},
-            pulse: state.form.pulse ?? null,
-            resp: state.form.resp ?? null,
-            temperature: state.form.temperature ?? null,
-            rhythm: state.form.rhythm || 0,
-            rhythm_detail: state.form.rhythm_detail,
-            ventilator_spo2: state.form.ventilator_spo2 ?? null,
-            consciousness_level: state.form.consciousness_level,
-          };
-        }
-      } else {
-        data = baseData;
-      }
 
       if (id) {
         const { data: obj } = await request(routes.updateDailyReport, {
@@ -321,25 +274,13 @@ export const DailyRounds = (props: any) => {
             msg: "Consultation Updates details created successfully",
           });
           if (["NORMAL", "TELEMEDICINE"].includes(state.form.rounds_type)) {
-            if (data.clone_last) {
-              navigate(
-                `/facility/${facilityId}/patient/${patientId}/consultation/${consultationId}/daily-rounds/${obj.id}/update`,
-              );
-            } else {
-              navigate(
-                `/facility/${facilityId}/patient/${patientId}/consultation/${consultationId}`,
-              );
-            }
+            navigate(
+              `/facility/${facilityId}/patient/${patientId}/consultation/${consultationId}`,
+            );
           } else {
-            if (data.clone_last) {
-              navigate(
-                `/facility/${facilityId}/patient/${patientId}/consultation/${consultationId}/daily-rounds/${obj.id}/update`,
-              );
-            } else {
-              navigate(
-                `/facility/${facilityId}/patient/${patientId}/consultation/${consultationId}/daily_rounds/${obj.id}/update`,
-              );
-            }
+            navigate(
+              `/facility/${facilityId}/patient/${patientId}/consultation/${consultationId}/daily_rounds/${obj.id}/update`,
+            );
           }
         }
       }
@@ -456,199 +397,185 @@ export const DailyRounds = (props: any) => {
           </div>
         </div>
 
-        {!id && hasPreviousLog && (
-          <CheckBoxFormField
-            {...field("clone_last")}
-            label="Copy values from previous log?"
+        <div className="grid grid-cols-1 gap-x-6 md:grid-cols-2">
+          <TextAreaFormField
+            {...field("physical_examination_info")}
+            label="Physical Examination Info"
+            rows={5}
           />
-        )}
+          <TextAreaFormField
+            {...field("other_details")}
+            label="Other Details"
+            rows={5}
+          />
+          <SymptomsSelect
+            {...field("additional_symptoms")}
+            label="Symptoms"
+            className="md:col-span-2"
+          />
 
-        {(!state.form.clone_last || id) && (
-          <div className="grid grid-cols-1 gap-x-6 md:grid-cols-2">
-            <TextAreaFormField
-              {...field("physical_examination_info")}
-              label="Physical Examination Info"
-              rows={5}
-            />
-            <TextAreaFormField
-              {...field("other_details")}
-              label="Other Details"
-              rows={5}
-            />
-            <SymptomsSelect
-              {...field("additional_symptoms")}
-              label="Symptoms"
-              className="md:col-span-2"
-            />
+          {state.form.additional_symptoms?.includes(9) && (
+            <div className="md:col-span-2">
+              <TextAreaFormField
+                {...field("other_symptoms")}
+                required
+                label="Other Symptoms Details"
+                placeholder="Enter the other symptoms here"
+              />
+            </div>
+          )}
 
-            {state.form.additional_symptoms?.includes(9) && (
-              <div className="md:col-span-2">
-                <TextAreaFormField
-                  {...field("other_symptoms")}
-                  required
-                  label="Other Symptoms Details"
-                  placeholder="Enter the other symptoms here"
-                />
-              </div>
-            )}
+          <SelectFormField
+            {...field("action")}
+            label="Action"
+            options={TELEMEDICINE_ACTIONS}
+            optionLabel={(option) => option.desc}
+            optionValue={(option) => option.text}
+            value={prevAction}
+            onChange={(event) => {
+              handleFormFieldChange(event);
+              setPreviousAction(event.value);
+            }}
+          />
 
-            <SelectFormField
-              {...field("action")}
-              label="Action"
-              options={TELEMEDICINE_ACTIONS}
-              optionLabel={(option) => option.desc}
-              optionValue={(option) => option.text}
-              value={prevAction}
-              onChange={(event) => {
-                handleFormFieldChange(event);
-                setPreviousAction(event.value);
-              }}
-            />
+          <SelectFormField
+            {...field("review_interval")}
+            label="Review After"
+            labelSuffix={getExpectedReviewTime()}
+            options={REVIEW_AT_CHOICES}
+            optionLabel={(option) => option.text}
+            optionValue={(option) => option.id}
+            value={prevReviewInterval}
+            onChange={(event) => {
+              handleFormFieldChange(event);
+              setPreviousReviewInterval(Number(event.value));
+            }}
+          />
 
-            <SelectFormField
-              {...field("review_interval")}
-              label="Review After"
-              labelSuffix={getExpectedReviewTime()}
-              options={REVIEW_AT_CHOICES}
-              optionLabel={(option) => option.text}
-              optionValue={(option) => option.id}
-              value={prevReviewInterval}
-              onChange={(event) => {
-                handleFormFieldChange(event);
-                setPreviousReviewInterval(Number(event.value));
-              }}
-            />
+          {["NORMAL", "TELEMEDICINE"].includes(state.form.rounds_type) && (
+            <>
+              <h3 className="mb-6 md:col-span-2">Vitals</h3>
 
-            {["NORMAL", "TELEMEDICINE"].includes(state.form.rounds_type) && (
-              <>
-                <h3 className="mb-6 md:col-span-2">Vitals</h3>
+              <BloodPressureFormField {...field("bp")} label="Blood Pressure" />
 
-                <BloodPressureFormField
-                  {...field("bp")}
-                  label="Blood Pressure"
-                />
+              <RangeAutocompleteFormField
+                {...field("pulse")}
+                label="Pulse"
+                unit="bpm"
+                start={0}
+                end={200}
+                step={1}
+                thresholds={[
+                  {
+                    value: 0,
+                    className: "text-danger-500",
+                    label: "Bradycardia",
+                  },
+                  {
+                    value: 40,
+                    className: "text-primary-500",
+                    label: "Normal",
+                  },
+                  {
+                    value: 100,
+                    className: "text-danger-500",
+                    label: "Tachycardia",
+                  },
+                ]}
+              />
 
-                <RangeAutocompleteFormField
-                  {...field("pulse")}
-                  label="Pulse"
-                  unit="bpm"
-                  start={0}
-                  end={200}
-                  step={1}
-                  thresholds={[
-                    {
-                      value: 0,
-                      className: "text-danger-500",
-                      label: "Bradycardia",
-                    },
-                    {
-                      value: 40,
-                      className: "text-primary-500",
-                      label: "Normal",
-                    },
-                    {
-                      value: 100,
-                      className: "text-danger-500",
-                      label: "Tachycardia",
-                    },
-                  ]}
-                />
+              <TemperatureFormField
+                {...field("temperature")}
+                label="Temperature"
+              />
 
-                <TemperatureFormField
-                  {...field("temperature")}
-                  label="Temperature"
-                />
+              <RangeAutocompleteFormField
+                {...field("resp")}
+                label="Respiratory Rate"
+                unit="bpm"
+                start={0}
+                end={150}
+                step={1}
+                thresholds={[
+                  {
+                    value: 0,
+                    className: "text-danger-500",
+                    label: "Bradypnea",
+                  },
+                  {
+                    value: 12,
+                    className: "text-primary-500",
+                    label: "Normal",
+                  },
+                  {
+                    value: 16,
+                    className: "text-danger-500",
+                    label: "Tachypnea",
+                  },
+                ]}
+              />
 
-                <RangeAutocompleteFormField
-                  {...field("resp")}
-                  label="Respiratory Rate"
-                  unit="bpm"
-                  start={0}
-                  end={150}
-                  step={1}
-                  thresholds={[
-                    {
-                      value: 0,
-                      className: "text-danger-500",
-                      label: "Bradypnea",
-                    },
-                    {
-                      value: 12,
-                      className: "text-primary-500",
-                      label: "Normal",
-                    },
-                    {
-                      value: 16,
-                      className: "text-danger-500",
-                      label: "Tachypnea",
-                    },
-                  ]}
-                />
+              <RangeAutocompleteFormField
+                {...field("ventilator_spo2")}
+                label="SPO2"
+                unit="%"
+                start={0}
+                end={100}
+                step={1}
+                thresholds={[
+                  {
+                    value: 0,
+                    className: "text-danger-500",
+                    label: "Low",
+                  },
+                  {
+                    value: 90,
+                    className: "text-primary-500",
+                    label: "Normal",
+                  },
+                  {
+                    value: 100,
+                    className: "text-danger-500",
+                    label: "High",
+                  },
+                ]}
+              />
 
-                <RangeAutocompleteFormField
-                  {...field("ventilator_spo2")}
-                  label="SPO2"
-                  unit="%"
-                  start={0}
-                  end={100}
-                  step={1}
-                  thresholds={[
-                    {
-                      value: 0,
-                      className: "text-danger-500",
-                      label: "Low",
-                    },
-                    {
-                      value: 90,
-                      className: "text-primary-500",
-                      label: "Normal",
-                    },
-                    {
-                      value: 100,
-                      className: "text-danger-500",
-                      label: "High",
-                    },
-                  ]}
-                />
+              <SelectFormField
+                {...field("rhythm")}
+                label="Rhythm"
+                options={RHYTHM_CHOICES}
+                optionLabel={(option) => option.desc}
+                optionValue={(option) => option.id}
+              />
 
-                <SelectFormField
-                  {...field("rhythm")}
-                  label="Rhythm"
-                  options={RHYTHM_CHOICES}
-                  optionLabel={(option) => option.desc}
-                  optionValue={(option) => option.id}
-                />
+              <TextAreaFormField
+                {...field("rhythm_detail")}
+                className="md:col-span-1"
+                label="Rhythm Description"
+                rows={7}
+              />
 
-                <TextAreaFormField
-                  {...field("rhythm_detail")}
-                  className="md:col-span-1"
-                  label="Rhythm Description"
-                  rows={7}
-                />
-
-                <RadioFormField
-                  label="Level Of Consciousness"
-                  {...field("consciousness_level")}
-                  options={CONSCIOUSNESS_LEVEL.map((level) => ({
-                    label: level.text,
-                    value: level.id,
-                  }))}
-                  optionDisplay={(option) => option.label}
-                  optionValue={(option) => option.value}
-                  containerClassName="grid gap-1 grid-cols-1"
-                />
-              </>
-            )}
-          </div>
-        )}
+              <RadioFormField
+                label="Level Of Consciousness"
+                {...field("consciousness_level")}
+                options={CONSCIOUSNESS_LEVEL.map((level) => ({
+                  label: level.text,
+                  value: level.id,
+                }))}
+                optionDisplay={(option) => option.label}
+                optionValue={(option) => option.value}
+                containerClassName="grid gap-1 grid-cols-1"
+              />
+            </>
+          )}
+        </div>
 
         <div className="mt-4 flex flex-col-reverse justify-end gap-2 md:flex-row">
           <Cancel onClick={() => goBack()} />
           <Submit
             disabled={
               buttonText === "Save" &&
-              state.form.clone_last !== null &&
-              !state.form.clone_last &&
               formFields.every(
                 (field: string) => state.form[field] == initialData[field],
               ) &&
