5a48c589d4d16cf488a760c25cbd3ec524218f49
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2024-05-15 11:19:49 +0530

    Update src/Common/constants.tsx

--- src/Common/constants.tsx
+++ src/Common/constants.tsx
@@ -230,7 +230,7 @@ export const getBedTypes = ({
 };
 
 export const DOCTOR_SPECIALIZATION: Array<OptionsType> = [
-  { id: 1, text: "General Medicine Doctors", desc: "bg-doctors-general" },
+  { id: 1, text: "General Medicine", desc: "bg-doctors-general" },
   { id: 2, text: "Pulmonology Doctors", desc: "bg-doctors-general" },
   { id: 3, text: "Intensivist", desc: "bg-doctors-general" },
   { id: 4, text: "Pediatrician", desc: "bg-doctors-general" },
