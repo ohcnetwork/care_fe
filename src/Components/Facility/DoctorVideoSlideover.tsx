43dd23c9676eadfd87441867355cba2cdea71ef2
Khavin Shankar <khavinshankar@gmail.com>
2024-06-03 16:39:07 +0530

    clean special characters in phone number in whatsapp link

--- src/Components/Facility/DoctorVideoSlideover.tsx
+++ src/Components/Facility/DoctorVideoSlideover.tsx
@@ -172,7 +172,7 @@ function UserListItem({ user }: { user: UserAnnotatedWithGroup }) {
   function connectOnWhatsApp(e: React.MouseEvent<HTMLAnchorElement>) {
     e.stopPropagation();
     if (!user.alt_phone_number) return;
-    const phoneNumber = user.alt_phone_number;
+    const phoneNumber = user.alt_phone_number?.replace(/\D+/g, "");
     const message = `${courtesyTitle(user)} ${user.first_name} ${user.last_name}, I have a query regarding a patient.\n\nPatient Link: ${window.location.href}`;
     const encodedMessage = encodeURIComponent(message);
     const whatsappAppURL = `whatsapp://send?phone=${phoneNumber}&text=${encodedMessage}`;
