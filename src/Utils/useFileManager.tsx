df8817d1bbb2ade9d235631b15973e43ce9a7be9
Rithvik Nishad <mail@rithviknishad.dev>
2024-07-17 22:30:31 +0530

    Fix usages of gray shade that had spelling mistake with grey (#8167)
    * Fix usages of gray shade that had spelling mistake with grey
    
    * fix colors
    
    * fix colors
    
    * fixes 8169; adds missing gray color
    
    * disable prettier on eslint

--- src/Utils/useFileManager.tsx
+++ src/Utils/useFileManager.tsx
@@ -224,10 +224,12 @@ export default function useFileManager(
                 className="text-lg text-danger-500"
               />
             </div>
-            <div className="text-grey-200 text-sm">
+            <div className="text-sm">
               <h1 className="text-xl text-black">Archive File</h1>
-              This action is irreversible. Once a file is archived it cannot be
-              unarchived.
+              <span className="text-sm text-secondary-600">
+                This action is irreversible. Once a file is archived it cannot
+                be unarchived.
+              </span>
             </div>
           </div>
         }
