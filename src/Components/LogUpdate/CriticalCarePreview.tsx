8295e03d9ceb0b0fdfa29ce47ebd18581d06f5b9
rithviknishad <mail@rithviknishad.dev>
2024-08-13 11:04:27 +0530

    Resp. support section preview

--- src/Components/LogUpdate/CriticalCarePreview.tsx
+++ src/Components/LogUpdate/CriticalCarePreview.tsx
@@ -8,9 +8,11 @@ import React from "react";
 import { ABGAnalysisFields } from "./Sections/ABGAnalysis";
 import {
   classNames,
+  properRoundOf,
   rangeValueDescription,
   ValueDescription,
 } from "../../Utils/utils";
+import { VentilatorFields } from "./Sections/RespiratorySupport/Ventilator";
 
 type Props = {
   facilityId: string;
@@ -31,6 +33,11 @@ export default function CriticalCarePreview(props: Props) {
   if (!data) {
     return <Loading />;
   }
+
+  const tOption = (prefix: string, key: keyof typeof data) => {
+    const value = data[key];
+    return value && t(`${prefix}__${value}`);
+  };
 
   return (
     <div className="w-full transition-all duration-200 ease-in-out md:mx-auto md:max-w-5xl md:pt-8">
@@ -58,10 +65,7 @@ export default function CriticalCarePreview(props: Props) {
         <Section title="Neurological Monitoring">
           <Detail
             label="Level of Consciousness"
-            value={
-              data.consciousness_level &&
-              t(`CONSCIOUSNESS_LEVEL__${data.consciousness_level}`)
-            }
+            value={tOption("CONSCIOUSNESS_LEVEL", "consciousness_level")}
           />
           <div className="grid gap-x-4 gap-y-2 py-2 md:grid-cols-2">
             {(["left", "right"] as const).map((dir) => (
@@ -83,10 +87,10 @@ export default function CriticalCarePreview(props: Props) {
                 )}
                 <Detail
                   label="Light Reaction"
-                  value={
-                    data[`${dir}_pupil_light_reaction`] &&
-                    t(`PUPIL_REACTION__${data[`${dir}_pupil_light_reaction`]}`)
-                  }
+                  value={tOption(
+                    "PUPIL_REACTION",
+                    `${dir}_pupil_light_reaction`,
+                  )}
                 />
                 <Detail
                   label="Light Reaction Description"
@@ -130,10 +134,7 @@ export default function CriticalCarePreview(props: Props) {
                       {key.replace("extremity", "").replaceAll("_", " ")}
                     </span>
                   }
-                  value={
-                    data[`limb_response_${key}`] &&
-                    t(`LIMB_RESPONSE__${data[`limb_response_${key}`]}`)
-                  }
+                  value={tOption("LIMB_RESPONSE", `limb_response_${key}`)}
                 />
               </div>
             ))}
@@ -170,16 +171,127 @@ export default function CriticalCarePreview(props: Props) {
           <Detail label="Dosage" value={data.insulin_intake_dose} />
           <Detail
             label="Frequency"
-            value={
-              data.insulin_intake_frequency &&
-              t(`INSULIN_INTAKE_FREQUENCY__${data.insulin_intake_frequency}`)
-            }
+            value={tOption(
+              "INSULIN_INTAKE_FREQUENCY",
+              "insulin_intake_frequency",
+            )}
           />
         </Section>
 
         <Section title="Dialysis">
-          <Detail label="Fluid Balance" value={data.dialysis_fluid_balance} />
-          <Detail label="Net Balance" value={data.dialysis_net_balance} />
+          <Detail
+            label="Fluid Balance"
+            value={data.dialysis_fluid_balance}
+            suffix="ml/h"
+          />
+          <Detail
+            label="Net Balance"
+            value={data.dialysis_net_balance}
+            suffix="ml/h"
+          />
+        </Section>
+
+        <Section title="Respiratory Support">
+          <Detail
+            label="Bilateral Air Entry"
+            value={data.bilateral_air_entry}
+          />
+          <RangeDetail
+            label={
+              <span>
+                EtCO<sub>2</sub>
+              </span>
+            }
+            value={data.etco2}
+            valueDescriptions={rangeValueDescription({ low: 34, high: 45 })}
+            max={200}
+            unit="mmHg"
+          />
+
+          <Detail
+            label="Interface"
+            value={tOption("RESPIRATORY_SUPPORT", "ventilator_interface")}
+          />
+
+          {data.ventilator_interface === "OXYGEN_SUPPORT" && (
+            <>
+              <Detail
+                label="Oxygen Modality"
+                value={tOption("OXYGEN_MODALITY", "ventilator_oxygen_modality")}
+              />
+              {data.ventilator_oxygen_modality_flow_rate ?? "-"}
+              {data.ventilator_oxygen_modality === "HIGH_FLOW_NASAL_CANNULA" ? (
+                <>
+                  <RangeDetail
+                    label="Flow Rate"
+                    value={data.ventilator_oxygen_modality_flow_rate}
+                    max={70}
+                    unit=""
+                    valueDescriptions={rangeValueDescription({
+                      low: 34,
+                      high: 60,
+                    })}
+                  />
+                  <RangeDetail
+                    label={
+                      <span>
+                        FiO<sub>2</sub>
+                      </span>
+                    }
+                    value={data.ventilator_fi02}
+                    max={100}
+                    unit="%"
+                    valueDescriptions={rangeValueDescription({ high: 60 })}
+                  />
+                </>
+              ) : (
+                <RangeDetail
+                  label="Oxygen Flow Rate"
+                  value={data.ventilator_oxygen_modality_oxygen_rate}
+                  max={50}
+                  unit=""
+                  valueDescriptions={rangeValueDescription({
+                    low: 4,
+                    high: 10,
+                  })}
+                />
+              )}
+              <RangeDetail
+                label={
+                  <span>
+                    SpO<sub>2</sub>
+                  </span>
+                }
+                value={data.ventilator_spo2}
+                max={100}
+                unit="%"
+                valueDescriptions={rangeValueDescription({ low: 89 })}
+              />
+            </>
+          )}
+
+          {(data.ventilator_interface === "INVASIVE" ||
+            data.ventilator_interface === "NON_INVASIVE") && (
+            <>
+              <br />
+              <Detail
+                label="Ventilator Mode"
+                value={tOption("VENTILATOR_MODE", "ventilator_mode")}
+              />
+              {VentilatorFields.map((field) => {
+                const value = data[field.key];
+                return (
+                  <RangeDetail
+                    label={field.label}
+                    value={value}
+                    valueDescriptions={field.valueDescription}
+                    max={field.max}
+                    unit={field.unit}
+                  />
+                );
+              })}
+            </>
+          )}
         </Section>
       </Card>
     </div>
@@ -200,13 +312,20 @@ const Section = (props: { title: string; children: React.ReactNode }) => {
 
 const Detail = (props: {
   label: React.ReactNode;
-  value?: string | number;
+  value?: string | number | boolean;
   suffix?: React.ReactNode;
 }) => {
-  const value = props.value === "" ? null : props.value;
+  let value = props.value;
+  value = value === "" ? undefined : value;
+  value = value === true ? "Yes" : value;
+  value = value === false ? "No" : value;
+
+  value = typeof value === "string" ? parseFloat(value) || value : value;
+  value = typeof value === "number" ? properRoundOf(value) : value;
+
   return (
     <p>
-      <span className="font-semibold">{props.label}: </span>
+      <span className="pr-1 font-semibold">{props.label}: </span>
       {value != null ? (
         <span>
           {value} {props.suffix}
--- src/Components/LogUpdate/Sections/RespiratorySupport/Ventilator.tsx
+++ src/Components/LogUpdate/Sections/RespiratorySupport/Ventilator.tsx
@@ -1,7 +1,95 @@
-import { rangeValueDescription } from "../../../../Utils/utils";
+import {
+  rangeValueDescription,
+  ValueDescription,
+} from "../../../../Utils/utils";
 import RangeFormField from "../../../Form/FormFields/RangeFormField";
+import { DailyRoundsModel } from "../../../Patient/models";
 import { LogUpdateSectionProps } from "../../utils";
 import VentilatorModeSelector from "./VentilatorModeSelector";
+
+export const VentilatorFields = [
+  {
+    key: "ventilator_peep",
+    label: "PEEP",
+    unit: "cm/H2O",
+    min: 0,
+    max: 30,
+    step: 0.1,
+    valueDescription: rangeValueDescription({ low: 10 }),
+  },
+  {
+    key: "ventilator_pip",
+    label: "Peak Inspiratory Pressure (PIP)",
+    unit: "cm H2O",
+    min: 0,
+    max: 100,
+    valueDescription: rangeValueDescription({ low: 11, high: 30 }),
+  },
+  {
+    key: "ventilator_mean_airway_pressure",
+    label: "Mean Airway Pressure (MAP)",
+    unit: "cm H2O",
+    min: 0,
+    max: 40,
+    valueDescription: rangeValueDescription({ low: 11, high: 25 }),
+  },
+  {
+    key: "ventilator_resp_rate",
+    label: "Respiratory Rate Ventilator",
+    unit: "bpm",
+    min: 0,
+    max: 100,
+    valueDescription: rangeValueDescription({ low: 39, high: 60 }),
+  },
+  {
+    key: "ventilator_pressure_support",
+    label: "Pressure Support",
+    unit: "",
+    min: 0,
+    max: 40,
+    valueDescription: rangeValueDescription({ low: 6, high: 15 }),
+  },
+  {
+    key: "ventilator_tidal_volume",
+    label: "Tidal Volume",
+    unit: "ml",
+    min: 0,
+    max: 1000,
+    valueDescription: rangeValueDescription({}),
+  },
+  {
+    key: "ventilator_fi02",
+    label: (
+      <span>
+        FiO<sub>2</sub>
+      </span>
+    ),
+    unit: "%",
+    min: 21,
+    max: 100,
+    valueDescription: rangeValueDescription({ high: 60 }),
+  },
+  {
+    key: "ventilator_spo2",
+    label: (
+      <span>
+        SpO<sub>2</sub>
+      </span>
+    ),
+    unit: "%",
+    min: 0,
+    max: 100,
+    valueDescription: rangeValueDescription({ low: 89 }),
+  },
+] satisfies {
+  key: keyof DailyRoundsModel;
+  label: React.ReactNode;
+  unit: string;
+  min: number;
+  max: number;
+  step?: number;
+  valueDescription: ValueDescription[];
+}[];
 
 const VentilatorRespiratorySupport = ({
   log,
@@ -16,95 +104,20 @@ const VentilatorRespiratorySupport = ({
           onChange={(ventilator_mode) => onChange({ ventilator_mode })}
         />
       </div>
-      <RangeFormField
-        label="PEEP"
-        unit="cm/H2O"
-        name="ventilator_peep"
-        onChange={(c) => onChange({ ventilator_peep: c.value })}
-        value={log.ventilator_peep}
-        min={0}
-        max={30}
-        step={0.1}
-        valueDescriptions={rangeValueDescription({ low: 10 })}
-      />
-      <RangeFormField
-        label="Peak Inspiratory Pressure (PIP)"
-        unit="cm H2O"
-        name="ventilator_pip"
-        onChange={(c) => onChange({ ventilator_pip: c.value })}
-        value={log.ventilator_pip}
-        min={0}
-        max={100}
-        valueDescriptions={rangeValueDescription({ low: 11, high: 30 })}
-      />
-      <RangeFormField
-        label="Mean Airway Pressure (MAP)"
-        unit="cm H2O"
-        name="ventilator_mean_airway_pressure"
-        onChange={(c) => onChange({ ventilator_mean_airway_pressure: c.value })}
-        value={log.ventilator_mean_airway_pressure}
-        min={0}
-        max={40}
-        valueDescriptions={rangeValueDescription({ low: 11, high: 25 })}
-      />
-      <RangeFormField
-        label="Respiratory Rate Ventilator"
-        unit="bpm"
-        name="ventilator_resp_rate"
-        onChange={(c) => onChange({ ventilator_resp_rate: c.value })}
-        value={log.ventilator_resp_rate}
-        min={0}
-        max={100}
-        valueDescriptions={rangeValueDescription({ low: 39, high: 60 })}
-      />
-      <RangeFormField
-        label="Pressure Support"
-        unit=""
-        name="ventilator_pressure_support"
-        onChange={(c) => onChange({ ventilator_pressure_support: c.value })}
-        value={log.ventilator_pressure_support}
-        min={0}
-        max={40}
-        valueDescriptions={rangeValueDescription({ low: 6, high: 15 })}
-      />
-      <RangeFormField
-        label="Tidal Volume"
-        unit="ml"
-        name="ventilator_tidal_volume"
-        onChange={(c) => onChange({ ventilator_tidal_volume: c.value })}
-        value={log.ventilator_tidal_volume}
-        min={0}
-        max={1000}
-        valueDescriptions={rangeValueDescription({})}
-      />
-      <RangeFormField
-        label={
-          <span>
-            FiO<sub>2</sub>
-          </span>
-        }
-        unit="%"
-        name="ventilator_fi02"
-        onChange={(c) => onChange({ ventilator_fi02: c.value })}
-        value={log.ventilator_fi02}
-        min={21}
-        max={100}
-        valueDescriptions={rangeValueDescription({ high: 60 })}
-      />
-      <RangeFormField
-        label={
-          <span>
-            SpO<sub>2</sub>
-          </span>
-        }
-        unit="%"
-        name="ventilator_spo2"
-        onChange={(c) => onChange({ ventilator_spo2: c.value })}
-        value={log.ventilator_spo2}
-        min={0}
-        max={100}
-        valueDescriptions={rangeValueDescription({ low: 89 })}
-      />
+      {VentilatorFields.map((field, index) => (
+        <RangeFormField
+          key={index}
+          label={field.label}
+          unit={field.unit}
+          name={field.key}
+          onChange={(c) => onChange({ [field.key]: c.value })}
+          value={log[field.key] as number}
+          min={field.min}
+          max={field.max}
+          step={field.step || 1}
+          valueDescriptions={field.valueDescription}
+        />
+      ))}
     </div>
   );
 };
