9d2640d891ce1631f55dd1226739bbb751c0c027
Sagar Sharma <sagarsharma2809@gmail.com>
2024-08-22 15:27:46 +0530

    Corrected Spelling mistake of daily rounds fields: ventilator_fi02 to ventilator_fio2 in frontend types definitions and usages (#8268)
    Co-authored-by: rithviknishad <mail@rithviknishad.dev>

--- src/Components/Facility/models.tsx
+++ src/Components/Facility/models.tsx
@@ -286,7 +286,7 @@ export type ABGPlotsFields =
   | "lactate"
   | "sodium"
   | "potassium"
-  | "ventilator_fi02";
+  | "ventilator_fio2";
 
 export type ABGPlotsRes = {
   ph: string;
@@ -297,7 +297,7 @@ export type ABGPlotsRes = {
   lactate: string;
   sodium: string;
   potassium: string;
-  ventilator_fi02: number;
+  ventilator_fio2: number;
 };
 
 export type DialysisPlotsFields =
@@ -392,7 +392,7 @@ export type PrimaryParametersPlotFields =
   | "insulin_intake_frequency"
   | "insulin_intake_dose"
   | "ventilator_spo2"
-  | "ventilator_fi02"
+  | "ventilator_fio2"
   | "rhythm"
   | "rhythm_detail";
 
@@ -409,7 +409,7 @@ export type PrimaryParametersPlotRes = {
   insulin_intake_frequency: number;
   insulin_intake_dose: string;
   ventilator_spo2: number;
-  ventilator_fi02: number;
+  ventilator_fio2: number;
   rhythm: number;
   rhythm_detail: string;
 };
@@ -421,7 +421,7 @@ export type VentilatorPlotFields =
   | "ventilator_pressure_support"
   | "ventilator_tidal_volume"
   | "ventilator_peep"
-  | "ventilator_fi02"
+  | "ventilator_fio2"
   | "ventilator_spo2"
   | "etco2"
   | "bilateral_air_entry"
@@ -435,7 +435,7 @@ export type VentilatorPlotRes = {
   ventilator_pressure_support: number;
   ventilator_tidal_volume: number;
   ventilator_peep: string;
-  ventilator_fi02: number;
+  ventilator_fio2: number;
   ventilator_spo2: number;
   etco2: number;
   bilateral_air_entry: boolean;
