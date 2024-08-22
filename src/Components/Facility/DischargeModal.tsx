627d02ee1fd3484b40bc61cb89f95c229b5b8795
JOSHIK ROSHAN <96165631+JOSHIK27@users.noreply.github.com>
2024-08-22 09:05:56 +0100

    Update src/Components/Facility/DischargeModal.tsx
    Co-authored-by: Rithvik Nishad <rithvikn2001@gmail.com>

--- src/Components/Facility/DischargeModal.tsx
+++ src/Components/Facility/DischargeModal.tsx
@@ -105,7 +105,7 @@ const DischargeModal = ({
     },
   });
 
-  const ConsultationDiagnosisList: ConsultationDiagnosis[] =
+  const diagnoses =
     data?.diagnoses?.map((diagnosis) => diagnosis) || [];
 
   const discharge_reason =
