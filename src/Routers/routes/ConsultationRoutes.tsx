1d46c7641fb0db27d6ed33bd6effb8348dec9c11
Khavin Shankar <khavinshankar@gmail.com>
2024-08-23 16:10:17 +0530

    cleaned up treatment summary and added active prescriptions to it

--- src/Routers/routes/ConsultationRoutes.tsx
+++ src/Routers/routes/ConsultationRoutes.tsx
@@ -7,7 +7,9 @@ import { DailyRounds } from "../../Components/Patient/DailyRounds";
 import { FileUpload } from "../../Components/Patient/FileUpload";
 import { make as CriticalCareRecording } from "../../Components/CriticalCareRecording/CriticalCareRecording.bs";
 import { ConsultationDetails } from "../../Components/Facility/ConsultationDetails";
-import TreatmentSummary from "../../Components/Facility/TreatmentSummary";
+import TreatmentSummary, {
+  ITreatmentSummaryProps,
+} from "../../Components/Facility/TreatmentSummary";
 import ConsultationDoctorNotes from "../../Components/Facility/ConsultationDoctorNotes";
 import PatientConsentRecords from "../../Components/Patient/PatientConsentRecords";
 
@@ -134,12 +136,11 @@ export default {
     <ConsultationDetails consultationId={consultationId} tab={"updates"} />
   ),
   "/facility/:facilityId/patient/:patientId/consultation/:consultationId/treatment-summary":
-    ({ facilityId, patientId, consultationId }: any) => (
+    ({ facilityId, patientId, consultationId }: ITreatmentSummaryProps) => (
       <TreatmentSummary
         facilityId={facilityId}
         consultationId={consultationId}
         patientId={patientId}
-        dailyRoundsListData={[]}
       />
     ),
   "/facility/:facilityId/patient/:patientId/consultation/:consultationId/notes":
