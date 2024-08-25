5bf2d9a70f3c6e186fa2086a697b620fb0b152be
JOSHIK ROSHAN <96165631+JOSHIK27@users.noreply.github.com>
2024-08-25 15:42:42 +0100

    Make Treating Doctor Field Optional for Domiciliary Care Patients (#8391)

--- src/Components/Facility/ConsultationForm.tsx
+++ src/Components/Facility/ConsultationForm.tsx
@@ -599,6 +599,9 @@ export const ConsultationForm = ({ facilityId, patientId, id }: Props) => {
         }
 
         case "treating_physician": {
+          if (state.form.suggestion === "DC") {
+            break;
+          }
           if (state.form.suggestion !== "DD" && !state.form[field]) {
             errors[field] = t("field_required");
             invalidForm = true;
@@ -1428,7 +1431,7 @@ export const ConsultationForm = ({ facilityId, patientId, id }: Props) => {
                           name={"treating_physician"}
                           label={t("treating_doctor")}
                           placeholder="Attending Doctors Name and Designation"
-                          required
+                          required={state.form.suggestion !== "DC"}
                           value={
                             state.form.treating_physician_object ?? undefined
                           }
