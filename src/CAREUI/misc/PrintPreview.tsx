b8e5fa8522ef55b6e1a0791e4f95c8ec080365f7
Zeeshan <95434634+Zeshanxviii@users.noreply.github.com>
2024-08-31 14:49:23 +0530

    Converted CSS to Tailwind and removed extra padding.

--- src/CAREUI/misc/PrintPreview.tsx
+++ src/CAREUI/misc/PrintPreview.tsx
@@ -14,18 +14,18 @@ type Props = {
 export default function PrintPreview(props: Props) {
   return (
     <Page title={props.title}>
-      <div className="mx-auto my-8 w-[50rem]">
-        <div className="top-0 z-20 flex justify-end gap-2 bg-secondary-100 px-2 py-4 xl:absolute xl:right-6 xl:top-8">
+      <div className="mx-auto my-8 w-[50rem] print:m-0 print:w-full print:p-0">
+        <div className="top-0 z-20 flex justify-end gap-2 bg-secondary-100 px-2 py-4 xl:absolute xl:right-6 xl:top-8 print:hidden">
           <ButtonV2 disabled={props.disabled} onClick={() => window.print()}>
             <CareIcon icon="l-print" className="text-lg" />
             Print
           </ButtonV2>
         </div>
 
-        <div className="bg-white p-10 text-sm shadow-2xl transition-all duration-200 ease-in-out">
+        <div className="bg-white p-10 text-sm shadow-2xl transition-all duration-200 ease-in-out print:p-2 print:text-sm print:shadow-none">
           <div
             id="section-to-print"
-            className={classNames("w-full", props.className)}
+            className={classNames("w-full", props.className, "print:w-full")}
           >
             {props.children}
           </div>
