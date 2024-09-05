c4453c681baa5413c9d9ea963f9d7e7185190b6e
Sulochan Khadka <122200551+Sulochan-khadka@users.noreply.github.com>
2024-09-05 20:33:56 +0530

    Applied_Changes

--- src/Components/Facility/ConsultationForm.tsx
+++ src/Components/Facility/ConsultationForm.tsx
@@ -1365,7 +1365,7 @@ export const ConsultationForm = ({ facilityId, patientId, id }: Props) => {
                         className="col-span-6"
                         ref={fieldRef["procedure"]}
                       >
-                        <FieldLabel>Procedure Suggestions</FieldLabel>
+                        <FieldLabel>{t("Procedures")}</FieldLabel>
                         <ProcedureBuilder
                           procedures={
                             Array.isArray(state.form.procedure)
