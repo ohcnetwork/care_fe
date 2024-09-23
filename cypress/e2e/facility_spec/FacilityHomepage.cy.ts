0b739307b4b6f8612816c3942391e3b58104892e
Khavin Shankar <khavinshankar@gmail.com>
2024-09-23 21:38:28 +0530

    fixed capacity export test

--- cypress/e2e/facility_spec/FacilityHomepage.cy.ts
+++ cypress/e2e/facility_spec/FacilityHomepage.cy.ts
@@ -98,10 +98,9 @@ describe("Facility Homepage Function", () => {
   });
 
   it("Verify Capacity Export Functionality", () => {
+    facilityHome.csvDownloadIntercept(capacitiesAlias, "&capacity");
     facilityHome.clickExportButton();
     facilityHome.clickMenuItem("Capacities");
-    cy.wait(3000);
-    facilityHome.csvDownloadIntercept(capacitiesAlias, "&capacity");
     facilityHome.verifyDownload(capacitiesAlias);
   });
 
