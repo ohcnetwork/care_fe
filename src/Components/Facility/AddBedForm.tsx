28b27c09a78ec93118cd5b7665c5fb6b410505b9
AdityaJ2305 <adityajindal2305@gmail.com>
2024-09-26 12:21:49 +0530

    added text in en locale files

--- src/Components/Facility/AddBedForm.tsx
+++ src/Components/Facility/AddBedForm.tsx
@@ -127,9 +127,9 @@ export const AddBedForm = ({ facilityId, locationId, bedId }: Props) => {
       const { res } = await request(routes.createFacilityBed, {
         body: { ...data, facility: facilityId, location: locationId },
       });
-      res?.ok && numberOfBeds>1 
-        ? onSuccess("Beds created successfully")
-        : onSuccess("Bed created successfully");
+      res?.ok && (numberOfBeds>1 
+        ? onSuccess(t("multiple_bed_created_notification"))
+        : onSuccess(t("single_bed_created_notification")))
     }
   };
 
