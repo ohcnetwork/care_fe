238b1acba658f08e5f8e1931187a6f372c428150
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2024-10-09 02:29:51 +0530

    patient export test

--- cypress/pageobject/Patient/PatientHome.ts
+++ cypress/pageobject/Patient/PatientHome.ts
@@ -10,5 +10,36 @@ class PatientHome {
   clickPreviousPage() {
     cy.get("#prev-pages").click();
   }
+
+  clickPatientExport() {
+    cy.get("#patient-export").click();
+  }
+
+  clickPatientFilterApply() {
+    cy.get("#apply-filter").click();
+  }
+
+  interceptPatientExportRequest() {
+    cy.intercept({
+      method: "GET",
+      url: "/api/v1/patient/*",
+    }).as("getPatients");
+  }
+
+  verifyPatientExportRequest() {
+    cy.wait("@getPatients").then((interception) => {
+      expect(interception.request.url).to.include("/api/v1/patient/");
+      expect(interception.request.url).to.include("&csv");
+      expect(interception.response.statusCode).to.eq(200);
+    });
+  }
+
+  typePatientModifiedBeforeDate(startDate: string) {
+    cy.clickAndTypeDate("input[name='modified_date_start']", startDate);
+  }
+
+  typePatientModifiedAfterDate(endDate: string) {
+    cy.clickAndTypeDate("input[name='modified_date_end']", endDate);
+  }
 }
 export default PatientHome;
