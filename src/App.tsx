b02062a836df8559e19870f7c7268de4a2346032
Rithvik Nishad <mail@rithviknishad.dev>
2024-10-16 15:53:11 +0530

    Fixes multiple tiny issues (#8784)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Suspense } from "react";
 import Routers from "./Routers";
-import ThemedFavicon from "./CAREUI/misc/ThemedFavicon";
 import Integrations from "./Integrations";
 import Loading from "./Components/Common/Loading";
 import HistoryAPIProvider from "./Providers/HistoryAPIProvider";
@@ -11,7 +10,6 @@ import { Toaster } from "./Components/ui/toaster";
 const App = () => {
   return (
     <Suspense fallback={<Loading />}>
-      <ThemedFavicon />
       <HistoryAPIProvider>
         <AuthUserProvider unauthorized={<Routers.SessionRouter />}>
           <FeatureFlagsProvider>
