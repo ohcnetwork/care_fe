567eaf48926c9b17808ff3e7a6aaabf495ab6ec4
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-10-22 21:50:36 +0530

    Resloved the UI and Changed GRID 3 cols

--- src/Components/Resource/ResourceBoardView.tsx
+++ src/Components/Resource/ResourceBoardView.tsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
+import dayjs from "dayjs";
 import { Link, navigate } from "raviger";
-import ListFilter from "./ListFilter";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import CareIcon from "../../CAREUI/icons/CareIcon";
+import { AdvancedFilterButton } from "../../CAREUI/interactive/FiltersSlideover";
 import { RESOURCE_CHOICES } from "../../Common/constants";
-import BadgesList from "./BadgesList";
-import { formatFilter } from "./Commons";
 import useFilters from "../../Common/hooks/useFilters";
+import routes from "../../Redux/api";
+import request from "../../Utils/request/request";
+import { classNames, formatDateTime, formatName } from "../../Utils/utils";
 import { ExportButton } from "../Common/Export";
 import ButtonV2 from "../Common/components/ButtonV2";
-import { useTranslation } from "react-i18next";
-import { AdvancedFilterButton } from "../../CAREUI/interactive/FiltersSlideover";
-import CareIcon from "../../CAREUI/icons/CareIcon";
-import SearchInput from "../Form/SearchInput";
 import Tabs from "../Common/components/Tabs";
-import request from "../../Utils/request/request";
-import routes from "../../Redux/api";
-import KanbanBoard from "../Kanban/Board";
 import { ResourceModel } from "../Facility/models";
-import { classNames, formatDateTime, formatName } from "../../Utils/utils";
-import dayjs from "dayjs";
+import SearchInput from "../Form/SearchInput";
+import KanbanBoard from "../Kanban/Board";
+import BadgesList from "./BadgesList";
+import { formatFilter } from "./Commons";
+import ListFilter from "./ListFilter";
 
 import PageTitle from "@/Components/Common/PageTitle";
 const resourceStatusOptions = RESOURCE_CHOICES.map((obj) => obj.text);
@@ -43,7 +43,7 @@ export default function BoardView() {
   return (
     <div className="flex-col px-2 pb-2">
       <div className="flex w-full flex-col items-center justify-between lg:flex-row">
-        <div className="w-1/3 lg:w-1/4">
+        <div className="mt-4 w-1/3 lg:w-1/4">
           <PageTitle
             title={t("resource")}
             hideBack
@@ -66,13 +66,15 @@ export default function BoardView() {
           />
         </div>
 
-        <div className="flex w-full flex-col items-center justify-between gap-2 pt-2 xl:flex-row">
+        <div className="mt-2 flex w-full flex-col items-center justify-between gap-2 pt-2 xl:flex-row">
           <SearchInput
             name="title"
             value={qParams.title}
             onChange={(e) => updateQuery({ [e.name]: e.value })}
             placeholder={t("search_resource")}
           />
+        </div>
+        <div className="mr-4 mt-2">
           <Tabs
             tabs={[
               { text: t("active"), value: 0 },
@@ -81,15 +83,14 @@ export default function BoardView() {
             onTabChange={(tab) => setBoardFilter(tab ? COMPLETED : ACTIVE)}
             currentTab={boardFilter !== ACTIVE ? 1 : 0}
           />
-          <div className="flex w-full flex-col gap-2 lg:mr-4 lg:w-fit lg:flex-row lg:gap-4">
-            <ButtonV2 className="py-[11px]" onClick={onListViewBtnClick}>
-              <CareIcon icon="l-list-ul" />
-              {t("list_view")}
-            </ButtonV2>
-            <AdvancedFilterButton
-              onClick={() => advancedFilter.setShow(true)}
-            />
-          </div>
+        </div>
+
+        <div className="mt-2 flex w-full flex-col gap-2 lg:mr-4 lg:w-fit lg:flex-row lg:gap-4">
+          <AdvancedFilterButton onClick={() => advancedFilter.setShow(true)} />
+          <ButtonV2 className="py-[11px]" onClick={onListViewBtnClick}>
+            <CareIcon icon="l-list-ul" />
+            {t("list_view")}
+          </ButtonV2>
         </div>
       </div>
 
