003b8f8a9f098346c0f06a843d8634830d31dba5
Mohamed amaan <mohamedamaan319@gmail.com>
2024-10-22 07:43:22 +0530

    shiftDetails responsive

--- src/Components/Shifting/ShiftDetails.tsx
+++ src/Components/Shifting/ShiftDetails.tsx
@@ -506,7 +506,7 @@ export default function ShiftDetails(props: { id: string }) {
           title={t("shifting_details")}
           backUrl="/shifting/board"
           options={
-            <div className="flex gap-2">
+            <div className="flex flex-col  md:flex-row gap-2">
               <ButtonV2
                 tooltip={
                   ["COMPLETED", "CANCELLED"].includes(data?.status || "")
