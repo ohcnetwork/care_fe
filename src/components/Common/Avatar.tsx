998dfa27c9b3f26a9eeb47b0260910b9d3cbd3c1
Aakash Singh <mail@singhaakash.dev>
2024-10-28 17:34:07 +0530

    Fix avatars with initials in webkit

--- src/components/Common/Avatar.tsx
+++ src/components/Common/Avatar.tsx
@@ -78,6 +78,11 @@ const Avatar: React.FC<AvatarProps> = ({
           xmlns="http://www.w3.org/2000/svg"
           version="1.1"
           viewBox="0 0 100 100"
+          style={{
+            width: "100%",
+            height: "100%",
+            zIndex: 100,
+          }}
         >
           <text
             fill="black"
