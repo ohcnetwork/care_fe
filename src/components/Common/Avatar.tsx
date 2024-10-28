06417f5539cb1d0dc8a2d96136efb53a75cf4edc
Aakash Singh <mail@singhaakash.dev>
2024-10-28 17:37:12 +0530

    remove z index

--- src/components/Common/Avatar.tsx
+++ src/components/Common/Avatar.tsx
@@ -78,11 +78,7 @@ const Avatar: React.FC<AvatarProps> = ({
           xmlns="http://www.w3.org/2000/svg"
           version="1.1"
           viewBox="0 0 100 100"
-          style={{
-            width: "100%",
-            height: "100%",
-            zIndex: 100,
-          }}
+          style={{ width: "100%", height: "100%" }}
         >
           <text
             fill="black"
