c47f2fe704bbb02a929e543112194fd0875caefa
Aakash Singh <mail@singhaakash.dev>
2024-11-06 21:50:28 +0530

    fix

--- src/components/Facility/Investigations/Reports/utils.tsx
+++ src/components/Facility/Investigations/Reports/utils.tsx
@@ -1,4 +1,4 @@
-import * as _ from "lodash";
+import _ from "lodash";
 import { memoize, findIndex } from "lodash-es";
 import { InvestigationResponse } from "./types";
 
--- src/components/Facility/Investigations/ShowInvestigation.tsx
+++ src/components/Facility/Investigations/ShowInvestigation.tsx
@@ -1,4 +1,4 @@
-import * as _ from "lodash";
+import _ from "lodash";
 import { set } from "lodash-es";
 import { useCallback, useReducer } from "react";
 import routes from "../../../Redux/api";
