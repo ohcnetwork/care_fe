e076c3ef9797fcd3c440a146efb75f96e7654462
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2024-11-10 04:09:47 +0530

    reorder the process to adjust with message delay

--- cypress/e2e/facility_spec/FacilityHomepage.cy.ts
+++ cypress/e2e/facility_spec/FacilityHomepage.cy.ts
@@ -141,35 +141,37 @@ describe("Facility Homepage Function", () => {
     manageUserPage.interceptFacilitySearchReq();
     manageUserPage.typeFacilitySearch(facilityName);
     manageUserPage.verifyFacilitySearchReq();
-    // verify facility name and notify button and click it
+    // verify facility name and card reflection
     facilityNotify.verifyUrlContains("Dummy+Facility+40");
     facilityPage.verifyFacilityBadgeContent(facilityName);
     manageUserPage.assertFacilityInCard(facilityName);
+    // send notification to a facility
     facilityHome.clickFacilityNotifyButton();
-    // check visiblity of pop-up and frontend error on empty message
     facilityNotify.verifyFacilityName(facilityName);
-    cy.submitButton("Notify");
-    facilityNotify.verifyErrorMessage(notificationErrorMsg);
-    // close pop-up and verify
-    facilityHome.verifyAndCloseNotifyModal();
-    // send notification
-    facilityHome.clickFacilityNotifyButton();
     facilityNotify.fillNotifyText(notificationMessage);
     facilityNotify.interceptPostNotificationReq();
     cy.submitButton("Notify");
     facilityNotify.verifyPostNotificationReq();
     cy.verifyNotification("Facility Notified");
     cy.closeNotification();
+    // Verify the frontend error on empty message
+    facilityHome.clickFacilityNotifyButton();
+    facilityNotify.verifyFacilityName(facilityName);
+    cy.submitButton("Notify");
+    facilityNotify.verifyErrorMessage(notificationErrorMsg);
+    // close pop-up and verify
+    facilityHome.verifyAndCloseNotifyModal();
     // signout as district admin and login as a Nurse
     loginPage.ensureLoggedIn();
     loginPage.clickSignOutBtn();
     loginPage.loginManuallyAsNurse();
-    // Visit Notification Sidebar
+    // Verify Notice Board Reflection
     facilityNotify.interceptGetNotificationReq();
     facilityNotify.visitNoticeBoard();
     facilityNotify.verifyGetNotificationReq();
     cy.verifyContentPresence("#notification-message", [notificationMessage]);
     facilityNotify.interceptGetNotificationReq();
+    // Verify Sidebar Notification Reflection
     facilityNotify.openNotificationSlide();
     facilityNotify.verifyGetNotificationReq();
     cy.verifyContentPresence("#notification-slide-msg", [notificationMessage]);
