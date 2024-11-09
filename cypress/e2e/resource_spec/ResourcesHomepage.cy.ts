9256e70980d89160619c3e449fae15838aec26f6
JavidSumra <javidsumra135@gmail.com>
2024-11-09 09:40:05 +0530

    fix resource update test suit

--- cypress/e2e/resource_spec/ResourcesHomepage.cy.ts
+++ cypress/e2e/resource_spec/ResourcesHomepage.cy.ts
@@ -1,6 +1,6 @@
+import FacilityPage from "../../pageobject/Facility/FacilityCreation";
 import LoginPage from "../../pageobject/Login/LoginPage";
 import ResourcePage from "../../pageobject/Resource/ResourcePage";
-import FacilityPage from "../../pageobject/Facility/FacilityCreation";
 
 describe("Resource Page", () => {
   let createdResource: string;
@@ -68,6 +68,10 @@ describe("Resource Page", () => {
     cy.visit(createdResource);
     resourcePage.clickUpdateStatus();
     resourcePage.updateStatus("APPROVED");
+    cy.clickAndSelectOption(
+      "input[name='assigned_facility_object']",
+      "Dummy Request Fulfilment Center, Ernakulam",
+    );
     resourcePage.clickSubmitButton();
     resourcePage.verifySuccessNotification(
       "Resource request updated successfully",
