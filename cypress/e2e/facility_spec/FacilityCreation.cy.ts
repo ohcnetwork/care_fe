9fef71015ef391d6af73ffccd3122bfdd024a217
kihan2518B <kp648027@gmail.com>
2024-12-05 00:16:25 +0530

    modified cypress tests to verify error messages.

--- cypress/e2e/facility_spec/FacilityCreation.cy.ts
+++ cypress/e2e/facility_spec/FacilityCreation.cy.ts
@@ -47,6 +47,15 @@ describe("Facility Creation", () => {
     "Required",
     "Invalid Phone Number",
   ];
+  const bedErrorMessage = [
+    "This field is required",
+    "Total capacity cannot be 0",
+    "This field is required",
+  ];
+  const doctorErrorMessage = [
+    "This field is required",
+    "This field is required",
+  ];
   const triageErrorMessage = ["This field is required"];
   const facilityType = "Primary Health Centres";
 
@@ -249,9 +258,15 @@ describe("Facility Creation", () => {
     facilityPage.submitForm();
     // add no bed capacity and verify form error message
     facilityPage.isVisibleselectBedType();
+    facilityPage.selectBedType("Oxygen Supported Bed");
+    facilityPage.saveAndExitBedCapacityForm();
+    userCreationPage.verifyErrorMessages(bedErrorMessage);
     facilityPage.clickcancelbutton();
     // add no doctor capacity and verify form error message
     facilityPage.isVisibleAreaOfSpecialization();
+    facilityPage.selectAreaOfSpecialization("Pulmonology");
+    facilityPage.clickdoctorcapacityaddmore();
+    userCreationPage.verifyErrorMessages(doctorErrorMessage);
     facilityPage.clickcancelbutton();
     cy.url().then((newUrl) => {
       facilityUrl1 = newUrl;
