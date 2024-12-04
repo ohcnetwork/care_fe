54eee2a9f1fe7cf29f914ee65af13df1de7477bb
Rishith25 <kamishetty.rishith@gmail.com>
2024-12-04 22:12:43 +0530

    FacilityHomepage.cy.ts test case wait

--- cypress/e2e/facility_spec/FacilityHomepage.cy.ts
+++ cypress/e2e/facility_spec/FacilityHomepage.cy.ts
@@ -174,7 +174,6 @@ describe("Facility Homepage Function", () => {
     facilityNotify.verifyPostNotificationReq();
     cy.verifyNotification("Facility Notified");
     cy.closeNotification();
-    cy.wait(2000);
     // Verify the frontend error on empty message
     facilityHome.clickFacilityNotifyButton();
     facilityNotify.verifyFacilityName(facilityName);
