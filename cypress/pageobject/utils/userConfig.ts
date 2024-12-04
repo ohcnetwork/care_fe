b43bb14dbe7e122ed04b8ceb10e625aa56390d7d
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2024-12-04 10:33:56 +0530

    refactored the users

--- cypress/pageobject/utils/userConfig.ts
+++ cypress/pageobject/utils/userConfig.ts
@@ -1,8 +1,7 @@
 export const users = {
   districtAdmin: { username: "devdistrictadmin", password: "Coronasafe@123" },
   devDoctor: { username: "devdoctor", password: "Coronasafe@123" },
-  staff: { username: "staffdev", password: "Coronasafe@123" },
   nurse: { username: "dummynurse1", password: "Coronasafe@123" },
 };
 
-export const nonAdminRoles = ["devDoctor", "staff", "nurse"] as const;
+export const nonAdminRoles = ["devDoctor", "nurse"] as const;
