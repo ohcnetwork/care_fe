1d804415dfe53a9b2c2957ab496911e1fc5cbcf5
Rishith25 <kamishetty.rishith@gmail.com>
2024-12-04 23:41:10 +0530

    revert PatientInsurance.ts

--- cypress/pageobject/Patient/PatientInsurance.ts
+++ cypress/pageobject/Patient/PatientInsurance.ts
@@ -21,9 +21,7 @@ class PatientInsurance {
   }
 
   clickAddInsruanceDetails() {
-    cy.intercept("GET", "**/api/hcx/payors/?").as("getPayors");
     cy.get("[data-testid=add-insurance-button]").click();
-    cy.wait("@getPayors").its("response.statusCode").should("eq", 200);
   }
 
   verifyPatientPolicyDetails(
