943bb79144892c71abfda32e06619ba30acb5184
Rishith25 <kamishetty.rishith@gmail.com>
2024-12-07 23:42:30 +0530

    Patient Registration cy.waits

--- cypress/pageobject/Patient/PatientCreation.ts
+++ cypress/pageobject/Patient/PatientCreation.ts
@@ -91,11 +91,15 @@ export class PatientPage {
   }
 
   clickPatientAntenatalStatusYes() {
-    cy.get("#is_antenatal-option-true").should("be.visible").click();
+    cy.get("#is_antenatal-option-true", { timeout: 2000 })
+      .should("be.visible")
+      .click();
   }
 
   clickPatientPostPartumStatusYes() {
-    cy.get("#is_postpartum-option-true").should("be.visible").click();
+    cy.get("#is_postpartum-option-true", { timeout: 2000 })
+      .should("be.visible")
+      .click();
   }
 
   clickCancelButton() {
