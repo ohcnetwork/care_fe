64acdb52f63b7c8ffe18cdef31fcea0b55175368
Rajasimhareddybolla <rajasimhabolla@gmail.com>
2024-12-07 17:23:52 +0530

    reverted changes in PrintPreview component and added the PrintView rendering

--- src/CAREUI/misc/PrintPreview.tsx
+++ src/CAREUI/misc/PrintPreview.tsx
@@ -21,39 +21,35 @@ type Props = {
   disabled?: boolean;
   className?: string;
   title: string;
-  hideControls?: boolean;
 };
 
 export default function PrintPreview(props: Props) {
   const normalScale = useBreakpoints({ default: 0.44, md: 1 });
   const { t } = useTranslation();
 
-  const content = (
-    <div className="mx-auto my-8 w-[50rem]">
-      <div className="top-0 z-20 flex gap-2 bg-secondary-100 px-2 py-4 xl:absolute xl:right-6 xl:top-8 xl:justify-end">
-        <Button variant="primary" disabled={props.disabled} onClick={print}>
-          <CareIcon icon="l-print" className="text-lg" />
-          {t("print")}
-        </Button>
+  return (
+    <Page title={props.title}>
+      <div className="mx-auto my-8 w-[50rem]">
+        <div className="top-0 z-20 flex gap-2 bg-secondary-100 px-2 py-4 xl:absolute xl:right-6 xl:top-8 xl:justify-end">
+          <Button variant="primary" disabled={props.disabled} onClick={print}>
+            <CareIcon icon="l-print" className="text-lg" />
+            {t("print")}
+          </Button>
+        </div>
+
+        <ZoomProvider initialScale={normalScale}>
+          <ZoomTransform className="origin-top-left bg-white p-10 text-sm shadow-2xl transition-all duration-200 ease-in-out lg:origin-top print:transform-none">
+            <div
+              id="section-to-print"
+              className={classNames("w-full", props.className)}
+            >
+              {props.children}
+            </div>
+          </ZoomTransform>
+
+          <ZoomControls disabled={props.disabled} />
+        </ZoomProvider>
       </div>
-
-      <ZoomProvider initialScale={normalScale}>
-        <ZoomTransform className="origin-top-left bg-white p-10 text-sm shadow-2xl transition-all duration-200 ease-in-out lg:origin-top print:transform-none">
-          <div
-            id="section-to-print"
-            className={classNames("w-full", props.className)}
-          >
-            {props.children}
-          </div>
-        </ZoomTransform>
-        {!props.hideControls && <ZoomControls disabled={props.disabled} />}
-      </ZoomProvider>
-    </div>
-  );
-
-  return props.hideControls ? (
-    content
-  ) : (
-    <Page title={props.title}>{content}</Page>
+    </Page>
   );
 }
