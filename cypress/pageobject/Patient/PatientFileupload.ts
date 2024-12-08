1099d9aa318d6f752b863963dc9050739365f621
Rishith25 <kamishetty.rishith@gmail.com>
2024-12-08 11:34:54 +0530

    Patient File Upload with waits

--- cypress/pageobject/Patient/PatientFileupload.ts
+++ cypress/pageobject/Patient/PatientFileupload.ts
@@ -36,7 +36,7 @@ export class PatientFileUpload {
 
   verifyUploadFilePresence(fileName: string) {
     cy.wait(2000);
-    cy.get("#file-div").should("be.visible").scrollIntoView();
+    cy.get("#file-div").scrollIntoView();
     cy.verifyContentPresence("#file-div", [fileName]);
   }
 
