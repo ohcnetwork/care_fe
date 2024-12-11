ffb3062e89b3ebeaf73578d9a0996ef720163944
Jacobjohnjeevan <mail@jacobjeevan.me>
2024-12-11 13:46:16 +0530

    add scrollIntoView for weekly working hours

--- cypress/pageobject/Users/ManageUserPage.ts
+++ cypress/pageobject/Users/ManageUserPage.ts
@@ -127,6 +127,7 @@ export class ManageUserPage {
   }
 
   clearProfessionalInfo() {
+    cy.get("input[name='weekly_working_hours']").scrollIntoView();
     cy.get("input[name='weekly_working_hours']").click().clear();
     cy.get("input[name='video_connect_link']").click().clear();
   }
@@ -180,6 +181,7 @@ export class ManageUserPage {
   }
 
   typeInWeeklyWorkingHours(hours: string) {
+    cy.get("input[name='weekly_working_hours']").scrollIntoView();
     cy.get("input[name='weekly_working_hours']").click().type(hours);
   }
 
@@ -191,6 +193,7 @@ export class ManageUserPage {
   }
 
   verifyWorkingHours(expectedHours: string) {
+    cy.get("input[name='weekly_working_hours']").scrollIntoView();
     cy.get("input[name='weekly_working_hours']").should("be.visible");
     cy.get("input[name='weekly_working_hours']").should(
       "have.value",
