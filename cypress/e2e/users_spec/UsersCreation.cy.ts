6f261093b4708d936c49c2e3e8cfc4c81b8edaac
Jacobjohnjeevan <mail@jacobjeevan.me>
2024-12-11 19:06:10 +0530

    Test fixes and removals
    - Removed logic to test profile in user creation (already done elsewhere)
    - Removed ununsed logic in UserProfilePage
    	- Makes uses of logic in ManageUsers instead since it's the same page
    - Removed UserFacilities (logic related to sidebar for managing facilities/no longer used)

--- cypress/e2e/users_spec/UsersCreation.cy.ts
+++ cypress/e2e/users_spec/UsersCreation.cy.ts
@@ -1,5 +1,4 @@
 import FacilityHome from "pageobject/Facility/FacilityHome";
-import ManageUserPage from "pageobject/Users/ManageUserPage";
 import UserProfilePage from "pageobject/Users/UserProfilePage";
 import { advanceFilters } from "pageobject/utils/advanceFilterHelpers";
 
@@ -7,21 +6,16 @@ import FacilityPage from "../../pageobject/Facility/FacilityCreation";
 import LoginPage from "../../pageobject/Login/LoginPage";
 import { UserCreationPage } from "../../pageobject/Users/UserCreation";
 import { UserPage } from "../../pageobject/Users/UserSearch";
-import {
-  generateEmergencyPhoneNumber,
-  generatePhoneNumber,
-} from "../../pageobject/utils/constants";
+import { generatePhoneNumber } from "../../pageobject/utils/constants";
 
 describe("User Creation", () => {
   const userPage = new UserPage();
   const loginPage = new LoginPage();
   const userProfilePage = new UserProfilePage();
-  const manageUserPage = new ManageUserPage();
   const userCreationPage = new UserCreationPage();
   const facilityPage = new FacilityPage();
   const facilityHome = new FacilityHome();
   const phoneNumber = generatePhoneNumber();
-  const emergencyPhoneNumber = generateEmergencyPhoneNumber();
   const fillFacilityName = "Dummy Facility 40";
   const makeId = (length: number) => {
     let result = "";
@@ -54,14 +48,6 @@ describe("User Creation", () => {
     "Please select the local body",
   ];
 
-  const EXPECTED_PROFILE_ERROR_MESSAGES = [
-    "This field is required",
-    "This field is required",
-    "Please enter valid phone number",
-  ];
-  const userName = "devdistrictadmin";
-  const firstName = "District Editted";
-  const lastName = "Cypress";
   const gender = "Male";
   const email = "test@test.com";
   const password = "Test@123";
@@ -74,9 +60,6 @@ describe("User Creation", () => {
   const district = "Ernakulam";
   const role = "Doctor";
   const homeFacility = "Dummy Shifting Center";
-  const weeklyWorkingHrs = "14";
-  const dob = "01011998";
-  const formattedDob = "01/01/1998";
   const newUserDob = "25081999";
 
   before(() => {
@@ -88,55 +71,6 @@ describe("User Creation", () => {
     cy.restoreLocalStorage();
     cy.clearLocalStorage(/filters--.+/);
     cy.awaitUrl("/users");
-  });
-
-  it("Update the existing user profile and verify its reflection", () => {
-    manageUserPage.navigateToProfile();
-    cy.verifyContentPresence("#username-profile-details", [userName]);
-    userProfilePage.clickEditProfileButton();
-    userCreationPage.clearFirstName();
-    userCreationPage.typeFirstName(firstName);
-    userCreationPage.clearLastName();
-    userCreationPage.typeLastName(lastName);
-    userProfilePage.selectGender(gender);
-    userProfilePage.clearPhoneNumber();
-    userProfilePage.typePhoneNumber(phoneNumber);
-    userProfilePage.clearAltPhoneNumber();
-    userProfilePage.typeWhatsappNumber(emergencyPhoneNumber);
-    userProfilePage.clearEmail();
-    userProfilePage.typeEmail(email);
-    userProfilePage.clearWorkingHours();
-    userProfilePage.typeWorkingHours(weeklyWorkingHrs);
-    userProfilePage.typeDateOfBirth(dob);
-    cy.intercept("PATCH", "/api/v1/users/*").as("updateUser");
-    userProfilePage.clickUpdateButton();
-    cy.wait("@updateUser").its("response.statusCode").should("eq", 200);
-    cy.verifyContentPresence("#contactno-profile-details", [
-      "+91" + phoneNumber,
-    ]);
-    cy.verifyContentPresence("#whatsapp-profile-details", [
-      "+91" + emergencyPhoneNumber,
-    ]);
-    cy.verifyContentPresence("#firstname-profile-details", [firstName]);
-    cy.verifyContentPresence("#lastname-profile-details", [lastName]);
-    cy.verifyContentPresence("#date_of_birth-profile-details", [formattedDob]);
-    cy.verifyContentPresence("#emailid-profile-details", [email]);
-    cy.verifyContentPresence("#gender-profile-details", [gender]);
-    cy.verifyContentPresence("#averageworkinghour-profile-details", [
-      weeklyWorkingHrs,
-    ]);
-  });
-
-  it("Update the existing user profile Form Mandatory File Error", () => {
-    manageUserPage.navigateToProfile();
-    userProfilePage.clickEditProfileButton();
-    userCreationPage.clearFirstName();
-    userCreationPage.clearLastName();
-    userProfilePage.clearPhoneNumber();
-    userProfilePage.clearAltPhoneNumber();
-    userProfilePage.clearWorkingHours();
-    userProfilePage.clickUpdateButton();
-    cy.verifyErrorMessages(EXPECTED_PROFILE_ERROR_MESSAGES);
   });
 
   it("create new user and verify reflection", () => {
