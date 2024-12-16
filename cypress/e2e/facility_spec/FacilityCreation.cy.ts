86492c3cab1da47cfafb8a7747b3f8e5823818cd
Rishith25 <kamishetty.rishith@gmail.com>
2024-12-16 19:28:12 +0530

    commented notification for check

--- cypress/e2e/facility_spec/FacilityCreation.cy.ts
+++ cypress/e2e/facility_spec/FacilityCreation.cy.ts
@@ -78,7 +78,7 @@ describe("Facility Creation with multiple user roles", () => {
     facilityPage.fillLocationDetails(testFacilityData.location);
     facilityPage.selectLocation("Kochi, Kerala");
     facilityPage.clickSaveFacilityButton();
-    facilityPage.verifyFacilityCreatedNotification();
+    // facilityPage.verifyFacilityCreatedNotification();
     // verify the facility card info
     cy.verifyContentPresence("#facility-details-card", [
       facilityName,
