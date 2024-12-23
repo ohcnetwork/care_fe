986e55d398c933c5800e87d3340eb8e524c96959
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2024-12-23 12:37:03 +0530

    modified the command function timeout to handle edge cases

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -229,11 +229,14 @@ Cypress.Commands.add("closeNotification", () => {
 });
 
 Cypress.Commands.add("verifyContentPresence", (selector, texts) => {
-  cy.get(selector).then(($el) => {
-    texts.forEach((text) => {
-      cy.wrap($el).should("contain", text);
+  const timeout = 10000;
+  cy.get(selector, { timeout })
+    .should("be.visible")
+    .then(($el) => {
+      texts.forEach((text) => {
+        cy.wrap($el).should("contain", text, { timeout });
+      });
     });
-  });
 });
 
 Cypress.Commands.add("verifyErrorMessages", (errorMessages: string[]) => {
