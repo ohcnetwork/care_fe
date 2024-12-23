5593f001b388e5dba0b6a9987ced88e3c92405ae
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-12-23 11:00:55 +0530

    Changed to shadcdn dialog,button

--- src/components/Notifications/NoticeBoard.tsx
+++ src/components/Notifications/NoticeBoard.tsx
@@ -4,6 +4,15 @@ import { useTranslation } from "react-i18next";
 
 import CareIcon from "@/CAREUI/icons/CareIcon";
 
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import { Avatar } from "@/components/Common/Avatar";
 import Loading from "@/components/Common/Loading";
 import Page from "@/components/Common/Page";
@@ -12,12 +21,12 @@ import routes from "@/Utils/request/api";
 import useTanStackQueryInstead from "@/Utils/request/useQuery";
 import { formatName, relativeTime } from "@/Utils/utils";
 
-import DialogModal from "../Common/Dialog";
 import { UserBareMinimum } from "../Users/models";
 import { NotificationData } from "./models";
 
 export const NoticeBoard = () => {
   const { t } = useTranslation();
+
   const { data, loading } = useTanStackQueryInstead(routes.getNotifications, {
     query: { offset: 0, event: "MESSAGE", medium_sent: "SYSTEM" },
   });
@@ -42,7 +51,7 @@ export const NoticeBoard = () => {
       <div className="text-md my-1 text-secondary-700  px-3">
         <div className="flex font-bold items-center text-black">
           {formatName(user)}
-          <span className="font-oblique text-gray-500 font-medium ml-1">
+          <span className="font-oblique text-gray-500 font-medium ml-2">
             {user.username}
           </span>
         </div>
@@ -71,19 +80,31 @@ export const NoticeBoard = () => {
       <>
         <h1 className="font-semibold text-lg text-black mb-1 truncate hover:text-clip">
           {" "}
-          {message.split("\n")[0]}
+          {message?.split("\n")[0]}
         </h1>
         <p className={classes}>{formattedMessage}</p>
       </>
     );
   };
 
-  const handleViewDetails = (notice: NotificationData) => {
-    setSelectedNotice(notice);
-  };
-
-  const handleCloseModal = () => {
-    setSelectedNotice(null);
+  const NoticeDialog: React.FC<{ notice: NotificationData }> = ({ notice }) => {
+    return (
+      <>
+        {" "}
+        <DialogHeader>
+          <div className="flex justify-between w-full  items-center mb-4">
+            <CareIcon
+              className="bg-primary-200 font-light text-lg p-2  text-primary-600 rounded-lg w-12 h-10 "
+              icon="l-envelope-open"
+            />
+          </div>
+        </DialogHeader>
+        <Message classes="" message={notice.message} />
+        <DialogFooter className="sm:justify-start w-full py-2 flex items-center bg-gray-200 rounded-lg mt-4">
+          <UserInfo user={notice.caused_by} createdDate={notice.created_date} />
+        </DialogFooter>
+      </>
+    );
   };
 
   let notices;
@@ -104,13 +125,21 @@ export const NoticeBoard = () => {
             <div className="h-1/4 flex justify-between items-center bg-gray-200 ">
               <UserInfo user={item.caused_by} createdDate={item.created_date} />
               <div className="col-span-1 mt-2 flex flex-col text-left border-2 border-gray-400 rounded-md mr-10">
-                <button
-                  onClick={() => handleViewDetails(item)}
-                  className="flex w-full items-center justify-center gap-2 rounded-lg border border-secondary-300 bg-secondary-200 p-2 text-sm font-semibold text-inherit transition-all hover:bg-secondary-300"
-                >
-                  <CareIcon icon="l-eye" className="text-lg" />{" "}
-                  {t("view_notice")}
-                </button>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button
+                      variant={"ghost"}
+                      onClick={() => setSelectedNotice(item)}
+                      className="flex w-full items-center justify-center gap-2 rounded-lg  p-2 text-sm font-semibold text-inherit "
+                    >
+                      <CareIcon icon="l-eye" className="text-lg" />
+                      {t("view_notice")}
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-md max-h-[80vh] overflow-y-auto">
+                    {selectedNotice && <NoticeDialog notice={selectedNotice} />}
+                  </DialogContent>
+                </Dialog>
               </div>
             </div>
           </div>
@@ -134,34 +163,6 @@ export const NoticeBoard = () => {
   return (
     <Page title={t("notice_board")} hideBack={true} breadcrumbs={false}>
       <div>{notices}</div>
-      {selectedNotice && (
-        <DialogModal show={true} onClose={handleCloseModal} title={""}>
-          <div className="m-0">
-            <div className="flex justify-between w-full  items-center mb-4">
-              <CareIcon
-                className="bg-primary-200 font-light text-lg p-2 mb-1 text-primary-600 rounded-lg w-12 h-10 "
-                icon="l-envelope-open"
-              />
-              <button
-                onClick={handleCloseModal}
-                aria-labe="close modal"
-                onKeyDown={(e) => e.key === "Escape" && handleCloseModal()}
-                className="px-4 py-2 bg-secondary-400 text-gray-600 rounded-md flex items-center"
-              >
-                <CareIcon icon="l-times" className="mr-1" />
-                {t("close")}
-              </button>
-            </div>
-            <Message classes="" message={selectedNotice.message} />
-            <div className="w-full py-2 flex items-center bg-gray-200 rounded-lg mt-4">
-              <UserInfo
-                user={selectedNotice.caused_by}
-                createdDate={selectedNotice.created_date}
-              />
-            </div>
-          </div>
-        </DialogModal>
-      )}
     </Page>
   );
 };
