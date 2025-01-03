7be24cdafc81ada287239514f2e06bc7ab2ac205
AdityaJ2305 <adityajindal2305@gmail.com>
2025-01-04 01:30:35 +0530

    fix bracket

--- src/Utils/request/errorHandler.ts
+++ src/Utils/request/errorHandler.ts
@@ -44,7 +44,7 @@ export function handleHttpError(error: Error) {
         return;
       }
     }
-    Notifications.BadRequest({ errs: cause });
+    Notifications.BadRequest({ errs: errs });
     return;
   }
 
