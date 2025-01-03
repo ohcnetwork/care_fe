ea545c448f1e88ece8b2f575db309f2fe75b6086
AdityaJ2305 <adityajindal2305@gmail.com>
2025-01-03 23:30:20 +0530

    deleted formcontext

--- src/pluginTypes.ts
+++ src/pluginTypes.ts
@@ -6,7 +6,6 @@ import { UserAssignedModel } from "@/components/Users/models";
 import { EncounterTabProps } from "@/pages/Encounters/EncounterShow";
 
 import { AppRoutes } from "./Routers/AppRouter";
-import { FormContextValue } from "./components/Form/FormContext";
 import { PatientMeta } from "./components/Patient/models";
 import { pluginMap } from "./pluginMap";
 import { PatientModel } from "./types/emr/patient";
@@ -27,21 +26,6 @@ export type ExtendFacilityConfigureComponentType = React.FC<{
   facilityId: string;
 }>;
 
-export type ExtendPatientRegisterFormComponentType = React.FC<{
-  facilityId: string;
-  patientId?: string;
-  state: {
-    form: {
-      [key: string]: any;
-    };
-    errors: {
-      [key: string]: string;
-    };
-  };
-  dispatch: React.Dispatch<any>;
-  field: FormContextValue<PatientForm>;
-}>;
-
 // Define supported plugin components
 export type SupportedPluginComponents = {
   DoctorConnectButtons: DoctorConnectButtonComponentType;
@@ -49,7 +33,6 @@ export type SupportedPluginComponents = {
   ManageFacilityOptions: ManageFacilityOptionsComponentType;
   EncounterContextEnabler: React.FC;
   ExtendFacilityConfigure: ExtendFacilityConfigureComponentType;
-  ExtendPatientRegisterForm: ExtendPatientRegisterFormComponentType;
 };
 
 // Create a type for lazy-loaded components
