bcb806b9bd6f8b3a17933ecfee60f562e5c0b01f
AdityaJ2305 <adityajindal2305@gmail.com>
2025-01-07 15:49:44 +0530

    added code changes from rithvik pr

--- src/Utils/request/errorHandler.ts
+++ src/Utils/request/errorHandler.ts
@@ -76,10 +76,10 @@ function isNotFound(error: HTTPError) {
 
 type PydanticError = {
   type: string;
-  loc: string[];
+  loc?: string[];
   msg: string;
-  input: unknown;
-  url: string;
+  input?: unknown;
+  url?: string;
 };
 
 function isStructuredError(err: HTTPError["cause"]): err is StructuredError {
@@ -110,12 +110,15 @@ function isPydanticError(errors: unknown): errors is PydanticError[] {
 
 function handlePydanticErrors(errors: PydanticError[]) {
   errors.map(({ type, loc, msg }) => {
-    const title = type
+    if (!loc) {
+      toast.error(msg);
+      return;
+    }
+    type = type
       .replace("_", " ")
       .replace(/\b\w/g, (char) => char.toUpperCase());
-
-    toast.error(`${title}: '${loc.join(".")}'`, {
-      description: msg,
+    toast.error(msg, {
+      description: `${type}: '${loc.join(".")}'`,
       duration: 8000,
     });
   });
