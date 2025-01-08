736b6fbc3f2e963798e46b8c020b921e3a851603
AdityaJ2305 <adityajindal2305@gmail.com>
2025-01-08 18:30:35 +0530

    desing update

--- src/components/Common/SearchByMultipleFields.tsx
+++ src/components/Common/SearchByMultipleFields.tsx
@@ -28,6 +28,8 @@ import {
 import { FieldError } from "@/components/Form/FieldValidators";
 import PhoneNumberFormField from "@/components/Form/FormFields/PhoneNumberFormField";
 
+import { isAppleDevice } from "@/Utils/utils";
+
 interface SearchOption {
   key: string;
   type: "text" | "phone";
@@ -108,12 +110,13 @@ const SearchByMultipleFields: React.FC<SearchByMultipleFieldsProps> = ({
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      if (
-        e.key === "/" &&
-        !(document.activeElement instanceof HTMLInputElement)
-      ) {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
         e.preventDefault();
         setOpen(true);
+      }
+      if (e.key === "Escape") {
+        inputRef.current?.focus();
+        setOpen(false);
       }
       if (open) {
         if (e.key === "ArrowDown") {
@@ -127,22 +130,6 @@ const SearchByMultipleFields: React.FC<SearchByMultipleFieldsProps> = ({
         } else if (e.key === "Enter") {
           handleOptionChange(focusedIndex);
         }
-
-        if (e.key === "Escape") {
-          inputRef.current?.focus();
-          setOpen(false);
-        }
-
-        options.forEach((option, i) => {
-          if (
-            e.key.toLocaleLowerCase() ===
-              option.shortcutKey.toLocaleLowerCase() &&
-            open
-          ) {
-            e.preventDefault();
-            handleOptionChange(i);
-          }
-        });
       }
     };
 
@@ -198,15 +185,40 @@ const SearchByMultipleFields: React.FC<SearchByMultipleFieldsProps> = ({
         );
       default:
         return (
-          <Input
-            id={id}
-            type="text"
-            placeholder={selectedOption.placeholder}
-            {...commonProps}
-          />
+          <div className="relative">
+            <Input
+              id={id}
+              type="text"
+              placeholder={selectedOption.placeholder}
+              {...commonProps}
+            />
+            <div className="absolute top-1/2 right-2 transform -translate-y-1/2 flex items-center space-x-2 text-xs text-gray-500">
+              {open ? (
+                <span>ESC</span>
+              ) : (
+                <span>
+                  {isAppleDevice ? (
+                    "⌘K"
+                  ) : (
+                    <div className="flex gap-1 font-medium">
+                      <span className="text-gray-500">Ctrl</span>
+                      <span className="text-gray-500">K</span>
+                    </div>
+                  )}
+                </span>
+              )}
+            </div>
+          </div>
         );
     }
-  }, [selectedOption, searchValue, handleSearchChange, t, inputClassName]);
+  }, [
+    selectedOption,
+    searchValue,
+    handleSearchChange,
+    t,
+    inputClassName,
+    open,
+  ]);
 
   return (
     <div
@@ -222,42 +234,78 @@ const SearchByMultipleFields: React.FC<SearchByMultipleFieldsProps> = ({
         aria-haspopup="listbox"
         className="flex items-center rounded-t-lg"
       >
-        <Popover open={open} onOpenChange={setOpen}>
-          <PopoverTrigger asChild>
-            <Button
-              variant="ghost"
-              className="focus:ring-0 px-2 ml-1"
-              size="sm"
-              onClick={() => setOpen(true)}
-            >
-              <CareIcon icon="l-search" className="mr-2 text-base" />/
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-[250px] p-0">
-            <Command>
-              <CommandList>
-                <CommandGroup>
-                  {options.map((option, index) => (
-                    <CommandItem
-                      key={option.key}
-                      onSelect={() => handleOptionChange(index)}
-                      className={cn({
-                        "bg-gray-100": focusedIndex === index,
-                        "hover:bg-secondary-100": true,
-                      })}
-                    >
-                      <CareIcon icon="l-search" className="mr-2 h-4 w-4" />
-                      <span className="flex-1">{t(option.key)}</span>
-                      <kbd className="ml-auto text-xs text-gray-400">
-                        {option.shortcutKey}
-                      </kbd>
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
+        {options.length > 1 && (
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="ghost"
+                className="focus:ring-0 px-2 ml-1"
+                size="sm"
+                onClick={() => setOpen(true)}
+              >
+                <CareIcon icon="l-search" className="mr-2 text-base" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[250px] p-0">
+              <Command>
+                <CommandList>
+                  <CommandGroup>
+                    <div className="p-2">
+                      <div className="mb-4">
+                        <p className="text-sm font-medium text-gray-600">
+                          Search by
+                        </p>
+                        <div className="flex mt-1">
+                          <Button
+                            onClick={() => setOpen(false)}
+                            variant="outline"
+                            size="xs"
+                            className="bg-primary-100 text-primary-700 hover:bg-primary-200 border-primary-400"
+                          >
+                            <CareIcon icon="l-check" className="" />
+                            {t(options[selectedOptionIndex].key)}
+                          </Button>
+                        </div>
+                      </div>
+
+                      <div>
+                        <p className="text-sm font-medium text-gray-600">
+                          Search by other options
+                        </p>
+                        {options.map((option, index) => {
+                          if (selectedOption.key === option.key) return null;
+
+                          return (
+                            <CommandItem
+                              key={option.key}
+                              onSelect={() => handleOptionChange(index)}
+                              className={cn({
+                                "bg-gray-100": focusedIndex === index,
+                                "hover:bg-secondary-100": true,
+                              })}
+                              onMouseEnter={() => setFocusedIndex(index)}
+                              onMouseLeave={() => setFocusedIndex(-1)}
+                            >
+                              <span className="flex-1">{t(option.key)}</span>
+                              {focusedIndex === index && (
+                                <kbd
+                                  className="ml-auto px-2 py-1 text-xs font-medium text-gray-800 bg-gray-200 rounded shadow-sm hover:bg-gray-300"
+                                  title="Press Enter to select"
+                                >
+                                  ⏎ Enter
+                                </kbd>
+                              )}
+                            </CommandItem>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+        )}
         <div className="w-full">{renderSearchInput}</div>
       </div>
       {error && (
