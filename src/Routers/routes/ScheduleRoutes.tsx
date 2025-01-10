48f34b8d4b8aa293777244fe413744d8dd0610d6
rithviknishad <mail@rithviknishad.dev>
2025-01-10 23:17:14 +0530

    remove unused / old hack for home facility appointments

--- src/Routers/routes/ScheduleRoutes.tsx
+++ src/Routers/routes/ScheduleRoutes.tsx
@@ -1,22 +1,9 @@
-import { Redirect } from "raviger";
-
-import useAuthUser from "@/hooks/useAuthUser";
-
 import { AppRoutes } from "@/Routers/AppRouter";
 import AppointmentDetail from "@/pages/Appointments/AppointmentDetail";
 import AppointmentsPage from "@/pages/Appointments/AppointmentsPage";
 import BookAppointment from "@/pages/Appointments/BookAppointment";
 
-const HomeFacilityRedirect = ({ suffix }: { suffix: string }) => {
-  const authUser = useAuthUser();
-  const facilityId = authUser.home_facility!;
-
-  return <Redirect to={`/facility/${facilityId}${suffix}`} />;
-};
-
 const ScheduleRoutes: AppRoutes = {
-  // Appointments
-  "/appointments": () => <HomeFacilityRedirect suffix="/appointments" />,
   "/facility/:facilityId/appointments": ({ facilityId }) => (
     <AppointmentsPage facilityId={facilityId} />
   ),
