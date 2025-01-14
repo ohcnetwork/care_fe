fc94d7776000daec36376a65de5ca67b9efd8fac
abhimanyurajeesh <abhimanyurajeesh@gmail.com>
2025-01-14 12:37:35 +0530

    Remove unused useFeatureFlags hook from featureFlags.tsx

--- src/Utils/featureFlags.tsx
+++ src/Utils/featureFlags.tsx
@@ -1,15 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
-import { createContext, useContext, useEffect, useState } from "react";
-
-import { FacilityModel } from "@/components/Facility/models";
+import { createContext, useEffect, useState } from "react";
 
 import useAuthUser from "@/hooks/useAuthUser";
 
-import routes from "@/Utils/request/api";
-import query from "@/Utils/request/query";
-
 export type FeatureFlag = "SCRIBE_ENABLED"; // "HCX_ENABLED" | "ABDM_ENABLED" |
-
 export interface FeatureFlagsResponse {
   user_flags: FeatureFlag[];
   facility_flags: {
@@ -47,27 +40,4 @@ export const FeatureFlagsProvider = (props: { children: React.ReactNode }) => {
       {props.children}
     </FeatureFlagsContext.Provider>
   );
-};
-
-export const useFeatureFlags = (facility?: FacilityModel | string) => {
-  const context = useContext(FeatureFlagsContext);
-  if (context === undefined) {
-    throw new Error(
-      "useFeatureFlags must be used within a FeatureFlagsProvider",
-    );
-  }
-
-  const facilityId = typeof facility === "string" ? facility : facility?.id;
-
-  const { data: facilityObject } = useQuery({
-    queryKey: ["facility", facilityId],
-    queryFn: query(routes.getPermittedFacility, {
-      pathParams: { id: facilityId || "" },
-    }),
-    enabled: !!facilityId,
-  });
-
-  const facilityFlags = facilityObject?.facility_flags || [];
-
-  return [...context.user_flags, ...facilityFlags];
 };
