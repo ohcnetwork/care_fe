39d317d9b4d45d240e20dd80b49fe5b1a7600942
AdityaJ2305 <adityajindal2305@gmail.com>
2025-01-19 15:58:22 +0530

    reset cypress file changes

--- cypress/pageObject/Users/UserCreation.ts
+++ cypress/pageObject/Users/UserCreation.ts
@@ -35,20 +35,12 @@ export class UserCreation {
   }
 
   fillUsername(username: string) {
-    cy.get('[data-cy="username-input"]')
-      .should("exist")
-      .should("be.visible")
-      .clear()
-      .type(username, { force: true });
+    cy.typeIntoField('[data-cy="username-input"]', username);
     return this;
   }
 
   fillPassword(password: string) {
-    cy.get('[data-cy="password-input"]')
-      .should("exist") // Ensure the field exists
-      .should("be.visible") // Ensure the field is visible
-      .clear() // Clear the field before typing
-      .type(password, { force: true }); // Type the password
+    cy.typeIntoField('[data-cy="password-input"]', password);
     return this;
   }
 
