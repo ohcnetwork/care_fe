b2366e4a08ed0233ad029736b52ed2c13286d57e
AdityaJ2305 <adityajindal2305@gmail.com>
2025-01-20 18:03:22 +0530

    optimize orgSkeleton

--- src/pages/Organization/components/OrganizationLayoutSkeleton.tsx
+++ src/pages/Organization/components/OrganizationLayoutSkeleton.tsx
@@ -1,5 +1,6 @@
-import { Card, CardContent } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
+
+import { SkeletonLoading } from "@/components/Common/SkeletonLoading";
 
 export default function OrganizationLayoutSkeleton() {
   return (
@@ -7,31 +8,12 @@ export default function OrganizationLayoutSkeleton() {
       <Skeleton className="h-8 w-48 mb-4" />
       <Skeleton className="h-4 w-24 mb-4" />
       <div className="flex space-x-4 mb-4">
-        {Array.from({ length: 3 }).map((_, index) => (
-          <Skeleton key={index} className="h-8 w-24" />
-        ))}
+        <SkeletonLoading count={3} element="NoteSkeleton" />
       </div>
       <Skeleton className="h-6 w-40 mb-4" />
       <Skeleton className="h-8 w-1/4 mb-4" />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {Array.from({ length: 6 }).map((_, i) => (
-          <Card key={i}>
-            <CardContent className="p-6">
-              <div className="flex space-x-4">
-                <div className="flex-1 space-y-4">
-                  <Skeleton className="h-6 w-1/2" />
-                  <div className="flex space-x-4">
-                    <Skeleton className="h-4 w-1/3" />
-                    <Skeleton className="h-4 w-1/3" />
-                  </div>
-                </div>
-                <div className="flex-1 flex items-center justify-center">
-                  <Skeleton className="h-6 w-1/2" />
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-        ))}
+        <SkeletonLoading count={6} element="CardGridSkeleton" />
       </div>
     </div>
   );
