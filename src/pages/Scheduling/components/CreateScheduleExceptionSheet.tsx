1a7ab4873f9796f12ae49b3a956333d8084137e5
mahendar <mahendarchikkolla@gmail.com>
2025-01-23 10:49:20 +0530

    date time overflow in exception form

--- src/pages/Scheduling/components/CreateScheduleExceptionSheet.tsx
+++ src/pages/Scheduling/components/CreateScheduleExceptionSheet.tsx
@@ -205,7 +205,7 @@ export default function CreateScheduleExceptionSheet({
                   )}
                 />
 
-                <div className="grid grid-cols-2 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   <FormField
                     control={form.control}
                     name="valid_from"
