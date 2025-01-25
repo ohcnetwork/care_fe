e87c5f7d847102a6117f6139fd07be6387dfc066
AdityaJ2305 <adityajindal2305@gmail.com>
2025-01-25 17:47:36 +0530

    rm eslint disable line

--- cypress/e2e/login_spec/loginpage.cy.ts
+++ cypress/e2e/login_spec/loginpage.cy.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-relative-import-paths/no-relative-import-paths
-import { LoginPage } from "../../pageObject/auth/LoginPage";
+import { LoginPage } from "pageObject/auth/LoginPage";
 
 describe("Login Page", () => {
   const loginPage = new LoginPage();
