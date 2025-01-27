4ad294bb17d1afacdbb2a52125efd092fe3c3a8f
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2025-01-27 15:42:59 +0530

    add questionnaire creation cypress test

--- src/components/Patient/PatientInfoCard.tsx
+++ src/components/Patient/PatientInfoCard.tsx
@@ -307,7 +307,10 @@ export default function PatientInfoCard(props: PatientInfoCardProps) {
           id="consultation-buttons"
         >
           {!completedEncounterStatus.includes(encounter.status) && (
-            <div className="flex w-full flex-col gap-3 lg:w-auto 2xl:flex-row">
+            <div
+              className="flex w-full flex-col gap-3 lg:w-auto 2xl:flex-row"
+              data-cy="update-encounter-button"
+            >
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                   <Button variant="primary">
@@ -321,6 +324,7 @@ export default function PatientInfoCard(props: PatientInfoCardProps) {
                       <Link
                         href={`/facility/${encounter.facility.id}/patient/${patient.id}/encounter/${encounter.id}/questionnaire/${option.slug}`}
                         className="cursor-pointer text-gray-800"
+                        data-cy="update-encounter-option"
                       >
                         {t(option.title)}
                       </Link>
