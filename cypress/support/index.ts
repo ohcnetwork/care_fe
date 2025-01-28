9f271fc0205870bcf31a6909cfbe8eb18d1b929d
AdityaJ2305 <adityajindal2305@gmail.com>
2025-01-28 14:22:16 +0530

    path added for cypress

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,4 +1,4 @@
-import { ErrorMessageItem } from "./commands";
+import { ErrorMessageItem } from "@/support/commands";
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
