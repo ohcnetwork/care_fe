d011d26d2bb13143950410e53fddfe4f593d1b42
AdityaJ2305 <adityajindal2305@gmail.com>
2025-01-29 23:31:14 +0530

    created new print comp for resource letter

--- src/Routers/routes/ResourceRoutes.tsx
+++ src/Routers/routes/ResourceRoutes.tsx
@@ -1,4 +1,5 @@
 import View from "@/components/Common/View";
+import PrintResourceLetter from "@/components/Resource/PrintResourceLetter";
 import BoardView from "@/components/Resource/ResourceBoard";
 import ResourceDetails from "@/components/Resource/ResourceDetails";
 import { ResourceDetailsUpdate } from "@/components/Resource/ResourceDetailsUpdate";
@@ -10,6 +11,7 @@ const ResourceRoutes: AppRoutes = {
   "/resource": () => <View name="resource" board={BoardView} list={ListView} />,
   "/resource/:id": ({ id }) => <ResourceDetails id={id} />,
   "/resource/:id/update": ({ id }) => <ResourceDetailsUpdate id={id} />,
+  "/resource/:id/print": ({ id }) => <PrintResourceLetter id={id} />,
 };
 
 export default ResourceRoutes;
