d51c8c7c963aa7f0c437e16c9634ee8bf96446dd
AdityaJ2305 <adityajindal2305@gmail.com>
2025-01-29 23:58:53 +0530

    add variant to button

--- src/components/Users/UserForm.tsx
+++ src/components/Users/UserForm.tsx
@@ -701,6 +701,7 @@ export default function UserForm({
           type="submit"
           className="w-full"
           data-cy="submit-user-form"
+          variant="primary_gradient"
           disabled={
             isLoadingUser ||
             !form.formState.isDirty ||
