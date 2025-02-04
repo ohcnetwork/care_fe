c13705a4e79ca4dccbf2d8e04f073ee80363a9c1
Rishith25 <kamishetty.rishith@gmail.com>
2025-02-04 09:35:52 +0530

    removed uri

--- src/components/Resource/ResourceCreate.tsx
+++ src/components/Resource/ResourceCreate.tsx
@@ -167,7 +167,6 @@ export default function ResourceCreate(props: ResourceProps) {
       crumbsReplacements={{
         [facilityId]: { name: facilityData?.name || "" },
         resource: {
-          uri: `/facility/${facilityId}/resource`,
           style: "pointer-events-none",
         },
       }}
