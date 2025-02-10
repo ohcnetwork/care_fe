b85ddae89170a8a923fe5145cc3e3104180b4a71
Rishith25 <kamishetty.rishith@gmail.com>
2025-02-10 19:35:28 +0530

    i8n added for search users

--- src/pages/Facility/settings/organizations/FacilityOrganizationUsers.tsx
+++ src/pages/Facility/settings/organizations/FacilityOrganizationUsers.tsx
@@ -84,7 +84,7 @@ export default function FacilityOrganizationUsers({ id, facilityId }: Props) {
             <div className="w-full lg:w-1/3">
               <Input
                 type="text"
-                placeholder="Search users..."
+                placeholder={t("search_user")}
                 value={qParams.search || ""}
                 onChange={(e) =>
                   updateQuery({
