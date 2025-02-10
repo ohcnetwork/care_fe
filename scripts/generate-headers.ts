5dbdc23208c6398a68c4c592be1a7cd9a4e8f6c2
rithviknishad <mail@rithviknishad.dev>
2025-02-10 20:35:40 +0530

    cleanup code and git ignore headers output

--- scripts/generate-headers.ts
+++ scripts/generate-headers.ts
@@ -1,19 +1,20 @@
-import * as fs from "fs";
-import * as path from "path";
+import { writeFile } from "fs/promises";
+import path from "path";
 
-const headers64 = process.env.HEADERS_64;
-const header_folder = path.join(__dirname, "..", "public");
+const headersPath = path.join(__dirname, "../public/_headers");
 
-if (headers64) {
-  console.log("HEADERS_64 environment variable is set.");
-  const headersBuffer = Buffer.from(headers64, "base64");
-  console.log("Decoded headers from base64.");
-  const headers = headersBuffer.toString("utf-8");
-  const headersPath = path.join(header_folder, "_headers");
-  console.log(`Writing headers to file at path: ${headersPath}`);
-  fs.writeFileSync(headersPath, headers, "utf-8");
-  console.log("Headers written to file successfully.");
-} else {
-  console.error("HEADERS_64 environment variable is not set.");
-  process.exit(0);
+async function main() {
+  const headersBase64 = process.env.HEADERS_BASE64;
+
+  if (!headersBase64) {
+    console.error("HEADERS_BASE64 environment variable is not set.");
+    return;
+  }
+
+  const headers = Buffer.from(headersBase64, "base64").toString("utf-8");
+  await writeFile(headersPath, headers, "utf-8");
+
+  console.log(`Headers written to: '${headersPath}'`);
 }
+
+main();
