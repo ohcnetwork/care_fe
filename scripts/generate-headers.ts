a8978cf34cb7426ed33e17f4984660021ceef6ce
Vysakh Premkumar <84713473+tellmeY18@users.noreply.github.com>
2025-02-10 13:58:44 +0530

    fix build dir

--- scripts/generate-headers.ts
+++ scripts/generate-headers.ts
@@ -2,13 +2,14 @@ import * as fs from "fs";
 import * as path from "path";
 
 const headers64 = process.env.HEADERS_64;
+const header_folder = path.join(__dirname, "..", "public");
 
 if (headers64) {
   console.log("HEADERS_64 environment variable is set.");
   const headersBuffer = Buffer.from(headers64, "base64");
   console.log("Decoded headers from base64.");
   const headers = headersBuffer.toString("utf-8");
-  const headersPath = path.join(__dirname, "build", "_headers");
+  const headersPath = path.join(header_folder, "_headers");
   console.log(`Writing headers to file at path: ${headersPath}`);
   fs.writeFileSync(headersPath, headers, "utf-8");
   console.log("Headers written to file successfully.");
