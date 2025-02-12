5e60b1a2ce08f1eaea42858ec36f244f1671ea1b
AdityaJ2305 <adityajindal2305@gmail.com>
2025-02-12 23:23:56 +0530

    redesign encounter card

--- src/components/Facility/EncounterCard.tsx
+++ src/components/Facility/EncounterCard.tsx
@@ -1,13 +1,12 @@
 import { t } from "i18next";
-import { BedSingle, CircleCheck, CircleDashed, Clock } from "lucide-react";
-import { Link } from "raviger";
+import { BadgeCheck, BedSingle, CircleDashed, Clock, Eye } from "lucide-react";
+import { navigate } from "raviger";
 
 import { cn } from "@/lib/utils";
 
-import CareIcon from "@/CAREUI/icons/CareIcon";
-
 import { Badge } from "@/components/ui/badge";
-import { buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 
 import { formatDateTime } from "@/Utils/utils";
 import { Encounter, completedEncounterStatus } from "@/types/emr/encounter";
@@ -20,88 +19,104 @@ export const EncounterCard = (props: EncounterCardProps) => {
   const { encounter } = props;
   return (
     <>
-      <div className="pb-16 block relative border-l-2 px-4 border-l-secondary-300 hover:border-primary-500 transition-all before:absolute before:-left-[7px] before:top-0 before:w-3 before:aspect-square before:bg-secondary-400 before:rounded-full hover:before:bg-primary-500 before:transition-all">
-        <Badge
-          variant={
-            completedEncounterStatus.includes(encounter.status)
-              ? "outline"
-              : "secondary"
-          }
-          className="inline-flex items-center gap-2 -translate-y-2"
-        >
+      <div className="pb-6 block relative border-l-2 px-4 border-l-secondary-300">
+        <div className="absolute -left-[12px] top-0 bg-white">
           {completedEncounterStatus.includes(encounter.status) ? (
-            <CircleCheck className="w-4 h-4 text-green-300" />
+            <BadgeCheck className="w-5 h-5 text-teal-300" />
           ) : (
-            <CircleDashed className="w-4 h-4 text-yellow-500" />
+            <CircleDashed className="w-5 h-5 text-purple-400" />
           )}
-          {t(`encounter_status__${encounter.status}`)}
-        </Badge>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-          {[
-            {
-              label: t("facility"),
-              value: (
-                <div className="flex items-center gap-2">
-                  {encounter.facility.name}
-                </div>
-              ),
-            },
-            {
-              label: t("encounter_class"),
-              value: (
-                <div className="flex items-center gap-2">
-                  <BedSingle className="w-4 h-4 text-blue-400" />
-                  {t(`encounter_class__${encounter.encounter_class}`)}
-                </div>
-              ),
-            },
-            {
-              label: t("priority"),
-              value: (
-                <div className="flex items-center gap-2">
-                  <Clock className="w-4 h-4 text-yellow-500" />
-                  {t(`encounter_priority__${encounter.priority.toLowerCase()}`)}
-                </div>
-              ),
-            },
-            {
-              label: t("start_date"),
-              value: encounter.period.start
-                ? formatDateTime(encounter.period.start)
-                : t("not_started"),
-            },
-            {
-              label: t("end_date"),
-              hide: !encounter.period.end,
-              value: formatDateTime(encounter.period.end),
-            },
-            {
-              label: t("external_id"),
-              hide: !encounter.external_identifier,
-              value: encounter.external_identifier,
-            },
-          ]
-            .filter((f) => !f.hide)
-            .map((field, i) => (
-              <div key={i}>
-                <div className="text-sm text-gray-600">{field.label}</div>
-                <div className="font-bold">{field.value}</div>
-              </div>
-            ))}
         </div>
+        <Card>
+          <CardContent className="p-4 sm:p-2 space-y-4">
+            <div className="flex flex-wrap gap-2 sm:gap-4">
+              <Badge
+                variant="outline"
+                className={cn(
+                  "inline-flex gap-2 py-1",
+                  completedEncounterStatus.includes(encounter.status)
+                    ? "bg-green-100 text-green-800 border-green-200"
+                    : "bg-purple-100 text-indigo-800 border-purple-200",
+                )}
+              >
+                {completedEncounterStatus.includes(encounter.status) ? (
+                  <BadgeCheck className="w-4 h-4 text-teal-700 " />
+                ) : (
+                  <CircleDashed className="w-4 h-4 text-indigo-800" />
+                )}
+                {t(`encounter_status__${encounter.status}`)}
+              </Badge>
+              <Badge
+                variant="outline"
+                className="inline-flex items-center gap-2 py-1 bg-gray-100 text-gray-800 border-gray-200"
+              >
+                <BedSingle />
+                {t(`encounter_class__${encounter.encounter_class}`)}
+              </Badge>
+            </div>
 
-        <Link
-          href={`/facility/${encounter.facility.id}/encounter/${encounter.id}/updates`}
-          className={cn(
-            buttonVariants({ variant: "secondary" }),
-            "mt-2 shadow-none border border-secondary-300",
-            !encounter.period.start && "pointer-events-none opacity-50",
-          )}
-        >
-          <CareIcon icon="l-plus-circle" />
-          {t("View Encounter")}
-        </Link>
+            <div className="grid sm:flex sm:flex-wrap sm:justify-between gap-4">
+              {[
+                {
+                  label: t("facility"),
+                  value: (
+                    <div className="flex items-center gap-2">
+                      {encounter.facility.name}
+                    </div>
+                  ),
+                },
+                {
+                  label: t("start_date"),
+                  value: encounter.period.start
+                    ? formatDateTime(encounter.period.start)
+                    : t("not_started"),
+                },
+                {
+                  label: t("priority"),
+                  value: (
+                    <div className="flex items-center gap-2">
+                      <Clock className="w-4 h-4 text-yellow-500" />
+                      {t(
+                        `encounter_priority__${encounter.priority.toLowerCase()}`,
+                      )}
+                    </div>
+                  ),
+                },
+                {
+                  label: t("end_date"),
+                  hide: !encounter.period.end,
+                  value: formatDateTime(encounter.period.end),
+                },
+                {
+                  label: t("external_id"),
+                  hide: !encounter.external_identifier,
+                  value: encounter.external_identifier,
+                },
+              ]
+                .filter((f) => !f.hide)
+                .map((field, i) => (
+                  <div key={i} className="w-full mx-3 sm:w-auto">
+                    <div className="text-gray-600">{field.label}</div>
+                    <div className="font-bold">{field.value}</div>
+                  </div>
+                ))}
+            </div>
+            <div className="w-full py-2 bg-gray-100 px-2">
+              <Button
+                variant="outline"
+                onClick={() =>
+                  navigate(
+                    `/facility/${encounter.facility.id}/encounter/${encounter.id}/updates`,
+                  )
+                }
+                className="p-2 border border-black"
+              >
+                <Eye />
+                {t("View Encounter")}
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </>
   );
