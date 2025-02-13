002ec2cd10ee220ae9b371d4b941c88e3dd6421a
AdityaJ2305 <adityajindal2305@gmail.com>
2025-02-14 01:06:44 +0530

    rm styling on questionnaire

--- src/components/Facility/ConsultationDetails/QuestionnaireResponsesList.tsx
+++ src/components/Facility/ConsultationDetails/QuestionnaireResponsesList.tsx
@@ -211,7 +211,7 @@ function ResponseCard({
     <Card
       className={cn(
         "flex flex-col py-3 px-4 transition-colors hover:bg-muted/50",
-        isPrintPreview && "border-black rounded-none shadow-none",
+        isPrintPreview && "shadow-none",
       )}
     >
       <div className="flex items-start justify-between">
@@ -328,12 +328,7 @@ export default function QuestionnaireResponsesList({
         ) : (
           <div>
             {questionnarieResponses?.results?.length === 0 ? (
-              <Card
-                className={cn(
-                  "p-6",
-                  isPrintPreview && "border border-black rounded-none",
-                )}
-              >
+              <Card className="p-6">
                 <div className="text-lg font-medium text-gray-500">
                   {t("no_questionnaire_responses")}
                 </div>
