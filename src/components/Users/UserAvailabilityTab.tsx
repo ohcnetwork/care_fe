f9b7db467c51b518b52e7d14648a93f596598c73
Mohamed amaan <mohamedamaan319@gmail.com>
2025-02-14 19:35:46 +0530

    galaxy z-fold responsiveness updated

--- src/components/Users/UserAvailabilityTab.tsx
+++ src/components/Users/UserAvailabilityTab.tsx
@@ -175,12 +175,17 @@ export default function UserAvailabilityTab({ userData: user }: Props) {
       />
 
       <div className="space-y-4">
-        <div className="flex items-center md:items-end justify-between gap-1 md:gap-0">
+        <div className="flex items-end justify-between gap-3 md:gap-0">
           <div className="flex bg-gray-100 rounded-lg p-0 md:p-1 gap-1 max-w-min">
             <Button
               variant={view === "schedule" ? "outline" : "ghost"}
               onClick={() => setQParams({ tab: "schedule" })}
-              className={cn(view === "schedule" && "shadow", "hover:bg-white")}
+              className={cn(
+                view === "schedule" && "shadow",
+                "hover:bg-white",
+                "text-xs md:text-sm",
+                "px-2 md:px-4",
+              )}
             >
               {t("schedule")}
             </Button>
@@ -190,6 +195,8 @@ export default function UserAvailabilityTab({ userData: user }: Props) {
               className={cn(
                 view === "exceptions" && "shadow",
                 "hover:bg-white",
+                "text-xs md:text-sm",
+                "px-2 md:px-4",
               )}
             >
               {t("exceptions")}
