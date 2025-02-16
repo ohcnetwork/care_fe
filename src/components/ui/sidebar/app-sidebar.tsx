4ba8831214a75db1e536415e6886782131eff455
Rishith25 <kamishetty.rishith@gmail.com>
2025-02-16 17:56:10 +0530

    selected Facility state changed to null

--- src/components/ui/sidebar/app-sidebar.tsx
+++ src/components/ui/sidebar/app-sidebar.tsx
@@ -60,6 +60,8 @@ export function AppSidebar({
   }, [user?.organizations, organizationId]);
 
   React.useEffect(() => {
+    setSelectedFacility(null);
+
     if (!user?.facilities || !facilityId || !facilitySidebar) return;
 
     const facility = user.facilities.find((f) => f.id === facilityId);
