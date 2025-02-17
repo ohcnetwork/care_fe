f23ba469288c8c902260fee4dafda6c78cdf81f4
AdityaJ2305 <adityajindal2305@gmail.com>
2025-02-17 23:25:05 +0530

    see details btn added, design sync with dept users and ..few more bugs

--- src/components/Facility/FacilityUsers.tsx
+++ src/components/Facility/FacilityUsers.tsx
@@ -22,7 +22,7 @@ import { useView } from "@/Utils/useView";
 
 export default function FacilityUsers(props: { facilityId: string }) {
   const { t } = useTranslation();
-  const { qParams, updateQuery, Pagination } = useFilters({
+  const { qParams, updateQuery, Pagination, resultsPerPage } = useFilters({
     limit: 15,
     cacheBlacklist: ["username"],
   });
@@ -38,8 +38,8 @@ export default function FacilityUsers(props: { facilityId: string }) {
       pathParams: { facility_id: facilityId },
       queryParams: {
         username: qParams.username,
-        limit: qParams.limit,
-        offset: (qParams.page - 1) * qParams.limit,
+        limit: resultsPerPage,
+        offset: ((qParams.page ?? 1) - 1) * resultsPerPage,
       },
     }),
     enabled: !!facilityId,
