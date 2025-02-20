abfe74e9585a8454aa0d8e61b73b1b17610db2c7
Tanuj Nainwal <nainwaltanuj9389@gmail.com>
2025-02-20 17:22:11 +0530

    done suggested changes

--- src/components/Common/AvatarEditModal.tsx
+++ src/components/Common/AvatarEditModal.tsx
@@ -104,8 +104,8 @@ const AvatarEditModal = ({
           openCamera.getTracks().forEach((track) => track.stop());
         }
       }
-    } catch (error: any) {
-      toast.error("Failed to stop camera: ", error);
+    } catch {
+      toast.error("Failed to stop camera");
     } finally {
       setIsCameraOpen(false);
     }
