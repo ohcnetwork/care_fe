373e7ceeb3b1c34697be1315c376120b1ed7395f
AdityaJ2305 <adityajindal2305@gmail.com>
2025-02-21 02:07:46 +0530

    color badges specific to status

--- src/components/ValueSet/ValueSetList.tsx
+++ src/components/ValueSet/ValueSetList.tsx
@@ -88,9 +88,14 @@ export function ValueSetList() {
                 <td className="whitespace-nowrap px-6 py-4">
                   <Badge
                     className={
-                      valueset.status === "active"
-                        ? "bg-green-100 text-green-800 hover:bg-green-200"
-                        : "bg-red-100 text-red-800 hover:bg-red-200"
+                      {
+                        active:
+                          "bg-green-100 text-green-800 hover:bg-green-200",
+                        draft:
+                          "bg-yellow-100 text-yellow-800 hover:bg-yellow-200",
+                        retired: "bg-red-100 text-red-800 hover:bg-red-200",
+                        unknown: "bg-gray-100 text-gray-800 hover:bg-gray-200",
+                      }[valueset.status]
                     }
                   >
                     {t(valueset.status)}
