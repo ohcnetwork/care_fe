9f5a449e882c731bd32ce3fcb4add47d65d06ba2
Khavin Shankar <khavinshankar@gmail.com>
2025-02-27 09:39:49 +0530

    add the commented code back

--- src/components/Facility/FacilityHome.tsx
+++ src/components/Facility/FacilityHome.tsx
@@ -220,6 +220,39 @@ export const FacilityHome = ({ facilityId }: Props) => {
                       </h2>
                     </div>
                   </div>
+                  <div className="flex-shrink-0">
+                    {/* <AlertDialog>
+                      TODO: add delete facility
+                      <AlertDialogTrigger asChild>
+                        <Trash2 className="mr-2 h-4 w-4" />
+                        {t("delete_facility")}
+                      </AlertDialogTrigger>
+                      <AlertDialogContent>
+                        <AlertDialogHeader>
+                          <AlertDialogTitle>
+                            {t("delete_facility")}
+                          </AlertDialogTitle>
+                          <AlertDialogDescription>
+                            {t("delete_facility_confirmation", {
+                              name: facilityData?.name,
+                            })}
+                          </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                          <AlertDialogCancel>{t("cancel")}</AlertDialogCancel>
+                          <div
+                            onClick={() => deleteFacility()}
+                            className={cn(
+                              buttonVariants({ variant: "destructive" }),
+                            )}
+                            // disabled={isDeleting}
+                          >
+                            {isDeleting ? t("deleting") : t("delete")}
+                          </div>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialog> */}
+                  </div>
                 </div>
               </div>
               <div className="absolute right-0 bottom-0 p-1 text-white [@media(max-width:35rem)]:top-0">
