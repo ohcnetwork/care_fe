5706e010a41f2b0f020ae5f8dd1ca9f4d3fe0926
Tanuj Nainwal <nainwaltanuj9389@gmail.com>
2025-02-27 14:10:11 +0530

    fix bug

--- src/pages/Scheduling/components/EditScheduleTemplateSheet.tsx
+++ src/pages/Scheduling/components/EditScheduleTemplateSheet.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { isBefore, parse } from "date-fns";
-import { ArrowRightIcon, Loader2, SaveIcon, Trash2Icon } from "lucide-react";
+import { Loader2, SaveIcon, Trash2Icon } from "lucide-react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
@@ -749,12 +749,12 @@ const NewAvailabilityCard = ({
             )}
           /> */}
 
-          <div className="flex items-center gap-4">
+          <div className="flex flex-wrap">
             <FormField
               control={form.control}
               name="start_time"
               render={({ field }) => (
-                <FormItem className="flex-1">
+                <FormItem className="flex flex-col w-full">
                   <FormLabel required>{t("start_time")}</FormLabel>
                   <FormControl>
                     <Input type="time" {...field} />
@@ -764,13 +764,11 @@ const NewAvailabilityCard = ({
               )}
             />
 
-            <ArrowRightIcon className="size-4 mt-5" />
-
             <FormField
               control={form.control}
               name="end_time"
               render={({ field }) => (
-                <FormItem className="flex-1">
+                <FormItem className="flex flex-col w-full mt-2">
                   <FormLabel required>{t("end_time")}</FormLabel>
                   <FormControl>
                     <Input type="time" {...field} />
