81886d78c472c11f97c10e0be05958dac81ffd28
Tanuj Nainwal <nainwaltanuj9389@gmail.com>
2025-02-28 21:00:47 +0530

    fix phone-input issue

--- src/components/Facility/FacilityForm.tsx
+++ src/components/Facility/FacilityForm.tsx
@@ -308,7 +308,6 @@ export default function FacilityForm({
                     <PhoneInput
                       data-cy="facility-phone"
                       placeholder={t("enter_phone_number")}
-                      maxLength={13}
                       {...field}
                     />
                   </FormControl>
