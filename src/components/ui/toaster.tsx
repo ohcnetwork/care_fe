91b12ed5a680c5cd44cb0aad02361ac7f9d7ff49
nikhila8606 <nikhila8606@gmail.com>
2025-03-06 12:04:35 +0530

    Changed the possition of Toaser to top right and check responsiveness

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -28,7 +28,7 @@ export function Toaster() {
           </Toast>
         );
       })}
-      <ToastViewport />
+      <ToastViewport className="fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:top-0 sm:left-1/2 sm:-translate-x-1/2 sm:flex-col md:top-0 md:left-auto md:right-0 md:translate-x-0 md:max-w-[420px]" />
     </ToastProvider>
   );
 }
