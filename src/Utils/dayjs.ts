fcd3952904e073f0ece82cf981eb24f095b79fb4
Aditya Singh <78854372+ARH-MNAJS@users.noreply.github.com>
2025-03-07 22:15:19 +0530

    Updated dayjs.ts

--- src/Utils/dayjs.ts
+++ src/Utils/dayjs.ts
@@ -3,10 +3,12 @@ import customParseFormat from "dayjs/plugin/customParseFormat";
 import duration from "dayjs/plugin/duration";
 import isBetween from "dayjs/plugin/isBetween";
 import relativeTime from "dayjs/plugin/relativeTime";
+import localizedFormat from "dayjs/plugin/localizedFormat";
 
 dayjs.extend(relativeTime);
 dayjs.extend(duration);
 dayjs.extend(customParseFormat);
 dayjs.extend(isBetween);
+dayjs.extend(customParseFormat);
 
 export default dayjs;
