cc9efe981550721e1e9c81a45f931b146c48870f
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-03-10 12:36:49 +0530

    Add infotip for slug using description from QuestionnaireList.tsx

--- src/components/Questionnaire/QuestionnaireList.tsx
+++ src/components/Questionnaire/QuestionnaireList.tsx
@@ -85,9 +85,16 @@ const RenderCard = ({
               </div>
 
               <div className="mb-4">
-                <h3 className="text-left text-xs font-medium uppercase tracking-wider text-gray-500">
-                  {t("slug")}
-                </h3>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <h3 className="text-left text-xs font-medium uppercase tracking-wider text-gray-500">
+                        {t("slug")}
+                      </h3>
+                    </TooltipTrigger>
+                    <TooltipContent>{t("slug_description")}</TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
                 <p className="text-sm text-gray-900 truncate">
                   {questionnaire.slug}
                 </p>
