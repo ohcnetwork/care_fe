b6c979fbb26d8276b2b71555d8afa52963a4e59c
nikhila8606 <nikhila8606@gmail.com>
2025-03-10 19:41:11 +0530

    Update toast file from develop branch

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,8 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      (className =
-        "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:top-0 sm:left-1/2 sm:-translate-x-1/2 sm:flex-col md:top-0 md:left-auto md:right-0 md:translate-x-0 md:max-w-[420px]"),
+      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
       className,
     )}
     {...props}
