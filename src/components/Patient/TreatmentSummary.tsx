bc4d172a7e5187464817d1d53ae2076d1fe262a9
AdityaJ2305 <adityajindal2305@gmail.com>
2025-03-11 18:03:12 +0530

    prefix translation

--- src/components/Patient/TreatmentSummary.tsx
+++ src/components/Patient/TreatmentSummary.tsx
@@ -422,7 +422,7 @@ export default function TreatmentSummary({
                     const notes = medication.note;
                     return {
                       medicine: medication.medication?.display,
-                      status: t(medication.status),
+                      status: t(`medication_status_${medication.status}`),
                       dosage: dosage,
                       frequency: instruction?.as_needed_boolean
                         ? `${t("as_needed_prn")} (${instruction?.as_needed_for?.display ?? "-"})`
@@ -462,7 +462,7 @@ export default function TreatmentSummary({
                       medication.medication.display ??
                       medication.medication.code,
                     dosage: medication.dosage_text,
-                    status: t(medication.status),
+                    status: t(`medication_status_${medication.status}`),
                     medication_taken_between: [
                       medication.effective_period?.start,
                       medication.effective_period?.end,
