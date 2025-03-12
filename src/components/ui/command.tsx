871ba6a961b0a13369b8d64bb830469c6e6030d3
rithviknishad <mail@rithviknishad.dev>
2025-03-12 08:00:27 +0530

    fix(ui): Adjust CommandDrawer styling with rounded top corners
    - Update CommandDrawer's DrawerContent to use rounded-t-md for top corners
    - Maintain consistent styling with previous Vaul drawer implementation

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -41,7 +41,7 @@ const CommandDrawer = ({
 }: React.ComponentProps<typeof Drawer>) => {
   return (
     <Drawer {...props}>
-      <DrawerContent className="max-h-[85vh] mx-2 rounded-md">
+      <DrawerContent className="max-h-[85vh] mx-2 rounded-t-md">
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-gray-500 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5 dark:[&_[cmdk-group-heading]]:text-gray-400">
           {children}
         </Command>
