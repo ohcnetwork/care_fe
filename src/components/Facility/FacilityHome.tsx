668c12cd9bab37f8307b2a6f3cef20148838315a
AdityaJ2305 <adityajindal2305@gmail.com>
2025-03-18 20:07:52 +0530

    rm changes in facilityhome

--- src/components/Facility/FacilityHome.tsx
+++ src/components/Facility/FacilityHome.tsx
@@ -296,7 +296,11 @@ export const FacilityHome = ({ facilityId }: Props) => {
                 <EditFacilitySheet
                   facilityId={facilityId}
                   trigger={
-                    <Button className="cursor-pointer" variant="outline">
+                    <Button
+                      className="cursor-pointer font-semibold"
+                      variant="outline"
+                      size="sm"
+                    >
                       <CareIcon icon="l-pen" />
                       {t("edit_facility_details")}
                     </Button>
