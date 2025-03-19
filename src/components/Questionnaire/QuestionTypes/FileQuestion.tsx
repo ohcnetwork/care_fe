551ac66de8dc88ed99ecb1983714ec2632fec861
Shivank Kacker <kacker.shivank@gmail.com>
2025-03-19 18:03:49 +0530

    stashing

--- src/components/Questionnaire/QuestionTypes/FileQuestion.tsx
+++ src/components/Questionnaire/QuestionTypes/FileQuestion.tsx
@@ -1,3 +1,11 @@
+import { useTranslation } from "react-i18next";
+
+import { cn } from "@/lib/utils";
+
+import { buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { FileUploadQuestion } from "@/types/files/files";
 import { QuestionValidationError } from "@/types/questionnaire/batch";
 import {
   QuestionnaireResponse,
@@ -17,6 +25,7 @@ interface FilesQuestionProps {
   errors: QuestionValidationError[];
   encounterId: string;
   facilityId: string;
+  index: number;
 }
 
 export function FilesQuestion(props: FilesQuestionProps) {
@@ -28,7 +37,60 @@ export function FilesQuestion(props: FilesQuestionProps) {
     errors,
     encounterId,
     facilityId,
+    index,
   } = props;
 
-  return <div className="space-y-4">FILE QUESTION</div>;
+  const { t } = useTranslation();
+
+  const values =
+    (questionnaireResponse.values?.[index]?.value as FileUploadQuestion[]) ||
+    [];
+  const value = values[0] ?? {};
+
+  const handleUpdate = (updates: Partial<FileUploadQuestion>) => {
+    updateQuestionnaireResponseCB(
+      questionnaireResponse.values.map((v, i) =>
+        i === index ? { type: "files", value: [{ ...value, ...updates }] } : v,
+      ),
+      questionnaireResponse.question_id,
+      questionnaireResponse.note,
+    );
+  };
+
+  return (
+    <div className="flex items-center gap-2">
+      <Input
+        placeholder={t("file_name")}
+        className="flex-1"
+        value={value.name}
+        onChange={(e) => handleUpdate({ name: e.target.value })}
+      />
+      <label
+        className={cn(
+          buttonVariants({ variant: "secondary" }),
+          "border border-secondary-300 cursor-pointer",
+        )}
+      >
+        {t("choose_file")}
+        <input
+          type="file"
+          className="hidden"
+          onChange={(e) => {
+            // get base64 encoded file data
+            const file = e.target.files?.[0];
+            if (!file) return;
+            const reader = new FileReader();
+            reader.onload = (event) => {
+              const fileData = event.target?.result as string;
+              handleUpdate({
+                file_data: fileData,
+                name: value.original_name || file.name,
+                original_name: file.name,
+              });
+            };
+          }}
+        />
+      </label>
+    </div>
+  );
 }
