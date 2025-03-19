d7c2e8b73c12254afb9d630f3c39857e8acd6666
Shivank Kacker <kacker.shivank@gmail.com>
2025-03-19 15:27:29 +0530

    Added support for loading recent and favourite valuesets

--- src/components/Questionnaire/ValueSetSelect.tsx
+++ src/components/Questionnaire/ValueSetSelect.tsx
@@ -1,11 +1,21 @@
-import { CaretSortIcon } from "@radix-ui/react-icons";
-import { useQuery } from "@tanstack/react-query";
+import { CaretSortIcon, StarFilledIcon, StarIcon } from "@radix-ui/react-icons";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { cn } from "@/lib/utils";
 
-import { Button } from "@/components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Command,
   CommandDrawer,
@@ -20,12 +30,15 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import useBreakpoints from "@/hooks/useBreakpoints";
 
 import routes from "@/Utils/request/api";
+import mutate from "@/Utils/request/mutate";
 import query from "@/Utils/request/query";
 import { Code, ValueSetSystem } from "@/types/questionnaire/code";
+import valuesetRoutes from "@/types/valueset/valuesetApi";
 
 interface Props {
   system: ValueSetSystem;
@@ -56,6 +69,8 @@ export default function ValueSetSelect({
   const [internalOpen, setInternalOpen] = useState(false);
   const [search, setSearch] = useState("");
   const isMobile = useBreakpoints({ default: true, sm: false });
+  const [activeTab, setActiveTab] = useState(0);
+  const [isClearingFavourites, setIsClearingFavourites] = useState(false);
 
   const searchQuery = useQuery({
     queryKey: ["valueset", system, "expand", count, search],
@@ -65,11 +80,122 @@ export default function ValueSetSelect({
     }),
   });
 
+  const favouritesQuery = useQuery({
+    queryKey: ["valueset", system, "favourites"],
+    queryFn: query(valuesetRoutes.favourites, { pathParams: { slug: system } }),
+  });
+
+  const addFavouriteMutation = useMutation({
+    mutationFn: (body: { code: string }) =>
+      mutate(valuesetRoutes.addFavourite, {
+        pathParams: { slug: system },
+        body,
+      })(body),
+    onSuccess: () => {
+      favouritesQuery.refetch();
+    },
+  });
+
+  const removeFavouriteMutation = useMutation({
+    mutationFn: (body: { code: string }) =>
+      mutate(valuesetRoutes.removeFavourite, {
+        pathParams: { slug: system },
+        body,
+      })(body),
+    onSuccess: () => {
+      favouritesQuery.refetch();
+    },
+  });
+
+  const clearFavouritesMutation = useMutation({
+    mutationFn: mutate(valuesetRoutes.clearFavourites, {
+      pathParams: { slug: system },
+    }),
+    onSuccess: () => {
+      favouritesQuery.refetch();
+      setIsClearingFavourites(false);
+    },
+  });
+
+  const recentsQuery = useQuery({
+    queryKey: ["valueset", system, "recents"],
+    queryFn: query(valuesetRoutes.recentViews, {
+      pathParams: { slug: system },
+    }),
+  });
+
+  const addRecentMutation = useMutation({
+    mutationFn: (body: Code) =>
+      mutate(valuesetRoutes.addRecentView, {
+        pathParams: { slug: system },
+        body,
+      })(body),
+    onSuccess: () => {
+      recentsQuery.refetch();
+    },
+  });
+
+  const resultsWithRecents = [
+    ...(recentsQuery.data?.filter((recent) =>
+      recent.display?.toLowerCase().includes(search.toLowerCase()),
+    ) || []),
+    ...(searchQuery.data?.results?.filter(
+      (r) => !recentsQuery.data?.find((recent) => recent.code === r.code),
+    ) || []),
+  ];
+
+  const favourites = favouritesQuery.data?.filter((favourite) =>
+    favourite.display?.toLowerCase().includes(search.toLowerCase()),
+  );
+
   useEffect(() => {
     if (controlledOpen || internalOpen) {
       setSearch("");
     }
   }, [controlledOpen, internalOpen]);
+
+  const Item = ({ option }: { option: Code }) => (
+    <CommandItem
+      key={option.code}
+      value={option.code}
+      onSelect={() => {
+        onSelect({
+          code: option.code,
+          display: option.display || "",
+          system: option.system || "",
+        });
+        setInternalOpen(false);
+        addRecentMutation.mutate(option);
+      }}
+      className="cursor-pointer"
+    >
+      <div className="flex items-center justify-between w-full gap-4">
+        <span>{option.display}</span>
+
+        <button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            favouritesQuery.data?.find(
+              (favourite) => favourite.code === option.code,
+            )
+              ? removeFavouriteMutation.mutate(option)
+              : addFavouriteMutation.mutate(option);
+          }}
+          className="hover:text-primary-500 transition-all text-secondary-900"
+        >
+          {favouritesQuery.data?.find(
+            (favourite) => favourite.code === option.code,
+          ) ? (
+            <StarFilledIcon />
+          ) : (
+            <StarIcon className="" />
+          )}
+        </button>
+      </div>
+    </CommandItem>
+  );
 
   const content = (
     <Command filter={() => 1}>
@@ -79,7 +205,19 @@ export default function ValueSetSelect({
         onValueChange={setSearch}
         autoFocus
       />
-      <CommandList>
+      <CommandList className="h-[300px] overflow-hidden">
+        <Tabs
+          value={activeTab.toString()}
+          onValueChange={(value) => {
+            setActiveTab(Number(value));
+          }}
+          className="md:hidden"
+        >
+          <TabsList className="flex">
+            <TabsTrigger value={"0"}>{t("search")}</TabsTrigger>
+            <TabsTrigger value={"1"}>{t("favourites")}</TabsTrigger>
+          </TabsList>
+        </Tabs>
         <CommandEmpty>
           {search.length < 3
             ? t("min_char_length_error", { min_length: 3 })
@@ -87,27 +225,84 @@ export default function ValueSetSelect({
               ? t("searching")
               : t("no_results_found")}
         </CommandEmpty>
+        <div className="flex">
+          <div
+            className={`${activeTab === 0 ? "block" : "hidden"} md:block flex-1 overflow-auto h-[300px]`}
+          >
+            <CommandGroup>
+              {resultsWithRecents.map((option) => (
+                <Item key={option.code} option={option} />
+              ))}
+            </CommandGroup>
+          </div>
 
-        <CommandGroup>
-          {searchQuery.data?.results.map((option) => (
-            <CommandItem
-              key={option.code}
-              value={option.code}
-              onSelect={() => {
-                onSelect({
-                  code: option.code,
-                  display: option.display || "",
-                  system: option.system || "",
-                });
-                setInternalOpen(false);
-              }}
-            >
-              <span>{option.display}</span>
-            </CommandItem>
-          ))}
-        </CommandGroup>
+          <div
+            className={`${activeTab === 1 ? "block" : "hidden"} md:block flex-1 ${(search.length < 3 && !searchQuery.isFetching) || (!favourites?.length && !resultsWithRecents.length) ? "" : "md:border-l"} border-gray-200`}
+          >
+            <CommandGroup>
+              <div className="flex items-center justify-between">
+                <span className="text-xs font-normal text-gray-700 p-1">
+                  {t("favourites")}
+                </span>
+                {!!favourites?.length && (
+                  <button>
+                    <span
+                      onClick={() => setIsClearingFavourites(true)}
+                      className="text-xs font-thin text-gray-700 p-1 cursor-pointer"
+                    >
+                      {t("clear")}
+                    </span>
+                  </button>
+                )}
+              </div>
+              {favouritesQuery.isFetched &&
+                favouritesQuery.data?.length === 0 && (
+                  <div className="flex items-center flex-col justify-center h-[200px] md:h-[250px] text-xs text-gray-500">
+                    {t("no_favourites", {
+                      star: "☆",
+                    })}
+                  </div>
+                )}
+              {favourites?.map((option) => (
+                <Item key={option.code} option={option} />
+              ))}
+            </CommandGroup>
+          </div>
+        </div>
       </CommandList>
     </Command>
+  );
+
+  const alert = (
+    <AlertDialog
+      open={isClearingFavourites}
+      onOpenChange={(open) => setIsClearingFavourites(open)}
+    >
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            {t("are_you_sure_clear_favourites")}
+          </AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={() => setIsClearingFavourites(false)}>
+            {t("cancel")}
+          </AlertDialogCancel>
+          <AlertDialogAction
+            className={cn(buttonVariants({ variant: "destructive" }))}
+            onClick={() => {
+              clearFavouritesMutation.mutate({});
+            }}
+          >
+            {clearFavouritesMutation.isPending ? (
+              <Loader2 className="h-4 w-4 animate-spin mr-2" />
+            ) : (
+              t("confirm")
+            )}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 
   if (isMobile && !hideTrigger) {
@@ -132,41 +327,48 @@ export default function ValueSetSelect({
         <CommandDrawer open={internalOpen} onOpenChange={setInternalOpen}>
           {content}
         </CommandDrawer>
+        {alert}
       </>
     );
   }
 
   return (
-    <Popover
-      open={controlledOpen || internalOpen}
-      onOpenChange={setInternalOpen}
-    >
-      {!hideTrigger && (
-        <PopoverTrigger asChild disabled={disabled}>
-          <Button
-            variant="outline"
-            role="combobox"
-            className={cn(
-              "w-full justify-between",
-              wrapTextForSmallScreen
-                ? "h-auto md:h-9 whitespace-normal text-left md:truncate"
-                : "truncate",
-              !value?.display && "text-gray-400",
-            )}
-          >
-            <span>{value?.display || placeholder}</span>
-            <CaretSortIcon className="ml-2 size-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-      )}
+    <>
+      <Popover
+        open={controlledOpen || internalOpen}
+        onOpenChange={setInternalOpen}
+      >
+        {!hideTrigger && (
+          <PopoverTrigger asChild disabled={disabled}>
+            <Button
+              variant="outline"
+              role="combobox"
+              className={cn(
+                "w-full justify-between",
+                wrapTextForSmallScreen
+                  ? "h-auto md:h-9 whitespace-normal text-left md:truncate"
+                  : "truncate",
+                !value?.display && "text-gray-400",
+              )}
+            >
+              <span>{value?.display || placeholder}</span>
+              <CaretSortIcon className="ml-2 size-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+        )}
 
-      {hideTrigger ? (
-        content
-      ) : (
-        <PopoverContent className="w-[300px] p-0" align="start">
-          {content}
-        </PopoverContent>
-      )}
-    </Popover>
+        {hideTrigger ? (
+          content
+        ) : (
+          <PopoverContent
+            className={`transition-all w-[600px] p-0`}
+            align="start"
+          >
+            {content}
+          </PopoverContent>
+        )}
+      </Popover>
+      {alert}
+    </>
   );
 }
