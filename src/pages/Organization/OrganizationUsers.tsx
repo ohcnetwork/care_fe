de5e80e7797d9f6e6ed0ec8520ccd9f8658dccdf
Gurdeep Singh <gurdeepsingh8975@gmail.com>
2025-03-22 22:05:36 +0530

    Final Changes

--- src/pages/Organization/OrganizationUsers.tsx
+++ src/pages/Organization/OrganizationUsers.tsx
@@ -149,7 +149,7 @@ export default function OrganizationUsers({ id, navOrganizationId }: Props) {
           />
         </div>
         {isFetchingUsers ? (
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
             <CardGridSkeleton count={6} />
           </div>
         ) : (
