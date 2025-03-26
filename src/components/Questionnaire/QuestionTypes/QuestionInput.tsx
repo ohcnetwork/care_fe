162b223ae343207c7dc9e0a35e8481dda6494f61
yash-learner <yash.topaz@gmail.com>
2025-03-26 16:45:10 +0530

    clean up

--- src/components/Questionnaire/QuestionTypes/QuestionInput.tsx
+++ src/components/Questionnaire/QuestionTypes/QuestionInput.tsx
@@ -238,7 +238,7 @@ export function QuestionInput({
           return (
             <div
               key={index}
-              className={cn("", removeButton && "gap-2 flex items-end")}
+              className={cn(removeButton && "gap-2 flex items-end")}
             >
               <div
                 className={cn("space-y-1", { "flex-1": removeButton })}
