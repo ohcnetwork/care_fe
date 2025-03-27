39b01db44199854e3e873ee0c2286231a8248d3c
Shivank Kacker <kacker.shivank@gmail.com>
2025-03-27 18:15:37 +0530

    made description not optional

--- src/types/emr/role/role.ts
+++ src/types/emr/role/role.ts
@@ -3,6 +3,6 @@ import { Permission } from "@/types/emr/permission/permission";
 export interface Role {
   id: string;
   name: string;
-  description?: string;
+  description: string;
   permissions: Permission[];
 }
