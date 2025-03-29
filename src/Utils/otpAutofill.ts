4ecdae155232b172e8a574241f5d9416caad4364
Mohamed amaan <mohamedamaan319@gmail.com>
2025-03-29 18:10:33 +0530

    fixing error v4

--- src/Utils/otpAutofill.ts
+++ src/Utils/otpAutofill.ts
@@ -8,29 +8,33 @@ export const autofillOtp = (
   onSuccess: (otp: string) => void,
   onError: () => void,
 ) => {
-  if ("OTPCredential" in window) {
+  if (navigator.credentials && "OTPCredential" in window) {
     toast.success("OTPCredential API is available."); // Testing toast
     navigator.credentials
       .get({ otp: { transport: ["sms"] } } as CredentialRequestOptions)
       .then((otpCredential) => {
-        if (otpCredential) {
-          toast.error("OTP received.");
+        if (!otpCredential) {
+          toast.error("No OTP received.");
+          onError();
           return;
         }
 
-        const otp = otpCredential as unknown as OTPCredential | null;
-        if (otp) {
+        const otp = otpCredential as unknown as OTPCredential;
+
+        if (otp.code) {
           toast.success(`Your OTP is: ${otp.code}`);
           onSuccess(otp.code);
         } else {
-          toast.error("No OTP received."); // Testing toast
+          toast.error("Received OTP is invalid.");
+          onError();
         }
       })
-      .catch(() => {
-        toast.error("OTP retrieval failed."); // Testing toast
+      .catch((error) => {
+        toast.error("OTP retrieval failed.");
+        console.error("OTP retrieval error:", error);
         onError();
       });
   } else {
-    toast.error("OTPCredential API is not available."); // Testing toast
+    toast.error("OTPCredential API is not available.");
   }
 };
