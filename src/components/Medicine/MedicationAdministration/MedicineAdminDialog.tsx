824aeb8db3a834753549652a34b573d6d9de1d6d
Mohamed amaan <mohamedamaan319@gmail.com>
2025-04-03 22:54:25 +0530

    width updated

--- src/components/Medicine/MedicationAdministration/MedicineAdminDialog.tsx
+++ src/components/Medicine/MedicationAdministration/MedicineAdminDialog.tsx
@@ -67,7 +67,7 @@ export const MedicineAdminDialog = ({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-h-screen overflow-auto">
+      <DialogContent className="max-h-screen overflow-auto max-w-screen">
         <DialogHeader>
           <div className="flex items-center justify-between">
             <DialogTitle className="text-xl">
