aa5803b62a259c35cac28b45b90b42342ae4f0de
Mohamed amaan <mohamedamaan319@gmail.com>
2025-04-07 22:46:01 +0530

    max-w-xl added

--- src/components/Medicine/MedicationAdministration/MedicineAdminDialog.tsx
+++ src/components/Medicine/MedicationAdministration/MedicineAdminDialog.tsx
@@ -67,7 +67,7 @@ export const MedicineAdminDialog = ({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-h-screen overflow-auto max-w-screen">
+      <DialogContent className="max-h-screen overflow-auto max-w-xl">
         <DialogHeader>
           <div className="flex items-center justify-between">
             <DialogTitle className="text-xl">
