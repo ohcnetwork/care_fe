ddb27e1a39fd14552223274c68b52545b8c56525
rithviknishad <mail@rithviknishad.dev>
2025-04-07 18:45:54 +0530

    fix: Update PatientNavUser and PatientSwitcher to show in mobile view

--- src/components/ui/sidebar/nav-user.tsx
+++ src/components/ui/sidebar/nav-user.tsx
@@ -134,7 +134,7 @@ export function PatientNavUser() {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
               data-cy="user-menu-dropdown"
             >
-              {open && (
+              {(open || isMobile) && (
                 <>
                   <Avatar
                     className="size-8 rounded-lg"
@@ -151,7 +151,7 @@ export function PatientNavUser() {
                   <CaretSortIcon className="ml-auto size-4" />
                 </>
               )}
-              {!open && (
+              {!open && !isMobile && (
                 <div className="flex flex-row items-center">
                   <Avatar
                     name={patient?.name || phoneNumber}
