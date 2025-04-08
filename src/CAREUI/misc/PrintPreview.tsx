08484de65238b788a337b953e831007d3e959868
rithviknishad <mail@rithviknishad.dev>
2025-04-08 18:02:21 +0530

    fixes print preview

--- src/CAREUI/misc/PrintPreview.tsx
+++ src/CAREUI/misc/PrintPreview.tsx
@@ -4,17 +4,12 @@ import { useTranslation } from "react-i18next";
 import { cn } from "@/lib/utils";
 
 import CareIcon from "@/CAREUI/icons/CareIcon";
-import {
-  ZoomControls,
-  ZoomProvider,
-  ZoomTransform,
-} from "@/CAREUI/interactive/Zoom";
+import { ZoomProvider, ZoomTransform } from "@/CAREUI/interactive/Zoom";
 
 import { Button } from "@/components/ui/button";
 
 import Page from "@/components/Common/Page";
 
-import useAppHistory from "@/hooks/useAppHistory";
 import useBreakpoints from "@/hooks/useBreakpoints";
 
 type Props = {
@@ -25,26 +20,22 @@ type Props = {
 };
 
 export default function PrintPreview(props: Props) {
-  const normalScale = useBreakpoints({ default: 1 });
+  const initialScale = useBreakpoints({ default: 0.44, md: 1 });
   const { t } = useTranslation();
-  const { goBack } = useAppHistory();
 
   return (
-    <Page title={props.title}>
-      <div className="mx-auto my-8 xl:w-[50rem] border rounded-xl border-gray-200 shadow-2xl overflow-hidden">
-        <div className="top-0 z-20 flex gap-2 bg-secondary-100 px-2 py-4 xl:absolute xl:right-6 xl:top-8 xl:justify-end">
-          <Button variant="outline" onClick={() => goBack()}>
-            <CareIcon icon="l-arrow-left" className="text-lg" />
-            {t("back")}
-          </Button>
-          <Button variant="primary" disabled={props.disabled} onClick={print}>
-            <CareIcon icon="l-print" className="text-lg" />
-            {t("print")}
-          </Button>
-        </div>
-
-        <ZoomProvider initialScale={normalScale}>
-          <ZoomTransform className="origin-top-left bg-white p-10 text-sm shadow-2xl transition-all duration-200 ease-in-out lg:origin-top print:transform-none">
+    <Page
+      title={props.title}
+      options={
+        <Button variant="primary" disabled={props.disabled} onClick={print}>
+          <CareIcon icon="l-print" className="text-lg" />
+          {t("print")}
+        </Button>
+      }
+    >
+      <div className="mx-auto my-4 max-w-[95vw] print:max-w-none sm:my-8">
+        <ZoomProvider initialScale={initialScale}>
+          <ZoomTransform className="origin-top-left bg-white p-10 text-sm shadow-2xl transition-all duration-200 ease-in-out print:transform-none w-[50rem]">
             <div
               id="section-to-print"
               className={cn("w-full", props.className)}
@@ -52,8 +43,6 @@ export default function PrintPreview(props: Props) {
               {props.children}
             </div>
           </ZoomTransform>
-
-          <ZoomControls disabled={props.disabled} />
         </ZoomProvider>
       </div>
     </Page>
