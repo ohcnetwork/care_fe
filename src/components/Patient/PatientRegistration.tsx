8626c52ffc248a1c6ee6a3b641cc3626257a6941
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2025-04-08 07:55:57 +0530

    validation fix

--- src/components/Patient/PatientRegistration.tsx
+++ src/components/Patient/PatientRegistration.tsx
@@ -179,7 +179,19 @@ export default function PatientRegistration(
       same_phone_number: false,
       same_address: true,
     },
+    mode: "all",
+    criteriaMode: "all",
+    reValidateMode: "onChange",
   });
+
+  useEffect(() => {
+    const subscription = form.watch((value, { name }) => {
+      if (name && form.formState.errors[name]) {
+        form.clearErrors(name);
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, [form]);
 
   const { mutate: createPatient, isPending: isCreatingPatient } = useMutation({
     mutationKey: ["create_patient"],
