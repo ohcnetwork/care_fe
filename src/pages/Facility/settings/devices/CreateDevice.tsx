183db8c68c02f54a8a934a71fc323b6c6cee3e4b
nikhila8606 <nikhila8606@gmail.com>
2025-04-11 15:53:16 +0530

    Implmented device type and icon

--- src/pages/Facility/settings/devices/CreateDevice.tsx
+++ src/pages/Facility/settings/devices/CreateDevice.tsx
@@ -1,12 +1,13 @@
-import { navigate } from "raviger";
+import { CubeIcon } from "@radix-ui/react-icons";
+import { navigate, useQueryParams } from "raviger";
 import { useTranslation } from "react-i18next";
 import { toast } from "sonner";
 
 import { Separator } from "@/components/ui/separator";
 
-import PageTitle from "@/components/Common/PageTitle";
-
 import DeviceForm from "@/pages/Facility/settings/devices/components/DeviceForm";
+
+import { usePluginDevices } from "./hooks/usePluginDevices";
 
 interface Props {
   facilityId: string;
@@ -14,10 +15,25 @@ interface Props {
 
 export default function CreateDevice({ facilityId }: Props) {
   const { t } = useTranslation();
+  const [qParams] = useQueryParams<{ type?: string }>();
+  const pluginDevices = usePluginDevices();
+
+  const device = pluginDevices.find((device) => device.type === qParams.type);
+
+  const Icon = device?.icon || CubeIcon;
 
   return (
     <div className="space-y-3 max-w-3xl mx-auto">
-      <PageTitle title={t("add_device")} />
+      <div className="inline-flex items-center">
+        <Icon className="size-5 mr-2" />
+        <span className="text-2xl font-bold">
+          {t("add")}{" "}
+          {qParams.type && (
+            <>{qParams.type.charAt(0).toUpperCase() + qParams.type.slice(1)} </>
+          )}
+          {t("device")}
+        </span>
+      </div>
       <Separator />
 
       <div className="pt-4">
