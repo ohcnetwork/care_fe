369919b72428529ae1056f6fb80da3b7b588ec12
yash-learner <yash.topaz@gmail.com>
2025-04-22 23:51:35 +0530

    Add padding to duration as well

--- src/components/Questionnaire/QuestionTypes/MedicationRequestQuestion.tsx
+++ src/components/Questionnaire/QuestionTypes/MedicationRequestQuestion.tsx
@@ -907,7 +907,7 @@ const MedicationRequestGridRow: React.FC<MedicationRequestGridRowProps> = ({
         />
       </div>
       {/* Duration */}
-      <div className="lg:px-2 lg:py-1 lg:border-r border-gray-200 overflow-hidden">
+      <div className="lg:px-2 px-1 pb-1 lg:py-1 lg:border-r border-gray-200 overflow-hidden">
         <Label className="mb-1.5 block text-sm lg:hidden">
           {t("duration")}
         </Label>
