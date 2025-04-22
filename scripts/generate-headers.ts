ff3047af2bfa890d4e1c2fc3c32bc1cc6077122f
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2025-04-22 12:24:29 +0530

    test digitalocean

--- scripts/generate-headers.ts
+++ scripts/generate-headers.ts
@@ -1,5 +1,6 @@
 import { writeFile } from "fs/promises";
 import path from "path";
+import yaml from "yaml";
 
 const headers = process.env.HEADERS;
 const header_folder = path.join(__dirname, "..", "public");
@@ -11,23 +12,51 @@ async function writeHeaders() {
   }
 
   console.log("HEADERS environment variable is set.");
-  const headersPath = path.join(header_folder, "_headers");
-  console.log(`Writing headers to file at path: ${headersPath}`);
 
   try {
     const headerEntries = headers.split(" | ");
-    let formattedHeaders = "/*\n";
 
+    // Generate standard _headers file
+    const headersPath = path.join(header_folder, "_headers");
+    console.log(`Writing eaders to file at path: ${headersPath}`);
+
+    let formattedHeaders = "/*\n";
     for (const header of headerEntries) {
       formattedHeaders += `  ${header}\n`;
     }
 
     await writeFile(headersPath, formattedHeaders, "utf-8");
     console.log("Headers written to file successfully.");
+
+    // Generate headers for Digital Ocean
+    const appYamlPath = path.join(header_folder, "app.yaml");
+    console.log(
+      `Writing Digital Ocean headers to file at path: ${appYamlPath}`,
+    );
+
+    const headerMap: Record<string, string> = {};
+    for (const header of headerEntries) {
+      const [key, value] = header.split(": ");
+      headerMap[key] = value;
+    }
+
+    const appConfig = {
+      routes: [
+        {
+          path: "/*",
+          http_headers: headerMap,
+        },
+      ],
+    };
+
+    const yamlContent = yaml.stringify(appConfig);
+    await writeFile(appYamlPath, yamlContent, "utf-8");
+    console.log("Digital Ocean headers written successfully.");
+
     process.exit(0);
   } catch (error) {
-    console.error("Error writing headers to file:", error);
-    process.exit(0);
+    console.error("Error writing headers to files:", error);
+    process.exit(1);
   }
 }
 
