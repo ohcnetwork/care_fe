ce928a6efb8980b533f6002e96bc8cd6a8320914
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2025-04-23 15:17:26 +0530

    remove the flag to add screenshot and video

--- cypress.config.ts
+++ cypress.config.ts
@@ -38,8 +38,6 @@ export default defineConfig({
     requestTimeout: 15000,
     numTestsKeptInMemory: 15,
     experimentalMemoryManagement: true,
-    video: true,
-    screenshotOnRunFailure: true,
   },
   env: {
     API_URL: process.env.REACT_CARE_API_URL,
