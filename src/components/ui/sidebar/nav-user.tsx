2d9a984014b5cc82e4dc323b35e30cb8d325237f
Mohamed amaan <mohamedamaan319@gmail.com>
2025-04-25 01:16:13 +0530

    unwant change

--- src/components/ui/sidebar/nav-user.tsx
+++ src/components/ui/sidebar/nav-user.tsx
@@ -196,12 +196,7 @@ export function PatientNavUser() {
               </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem
-              data-cy="user-menu-logout"
-              onClick={() => {
-                signOut();
-              }}
-            >
+            <DropdownMenuItem data-cy="user-menu-logout" onClick={signOut}>
               <LogOut />
               {t("logout")}
             </DropdownMenuItem>
