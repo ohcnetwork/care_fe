79fc17f94a45a16334505e58382e61f9aef16c97
Aditya Jindal <adityajindal2305@gmail.com>
2025-04-25 13:47:55 +0530

    return null
    Co-authored-by: Khavin Shankar <khavinshankar@gmail.com>

--- src/components/Location/BedListing.tsx
+++ src/components/Location/BedListing.tsx
@@ -17,7 +17,7 @@ export function BedListing({
   onBedSelect,
   onCheckStatus,
 }: BedListingProps) {
-  if (beds.length === 0) return <></>;
+  if (beds.length === 0) return null;
   return (
     <RadioGroup
       value={selectedBed || ""}
