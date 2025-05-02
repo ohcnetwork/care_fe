00aad2ab05b402cbd821e5b47fd91f733bf42b54
Rithvik Nishad <mail@rithviknishad.dev>
2025-05-02 14:01:40 +0530

    Update src/atoms/user-atom.ts

--- src/atoms/user-atom.ts
+++ src/atoms/user-atom.ts
@@ -3,7 +3,7 @@ import { atomWithStorage, createJSONStorage } from "jotai/utils";
 import { AuthUserModel } from "@/components/Users/models";
 
 export const userAtom = atomWithStorage<AuthUserModel | undefined>(
-  "user-atom",
+  "care-auth-user",
   undefined,
   createJSONStorage(() => sessionStorage),
 );
