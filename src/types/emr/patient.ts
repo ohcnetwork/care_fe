f961a296bf0019ee2bbb1c498f0ee06fa8283df0
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2025-05-02 21:18:22 +0530

    removed unused types

--- src/types/emr/patient.ts
+++ src/types/emr/patient.ts
@@ -36,7 +36,6 @@ export interface Patient {
   updated_by: UserBareMinimum | null;
   permissions: string[];
   nationality?: string;
-  partial_id: string;
 }
 
 export interface PartialPatientModel {
