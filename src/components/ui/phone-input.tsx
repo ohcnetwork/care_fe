c1544dd9ee3971f20d3145b2371d858102d4b8e7
karanziie1990 <karanwithgcloud@gmail.com>
2025-05-03 17:11:22 +0530

    fix: correct usage of careConfig.defaultCountry.name

--- src/components/ui/phone-input.tsx
+++ src/components/ui/phone-input.tsx
@@ -50,7 +50,7 @@ function PhoneInput({
       flagComponent={FlagComponent}
       countrySelectComponent={CountrySelect}
       inputComponent={InputComponent}
-      defaultCountry={careConfig.defaultCountry}
+      defaultCountry={careConfig.defaultCountry.name}
       smartCaret={true}
       /**
        * Handles the onChange event.
