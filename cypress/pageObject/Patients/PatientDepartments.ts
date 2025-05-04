e7931e2fe87ee5442f238f4888a4ce4e3c05088a
AdityaJ2305 <adityajindal2305@gmail.com>
2025-05-04 18:41:21 +0530

    minor one

--- cypress/pageObject/Patients/PatientDepartments.ts
+++ cypress/pageObject/Patients/PatientDepartments.ts
@@ -79,7 +79,7 @@ export class PatientDepartments {
 
   verifyRemoveUserRequest() {
     cy.wait("@removeUser").then((interception) => {
-      expect(interception.response?.statusCode).to.equal(200);
+      expect(interception.response?.statusCode).to.equal(204);
     });
     return this;
   }
