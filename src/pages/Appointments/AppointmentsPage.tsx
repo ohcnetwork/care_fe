67ceb1e9b898dc3922c56be52d0659f564b39b2c
Spandan-Mishra <spandanmishra69@gmail.com>
2025-05-07 15:47:39 +0530

    fixed for smaller devices

--- src/pages/Appointments/AppointmentsPage.tsx
+++ src/pages/Appointments/AppointmentsPage.tsx
@@ -556,7 +556,7 @@ export default function AppointmentsPage({
       </div>
 
       {activeTab === "board" ? (
-        <ScrollArea className="w-[calc(100vw-8rem)]">
+        <ScrollArea className="md:w-[calc(100vw-8rem)]">
           <div className="flex w-max space-x-4">
             {(
               [
