de715d2bdd50459d6fa80e89ed6a4975dee1ed0c
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2025-05-08 09:58:19 +0530

    add logic for the patient tab hightlight

--- src/components/ui/sidebar/nav-main.tsx
+++ src/components/ui/sidebar/nav-main.tsx
@@ -1,4 +1,3 @@
-import { t } from "i18next";
 import { ChevronRight } from "lucide-react";
 import { ActiveLink } from "raviger";
 import { useState } from "react";
@@ -77,7 +76,7 @@ export function NavMain({ links }: { links: NavigationLink[] }) {
                             "cursor-pointer hover:bg-gray-200 hover:text-green-700",
                             {
                               "group-data-collapsible:data-[active=true]:bg-white group-data-collapsible:data-[active=true]:text-green-700 group-collapsible:data-[active=true]:shadow":
-                                link.name == t("patients") &&
+                                link.name == "patients" &&
                                 /^\/facility\/[^/]+\/patient\/[^/]+$/.test(
                                   currentPath,
                                 ),
