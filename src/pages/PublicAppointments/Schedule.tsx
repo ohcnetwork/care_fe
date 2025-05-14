6cf56292588b53858ab6293adf426c8ccdef9e7d
Mohamed amaan <mohamedamaan319@gmail.com>
2025-05-14 16:17:13 +0530

    revert localstorage change

--- src/pages/PublicAppointments/Schedule.tsx
+++ src/pages/PublicAppointments/Schedule.tsx
@@ -334,6 +334,11 @@ export function ScheduleAppointment(props: AppointmentsProps) {
                   if (appointmentId && appointment) {
                     handleRescheduleAppointment(appointment);
                   } else {
+                    localStorage.setItem(
+                      "selectedSlot",
+                      JSON.stringify(selectedSlot),
+                    );
+                    localStorage.setItem("reason", reason);
                     navigate(
                       `/facility/${facilityId}/appointments/${staffId}/patient-select`,
                     );
