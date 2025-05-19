5df703e58a4c55a9cbcd36bb31708f9e95fd2ee3
Sindhu <sindhureddyvari3@gmail.com>
2025-05-19 15:24:11 +0530

    removed try-catch block

--- src/components/Users/UserAvatar.tsx
+++ src/components/Users/UserAvatar.tsx
@@ -61,33 +61,29 @@ export default function UserAvatar({ username }: { username: string }) {
     formData.append("profile_picture", file);
     const url = `${careConfig.apiUrl}/api/v1/users/${userData.username}/profile_picture/`;
 
-    try {
-      await uploadFile(
-        url,
-        formData,
-        "POST",
-        { Authorization: getAuthorizationHeader() },
-        async (xhr: XMLHttpRequest) => {
-          if (xhr.status === 200) {
-            setEditAvatar(false);
-            await sleep(1000);
-            queryClient.invalidateQueries({
-              queryKey: ["getUserDetails", username],
-            });
-            if (authUser.username === username) {
-              queryClient.invalidateQueries({ queryKey: ["currentUser"] });
-            }
-            toast.success(t("avatar_updated_success"));
+    await uploadFile(
+      url,
+      formData,
+      "POST",
+      { Authorization: getAuthorizationHeader() },
+      async (xhr: XMLHttpRequest) => {
+        if (xhr.status === 200) {
+          setEditAvatar(false);
+          await sleep(1000);
+          queryClient.invalidateQueries({
+            queryKey: ["getUserDetails", username],
+          });
+          if (authUser.username === username) {
+            queryClient.invalidateQueries({ queryKey: ["currentUser"] });
           }
-        },
-        null,
-        () => {
-          onError();
-        },
-      );
-    } catch {
-      setEditAvatar(false);
-    }
+          toast.success(t("avatar_updated_success"));
+        }
+      },
+      null,
+      () => {
+        onError();
+      },
+    );
   };
 
   const handleAvatarDelete = async (
