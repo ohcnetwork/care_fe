0e524bb2d7543f57d6f32697c1c3d028529c0069
Jacob John Jeevan <mail@jacobjeevan.me>
2025-05-29 12:49:26 +0530

    reports -> report in api

--- src/types/reportTemplate/reportApi.ts
+++ src/types/reportTemplate/reportApi.ts
@@ -5,29 +5,29 @@ import { ReportModel } from "./report";
 
 export default {
   viewReports: {
-    path: "/api/v1/reports/",
+    path: "/api/v1/report/",
     method: HttpMethod.GET,
     TRes: Type<PaginatedResponse<ReportModel>>(),
   },
   retrieveReport: {
-    path: "/api/v1/reports/{id}/",
+    path: "/api/v1/report/{id}/",
     method: HttpMethod.GET,
     TRes: Type<ReportModel>(),
   },
   editReport: {
-    path: "/api/v1/reports/{id}/",
+    path: "/api/v1/report/{id}/",
     method: HttpMethod.PUT,
     TBody: Type<Partial<ReportModel>>(),
     TRes: Type<ReportModel>(),
   },
   archiveReport: {
-    path: "/api/v1/reports/{id}/archive/",
+    path: "/api/v1/report/{id}/archive/",
     method: HttpMethod.POST,
     TRes: Type<ReportModel>(),
     TBody: Type<{ archive_reason: string }>(),
   },
   markReportCompleted: {
-    path: "/api/v1/reports/{id}/mark_upload_completed/",
+    path: "/api/v1/report/{id}/mark_upload_completed/",
     method: HttpMethod.POST,
     TRes: Type<ReportModel>(),
   },
