1c0a4fd454430c96296aa4032cd9995421a7d249
Jacob John Jeevan <mail@jacobjeevan.me>
2025-06-02 18:37:14 +0530

    default to dropdown for choice qns if repeat

--- src/components/Questionnaire/QuestionTypes/ChoiceQuestion.tsx
+++ src/components/Questionnaire/QuestionTypes/ChoiceQuestion.tsx
@@ -91,7 +91,7 @@ export const ChoiceQuestion = memo(function ChoiceQuestion({
           value={currentCoding}
           onSelect={handleCodingChange}
         ></ValueSetSelect>
-      ) : selectType === "dropdown" ? (
+      ) : selectType === "dropdown" || question.repeats ? (
         <Select
           value={currentValue}
           onValueChange={handleValueChange}
