819a4a5420d37fce6d63059cf0cc2fffa363f274
Shivam Kumar <155747305+maishivamhoo123@users.noreply.github.com>
2025-06-14 09:15:21 +0530

    Update src/pages/Encounters/tabs/EncounterOverviewTab.tsx
    Co-authored-by: Aditya Jindal <adityajindal2305@gmail.com>

--- src/pages/Encounters/tabs/EncounterOverviewTab.tsx
+++ src/pages/Encounters/tabs/EncounterOverviewTab.tsx
@@ -59,7 +59,7 @@ export const EncounterOverviewTab = ({
         <div className="flex-1 space-y-4" data-cy="encounter-overview">
           {canEdit && (
             <div className="flex flex-row justify-between gap-2">
-              <div className="grid grid-cols-2 sm:grid-cols-3  sm:w-auto gap-2 justify-start">
+              <div className="flex flex-wrap gap-2 justify-start">
                 {actionLinks.map((link) => {
                   return (
                     <Button
