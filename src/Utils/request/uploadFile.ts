e6819a0a812d8b901022581363cd065431a952a0
Mohamed amaan <mohamedamaan319@gmail.com>
2025-06-21 14:06:00 +0530

    single and multifile upload hook

--- src/Utils/request/uploadFile.ts
+++ src/Utils/request/uploadFile.ts
@@ -15,13 +15,6 @@ const uploadFile = async (
   onLoad: (xhr: XMLHttpRequest) => void,
   setUploadPercent: Dispatch<SetStateAction<number>> | null,
   onError: () => void,
-  options?: {
-    fileSize?: number;
-    totalBytes?: number;
-    bytesUploadedSoFar?: number;
-    setOverallProgress?: (percent: number) => void;
-    lastFilePercentRef?: { current: number };
-  },
 ): Promise<void> => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
@@ -50,61 +43,13 @@ const uploadFile = async (
         handleHttpError(httpError);
         reject(httpError);
       } else {
-        if (
-          options &&
-          options.setOverallProgress &&
-          options.fileSize &&
-          options.totalBytes &&
-          options.lastFilePercentRef
-        ) {
-          options.bytesUploadedSoFar =
-            (options.bytesUploadedSoFar || 0) +
-            (options.fileSize -
-              Math.round(
-                (options.lastFilePercentRef.current / 100) * options.fileSize,
-              ));
-          options.setOverallProgress(
-            options.totalBytes > 0
-              ? (options.bytesUploadedSoFar / options.totalBytes) * 100
-              : 0,
-          );
-        }
         resolve();
       }
     };
 
-    if (setUploadPercent != null || (options && options.setOverallProgress)) {
+    if (setUploadPercent != null) {
       xhr.upload.onprogress = (event: ProgressEvent) => {
-        if (setUploadPercent) {
-          handleUploadPercentage(event, setUploadPercent);
-        }
-        if (
-          options &&
-          options.setOverallProgress &&
-          options.fileSize &&
-          options.totalBytes &&
-          options.lastFilePercentRef
-        ) {
-          const percent = event.lengthComputable
-            ? (event.loaded / event.total) * 100
-            : 0;
-          if (typeof percent === "number") {
-            options.bytesUploadedSoFar =
-              (options.bytesUploadedSoFar || 0) -
-              Math.round(
-                (options.lastFilePercentRef.current / 100) * options.fileSize,
-              );
-            options.bytesUploadedSoFar += Math.round(
-              (percent / 100) * options.fileSize,
-            );
-            options.lastFilePercentRef.current = percent;
-            options.setOverallProgress(
-              options.totalBytes > 0
-                ? (options.bytesUploadedSoFar / options.totalBytes) * 100
-                : 0,
-            );
-          }
-        }
+        handleUploadPercentage(event, setUploadPercent);
       };
     }
 
