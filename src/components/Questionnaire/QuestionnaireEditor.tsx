deb83b313f4b9aac1d6d46e2d036598d03746c3d
Mohammed Nihal <57055998+nihal467@users.noreply.github.com>
2025-06-21 15:32:50 +0530

    questionnaire cypress test

--- src/components/Questionnaire/QuestionnaireEditor.tsx
+++ src/components/Questionnaire/QuestionnaireEditor.tsx
@@ -794,6 +794,7 @@ export default function QuestionnaireEditor({ id }: QuestionnaireEditorProps) {
             </DropdownMenu>
           )}
           <Button
+            type="submit"
             onClick={handleSave}
             disabled={isCreating || isUpdating}
             data-cy="save-questionnaire-form"
