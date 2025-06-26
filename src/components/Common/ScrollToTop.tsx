14850ead9d0b3d656f754033129adccc437790a8
AdityaJ2305 <adityajindal2305@gmail.com>
2025-06-26 17:35:42 +0530

    update css

--- src/components/Common/ScrollToTop.tsx
+++ src/components/Common/ScrollToTop.tsx
@@ -25,7 +25,7 @@ export function ScrollToTopButton({
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  const smRight = rightOffset ? `sm:right-${rightOffset}` : "";
+  const smRight = rightOffset ? `right-${rightOffset}` : "";
 
   if (!isVisible) return null;
 
@@ -33,8 +33,8 @@ export function ScrollToTopButton({
     <Button
       onClick={scrollToTop}
       className={cn(
-        "fixed bottom-10 right-2 z-50 rounded-full shadow-md",
         smRight,
+        "fixed bottom-10 max-sm:right-2 z-50 rounded-full shadow-md",
       )}
       variant="outline_primary"
       size="icon"
