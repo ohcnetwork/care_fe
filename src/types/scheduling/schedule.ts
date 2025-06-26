f2b25ac3d7f6634b7582eef9cbe2165885728f6a
Abhimanyu Rajeesh <63541653+abhimanyurajeesh@users.noreply.github.com>
2025-06-26 15:12:20 +0530

    Refactor location badge  (#12725)

--- src/types/scheduling/schedule.ts
+++ src/types/scheduling/schedule.ts
@@ -1,5 +1,7 @@
 import { DayOfWeek } from "@/CAREUI/interactive/WeekdayCheckbox";
 
+import { Badge } from "@/components/ui/badge";
+
 import { Time } from "@/Utils/types";
 import { Patient } from "@/types/emr/patient";
 import { FacilityBareMinimum } from "@/types/facility/facility";
@@ -142,7 +144,10 @@ export const APPOINTMENT_STATUS_COLORS = {
   cancelled: "destructive",
   entered_in_error: "destructive",
   rescheduled: "yellow",
-} as const satisfies Record<AppointmentStatus, string>;
+} as const satisfies Record<
+  AppointmentStatus,
+  React.ComponentProps<typeof Badge>["variant"]
+>;
 
 export type AppointmentNonCancelledStatus =
   (typeof AppointmentNonCancelledStatuses)[number];
