7ba509d34e94aef7a4d35b7d0a5c589190e1a72f
Krupa Anna Kuruvilla <krupaannakuruvilla@gmail.com>
2025-06-28 19:07:22 +0530

    Removed div wrapper

--- src/components/Common/LanguageSelector.tsx
+++ src/components/Common/LanguageSelector.tsx
@@ -33,20 +33,18 @@ export const LanguageSelector = () => {
   );
 
   return (
-    <div className="relative flex items-center w-full">
-      <Select value={i18n.language} onValueChange={handleLanguage}>
-        <SelectTrigger className="focus:border-primary-500 focus:outline-none focus:ring-primary-500">
-          <SelectValue placeholder={t("select_language")} />
-        </SelectTrigger>
-        <SelectContent>
-          {availableLocales.map((lang) => (
-            <SelectItem key={lang} value={lang}>
-              {LANGUAGES[lang]}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-    </div>
+    <Select value={i18n.language} onValueChange={handleLanguage}>
+      <SelectTrigger>
+        <SelectValue placeholder={t("select_language")} />
+      </SelectTrigger>
+      <SelectContent>
+        {availableLocales.map((lang) => (
+          <SelectItem key={lang} value={lang}>
+            {LANGUAGES[lang]}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 };
 
