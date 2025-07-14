f993d555aac16afe91fb0539e68d78cdfd1af5da
Jeffrin2005 <jeffrinjojo1@gmail.com>
2025-07-14 17:53:14 +0530

    remove coderabbit suggestion because another issue is there for getting the captured image

--- src/components/Common/AvatarEditModal.tsx
+++ src/components/Common/AvatarEditModal.tsx
@@ -160,8 +160,6 @@ export default function AvatarEditModal({
               type: blob.type,
             });
             setSelectedFile(myFile);
-            setCrop({ x: 0, y: 0 });
-            setZoom(1);
           } else {
             toast.error(t("failed_to_capture_image"));
           }
