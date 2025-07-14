de45da1d8b0c1fd896242e463142a6ee1cb57bf4
SiddanthNayak <siddanth06@gmail.com>
2025-07-14 12:36:23 +0530

    removed local loginMode state

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -97,9 +97,6 @@ const Login = (props: LoginProps) => {
   const [isCaptchaEnabled, setCaptcha] = useState(false);
   const { t } = useTranslation();
   const [forgotPassword, setForgotPassword] = useState(forgot);
-  const [loginMode, setLoginMode] = useState<LoginMode>(
-    mode === "patient" ? "patient" : "staff",
-  );
   const [isOtpSent, setIsOtpSent] = useState(false);
   const [phone, setPhone] = useState("");
   const [otp, setOtp] = useState("");
@@ -123,8 +120,8 @@ const Login = (props: LoginProps) => {
 
   // Remember the last login mode
   useEffect(() => {
-    localStorage.setItem(LocalStorageKeys.loginPreference, loginMode);
-  }, [loginMode]);
+    localStorage.setItem(LocalStorageKeys.loginPreference, mode);
+  }, [mode]);
 
   // Send OTP Mutation
   const { mutate: sendOtp, isPending: sendOtpPending } = useMutation({
@@ -367,9 +364,8 @@ const Login = (props: LoginProps) => {
               <CardContent>
                 <Tabs
                   defaultValue="staff"
-                  value={loginMode}
+                  value={mode}
                   onValueChange={(value) => {
-                    setLoginMode(value as LoginMode);
                     setQueryParams({ mode: value as LoginMode });
                     if (value === "staff") {
                       resetPatientLogin();
