18508ead4a9fba01768bf890da819c81bf010af6
nikhil022 <niksocial2@gmail.com>
2025-07-18 16:51:13 +0530

    lint fix

--- src/components/Medicine/MedicationAdministration/MedicineAdminForm.tsx
+++ src/components/Medicine/MedicationAdministration/MedicineAdminForm.tsx
@@ -1,4 +1,4 @@
-import { format, formatDistanceToNow, subDays, startOfMinute } from "date-fns";
+import { formatDistanceToNow, startOfMinute, subDays } from "date-fns";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
