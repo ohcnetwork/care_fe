4761da9e158c51182d52831356bd88335da3a2a0
Akhileswaran-K-R <akhilarathi956@gmail.com>
2025-07-29 02:09:07 +0530

    removed truncate and added wrapping of text

--- src/components/Questionnaire/ValueSetSelect.tsx
+++ src/components/Questionnaire/ValueSetSelect.tsx
@@ -250,7 +250,7 @@ export default function ValueSetSelect({
                   !value?.display && "text-gray-400",
                 )}
               >
-                <span className="truncate block max-w-full">
+                <span className="block max-w-full whitespace-normal break-words">
                   {value?.display || placeholder}
                   {value?.display && showCode && (
                     <span className="text-xs ml-1">({value?.code})</span>
