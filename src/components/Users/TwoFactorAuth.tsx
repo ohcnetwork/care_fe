ef9fa3a8984158177fcb1a30e13327bdbbd602de
Geyamrutha Poluru <shinchanharry148@gmail.com>
2025-08-12 19:22:49 +0530

    Fix: Misalignment of buttons and text for mobile view in user profile page

--- src/components/Users/TwoFactorAuth.tsx
+++ src/components/Users/TwoFactorAuth.tsx
@@ -174,7 +174,7 @@ export const TwoFactorAuth = ({ userData }: userChildProps) => {
                 <Button
                   onClick={handleSetup}
                   variant="outline"
-                  className="border-blue-600 text-blue-600 hover:bg-blue-50"
+                  className="border-blue-600 text-blue-600 hover:bg-blue-50 disabled:opacity-50 block mx-auto sm:mx-0"
                   disabled={isSettingUp}
                 >
                   {isSettingUp ? (
