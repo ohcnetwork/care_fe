4a485aff612f78c50e1063a4fe3574ef274239e1
nikhila8606 <nikhila8606@gmail.com>
2025-08-14 18:08:49 +0530

    Added badge for rooms

--- src/components/Location/LocationTree.tsx
+++ src/components/Location/LocationTree.tsx
@@ -1,7 +1,10 @@
 import { format } from "date-fns";
+import { Circle } from "lucide-react";
 import React from "react";
 
 import CareIcon from "@/CAREUI/icons/CareIcon";
+
+import { Badge } from "@/components/ui/badge";
 
 import { LocationList } from "@/types/location/location";
 
@@ -34,11 +37,14 @@ function LocationNode({
       <div className="flex flex-col gap-2">
         <div className="flex items-center text-sm">
           <span className="size-2 rounded-full bg-gray-400 mr-2" />
-          <span
-            className={isLast ? "font-semibold" : "text-gray-700 font-medium"}
-          >
-            {location.name}
-          </span>
+          {isLast ? (
+            <Badge variant="blue">
+              <Circle className="size-3.5" strokeWidth={2} />
+              {location.name}
+            </Badge>
+          ) : (
+            <span className="text-gray-700 font-medium">{location.name}</span>
+          )}
         </div>
         {children}
         {isLast && (startTime || endTime) && (
@@ -67,11 +73,14 @@ function LocationNode({
           <div className="relative border-l border-b border-gray-400 rounded-bl-sm size-2 -mt-1.5 mr-2">
             <div className="absolute -bottom-0.75 -right-0.75 size-1.5 bg-gray-400 rounded-full" />
           </div>
-          <span
-            className={isLast ? "font-semibold" : "text-gray-700 font-medium"}
-          >
-            {location.name}
-          </span>
+          {isLast ? (
+            <Badge variant="sky">
+              <Circle className="size-3.5" strokeWidth={2} />
+              {location.name}
+            </Badge>
+          ) : (
+            <span className="text-gray-700 font-medium">{location.name}</span>
+          )}
         </div>
         {children}
         {isLast && (startTime || endTime) && (
