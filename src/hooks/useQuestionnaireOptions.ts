ec3271adeb27b276999ca0af596af8c19870b937
Nikhila C <115739037+NikhilA8606@users.noreply.github.com>
2025-08-15 15:09:59 +0530

    fix : List Encounter Action Questionnaires (#13380)

--- src/hooks/useQuestionnaireOptions.ts
+++ src/hooks/useQuestionnaireOptions.ts
@@ -4,15 +4,17 @@ import query from "@/Utils/request/query";
 import questionnaireApi from "@/types/questionnaire/questionnaireApi";
 
 export default function useQuestionnaireOptions(slug: string) {
-  return useQuery({
-    queryKey: ["questionnaires", slug] as const,
-    queryFn: query(questionnaireApi.list, {
-      queryParams: {
-        tag_slug: slug,
-        status: "active",
-        subject_type: "encounter",
-      },
-      silent: (res) => res.status === 404,
-    }),
-  }).data;
+  return (
+    useQuery({
+      queryKey: ["questionnaires", slug] as const,
+      queryFn: query(questionnaireApi.list, {
+        queryParams: {
+          tag_slug: slug,
+          status: "active",
+          subject_type: "encounter",
+        },
+        silent: (res) => res.status === 404,
+      }),
+    }).data ?? { results: [] }
+  );
 }
