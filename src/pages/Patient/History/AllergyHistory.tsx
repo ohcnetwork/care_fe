492b198b2162952e97ce518b6f81537097d79f35
AdityaJ2305 <adityajindal2305@gmail.com>
2025-08-20 18:25:44 +0530

    Use custom icon

--- src/pages/Patient/History/AllergyHistory.tsx
+++ src/pages/Patient/History/AllergyHistory.tsx
@@ -1,5 +1,7 @@
 import { useTranslation } from "react-i18next";
 
+import { AllergyIcon } from "@/CAREUI/icons/CustomIcons";
+
 import { AllergyList } from "@/components/Patient/allergy/list";
 
 export const AllergyHistory = ({ patientId }: { patientId: string }) => {
@@ -7,11 +9,7 @@ export const AllergyHistory = ({ patientId }: { patientId: string }) => {
   return (
     <div className="max-w-5xl mx-auto">
       <div className="flex items-center gap-2 mb-5">
-        <img
-          src="/images/icons/allergy.svg"
-          alt="allergy"
-          className="size-8 p-1 border border-yellow-300 bg-yellow-100/80 rounded-md"
-        />
+        <AllergyIcon className="size-8 p-1 border text-yellow-800 border-yellow-300 bg-yellow-100/80 rounded-md" />
         <h4 className="text-xl">{t("allergies")}</h4>
       </div>
       <AllergyList patientId={patientId} showTimeline={true} />
