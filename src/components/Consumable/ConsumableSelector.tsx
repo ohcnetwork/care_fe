c1d680a8082ce71d9455239f54c31b7fcec1f3b4
abhimanyurajeesh <abhimanyurajeesh@gmail.com>
2025-09-04 22:31:50 +0530

    small fix

--- src/components/Consumable/ConsumableSelector.tsx
+++ src/components/Consumable/ConsumableSelector.tsx
@@ -68,7 +68,7 @@ export default function ConsumableSelector({
   });
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <Popover open={open} onOpenChange={setOpen} modal>
       <PopoverTrigger asChild>
         <Button
           variant="outline"
