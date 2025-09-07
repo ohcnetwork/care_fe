c3e9adde8dc62a66a6d582728baa7326968317d2
AdityaJ2305 <adityajindal2305@gmail.com>
2025-09-07 23:05:16 +0530

    text-gray-500 for unit's code

--- src/pages/Facility/settings/productKnowledge/ProductKnowledgeForm.tsx
+++ src/pages/Facility/settings/productKnowledge/ProductKnowledgeForm.tsx
@@ -747,7 +747,7 @@ function ProductKnowledgeFormContent({
                                         >
                                           <span>
                                             {t(`unit_${duration.code}`)}
-                                            <span className="text-sm ml-1">
+                                            <span className="text-sm ml-1 text-gray-500">
                                               ({duration.code})
                                             </span>
                                           </span>
