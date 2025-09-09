efbdd01bb22f6a0c754e9520c6d009f5be6059fc
yash-learner <yash.topaz@gmail.com>
2025-09-09 16:38:03 +0530

    Fix prettier error

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -5,7 +5,11 @@ import { t } from "i18next";
 
 import dayjs from "@/Utils/dayjs";
 import { Time } from "@/Utils/types";
-import { PatientListRead, PatientRead, PublicPatientRead } from "@/types/emr/patient/patient";
+import {
+  PatientListRead,
+  PatientRead,
+  PublicPatientRead,
+} from "@/types/emr/patient/patient";
 
 const DATE_FORMAT = "DD/MM/YYYY";
 const TIME_FORMAT = "hh:mm A";
