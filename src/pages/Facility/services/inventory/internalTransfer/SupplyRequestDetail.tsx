f875125aacaad61ac52677a3362738048b18feca
Priyank0703 <priyank.patel2341@gmail.com>
2025-09-09 22:06:41 +0530

    fix: #13656 solved UI issue in Purchase Order details page in mobile view by adding css

--- src/pages/Facility/services/inventory/internalTransfer/SupplyRequestDetail.tsx
+++ src/pages/Facility/services/inventory/internalTransfer/SupplyRequestDetail.tsx
@@ -429,94 +429,106 @@ export default function SupplyRequestDetail({
           <TableSkeleton count={5} />
         ) : deliveries.length > 0 ? (
           <div className="overflow-hidden rounded-md border-2 border-white shadow-md">
-            <Table className="rounded-md">
-              <TableHeader className="bg-gray-100 text-gray-700 text-sm">
-                <TableRow className="divide-x">
-                  <TableHead className="text-gray-700">
-                    {t("item_received")}
-                  </TableHead>
-                  <TableHead className="text-gray-700">
-                    {t("quantity_received")}
-                  </TableHead>
-                  <TableHead className="text-gray-700">{t("lot")}</TableHead>
-                  <TableHead className="text-gray-700">{t("expiry")}</TableHead>
-                  <TableHead className="text-gray-700">
-                    {t("condition")}
-                  </TableHead>
-                  <TableHead className="text-gray-700">
-                    {t("delivered_by")}
-                  </TableHead>
-                  <TableHead className="text-gray-700">{t("status")}</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody className="bg-white text-base">
-                {deliveries.map((delivery: SupplyDeliveryRead) => (
-                  <TableRow
-                    key={delivery.id}
-                    className="hover:bg-gray-50 divide-x"
-                  >
-                    <TableCell className="font-semibold text-gray-950">
-                      {delivery.supplied_item?.product_knowledge.name ||
-                        delivery.supplied_inventory_item?.product
-                          ?.product_knowledge.name}
-                    </TableCell>
-                    <TableCell className="font-medium text-gray-950">
-                      {delivery.supplied_item_quantity}{" "}
-                      {delivery.supplied_item?.product_knowledge.base_unit
-                        .display ||
-                        delivery.supplied_inventory_item?.product
-                          ?.product_knowledge.base_unit.display}
-                    </TableCell>
-                    <TableCell className="font-medium text-gray-950">
-                      {delivery.supplied_inventory_item?.product?.batch
-                        ?.lot_number ||
-                        delivery.supplied_item?.batch?.lot_number ||
-                        "-"}
-                    </TableCell>
-                    <TableCell className="font-medium text-gray-950">
-                      {delivery.supplied_inventory_item?.product
-                        ?.expiration_date
-                        ? formatDate(
-                            new Date(
-                              delivery.supplied_inventory_item.product.expiration_date,
-                            ),
-                            "dd/MM/yyyy",
-                          )
-                        : delivery.supplied_item?.expiration_date
+            <div className="overflow-x-auto">
+              <Table className="rounded-md min-w-[800px]">
+                <TableHeader className="bg-gray-100 text-gray-700 text-sm">
+                  <TableRow className="divide-x">
+                    <TableHead className="text-gray-700">
+                      {t("item_received")}
+                    </TableHead>
+                    <TableHead className="text-gray-700">
+                      {t("quantity_received")}
+                    </TableHead>
+                    <TableHead className="text-gray-700">{t("lot")}</TableHead>
+                    <TableHead className="text-gray-700">
+                      {t("expiry")}
+                    </TableHead>
+                    <TableHead className="text-gray-700">
+                      {t("condition")}
+                    </TableHead>
+                    <TableHead className="text-gray-700">
+                      {t("delivered_by")}
+                    </TableHead>
+                    <TableHead className="text-gray-700">
+                      {t("status")}
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody className="bg-white text-base">
+                  {deliveries.map((delivery: SupplyDeliveryRead) => (
+                    <TableRow
+                      key={delivery.id}
+                      className="hover:bg-gray-50 divide-x"
+                    >
+                      <TableCell className="font-semibold text-gray-950 whitespace-normal break-words">
+                        {delivery.supplied_item?.product_knowledge.name ||
+                          delivery.supplied_inventory_item?.product
+                            ?.product_knowledge.name}
+                      </TableCell>
+                      <TableCell className="font-medium text-gray-950">
+                        {delivery.supplied_item_quantity}{" "}
+                        {delivery.supplied_item?.product_knowledge.base_unit
+                          .display ||
+                          delivery.supplied_inventory_item?.product
+                            ?.product_knowledge.base_unit.display}
+                      </TableCell>
+                      <TableCell className="font-medium text-gray-950">
+                        {delivery.supplied_inventory_item?.product?.batch
+                          ?.lot_number ||
+                          delivery.supplied_item?.batch?.lot_number ||
+                          "-"}
+                      </TableCell>
+                      <TableCell className="font-medium text-gray-950">
+                        {delivery.supplied_inventory_item?.product
+                          ?.expiration_date
                           ? formatDate(
-                              new Date(delivery.supplied_item.expiration_date),
+                              new Date(
+                                delivery.supplied_inventory_item.product.expiration_date,
+                              ),
                               "dd/MM/yyyy",
                             )
-                          : "-"}
-                    </TableCell>
-                    <TableCell>
-                      {delivery.supplied_item_condition && (
+                          : delivery.supplied_item?.expiration_date
+                            ? formatDate(
+                                new Date(
+                                  delivery.supplied_item.expiration_date,
+                                ),
+                                "dd/MM/yyyy",
+                              )
+                            : "-"}
+                      </TableCell>
+                      <TableCell>
+                        {delivery.supplied_item_condition && (
+                          <Badge
+                            variant={
+                              delivery.supplied_item_condition === "damaged"
+                                ? "destructive"
+                                : "secondary"
+                            }
+                            className="capitalize"
+                          >
+                            {t(delivery.supplied_item_condition)}
+                          </Badge>
+                        )}
+                      </TableCell>
+                      <TableCell className="font-medium text-gray-950">
+                        {delivery.origin?.name ||
+                          delivery.supplier?.name ||
+                          "-"}
+                      </TableCell>
+                      <TableCell>
                         <Badge
                           variant={
-                            delivery.supplied_item_condition === "damaged"
-                              ? "destructive"
-                              : "secondary"
+                            SUPPLY_DELIVERY_STATUS_COLORS[delivery.status]
                           }
-                          className="capitalize"
                         >
-                          {t(delivery.supplied_item_condition)}
+                          {t(delivery.status)}
                         </Badge>
-                      )}
-                    </TableCell>
-                    <TableCell className="font-medium text-gray-950">
-                      {delivery.origin?.name || delivery.supplier?.name || "-"}
-                    </TableCell>
-                    <TableCell>
-                      <Badge
-                        variant={SUPPLY_DELIVERY_STATUS_COLORS[delivery.status]}
-                      >
-                        {t(delivery.status)}
-                      </Badge>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
           </div>
         ) : (
           <div className="flex items-center justify-center p-4 bg-white rounded-md">
