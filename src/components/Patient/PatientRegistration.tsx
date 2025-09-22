f76efd9d1cc3b67febf647287e360636aa419b56
nikhila8606 <nikhila8606@gmail.com>
2025-09-22 17:55:39 +0530

    dob-and-age-bug-fiz

--- src/components/Patient/PatientRegistration.tsx
+++ src/components/Patient/PatientRegistration.tsx
@@ -172,7 +172,7 @@ export const PatientRegistration = ({ patientId }: { patientId?: string }) => {
         !data.date_of_birth && data.year_of_birth
           ? new Date().getFullYear() - data.year_of_birth
           : undefined,
-      blood_group: data.blood_group,
+      blood_group: data.blood_group || undefined,
       tags: [], // This is only used for create patient
 
       address: data.address || "",
@@ -261,9 +261,11 @@ export const PatientRegistration = ({ patientId }: { patientId?: string }) => {
     const basePayload = {
       ...values,
 
-      age: values.age_or_dob === "dob" ? undefined : values.age,
+      age: values.age_or_dob === "dob" ? undefined : values.age || undefined,
       date_of_birth:
-        values.age_or_dob === "dob" ? values.date_of_birth : undefined,
+        values.age_or_dob === "dob"
+          ? values.date_of_birth || undefined
+          : undefined,
 
       emergency_phone_number: values.emergency_phone_number_same_as_phone_number
         ? values.phone_number
@@ -455,6 +457,7 @@ const PatientBasicsContent = ({
                   placeholder={t("enter_phone_number")}
                   onChange={(value) => {
                     field.onChange(value);
+                    form.trigger("phone_number");
                     if (
                       form.getValues(
                         "emergency_phone_number_same_as_phone_number",
@@ -522,6 +525,7 @@ const PatientBasicsContent = ({
                   value: g.id,
                   label: t(`GENDER__${g.id}`),
                 }))}
+                required={true}
               />
             </FormControl>
             <FormMessage />
@@ -550,7 +554,15 @@ const PatientBasicsContent = ({
                     render={({ field }) => (
                       <FormItem className="w-full">
                         <FormControl>
-                          <Input {...field} type="date" />
+                          <Input
+                            {...field}
+                            type="date"
+                            max={format(new Date(), "yyyy-MM-dd")}
+                            value={field.value ?? ""}
+                            onChange={(e) => {
+                              field.onChange(e.target.value || null);
+                            }}
+                          />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
@@ -565,22 +577,11 @@ const PatientBasicsContent = ({
                       <FormItem className="w-full">
                         <FormControl>
                           <Input
-                            type="number"
-                            inputMode="numeric"
-                            pattern="[0-9]*"
                             placeholder={t("age")}
-                            min={1}
-                            max={120}
-                            {...field}
-                            onChange={(e) =>
-                              form.setValue(
-                                "age",
-                                e.target.value
-                                  ? Number(e.target.value)
-                                  : (null as unknown as number),
-                                { shouldDirty: true },
-                              )
-                            }
+                            value={field.value ?? ""}
+                            onChange={(e) => {
+                              field.onChange(Number(e.target.value) || null);
+                            }}
                           />
                         </FormControl>
                         <FormMessage />
@@ -951,8 +952,9 @@ const getFormSchema = (t: TFunction) => {
         .string()
         .date()
         .refine((date) => !isFuture(date), t("date_cannot_be_future"))
-        .optional(),
-      age: validators().age.optional(),
+        .optional()
+        .nullable(),
+      age: validators().age.optional().nullable(),
       blood_group: z.nativeEnum(BloodGroupChoices).optional(),
       tags: z.array(z.string()),
 
