7fff260f58975e429ac96743aa41d501fa6f1567
AdityaJ2305 <adityajindal2305@gmail.com>
2025-09-26 01:56:19 +0530

    minor

--- src/components/Medicine/PrescriptionView.tsx
+++ src/components/Medicine/PrescriptionView.tsx
@@ -72,7 +72,7 @@ export default function PrescriptionView({
             {t("prescribed_by")}: {formatName(prescription.prescribed_by)}
           </p>
         </div>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 flex-wrap ">
           {canWrite && (
             <Button
               asChild
