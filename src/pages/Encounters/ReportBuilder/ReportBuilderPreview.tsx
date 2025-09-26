464790e145baf249d7a9b82d7f3ee7de7bf17552
Amjith Titus <amjithtitus09@gmail.com>
2025-09-26 15:33:03 +0530

    lint fix

--- src/pages/Encounters/ReportBuilder/ReportBuilderPreview.tsx
+++ src/pages/Encounters/ReportBuilder/ReportBuilderPreview.tsx
@@ -375,9 +375,7 @@ function SectionPreview({ section }: { section: SectionConfig }) {
       ) : isTextStyle ? (
         isCustomSection ? (
           <ul className="list-disc list-inside">
-            {text?.map((item, index) => (
-              <li key={index}>{t(item)}</li>
-            ))}
+            {text?.map((item, index) => <li key={index}>{t(item)}</li>)}
           </ul>
         ) : (
           <ul className="list-disc list-inside">
