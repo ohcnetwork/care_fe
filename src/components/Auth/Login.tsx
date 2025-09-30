32cd3e122c2c3303e3576f7903d8d464b0e5fd91
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2025-09-30 17:26:27 +0530

    Refactor OTP API routes and types (#13473)

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -40,11 +40,11 @@ import { LocalStorageKeys } from "@/common/constants";
 
 import FiltersCache from "@/Utils/FiltersCache";
 import ViewCache from "@/Utils/ViewCache";
-import routes from "@/Utils/request/api";
 import mutate from "@/Utils/request/mutate";
 import { HTTPError } from "@/Utils/request/types";
 import authApi from "@/types/auth/authApi";
-import { TokenData } from "@/types/auth/otp";
+import { TokenData } from "@/types/otp/otp";
+import otpApi from "@/types/otp/otpApi";
 
 import { AuthHero } from "./AuthHero";
 
@@ -127,7 +127,7 @@ const Login = (props: LoginProps) => {
 
   // Send OTP Mutation
   const { mutate: sendOtp, isPending: sendOtpPending } = useMutation({
-    mutationFn: mutate(routes.otp.sendOtp),
+    mutationFn: mutate(otpApi.send),
     onSuccess: () => {
       setIsOtpSent(true);
       setOtpError("");
@@ -147,9 +147,7 @@ const Login = (props: LoginProps) => {
   // Verify OTP Mutation
   const { mutate: verifyOtp, isPending: verifyOtpPending } = useMutation({
     mutationFn: async (data: OtpLoginData) => {
-      const response = await mutate(routes.otp.loginByOtp, { silent: true })(
-        data,
-      );
+      const response = await mutate(otpApi.login, { silent: true })(data);
       if ("errors" in response) {
         throw response;
       }
