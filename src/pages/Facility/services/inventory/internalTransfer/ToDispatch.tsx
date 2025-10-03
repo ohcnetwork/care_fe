85fd79b68a564115e8d69147c6c930069ecb86bf
Praneeth <praneeth@PRABHAASINI>
2025-10-03 00:17:04 +0000

    Minor touch up

--- src/pages/Facility/services/inventory/internalTransfer/ToDispatch.tsx
+++ src/pages/Facility/services/inventory/internalTransfer/ToDispatch.tsx
@@ -55,7 +55,7 @@ export default function ToDispatch({ facilityId, locationId }: Props) {
             {location?.name} ({t("internal_transfers")})
           </p>
 
-          <div className="flex items-center justify-between gap-4 flex-wrap">
+          <div className="flex items-center justify-between flex-wrap gap-4">
             <div>
               <h2 className="text-xl font-semibold text-gray-900">
                 {t("to_dispatch")}
