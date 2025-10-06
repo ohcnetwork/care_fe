da069878cc81e352cb3cfb8c12c85d72fdfc395c
Bodhish Thomas <bodhish@gmail.com>
2025-10-07 00:07:29 +0530

    replace start command

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run start",
+    command: "npm run dev",
     url: "http://localhost:4000",
     reuseExistingServer: !process.env.CI,
   },
