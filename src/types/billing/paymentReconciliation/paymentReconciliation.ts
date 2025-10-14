a52ea98f77e4d4ab4c6f3fa44c1b9ab9b7c02e19
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2025-10-14 13:47:57 +0530

    fixing

--- src/types/billing/paymentReconciliation/paymentReconciliation.ts
+++ src/types/billing/paymentReconciliation/paymentReconciliation.ts
@@ -91,5 +91,5 @@ export interface PaymentReconciliationRead extends PaymentReconciliationBase {
 }
 
 export interface PaymentReconciliationCancel {
-  reason: string;
+  reason: PaymentReconciliationStatus;
 }
