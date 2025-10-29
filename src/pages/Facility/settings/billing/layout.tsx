a904c1f190a1696e95908a1b8cdc25eedfc6416a
Praneeth <praneeth@PRABHAASINI>
2025-10-29 10:55:36 +0000

    empty dependeny array for useEffect

--- src/pages/Facility/settings/billing/layout.tsx
+++ src/pages/Facility/settings/billing/layout.tsx
@@ -99,7 +99,7 @@ export function BillingSettingsLayout() {
     if (path === basePath) {
       navigate(`${basePath}/discount_codes`, { replace: true });
     }
-  }, [basePath]);
+  }, []);
 
   return (
     <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
