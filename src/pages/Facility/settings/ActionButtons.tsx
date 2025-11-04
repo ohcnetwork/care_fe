da5496e88ddb76a508eb26996b87be078c9ab144
nikhila8606 <nikhila8606@gmail.com>
2025-11-04 10:58:39 +0530

    Action buttons access logic correction

--- src/pages/Facility/settings/ActionButtons.tsx
+++ src/pages/Facility/settings/ActionButtons.tsx
@@ -3,24 +3,15 @@ import { useTranslation } from "react-i18next";
 
 import CareIcon from "@/CAREUI/icons/CareIcon";
 
-import { getPermissions } from "@/common/Permissions";
 import { Button } from "@/components/ui/button";
-import { usePermissions } from "@/context/PermissionContext";
-import useCurrentFacility from "@/pages/Facility/utils/useCurrentFacility";
 
 interface ActionButtonsProps {
-  editPath: string;
+  editPath?: string;
   viewPath: string;
 }
 
 export function ActionButtons({ editPath, viewPath }: ActionButtonsProps) {
   const { t } = useTranslation();
-  const { hasPermission } = usePermissions();
-  const { facility } = useCurrentFacility();
-  const { canWriteTokenCategory } = getPermissions(
-    hasPermission,
-    facility?.permissions ?? [],
-  );
 
   return (
     <>
@@ -30,7 +21,7 @@ export function ActionButtons({ editPath, viewPath }: ActionButtonsProps) {
           {t("view")}
         </Link>
       </Button>
-      {canWriteTokenCategory && (
+      {editPath && (
         <Button variant="outline" size="sm" asChild className="w-19 h-9 lg:h-8">
           <Link basePath="/" href={editPath}>
             <CareIcon icon="l-edit" className="size-5 text-sm" />
