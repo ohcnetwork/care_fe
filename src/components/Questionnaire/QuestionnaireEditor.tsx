918015ffa2d712db612c0492f30cf9b13bdd1c2a
abhimanyurajeesh <abhimanyurajeesh@gmail.com>
2025-11-17 15:27:05 +0530

    Remove unused facilityId prop from QuestionnaireEditor component

--- src/components/Questionnaire/QuestionnaireEditor.tsx
+++ src/components/Questionnaire/QuestionnaireEditor.tsx
@@ -1356,7 +1356,6 @@ export default function QuestionnaireEditor({ id }: QuestionnaireEditorProps) {
                 patientId="preview"
                 subjectType={form.watch("subject_type")}
                 encounterId="preview"
-                facilityId="preview"
               />
             </CardContent>
           </Card>
