13fb81256b0d1cefe360f3b2bb28df8231885a5b
yash-learner <yash.topaz@gmail.com>
2025-11-18 01:39:19 +0530

    Remove unnecessary chaining

--- src/pages/Facility/queues/ManageQueueFinishedTab.tsx
+++ src/pages/Facility/queues/ManageQueueFinishedTab.tsx
@@ -96,7 +96,7 @@ export function ManageQueueFinishedTab({
                         {
                           phone_number: token.patient.phone_number,
                           year_of_birth:
-                            token.patient?.year_of_birth?.toString() ?? "",
+                            token.patient.year_of_birth?.toString() ?? "",
                           partial_id: token.patient.id.slice(0, 5),
                           queue_id: token.queue.id,
                           token_id: token.id,
