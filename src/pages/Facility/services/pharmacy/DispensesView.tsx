45eed1acc2c7f35469f1956324922145ed2b8b0b
NikhilA8606 <nikhila8606@gmail.com>
2025-12-15 13:39:41 +0530

    cleanups

--- src/pages/Facility/services/pharmacy/DispensesView.tsx
+++ src/pages/Facility/services/pharmacy/DispensesView.tsx
@@ -120,18 +120,16 @@ export default function DispensesView({
                 {t("prescription_tags")}:
               </span>
               <div className="flex flex-wrap items-start gap-2 text-sm whitespace-nowrap">
-                <>
-                  {prescriptionTags?.map((tag) => (
-                    <Badge
-                      key={tag.id}
-                      variant="secondary"
-                      className="capitalize"
-                      title={tag.description}
-                    >
-                      {getTagHierarchyDisplay(tag)}
-                    </Badge>
-                  ))}
-                </>
+                {prescriptionTags?.map((tag) => (
+                  <Badge
+                    key={tag.id}
+                    variant="secondary"
+                    className="capitalize"
+                    title={tag.description}
+                  >
+                    {getTagHierarchyDisplay(tag)}
+                  </Badge>
+                ))}
               </div>
             </div>
           )}
