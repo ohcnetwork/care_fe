49e1836f0c8882e85ac3bec3f095c59b451509bf
Harsh <harsh9425181074@gmail.com>
2025-12-18 20:15:32 +0800

    fix: add index.ts to export Observation and ObservationWithUser type aliases
    Create index file in observation module to export type aliases for
    Observation and ObservationWithUser, mapping them to ObservationListRead.
    This fixes unresolved import errors in components using these types.

---
+++ src/types/emr/observation/index.ts
@@ -0,0 +1,7 @@
+import type { ObservationListRead } from "./observation";
+
+export type Observation = ObservationListRead;
+export type ObservationWithUser = ObservationListRead;
+
+export * from "./observation";
+export * from "./observationApi";
