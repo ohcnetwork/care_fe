64063a341aa09c3772679a15c1b0e40851c3fc43
yash-learner <yash.topaz@gmail.com>
2025-12-23 16:21:46 +0530

    Clean up remaining routes and add some of them in api specific ones

--- src/Utils/request/api.tsx
+++ src/Utils/request/api.tsx
@@ -1,11 +1,3 @@
-import { PaginatedResponse } from "@/Utils/request/types";
-import {
-  BatchRequestBody,
-  BatchRequestResponse,
-} from "@/types/base/batch/batch";
-import { Code } from "@/types/base/code/code";
-import { UserReadMinimal } from "@/types/user/user";
-
 /**
  * A fake function that returns an empty object casted to type T
  * @returns Empty object as type T
@@ -33,38 +25,3 @@ export const API = <TResponse, TBody = undefined>(
     TBody: Type<TBody>(),
   };
 };
-
-/**
- * @deprecated use object specific api instead
- */
-const routes = {
-  getScheduleAbleFacilityUser: {
-    path: "/api/v1/facility/{facility_id}/schedulable_users/{user_id}/",
-    TRes: Type<UserReadMinimal>(),
-  },
-
-  getScheduleAbleFacilityUsers: {
-    path: "/api/v1/facility/{facility_id}/schedulable_users/",
-    TRes: Type<PaginatedResponse<UserReadMinimal>>(),
-  },
-
-  valueset: {
-    expand: {
-      path: "/api/v1/valueset/{system}/expand/",
-      method: "POST",
-      TBody: Type<{ search: string; count: number }>(),
-      TRes: Type<{ results: Code[] }>(),
-    },
-  },
-
-  batchRequest: {
-    path: "/api/v1/batch_requests/",
-    method: "POST",
-    TRes: Type<{
-      results: BatchRequestResponse[];
-    }>(),
-    TBody: Type<BatchRequestBody>(),
-  },
-} as const;
-
-export default routes;
