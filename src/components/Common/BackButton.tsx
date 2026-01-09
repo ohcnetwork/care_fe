62cb6e89e07bc87964908e5c3158694b203d611d
abhimanyurajeesh <abhimanyurajeesh@gmail.com>
2026-01-09 15:17:34 +0530

    feat: improve BackButton component logic and add back button translation

--- src/components/Common/BackButton.tsx
+++ src/components/Common/BackButton.tsx
@@ -3,6 +3,7 @@ import { Link } from "raviger";
 import { Button } from "@/components/ui/button";
 
 import useAppHistory from "@/hooks/useAppHistory";
+import { t } from "i18next";
 
 type BackButtonProps = {
   to?: string;
@@ -11,15 +12,21 @@ type BackButtonProps = {
 export default function BackButton({ to, ...props }: BackButtonProps) {
   const { history } = useAppHistory();
 
-  to ??= history[1];
+  const backUrl = history[1] || to;
 
-  if (!to) {
+  if (!backUrl) {
     return null;
   }
 
   return (
-    <Button variant="outline" data-shortcut-id="go-back" asChild {...props}>
-      <Link basePath="/" href={to}>
+    <Button
+      variant="outline"
+      data-shortcut-id="go-back"
+      asChild
+      {...props}
+      aria-label={t("back_button")}
+    >
+      <Link basePath="/" href={backUrl}>
         {props.children}
       </Link>
     </Button>
