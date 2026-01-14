8294770e4b702c39606f6235f1cc2853af1656d0
Amjith Titus <amjithtitus09@gmail.com>
2026-01-14 14:52:45 +0530

    Minor fix

--- src/pages/Facility/services/inventory/externalSupply/deliveryOrder/useDeliveryRowItem.ts
+++ src/pages/Facility/services/inventory/externalSupply/deliveryOrder/useDeliveryRowItem.ts
@@ -83,8 +83,8 @@ export function useDeliveryRowItem({ form, index }: UseDeliveryRowItemProps) {
       discount_components: [],
       charge_item_category: undefined,
       is_manually_edited: false,
-      supplied_item_pack_quantity: undefined,
-      supplied_item_pack_size: undefined,
+      supplied_item_pack_quantity: 1,
+      supplied_item_pack_size: 1,
     };
 
     Object.entries(fieldsToReset).forEach(([field, value]) => {
