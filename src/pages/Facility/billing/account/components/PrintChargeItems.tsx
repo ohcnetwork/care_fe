aa1106ea8a0f2153879ef23842083723c991a92a
Amjith Titus <amjithtitus09@gmail.com>
2026-01-14 21:05:51 +0530

    Fix alignment

--- src/pages/Facility/billing/account/components/PrintChargeItems.tsx
+++ src/pages/Facility/billing/account/components/PrintChargeItems.tsx
@@ -730,7 +730,7 @@ export const PrintChargeItems = (props: {
                             <TableHead className="text-center font-bold">
                               {t("total_paid")}
                             </TableHead>
-                            <TableHead className="text-right font-bold">
+                            <TableHead className="text-center font-bold">
                               {t("amount_due")}
                             </TableHead>
                             <TableHead className="text-center font-bold">
@@ -746,7 +746,7 @@ export const PrintChargeItems = (props: {
                             <TableCell className="text-center">
                               <MonetaryDisplay amount={account.total_paid} />
                             </TableCell>
-                            <TableCell className="text-right">
+                            <TableCell className="text-center">
                               <MonetaryDisplay amount={account.total_balance} />
                             </TableCell>
                             <TableCell className="text-center">
