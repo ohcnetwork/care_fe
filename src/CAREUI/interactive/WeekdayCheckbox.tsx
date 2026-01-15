d43d536a19acece720550cd5b237349d2e28934a
abhi-battula <abhinavgoudbattula@gmail.com>
2026-01-15 18:37:09 +0530

    Add Select All option to weekday picker

--- src/CAREUI/interactive/WeekdayCheckbox.tsx
+++ src/CAREUI/interactive/WeekdayCheckbox.tsx
@@ -48,30 +48,54 @@ export default function WeekdayCheckbox({
   };
 
   return (
-    <div className="flex gap-2 md:gap-4">
-      {dayOfWeekKeys.map((day) => {
-        const dow = DayOfWeek[day as keyof typeof DayOfWeek];
-        const isSelected = selectedDays.includes(dow);
+    <div
+      className={
+        format === "alphabet" ? "flex flex-col gap-2" : "flex gap-2 md:gap-4"
+      }
+    >
+      <div className="flex gap-2 md:gap-4">
+        {dayOfWeekKeys.map((day) => {
+          const dow = DayOfWeek[day as keyof typeof DayOfWeek];
+          const isSelected = selectedDays.includes(dow);
 
-        return (
-          <Button
-            key={dow}
-            type="button"
-            variant={isSelected ? "primary" : "outline"}
-            onClick={() => handleDayToggle(dow)}
-            size={format === "alphabet" ? "icon" : "default"}
-            aria-pressed={isSelected}
-            aria-checked={isSelected}
-            aria-label={t(`DAYS_OF_WEEK__${dow}`)}
-          >
-            {format === "alphabet"
-              ? day[0]
-              : format === "short"
-                ? t(`DAYS_OF_WEEK_SHORT__${dow}`)
-                : t(`DAYS_OF_WEEK__${dow}`)}
-          </Button>
-        );
-      })}
+          return (
+            <Button
+              key={dow}
+              type="button"
+              variant={isSelected ? "primary" : "outline"}
+              onClick={() => handleDayToggle(dow)}
+              size={format === "alphabet" ? "icon" : "default"}
+              aria-pressed={isSelected}
+              aria-checked={isSelected}
+              aria-label={t(`DAYS_OF_WEEK__${dow}`)}
+            >
+              {format === "alphabet"
+                ? day[0]
+                : format === "short"
+                  ? t(`DAYS_OF_WEEK_SHORT__${dow}`)
+                  : t(`DAYS_OF_WEEK__${dow}`)}
+            </Button>
+          );
+        })}
+      </div>
+      <div>
+        <Button
+          type="button"
+          // variant={selectedDays.length === dayOfWeekKeys.length ? "primary" : "outline" }
+          variant={"outline_primary"}
+          onClick={() => {
+            if (selectedDays.length === dayOfWeekKeys.length) {
+              onChange([]);
+            } else {
+              onChange(dayOfWeekKeys.map((day) => DayOfWeek[day]));
+            }
+          }}
+        >
+          {selectedDays.length === dayOfWeekKeys.length
+            ? t(`unselect_all`)
+            : t(`select_all`)}
+        </Button>
+      </div>
     </div>
   );
 }
