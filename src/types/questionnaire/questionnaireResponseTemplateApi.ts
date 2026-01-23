6ced8d135b5cf492e599efc618ef4d0130777ea1
rithviknishad <mail@rithviknishad.dev>
2026-01-23 06:26:13 +0530

    adds questionnaire response template specs

---
+++ src/types/questionnaire/questionnaireResponseTemplateApi.ts
@@ -0,0 +1,42 @@
+import {
+  QuestionnaireResponseTemplateCreateSpec,
+  QuestionnaireResponseTemplateReadSpec,
+  QuestionnaireResponseTemplateRetrieveSpec,
+  QuestionnaireResponseTemplateUpdateSpec,
+} from "@/types/questionnaire/questionnaireResponseTemplate";
+import { HttpMethod, PaginatedResponse, Type } from "@/Utils/request/types";
+
+export const questionnaireResponseTemplateApi = {
+  list: {
+    path: "/api/v1/questionnaire_response_template/",
+    method: HttpMethod.GET,
+    TRes: Type<PaginatedResponse<QuestionnaireResponseTemplateReadSpec>>(),
+    TQuery: Type<{
+      questionnaire?: string; // questionnaire external_id
+      limit?: number;
+      offset?: number;
+    }>(),
+  },
+  retrieve: {
+    path: "/api/v1/questionnaire_response_template/{id}/",
+    method: HttpMethod.GET,
+    TRes: Type<QuestionnaireResponseTemplateRetrieveSpec>(),
+  },
+  create: {
+    path: "/api/v1/questionnaire_response_template/",
+    method: HttpMethod.POST,
+    TRes: Type<QuestionnaireResponseTemplateRetrieveSpec>(),
+    TReq: Type<QuestionnaireResponseTemplateCreateSpec>(),
+  },
+  update: {
+    path: "/api/v1/questionnaire_response_template/{id}/",
+    method: HttpMethod.PUT,
+    TRes: Type<QuestionnaireResponseTemplateRetrieveSpec>(),
+    TReq: Type<QuestionnaireResponseTemplateUpdateSpec>(),
+  },
+  delete: {
+    path: "/api/v1/questionnaire_response_template/{id}/",
+    method: HttpMethod.DELETE,
+    TRes: Type<Record<string, never>>(),
+  },
+} as const;
