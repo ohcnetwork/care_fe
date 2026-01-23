9d233f8a8ecfd7e0e5dac91de711ef9c95b8c9e5
Abhinandan-basotra <abhinandanbasotra22@gmail.com>
2026-01-23 15:29:28 +0530

    fix:added_comment_if_no_category_present_in_tokens

--- src/pages/Appointments/components/TokenGenerationSheet.tsx
+++ src/pages/Appointments/components/TokenGenerationSheet.tsx
@@ -150,14 +150,22 @@ export function TokenGenerationSheet({
                     <SelectValue placeholder={t("select_token_category")} />
                   </SelectTrigger>
                   <SelectContent>
-                    {tokenCategories?.results?.map(
-                      (category: TokenCategoryRead) => (
-                        <SelectItem key={category.id} value={category.id}>
-                          {category.name}
-                          {category.default && ` (${t("default")})`}
+                    <SelectContent>
+                      {tokenCategories?.results?.length ? (
+                        tokenCategories.results.map(
+                          (category: TokenCategoryRead) => (
+                            <SelectItem key={category.id} value={category.id}>
+                              {category.name}
+                              {category.default && ` (${t("default")})`}
+                            </SelectItem>
+                          ),
+                        )
+                      ) : (
+                        <SelectItem disabled value="no-options">
+                          {t("no_token_category_avaialable")}
                         </SelectItem>
-                      ),
-                    )}
+                      )}
+                    </SelectContent>
                   </SelectContent>
                 </Select>
               </div>
