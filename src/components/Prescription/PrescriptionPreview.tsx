e02ffba99d42b7ab44b0f22ed3e3adf56d3da61c
abhimanyurajeesh <abhimanyurajeesh@gmail.com>
2026-02-02 13:45:20 +0530

    fix: simplify display of as-needed medication instructions in PrescriptionPreview and MedicationsTable

--- src/components/Prescription/PrescriptionPreview.tsx
+++ src/components/Prescription/PrescriptionPreview.tsx
@@ -66,7 +66,7 @@ const PrescriptionContent = ({
                 status: t(`medication_status_${medication.status}`),
                 dosage: dosage,
                 frequency: instruction?.as_needed_boolean
-                  ? `${t("as_needed_prn")} (${instruction?.as_needed_for?.display ?? "-"})`
+                  ? `${t("as_needed_prn")}`
                   : (frequency?.meaning ?? "-") +
                     (instruction?.additional_instruction?.[0]?.display
                       ? `, ${instruction.additional_instruction[0].display}`
