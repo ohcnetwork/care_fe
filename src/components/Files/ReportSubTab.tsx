c2598d00156448770c29d666da30fdeb0f9a4afe
NikhilA8606 <nikhila8606@gmail.com>
2026-02-26 17:14:03 +0530

    removed unwanted check

--- src/components/Files/ReportSubTab.tsx
+++ src/components/Files/ReportSubTab.tsx
@@ -379,7 +379,7 @@ export function ReportSubTab({ associatingId, reportType }: ReportTabProps) {
         archivedLabel="archived_reports"
       />
 
-      {!reportsLoading && filteredReports.length === 0 ? (
+      {!reportsLoading ? (
         <EmptyState
           icon={<CareIcon icon="l-file-alt" className="text-primary size-6" />}
           title={t("no_reports_found")}
