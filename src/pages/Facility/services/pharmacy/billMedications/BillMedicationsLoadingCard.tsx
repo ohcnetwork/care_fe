a31bb488e311aec25f99083757632b8d9065b7f9
rithviknishad <mail@rithviknishad.dev>
2026-03-12 14:22:10 +0530

    loader rename

--- src/pages/Facility/services/pharmacy/billMedications/BillMedicationsLoadingCard.tsx
+++ src/pages/Facility/services/pharmacy/billMedications/BillMedicationsLoadingCard.tsx
@@ -2,6 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { useTranslation } from "react-i18next";
 
 export const BillMedicationsLoadingCard = () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <div className="relative flex justify-between col-start-1 col-span-7 bg-white pt-4 pr-2 pb-2 pl-4">
@@ -18,20 +20,7 @@ export const BillMedicationsLoadingCard = () => {
         </div>
       </div>
 
-      <HeaderRow />
-
-      {Array.from({ length: 3 }).map((_, index) => (
-        <MedicineLineItem key={index} />
-      ))}
-    </>
-  );
-};
-
-const HeaderRow = () => {
-  const { t } = useTranslation();
-
-  return (
-    <>
+      {/* Header Row */}
       <div className="col-start-1 bg-gray-100 py-1 px-3 flex items-center">
         <Skeleton className="size-4" />
       </div>
@@ -64,34 +53,32 @@ const HeaderRow = () => {
           {t("actions")}
         </span>
       </div>
-    </>
-  );
-};
 
-const MedicineLineItem = () => {
-  return (
-    <div className="contents group divide-x divide-gray-200">
-      <div className="col-start-1 bg-white group-hover:bg-gray-100 group-focus-within:bg-gray-100 py-1 px-3 flex items-center transition-all duration-200 ease-in-out">
-        <Skeleton className="size-4" />
-      </div>
-      <div className="bg-white py-2 px-3 flex justify-between items-center gap-4">
-        <Skeleton className="w-full h-10" />
-      </div>
-      <div className="bg-white py-2 px-3 flex justify-between items-center gap-4">
-        <Skeleton className="w-full h-10" />
-      </div>
-      <div className="bg-white py-2 px-3 flex justify-between items-center gap-4">
-        <Skeleton className="w-full h-10" />
-      </div>
-      <div className="bg-white py-2 px-3 flex justify-between items-center gap-4">
-        <Skeleton className="w-full h-10" />
-      </div>
-      <div className="bg-white py-2 px-3 flex items-center justify-center">
-        <Skeleton className="w-full h-10" />
-      </div>
-      <div className="bg-white py-1 px-2 flex items-center justify-center">
-        <Skeleton className="w-full h-10" />
-      </div>
-    </div>
+      {Array.from({ length: 3 }).map((_, index) => (
+        <div key={index} className="contents group divide-x divide-gray-200">
+          <div className="col-start-1 bg-white group-hover:bg-gray-100 group-focus-within:bg-gray-100 py-1 px-3 flex items-center transition-all duration-200 ease-in-out">
+            <Skeleton className="size-4" />
+          </div>
+          <div className="bg-white py-2 px-3 flex justify-between items-center gap-4">
+            <Skeleton className="w-full h-10" />
+          </div>
+          <div className="bg-white py-2 px-3 flex justify-between items-center gap-4">
+            <Skeleton className="w-full h-10" />
+          </div>
+          <div className="bg-white py-2 px-3 flex justify-between items-center gap-4">
+            <Skeleton className="w-full h-10" />
+          </div>
+          <div className="bg-white py-2 px-3 flex justify-between items-center gap-4">
+            <Skeleton className="w-full h-10" />
+          </div>
+          <div className="bg-white py-2 px-3 flex items-center justify-center">
+            <Skeleton className="w-full h-10" />
+          </div>
+          <div className="bg-white py-1 px-2 flex items-center justify-center">
+            <Skeleton className="w-full h-10" />
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
