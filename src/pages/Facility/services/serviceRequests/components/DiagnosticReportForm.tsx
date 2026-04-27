bf57f69342433f2359ddb5b7392c0dbf1852170d
Jacob Jeevan <mail@jacobjeevan.me>
2026-04-27 15:57:26 +0530

    autoselect default unit for root/component if available

--- src/pages/Facility/services/serviceRequests/components/DiagnosticReportForm.tsx
+++ src/pages/Facility/services/serviceRequests/components/DiagnosticReportForm.tsx
@@ -344,6 +344,7 @@ export function DiagnosticReportForm({
     definitionId: string,
     index: number,
     value: string,
+    unit: string,
   ) {
     setObservations((prev) => {
       const observationsList = [...(prev[definitionId] || [])];
@@ -351,7 +352,7 @@ export function DiagnosticReportForm({
         observationsList[index] = {
           id: "",
           value: "",
-          unit: "",
+          unit,
           status: ObservationStatus.AMENDED,
           components: {},
         };
@@ -724,7 +725,7 @@ export function DiagnosticReportForm({
             component.code.code
           ] || {
             value: "",
-            unit: "",
+            unit: component.permitted_unit?.code || "",
             interpretation: "",
           };
 
@@ -904,7 +905,7 @@ export function DiagnosticReportForm({
                       {
                         id: "",
                         value: "",
-                        unit: "",
+                        unit: definition.permitted_unit?.code || "",
                         interpretation: "",
                         status: ObservationStatus.AMENDED,
                         components: {},
@@ -1022,6 +1023,7 @@ export function DiagnosticReportForm({
                                               definition.id,
                                               index,
                                               e.target.value,
+                                              observationData.unit,
                                             )
                                           }
                                           placeholder={t("result_value")}
@@ -1065,7 +1067,8 @@ export function DiagnosticReportForm({
                                       {
                                         id: "",
                                         value: "",
-                                        unit: "",
+                                        unit:
+                                          definition.permitted_unit?.code || "",
                                         status: ObservationStatus.AMENDED,
                                         components: {},
                                       },
