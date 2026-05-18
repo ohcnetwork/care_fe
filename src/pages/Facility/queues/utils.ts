d861ed45c0096c5cd9f9f435e067e27e2e6585bf
NikhilA8606 <nikhila8606@gmail.com>
2026-05-18 11:58:21 +0530

    created seperated hook for useToken

--- src/pages/Facility/queues/utils.ts
+++ src/pages/Facility/queues/utils.ts
@@ -1,14 +1,10 @@
 import { TokenRetrieve, TokenStatus } from "@/types/tokens/token/token";
 import tokenApi from "@/types/tokens/token/tokenApi";
 import { TokenQueueSummary } from "@/types/tokens/tokenQueue/tokenQueue";
-import mutate from "@/Utils/request/mutate";
 import query from "@/Utils/request/query";
-import queryClient from "@/Utils/request/queryClient";
 import careConfig from "@careConfig";
-import { useInfiniteQuery, useMutation, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { useQueryParams } from "raviger";
-import { useTranslation } from "react-i18next";
-import { toast } from "sonner";
 
 export function getTokenQueueStatusCount(
   summary: TokenQueueSummary,
@@ -72,54 +68,4 @@ export function getTokenStatus({
     return t("waiting");
   }
   return t(`${token.status.toLowerCase()}`);
-}
-
-export function useToken({
-  facilityId,
-  queueId,
-  tokenId,
-  onSuccess,
-}: {
-  facilityId: string;
-  queueId: string;
-  tokenId: string;
-  onSuccess?: () => void;
-}) {
-  const { t } = useTranslation();
-  const { data: token } = useQuery({
-    queryKey: ["token", facilityId, queueId, tokenId],
-    queryFn: query(tokenApi.get, {
-      pathParams: {
-        facility_id: facilityId,
-        queue_id: queueId ?? "",
-        id: tokenId ?? "",
-      },
-    }),
-    enabled: !!queueId && !!tokenId,
-  });
-
-  const { mutate: updateToken, isPending } = useMutation({
-    mutationFn: mutate(tokenApi.update, {
-      pathParams: {
-        facility_id: facilityId,
-        queue_id: token?.queue.id ?? "",
-        id: token?.id ?? "",
-      },
-    }),
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ["infinite-tokens", facilityId, token?.queue.id ?? ""],
-      });
-      queryClient.invalidateQueries({
-        queryKey: ["tokens", token?.patient?.id, facilityId],
-      });
-      queryClient.invalidateQueries({
-        queryKey: ["token-queue-summary", facilityId, token?.queue.id ?? ""],
-      });
-      toast.success(t("token_assigned_to_service_point"));
-      onSuccess?.();
-    },
-  });
-
-  return { token, updateToken, isPending };
 }
