caa099500a5c14f1abc7eaf649a1a20bb53244ae
sreya-jayan <sreyajayan35@gmail.com>
2026-05-21 14:17:29 +0530

    fix: remove individual refine from factor and amount fields

--- src/pages/Facility/settings/billing/discount/discount-components/DiscountMonetaryComponentForm.tsx
+++ src/pages/Facility/settings/billing/discount/discount-components/DiscountMonetaryComponentForm.tsx
@@ -62,14 +62,8 @@ export function DiscountMonetaryComponentForm({
         .object({
           monetary_component_type: z.literal(MonetaryComponentType.discount),
           code: CodeSchema.optional(),
-          factor: zodDecimal({ min: 0, max: 100 })
-            .optional()
-            .nullable()
-            .refine((val) => val !== null, { message: t("field_required") }),
-          amount: zodDecimal({ min: 0 })
-            .optional()
-            .nullable()
-            .refine((val) => val !== null, { message: t("field_required") }),
+          factor: zodDecimal({ min: 0, max: 100 }).optional().nullable(),
+          amount: zodDecimal({ min: 0 }).optional().nullable(),
           title: z
             .string()
             .trim()
