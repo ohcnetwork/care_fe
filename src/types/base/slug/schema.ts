462942178bcc6cd7db615825dcf8dcc0b1f81703
rithviknishad <mail@rithviknishad.dev>
2026-06-15 12:40:44 +0530

    update regexp to match with what backend has

--- src/types/base/slug/schema.ts
+++ src/types/base/slug/schema.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 export const SLUG_VALUE_MIN_LENGTH = 5;
 export const SLUG_VALUE_MAX_LENGTH = 36;
 
+export const SLUG_VALUE_PATTERN = /^[a-zA-Z0-9][a-zA-Z0-9_-]*[a-zA-Z0-9]$/;
+
 /**
  * Validation schema for slug values used in various resources.
  * Slug values must be between 5 and 36 characters, and can include letters, numbers, hyphens, and underscores.
@@ -19,7 +21,7 @@ export const slugValueSchema = () => {
     .trim()
     .min(SLUG_VALUE_MIN_LENGTH, { message: characterCountMessage })
     .max(SLUG_VALUE_MAX_LENGTH, { message: characterCountMessage })
-    .regex(/^[a-z0-9-]+$/, {
+    .regex(SLUG_VALUE_PATTERN, {
       message: t("slug_format_message"),
     });
 };
