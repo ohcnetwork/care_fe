ee143a13c02cd865ebbf1c539f74f494d455e967
rithviknishad <mail@rithviknishad.dev>
2026-06-22 13:13:19 +0530

    fix error boundary

--- src/components/Files/Drawings/DrawingIcon.tsx
+++ src/components/Files/Drawings/DrawingIcon.tsx
@@ -1,28 +1,24 @@
 import ErrorBoundary from "@/components/Common/ErrorBoundary";
 import { Skeleton } from "@/components/ui/skeleton";
-import { DrawingApplicationManifest } from "@/pluginTypes";
 import { usePluginDrawingApplication } from "./usePluginDrawingApplications";
 
-const ApplicationIcon = ({ app }: { app: DrawingApplicationManifest }) => {
-  if (!app.icon) {
+const ApplicationIcon = ({ application }: { application: string }) => {
+  const { drawingApplication: app, isLoading } =
+    usePluginDrawingApplication(application);
+
+  if (isLoading || !app.icon) {
     return fallback;
   }
+
   return <app.icon className="size-full" />;
 };
 
 const fallback = <Skeleton className="size-full" />;
 
 export const DrawingIcon = ({ application }: { application: string }) => {
-  const { drawingApplication, isLoading } =
-    usePluginDrawingApplication(application);
-
-  if (isLoading) {
-    return fallback;
-  }
-
   return (
     <ErrorBoundary fallback={fallback}>
-      <ApplicationIcon app={drawingApplication!} />
+      <ApplicationIcon application={application} />
     </ErrorBoundary>
   );
 };
