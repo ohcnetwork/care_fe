f7404453ac99b32b2af1cc5981321299a220abe9
NikhilA8606 <nikhila8606@gmail.com>
2026-07-08 18:14:25 +0530

    minor cleanup

--- src/components/Medicine/PrescriptionListSelector.tsx
+++ src/components/Medicine/PrescriptionListSelector.tsx
@@ -76,7 +76,6 @@ export default function PrescriptionListSelector({
         });
         return response;
       },
-      enabled: !!facilityId,
       initialPageParam: 0,
       getNextPageParam: (lastPage, allPages) => {
         const currentOffset = allPages.length * RESULTS_PER_PAGE_LIMIT;
