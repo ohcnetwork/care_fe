931c1ba0e35a3b6f79eea6a20d378f5e336bfcb8
abhimanyurajeesh <abhimanyurajeesh@gmail.com>
2026-09-04 00:04:03 +0530

    rest

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -6,13 +6,7 @@ import { cn } from "@/lib/utils";
 function Drawer({
   ...props
 }: React.ComponentProps<typeof DrawerPrimitive.Root>) {
-  return (
-    <DrawerPrimitive.Root
-      data-slot="drawer"
-      repositionInputs={false}
-      {...props}
-    />
-  );
+  return <DrawerPrimitive.Root data-slot="drawer" {...props} />;
 }
 
 function DrawerTrigger({
